6b000bacbf1f3d8d7cb6a820d68da07210dd637b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:48 +0000

    Add package mtap/1.2.0/mtap-1.2.0-py3-none-any.whl

---
+++ packages/mtap/mtap-1.2.0-py3-none-any.whl/mtap/__init__.py
@@ -0,0 +1,44 @@
+# Copyright 2019 Regents of the University of Minnesota.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Public API and access points for the MTAP Framework."""
+
+from mtap._config import Config
+from mtap._events_service import EventsServer
+from mtap.data import (
+    Document,
+    Event,
+    EventsClient,
+    GenericLabel,
+    label,
+    label_index,
+    Location,
+)
+from mtap.processing import (
+    DocumentProcessor,
+    EventProcessor,
+    Pipeline,
+    ProcessorServer,
+    descriptions,
+    processor,
+    processor_parser,
+    run_processor,
+    RemoteProcessor,
+    LocalProcessor,
+)
+from mtap.version import (
+    __version__,
+    __version_tuple__,
+    version,
+    version_tuple
+)
---
+++ packages/mtap/mtap-1.2.0-py3-none-any.whl/mtap/examples/tutorial/pipeline.py
@@ -0,0 +1,31 @@
+# Copyright 2019 Regents of the University of Minnesota.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Hello world tutorial pipeline."""
+import sys
+
+if __name__ == '__main__':
+    from mtap import Document, Event, Pipeline, EventsClient
+    from mtap.processing import RemoteProcessor
+
+    with EventsClient(sys.argv[1]) as client:
+        pipeline = Pipeline(
+            RemoteProcessor(processor_name='hello', address=sys.argv[2]),
+        )
+        with Event(event_id='1', client=client) as event:
+            document = Document(document_name='name', text='YOUR NAME')
+            event.add_document(document)
+            pipeline.run(document)
+            index = document.get_label_index('hello')
+            for label in index:
+                print(label.response)
