5afdf6a6e7e36bf76afd35972e5d32a8eeb41f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:01 +0000

    Add package geomapdemo/0.1.1/geomapdemo-0.1.1-py2.py3-none-any.whl

---
+++ packages/geomapdemo/geomapdemo-0.1.1-py2.py3-none-any.whl/geomapdemo/geomapdemo.py
@@ -0,0 +1,107 @@
+"""Main module."""
+
+import random
+import string
+import ipyleaflet
+
+
+
+class Map(ipyleaflet.Map):
+
+    def __init__(self, center, zoom, **kwargs) -> None:
+
+        if "scroll_wheel_zoom" not in kwargs:
+            kwargs["scroll_wheel_zoom"] = True
+        
+        
+        super().__init__(center=center , zoom=zoom, **kwargs)
+    def add_search_control(self, position ="topleft", **kwargs):
+        """Adds a search control to the map
+        Args:
+            **kwargs: Keyword arguments to be passed to the search control.
+        """  
+        if "url" not in kwargs:
+            kwargs["url"] = "https://nominatim.openstreetmap.org/search?format=json&q={s}"     
+        search_control = ipyleaflet.SearchControl(position=position, **kwargs)
+        self.add_control(search_control)
+
+    def add_draw_control(self, **kwargs):
+        """Adds a draw control to the map
+        Args:
+            **kwargs: Keyword arguments to be passed to the draw control.
+        """  
+        draw_control = ipyleaflet.DrawControl(**kwargs)
+
+        draw_control.polyline =  {
+        "shapeOptions": {
+            "color": "#6bc2e5",
+            "weight": 8,
+            "opacity": 1.0
+            }   
+        }
+        draw_control.polygon = {
+            "shapeOptions": {
+                "fillColor": "#6be5c3",
+                "color": "#6be5c3",
+                "fillOpacity": 1.0
+            },
+            "drawError": {
+                "color": "#dd253b",
+                "message": "Oups!"
+            },
+            "allowIntersection": False
+        }
+        draw_control.circle = {
+            "shapeOptions": {
+                "fillColor": "#efed69",
+                "color": "#efed69",
+                "fillOpacity": 1.0
+            }
+        }
+        draw_control.rectangle = {
+            "shapeOptions": {
+                "fillColor": "#fca45d",
+                "color": "#fca45d",
+                "fillOpacity": 1.0
+            }
+        }
+        self.add_control(draw_control)
+
+
+
+
+
+def generate_random_string(length=10, upper=False, punctuations=False, digits=False):
+    """Generates a random string of fixed length
+
+    Args:
+        length (int, optional): The length of the string. Defaults to 10.
+        upper (bool, optional): Whether to include uppercase letters. Defaults to False.
+        punctuations (bool, optional):Whether to include punctuations. Defaults to False.
+        digits (bool, optional): Whether to include digits. Defaults to False.
+
+    Returns:
+        str: The generated string.
+    """    
+    letters = string.ascii_lowercase
+    if upper:
+        letters += string.ascii_uppercase
+    if digits:
+        letters += string.digits
+    if punctuations:
+        letters += string.punctuation
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+def generate_lucky_number(length=1):
+    """Generates a random number of fixed length
+
+    Args:
+        length (int, optional): the length of the number. Defaults to 1.
+
+    Returns:
+        int: The generated number.
+    """    
+    result_str = ''.join(random.choice(string.digits) for i in range(length))
+    return int(result_str)
