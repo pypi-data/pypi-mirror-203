92e3f06caaa96f0fa4bfc4314de46eb2ba00f2a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:44 +0000

    Add package megabots/0.0.6/megabots-0.0.6.tar.gz

---
+++ packages/megabots/megabots-0.0.6.tar.gz/megabots-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.6"
+
+setup(
+    name="megabots",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+        "langchain",
+        "tiktoken",
+        "unstructured",
+        "fastapi",
+        "faiss-cpu",
+        "pdfminer.six",
+    ],
+    author="Megaklis Vasilakis",
+    author_email="megaklis.vasilakis@gmail.com",
+    description="Create a question answering over docs bot with one line of code.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/momegas/megabots",
+    classifiers=[
+        # Choose appropriate classifiers from
+        # https://pypi.org/classifiers/
+        "Development Status :: 4 - Beta"
+    ],
+)
