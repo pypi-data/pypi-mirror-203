79bdb9683b2ba2056010112d84098213197644eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:55 +0000

    Add package hb-test-generator/0.1.6/hb-test-generator-0.1.6.tar.gz

---
+++ packages/hb-test-generator/hb-test-generator-0.1.6.tar.gz/hb-test-generator-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hb-test-generator",
+    version="0.1.6",
+    description="A package to generate tests & write it into files using OpenAI's GPT-3",
+    author="Maksymenkov Eugene",
+    author_email="foatei@gmail.com",
+    url="https://github.com/halalbooking/hb_test_generator",
+    packages=['hb_test_generator'],
+    install_requires=[
+        "openai",
+    ],
+    entry_points={
+        "console_scripts": [
+            "hb-generate-tests=generate_tests:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
