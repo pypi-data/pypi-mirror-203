a056d0e4b397da71ae76722a07faba6630d2cd2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:10 +0000

    Add package flm-citations/0.2.4/flm_citations-0.2.4-py3-none-any.whl

---
+++ packages/flm-citations/flm_citations-0.2.4-py3-none-any.whl/flm_citations/__init__.py
@@ -0,0 +1,25 @@
+
+
+### FIXME: Merge feature config of imported configs don't work yet ... :/
+### Need to fix this in flm.main.run().  Probably all $import's should be
+### processed first, before merging anything.
+
+flm_default_import_config = {
+    'flm': {
+        'features': {
+            'flm_citations': {},
+            # {
+            #     'sources': [
+            #         {
+            #             '$defaults': True,
+            #         },
+            #     ],
+            # },
+        },
+    },
+}
+
+
+def FeatureClass(*args, **kwargs):
+    from .feature import FeatureClass as _FeatureClass
+    return _FeatureClass(*args, **kwargs)
