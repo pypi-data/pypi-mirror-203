b3b1c2d9db3b73e66ec8c9ea8cf5444cf1181a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:19 +0000

    Add package symdexer/0.2.3/symdexer-0.2.3-py3-none-any.whl

---
+++ packages/symdexer/symdexer-0.2.3-py3-none-any.whl/symdexer/cache.py
@@ -0,0 +1,97 @@
+import sqlite3
+from pathlib import Path
+from typing import Generator
+
+from symdexer.symbols import iter_symbols
+from symdexer.modules import walk_modules, Module
+
+
+class Cache:
+    def __init__(self, path: Path):
+        self.path = path
+
+    def __enter__(self):
+        self.db = sqlite3.connect(self.path).__enter__()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.db.__exit__(*args, **kwargs)
+
+    def reset(self):
+        self.db.executescript(
+            """
+            DROP TABLE IF EXISTS Module;
+
+            DROP TABLE IF EXISTS Symbol;
+
+            CREATE TABLE Module (
+                name TEXT PRIMARY KEY,
+                path TEXT NOT NULL,
+                changed NUMBER NOT NULL
+            );
+
+            CREATE TABLE Symbol (
+                name TEXT NOT NULL,
+                type TEXT NOT NULL,
+                module TEXT NOT NULL REFERENCES Module(name),
+                UNIQUE (name, type, module)
+            );
+            """
+        )
+
+    def update(self, packages: list[Path]):
+        for path in packages:
+            for module in walk_modules(path):
+                self._cache_module(module)
+
+        self.db.commit()
+
+    def _cache_module(self, module: Module):
+        # checks if the module already exists and is outdated
+        if self.db.execute(
+            """
+            SELECT name, changed
+            FROM Module
+            WHERE name = ? AND changed < ?
+            """,
+            (module.name, module.mtime),
+        ).fetchall():
+            return
+
+        self.db.execute(
+            """
+            INSERT OR IGNORE INTO Module (name, path, changed)
+            VALUES (?, ? ,?)
+            """,
+            (module.name, str(module.path.resolve()), module.mtime),
+        )
+
+        for symbol, sym_type in iter_symbols(module.path):
+            self.db.execute(
+                """
+                INSERT OR IGNORE INTO Symbol (name, type, module)
+                VALUES (?, ? ,?)
+                """,
+                (symbol, sym_type, module.name),
+            )
+
+    def search(self, symbols: list[str], fuzzy: bool, types: list[str]) -> Generator[tuple[str, str, str], None, None]:
+        symbol_t = "Symbol.name LIKE ?" if fuzzy else "Symbol.name = ?"
+        symbols_t = " OR ".join(symbol_t for _ in symbols)
+        types_t = " OR ".join("type = ?" for _ in types)
+
+        cursor = self.db.execute(
+            f"""
+            SELECT GROUP_CONCAT(Symbol.name, ", ") as names, module, path
+            FROM Symbol
+            INNER JOIN Module ON module = Module.name
+            WHERE ({symbols_t}) AND ({types_t})
+            GROUP BY
+                module
+            ORDER BY
+                LENGTH(module) + LENGTH(names) ASC
+            """,
+            (*symbols, *types),
+        )
+
+        return cursor.fetchall()
---
+++ packages/symdexer/symdexer-0.2.3-py3-none-any.whl/symdexer/main.py
@@ -0,0 +1,147 @@
+from pathlib import Path
+from argparse import ArgumentParser
+
+from symdexer.version import VERSION
+from symdexer.cache import Cache
+from symdexer.symbols import SYM_TYPES
+from symdexer.types import package_type
+
+
+def search_cache(cache_p: Path, symbols: list[str], fuzzy: bool, types: list[str]):
+    if not cache_p.is_file():
+        raise FileNotFoundError(f"Cache `{cache_p}` not found.")
+
+    with Cache(cache_p) as cache:
+        yield from cache.search(symbols, fuzzy, types)
+
+
+def find_command(cache_p: Path, symbols: list[str], fuzzy: bool, types: list[str]) -> None:
+    for names, module, _ in search_cache(cache_p, symbols, fuzzy, types):
+        yield f"from {module} import {names}"
+
+
+def locate_command(cache_p: Path, symbols: list[str], fuzzy: bool, types: list[str]) -> None:
+    for name, module, path in search_cache(cache_p, symbols, fuzzy, types):
+        yield name
+        yield module
+        yield path
+
+
+def index_command(cache_p: Path, packages: list[Path], reset: bool):
+    if not cache_p.exists():
+        reset = True
+
+    with Cache(cache_p) as cache:
+        if reset:
+            cache.reset()
+        cache.update(packages)
+
+    yield "Done indexing"
+
+
+def main():
+    parser = ArgumentParser("symdexer")
+
+    parser.add_argument("-v", "--version", action="version", version=VERSION)
+
+    parser.add_argument(
+        "-c",
+        "--cache-file",
+        dest="cache",
+        metavar="file",
+        default="symdex.db",
+        type=Path,
+        help="Name of the cache to store to and load from. Defaults to `symdex.db`",
+    )
+
+    sub_parsers = parser.add_subparsers(required=True, dest="command")
+
+    find_parser = sub_parsers.add_parser(
+        "find",
+        help="Searches the cache for symbols matching a list of patterns",
+    )
+    find_parser.add_argument(
+        "symbols",
+        metavar="SYMBOL",
+        nargs="+",
+        help="The symbols to look for",
+    )
+    find_parser.add_argument(
+        "-f",
+        "--fuzzy",
+        dest="fuzzy",
+        action="store_true",
+        default=False,
+        help="If the symbols should be searched based on a pattern",
+    )
+    find_parser.add_argument(
+        "-t",
+        "--types",
+        dest="types",
+        metavar="SYM_TYPE",
+        choices=SYM_TYPES,
+        default=SYM_TYPES,
+        nargs="+",
+        help="Symbol types to show",
+    )
+
+    locate_parser = sub_parsers.add_parser(
+        "locate",
+        help="Locates the source paths of the modules where the given symbols reside",
+    )
+    locate_parser.add_argument(
+        "symbols",
+        metavar="SYMBOL",
+        nargs="+",
+        help="The symbols to look for",
+    )
+    locate_parser.add_argument(
+        "-f",
+        "--fuzzy",
+        dest="fuzzy",
+        action="store_true",
+        default=False,
+        help="If the symbols should be searched based on a pattern",
+    )
+    locate_parser.add_argument(
+        "-t",
+        "--types",
+        dest="types",
+        metavar="SYM_TYPE",
+        choices=SYM_TYPES,
+        default=SYM_TYPES,
+        nargs="+",
+        help="Symbol types to show",
+    )
+
+    cache_parser = sub_parsers.add_parser(
+        "index",
+        help="Load symbols into cache",
+    )
+    cache_parser.add_argument(
+        "-r",
+        "--reset",
+        dest="reset",
+        action="store_true",
+        default=False,
+        help="If the cache should be completely wiped",
+    )
+    cache_parser.add_argument(
+        "packages",
+        metavar="PACKAGE",
+        nargs="+",
+        type=package_type,
+        help="The packages to add to cache. May be package names or paths",
+    )
+
+    args = parser.parse_args()
+
+    if args.command == "index":
+        res = index_command(args.cache, args.packages, args.reset)
+    elif args.command == "find":
+        res = find_command(args.cache, args.symbols, args.fuzzy, args.types)
+    elif args.command == "locate":
+        res = locate_command(args.cache, args.symbols, args.fuzzy, args.types)
+
+    for line in res:
+        print(line)
