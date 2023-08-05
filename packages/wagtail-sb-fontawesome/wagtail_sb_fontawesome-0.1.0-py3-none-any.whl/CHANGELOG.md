7cd447054f59d27f8898be2a3b89e8fe75835c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:58 +0000

    Add package wagtail-sb-fontawesome/0.1.0/wagtail_sb_fontawesome-0.1.0-py3-none-any.whl

---
+++ packages/wagtail-sb-fontawesome/wagtail_sb_fontawesome-0.1.0-py3-none-any.whl/CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [Unreleased]
+
+## [0.1.0] - 2023-04-15
+
+* Use of FontAwesome 4.7.0 in the wagtail admin
---
+++ packages/wagtail-sb-fontawesome/wagtail_sb_fontawesome-0.1.0-py3-none-any.whl/wagtail_sb_fontawesome/wagtail_hooks.py
@@ -0,0 +1,11 @@
+from django.templatetags.static import static
+from django.utils.html import format_html
+from wagtail.core import hooks
+
+
+@hooks.register("insert_global_admin_css")
+def fontawesome_css():
+    return format_html(
+        '<link rel="stylesheet" href="{}">',
+        static("fontawsome/css/font-awesome.min.css"),
+    )
