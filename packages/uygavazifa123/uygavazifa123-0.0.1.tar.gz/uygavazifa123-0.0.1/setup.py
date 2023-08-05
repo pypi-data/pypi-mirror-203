12e94c8e75589892f128d73670303fcca3417709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:48 +0000

    Add package uygavazifa123/0.0.1/uygavazifa123-0.0.1.tar.gz

---
+++ packages/uygavazifa123/uygavazifa123-0.0.1.tar.gz/uygavazifa123-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name = 'uygavazifa123',
+    version = '0.0.1',
+    author = 'Java',
+    author_email = 'hdhdhdvojdcjbfbfhzjs@gmail.com',
+    description = 'Ob-havo malumotlari'
+
+)
---
+++ packages/uygavazifa123/uygavazifa123-0.0.1.tar.gz/uygavazifa123-0.0.1/uygavazifa123/__init__.py
@@ -0,0 +1,5 @@
+def obhavo(a):
+    if a == 'Samarqand':
+        return "20 gradus"
+    else:
+        return "17 gradus"
