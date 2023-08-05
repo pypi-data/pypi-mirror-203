1efcbfd3332ba3fd471e28f18af4681f2ece6e7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:21 +0000

    Add package thirdai/0.6.8/thirdai-0.6.8-cp39-cp39-win_amd64.whl

---
+++ packages/thirdai/thirdai-0.6.8-cp39-cp39-win_amd64.whl/thirdai/_download.py
@@ -0,0 +1,46 @@
+import os
+import pathlib
+
+CACHE_DIR = pathlib.Path.home() / ".cache" / "thirdai"
+
+
+# TODO(josh): These methods aren't robust (they relies on the user having curl
+# and tar working on their machine), but the python requests library wasn't
+# working to download from dropbox so I am just going with this for now.
+# We should make this a more robust solution like huggingface's dataset
+# library.
+def _unzip_targz(targz_path, destination_parent_folder):
+    os.system(f"tar -xzf {targz_path} -C {destination_parent_folder}")
+
+
+def _download_file(url, download_path):
+    os.system(f"curl -L {url} -o {download_path}")
+
+
+def ensure_targz_installed(download_url, unzipped_dir_name):
+    global CACHE_DIR
+    CACHE_DIR.mkdir(parents=True, exist_ok=True)
+
+    unzipped_dir_path = CACHE_DIR / unzipped_dir_name
+    targz_download_path = CACHE_DIR / "temp.tar.gz"
+
+    cached = False
+    if not unzipped_dir_path.exists():
+        print(
+            f"Downloading {download_url}, which will be unzipped and saved in directory {unzipped_dir_path}"
+        )
+
+        _download_file(download_url, download_path=targz_download_path)
+        _unzip_targz(targz_download_path, destination_parent_folder=CACHE_DIR)
+        targz_download_path.unlink()
+
+        if not unzipped_dir_path.exists():
+            raise ValueError(
+                "Unzipped directory name was different than anticipated, caching will not work as expected."
+            )
+
+    else:
+        cached = True
+        print(f"{unzipped_dir_path} already exists, skipping download.")
+
+    return unzipped_dir_path, cached
