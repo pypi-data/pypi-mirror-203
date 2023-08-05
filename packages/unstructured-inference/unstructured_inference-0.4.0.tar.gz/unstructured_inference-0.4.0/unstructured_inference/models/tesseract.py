58df4b9575e89dfdcf8ac6bf248611010d6c98c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:21 +0000

    Add package unstructured-inference/0.4.0/unstructured_inference-0.4.0.tar.gz

---
+++ packages/unstructured-inference/unstructured_inference-0.4.0.tar.gz/unstructured_inference-0.4.0/unstructured_inference/models/tesseract.py
@@ -0,0 +1,20 @@
+from layoutparser.ocr.tesseract_agent import is_pytesseract_available, TesseractAgent
+
+from unstructured_inference.logger import logger
+
+ocr_agent: TesseractAgent = None
+
+
+def load_agent():
+    """Loads the Tesseract OCR agent as a global variable to ensure that we only load it once."""
+    global ocr_agent
+
+    if not is_pytesseract_available():
+        raise ImportError(
+            "Failed to load Tesseract. Ensure that Tesseract is installed. Example command: \n"
+            "    >>> sudo apt install -y tesseract-ocr"
+        )
+
+    if ocr_agent is None:
+        logger.info("Loading the Tesseract OCR agent ...")
+        ocr_agent = TesseractAgent(languages="eng")
