ae64f253a4e666d7a2be967ec21894b7e9d05bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:46 +0000

    Add package pyroscope-gridtools/0.0.3/pyroscope_gridtools-0.0.3.tar.gz

---
+++ packages/pyroscope-gridtools/pyroscope_gridtools-0.0.3.tar.gz/pyroscope_gridtools-0.0.3/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'Data fusion package for satellite data transformation.'
+LONG_DESCRIPTION = 'Data fusion package for transforming L2 satellite to L3 spatial-temporal gridded data.'
+
+# Setting up
+setup(
+    name = 'pyroscope_gridtools',         # Package name
+    packages = ['pyroscope_gridtools'],   
+    version = '0.0.3',      # Initial version
+    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description = 'Data fusion package for transforming L2 satellite to L3 spatial-temporal gridded data',  
+    author = 'Sally Zhao, Neil Gutkin',                 
+    author_email = 'zhaosally0@gmail.com',     
+    url = 'https://github.com/jwei-openscapes/aerosol-data-fusion',   # github repository  
+    keywords = ['data fusion', 'satellite', 'L2', 'L3'],   # Keywords
+    #packages = find_packages(),
+    entry_points ={
+        'console_scripts': [
+            'pyroscope_gridtools = pyroscope_gridtools.gtools:main'
+        ]
+    },
+    install_requires=[            
+            'numpy',
+            'joblib',
+            'cuda',
+            'netCDF4',
+            'pyhdf',
+            'pyyaml',
+            'numba',
+            'os', 
+            'argparse'
+        ],
+    classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      #supported versions
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    ],
+)
