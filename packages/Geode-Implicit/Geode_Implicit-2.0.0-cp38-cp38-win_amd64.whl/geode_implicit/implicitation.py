486840b1da1842e73c5dbd49c6ec7540ab380e90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:49 +0000

    Add package Geode-Implicit/2.0.0/Geode_Implicit-2.0.0-cp38-cp38-win_amd64.whl

---
+++ packages/Geode-Implicit/Geode_Implicit-2.0.0-cp38-cp38-win_amd64.whl/geode_implicit/implicitation.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+from geode_numerics import scalar_function
+
+from .bin.geode_implicit_py_implicitation import *
+ImplicitImplicitationLibrary.initialize()
