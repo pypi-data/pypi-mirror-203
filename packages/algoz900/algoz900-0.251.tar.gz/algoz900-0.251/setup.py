26b907c0fa7a52e2ef64400e9bd684ea3d5e2374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package algoz900/0.251/algoz900-0.251.tar.gz

---
+++ packages/algoz900/algoz900-0.251.tar.gz/algoz900-0.251/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='algoz900',
+    version='0.251',
+    packages=find_packages("src"),
+	include_package_data=True,
+	description= "Python version of Synthetic difference in differences",
+    install_requires=[
+        # any dependencies required by your package
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
