3ab244f4e2b3dcde9fdca8c3156b7ef0de434a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:16 +0000

    Add package luau/0.2.3/luau-0.2.3.tar.gz

---
+++ packages/luau/luau-0.2.3.tar.gz/luau-0.2.3/src/luau/convert.py
@@ -0,0 +1,135 @@
+
+READ_STR_AS_LITERAL_PREFIX = "<LITERAL_STRING_START>_!_"
+READ_STR_AS_LITERAL_SUFFIX = "_!_<LITERAL_STRING_END>"
+
+def _insert_comma(enabled: bool) -> str:
+	if enabled:
+		return ","
+	else:
+		return ""
+
+def _get_indent(indent_count: int) -> str:
+	return "\t"*indent_count
+
+def from_bool(value: bool, indent_count=0, add_comma_at_end=False) -> str:
+	return _get_indent(indent_count) + str(value).lower() + _insert_comma(add_comma_at_end)
+
+def from_str(value: str, indent_count=0, add_comma_at_end=False) -> str:
+	suffix_index = value.find(READ_STR_AS_LITERAL_SUFFIX)
+	
+	starts_with_prefix = READ_STR_AS_LITERAL_PREFIX == value[0:len(READ_STR_AS_LITERAL_PREFIX)]
+	if starts_with_prefix and suffix_index != -1:
+		raw_val = value.replace(READ_STR_AS_LITERAL_PREFIX, "").replace(READ_STR_AS_LITERAL_SUFFIX, "")
+		return _get_indent(indent_count) + raw_val + _insert_comma(add_comma_at_end)
+	else:
+		return _get_indent(indent_count) + f"\"{value}\"" + _insert_comma(add_comma_at_end)
+
+def from_number(value: int | float, indent_count=0, add_comma_at_end=False) -> str:
+	return _get_indent(indent_count) + str(value) + _insert_comma(add_comma_at_end)
+
+def from_nil(indent_count=0, add_comma_at_end=False) -> str:
+	return f"{_get_indent(indent_count)} nil" + _insert_comma(add_comma_at_end)
+
+
+def from_list(value: list, indent_count=0, add_comma_at_end=False, multi_line=True, skip_initial_indent=False):
+	
+	# start list
+	list_val = ""
+	if skip_initial_indent:
+		list_val += "{"
+	else:
+		list_val += _get_indent(indent_count) + "{"
+
+	# iterate through values
+	for v in value:
+		
+		# write entry
+		if type(v) == dict or type(v) == list:
+			entry = from_any(v, indent_count+1, False, multi_line, True) + _insert_comma(True)
+		else:
+			entry = from_any(v, indent_count, False, multi_line, True) + _insert_comma(True)
+
+		# add it to existing string
+		if multi_line:
+			list_val += "\n" + _get_indent(indent_count+1) + entry
+		else:
+			list_val += entry
+
+	# end the table on a new line if multi-line
+	if multi_line:
+		list_val += "\n" + _get_indent(indent_count)
+
+	# close the table
+	list_val += "}"
+
+	# indent as needed and return value
+	return _get_indent(indent_count) + list_val + _insert_comma(add_comma_at_end)
+
+def from_dict(value: dict, indent_count=0, add_comma_at_end=False, multi_line=True, skip_initial_indent=False):
+	
+	# start dictionary
+	list_val = ""
+	if skip_initial_indent:
+		list_val += "{"
+	else:
+		list_val += _get_indent(indent_count) + "{"
+
+	# iterate through key-val pairs
+	for k, v in value.items():
+
+		# write entry
+		if type(v) == dict or type(v) == list:
+			entry = f"[{from_any(k, 0, False)}] = {from_any(v, indent_count+1, False, multi_line, True)}" + _insert_comma(True)
+		else:
+			entry = f"[{from_any(k, 0, False)}] = {from_any(v, 0, False, multi_line, True)}" + _insert_comma(True)
+
+		# add it to existing string
+		if multi_line:
+			list_val += "\n" + _get_indent(indent_count+1) + entry
+		else:
+			list_val += entry
+
+	# end the table on a new line if multi-line
+	if multi_line:
+		list_val += "\n" + _get_indent(indent_count)
+
+	# close the table
+	list_val += "}"
+
+	# indent as needed and return value
+	if skip_initial_indent:
+		return _get_indent(0) + list_val + _insert_comma(add_comma_at_end)
+	else:
+		return _get_indent(indent_count) + list_val + _insert_comma(add_comma_at_end)
+
+def from_any(
+	value: int | str | None | float | dict | list = None, 
+	indent_count = 0, 
+	add_comma_at_end = False, 
+	multi_line=True,
+	skip_initial_indent=False
+) -> str:
+
+	if type(value) == list:
+
+		return from_list(value, indent_count, add_comma_at_end, multi_line, skip_initial_indent)
+
+	elif type(value) == dict:
+		
+		return from_dict(value, indent_count, add_comma_at_end, multi_line, skip_initial_indent)
+
+	elif type(value) == float or type(value) == int:
+		return from_number(value, indent_count, add_comma_at_end)
+		
+	elif type(value) == bool:
+		return from_bool(value, indent_count, add_comma_at_end)
+		
+	elif type(value) == str:
+		return from_str(value, indent_count, add_comma_at_end)
+		
+	return from_nil(indent_count, add_comma_at_end)
+
+
+# allows for processing of text in the other methods as is without being wrapped in quotes as a string
+def mark_as_literal(text: str) -> str:
+	return READ_STR_AS_LITERAL_PREFIX+text+READ_STR_AS_LITERAL_SUFFIX
---
+++ packages/luau/luau-0.2.3.tar.gz/luau-0.2.3/src/luau/roblox/wally.py
@@ -0,0 +1,74 @@
+import os
+import toml
+from .tool import get_tool_name
+from .rojo import get_rojo_project_path, get_rojo_name, build_sourcemap
+
+WALLY_SOURCE = "UpliftGames/wally"
+WALLY_VERSION = "0.3.1"
+WPT_SOURCE = "JohnnyMorganz/wally-package-types"
+WPT_VERSION = "1.2.0"
+
+def get_wally_name():
+	return get_tool_name(WALLY_SOURCE, WALLY_VERSION)
+
+def get_wally_package_types_name():
+	return get_tool_name(WPT_SOURCE, WPT_VERSION)
+
+def update_wally():
+
+	project_path = get_rojo_project_path()
+
+	wally_tool_name = get_wally_name()
+	rojo_tool_name = get_rojo_name()
+	wpt_tool_name = get_wally_package_types_name()
+
+	os.system(f"{wally_tool_name} install")
+	build_sourcemap()
+	os.system(f"{wpt_tool_name} --sourcemap sourcemap.json Packages")
+
+def get_wally_package_nickname(package_wally_path: str, prefix: str="ToolGen") -> str:
+	wally_config = toml.loads(open("wally.toml", "r").read())
+	generated_nickname = (package_wally_path.split("/")[1].split("@")[0]).title()
+	for nickname, package_path in wally_config["dependencies"].items():
+		if nickname == generated_nickname:
+			generated_nickname = prefix+"_"+generated_nickname.title()
+		if package_wally_path == package_path:
+			return nickname
+
+	# no maid installed
+	wally_config["dependencies"][generated_nickname] = package_wally_path
+
+	open("wally.toml", "w").write(toml.dumps(wally_config))
+	update_wally()
+
+	return generated_nickname
+
+def require_roblox_wally_package(
+	package_wally_path: str, 
+	is_private: bool = False, 
+	is_header: bool = True,
+	wally_directory_path: str ="game/ReplicatedStorage/Packages"
+) -> str:
+
+	package_nickname = get_wally_package_nickname(package_wally_path)
+
+	package_path = ""
+	roblox_path_keys = (wally_directory_path+"/"+package_nickname).split("/")
+	for i, key in enumerate(roblox_path_keys):
+		if i == 0:
+			package_path += key
+		else:
+			package_path += f":WaitForChild(\"{key}\")"
+
+	package_path = package_path.replace("game:WaitForChild(\"ReplicatedStorage\")", "game:GetService(\"ReplicatedStorage\")")
+	package_path = package_path.replace("game:WaitForChild(\"ServerStorage\")", "game:GetService(\"ServerStorage\")")
+	package_path = package_path.replace("game:WaitForChild(\"ReplicatedFirst\")", "game:GetService(\"ReplicatedFirst\")")
+
+	if is_header:
+		if is_private:
+			return f"local _{package_nickname} = require({package_path})"
+		else:
+			return f"local {package_nickname} = require({package_path})"
+	else:
+		return f"require({package_path})"
+
