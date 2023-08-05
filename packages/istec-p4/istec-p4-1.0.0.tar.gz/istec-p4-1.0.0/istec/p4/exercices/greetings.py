5b53cb50965cf71e684a29db6e1a11994abd4b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:16 +0000

    Add package istec-p4/1.0.0/istec-p4-1.0.0.tar.gz

---
+++ packages/istec-p4/istec-p4-1.0.0.tar.gz/istec-p4-1.0.0/istec/p4/exercices/greetings.py
@@ -0,0 +1,9 @@
+def hello():
+        print("Hello World!!")
+
+def good_morning():
+        print("Good Morning!!")
+        
+def good_afternoon():
+        print("Good Afternoon!!")
+        
---
+++ packages/istec-p4/istec-p4-1.0.0.tar.gz/istec-p4-1.0.0/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages 
+setup(
+    name="istec-p4",
+    version="1.0.0",
+    packages= find_packages()
+)
