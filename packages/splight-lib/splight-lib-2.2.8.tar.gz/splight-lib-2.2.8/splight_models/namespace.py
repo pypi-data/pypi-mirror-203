587b0aed898e73f47f45b944dce2422317bb2316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:49 +0000

    Add package splight-lib/2.2.8/splight-lib-2.2.8.tar.gz

---
+++ packages/splight-lib/splight-lib-2.2.8.tar.gz/splight-lib-2.2.8/splight_models/namespace.py
@@ -0,0 +1,18 @@
+from splight_models.base import SplightBaseModel
+from pydantic import validator
+from typing import Dict
+
+
+def namespace_transform(raw: str) -> str:
+    return raw.lower().replace("_", "")
+
+
+class Namespace(SplightBaseModel):
+    id: str
+    environment: Dict = {}
+    _extract_id = validator('id', pre=True, allow_reuse=True)(namespace_transform)
+
+    @property
+    def secret_name(self):
+        return f"{self.id}-secrets"
+
