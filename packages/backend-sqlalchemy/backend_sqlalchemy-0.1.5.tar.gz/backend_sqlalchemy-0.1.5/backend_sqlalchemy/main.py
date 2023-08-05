e045dc2b69ba7c5ca7c42be380284120e1216b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:02 +0000

    Add package backend-sqlalchemy/0.1.5/backend_sqlalchemy-0.1.5.tar.gz

---
+++ packages/backend-sqlalchemy/backend_sqlalchemy-0.1.5.tar.gz/backend_sqlalchemy-0.1.5/backend_sqlalchemy/main.py
@@ -0,0 +1,14 @@
+def sum_two(a, b):
+    return a + b
+
+
+def concat_name(first_name: str, last_name: str):
+    return f"{first_name} {last_name}"
+
+
+def join_array(array):
+    return ",".join(array)
+
+
+def str_contains_substr(_str, _substr):
+    return _substr in _str
