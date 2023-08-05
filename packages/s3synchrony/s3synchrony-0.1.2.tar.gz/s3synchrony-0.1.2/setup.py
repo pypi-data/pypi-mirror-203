62662e1f7e238688ab843006a416e223dfc4f676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:46 +0000

    Add package s3synchrony/0.1.2/s3synchrony-0.1.2.tar.gz

---
+++ packages/s3synchrony/s3synchrony-0.1.2.tar.gz/s3synchrony-0.1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+      long_description = fh.read()
+
+setuptools.setup(
+      name='s3synchrony',
+      version='0.1.2',
+      description='This package provides a service for synchronizing file creations, deletions, and modifications across users on an AWS S3 prefix.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Sevan Brodjian',
+      author_email='sevanbro7@gmail.com',
+      url='https://github.com/SevanBrodjian/s3synchrony',
+      packages=setuptools.find_packages(),
+      license='LICENSE.md',
+     )
