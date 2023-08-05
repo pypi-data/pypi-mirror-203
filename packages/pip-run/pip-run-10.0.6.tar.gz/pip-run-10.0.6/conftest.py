1e910f01d8fcae243d5d9364c3badbfafcdc6ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:31 +0000

    Add package pip-run/10.0.6/pip-run-10.0.6.tar.gz

---
+++ packages/pip-run/pip-run-10.0.6.tar.gz/pip-run-10.0.6/conftest.py
@@ -0,0 +1,53 @@
+import textwrap
+
+import jaraco.path
+import pytest
+
+import pip_run.mode.persist
+
+
+collect_ignore = ['examples']
+
+
+@pytest.fixture
+def reqs_files(tmp_path):
+    """Create a couple of requirements files."""
+    jaraco.path.build(
+        {
+            'reqs1.txt': textwrap.dedent(
+                """
+                abc
+                def
+                """
+            ).lstrip(),
+            'reqs2.txt': textwrap.dedent(
+                """
+                uvw
+                xyz
+                """
+            ).lstrip(),
+        },
+        tmp_path,
+    )
+    return tmp_path.glob('reqs*.txt')
+
+
+@pytest.fixture(scope="session")
+def monkeypatch_session():
+    with pytest.MonkeyPatch.context() as mp:
+        yield mp
+
+
+@pytest.fixture(autouse=True, scope='session')
+def alt_cache_dir(monkeypatch_session, tmp_path_factory):
+    alt_cache = tmp_path_factory.mktemp('cache')
+
+    class Paths:
+        user_cache_path = alt_cache
+
+    monkeypatch_session.setattr(pip_run.mode.persist, 'paths', Paths)
+
+
+@pytest.fixture(params=['persist', 'ephemeral'])
+def run_mode(monkeypatch, request):
+    monkeypatch.setenv('PIP_RUN_MODE', request.param)
