4faa6a2625facee144d0a22bc352d030f55f5f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:51 +0000

    Add package distributed-transcoder-common/0.2.3/distributed-transcoder-common-0.2.3.tar.gz

---
+++ packages/distributed-transcoder-common/distributed-transcoder-common-0.2.3.tar.gz/distributed-transcoder-common-0.2.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['distributed_transcoder_common']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['tortoise-orm>=0.19.3,<0.20.0']
+
+setup_kwargs = {
+    'name': 'distributed-transcoder-common',
+    'version': '0.2.3',
+    'description': 'A common library for for the distributed transcoder project',
+    'long_description': None,
+    'author': 'Eric Volpert',
+    'author_email': 'ericvolp12@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
