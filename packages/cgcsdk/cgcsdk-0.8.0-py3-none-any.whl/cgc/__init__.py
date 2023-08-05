01f0669920827db967d540b16e6db153436bc952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:44 +0000

    Add package cgcsdk/0.8.0/cgcsdk-0.8.0-py3-none-any.whl

---
+++ packages/cgcsdk/cgcsdk-0.8.0-py3-none-any.whl/cgc/__init__.py
@@ -0,0 +1,16 @@
+"""
+cgc
+Comtegra GPU Cloud REST API client
+"""
+from cgc.utils.version_control import get_version
+
+__version__ = get_version()
+__author__ = "Adrian Jastrzębski <a.jastrzebski@quantdevlabs.com>, Przemysław Kowalski <przemyslaw.kowalski@comtegra.pl>, Jakub Zboina <jakub.zboina@comtegra.pl>"
+__credits__ = "Quantum Dev Labs"
+
+
+# from cgc.utils.asdasd import asdasdasd as asdasdasdasdasdMongo
+
+# import cgc
+
+# cgc.asdasdasdasdasdMongo()
