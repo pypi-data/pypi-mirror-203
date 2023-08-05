2d25a9309b6c2121f2be5e55bfb9abdf3e3da946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:02 +0000

    Add package Allagash/0.4.3/Allagash-0.4.3.tar.gz

---
+++ packages/Allagash/Allagash-0.4.3.tar.gz/Allagash-0.4.3/allagash/__init__.py
@@ -0,0 +1,19 @@
+from .problem import (
+    Problem,
+    UnboundedException,
+    UndefinedException,
+    InfeasibleException,
+    NotSolvedException,
+)
+from .coverage import Coverage
+
+__all__ = [
+    "Problem",
+    "UnboundedException",
+    "UndefinedException",
+    "InfeasibleException",
+    "NotSolvedException",
+    "Coverage",
+]
+
+__version__ = "0.4.3"
