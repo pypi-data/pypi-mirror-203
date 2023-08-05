9f027404fdc4deab8d6bd65fc68619aee8be5c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:28 +0000

    Add package napari-griottes/0.4.0/napari_griottes-0.4.0.tar.gz

---
+++ packages/napari-griottes/napari_griottes-0.4.0.tar.gz/napari_griottes-0.4.0/src/napari_griottes/_reader.py
@@ -0,0 +1,217 @@
+"""
+This module is an example of a barebones numpy reader plugin for napari.
+
+It implements the Reader specification, but your plugin may choose to
+implement multiple readers or even other plugin contributions. see:
+https://napari.org/plugins/stable/guides.html#readers
+"""
+import networkx as nx
+import numpy as np
+import pandas
+import pandas as pd
+from tifffile import imread
+
+from ._widget import CNAME, POINT_PARAMS
+import logging 
+import json
+
+
+
+def napari_get_reader(path):
+    """A basic implementation of a Reader contribution.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    function or None
+        If the path is a recognized format, return a function that accepts the
+        same path or list of paths, and returns a list of layer data tuples.
+    """
+    if isinstance(path, list):
+        # reader plugins may be handed single path, or a list of paths.
+        # if it is a list, it is assumed to be an image stack...
+        # so we are only going to look at the first file.
+        path = path[0]
+
+    # if we know we cannot read the file, we immediately return None.
+    if path.endswith(".tif") or path.endswith(".tiff"):
+        return read_tif
+
+    if path.endswith(".csv"):
+        return read_csv
+    
+    if path.endswith(".json"):
+        return load_graph_from_json
+
+    # otherwise we return the *function* that can read ``path``.
+    if path.endswith(".npy"):
+        return reader_numpy
+
+    return None
+
+
+def read_tif(path="", **kwargs):
+    print(f"Opening {path}")
+    data = imread(path)
+    print(f"input shape: {data.shape}")
+    if data.ndim < 3:
+        channel_axis = None
+    else:
+        channel_axis = 0
+    if data.shape[-1] < 10:
+        dims = list(range(data.ndim))
+        last_dim = dims.pop()
+        dims.insert(0, last_dim)
+        data = data.transpose(*dims)
+        print(f"transpose -> {data.shape}")
+
+    return [(data, {"channel_axis": channel_axis, **kwargs}, "image")]
+
+
+def read_csv(path, **kwargs):
+    data = pandas.read_csv(path, index_col=None)
+    try:
+        return colorized_points(data, metadata={"path": path})
+    except Exception as e:
+        logging.error(f"Unable to apply colors: {e}")
+        return [
+            (
+                data[["y", "x"]].values,
+                
+                {"metadata": {"path": path}, "properties":data, **kwargs},
+                "points",
+            )
+        ]
+
+def colorized_points(data, **kwargs):
+    data.loc[:,"colors_napari"] = data["cell_type"] / data["cell_type"].max()
+    try:
+        out = data[["z", "y", "x"]].values 
+        ndims=3
+    except KeyError:
+        out = data[["y", "x"]].values 
+        ndims=2
+    return [
+        (
+            out,
+            {
+                **dict(
+                    ndim=ndims,
+                    properties=data,
+                    face_color="colors_napari",
+                    face_colormap='viridis',
+                    **POINT_PARAMS
+                ),
+                **kwargs,
+            },
+            "points",
+        )
+    ]
+
+
+def load_graph_from_json(filename):
+    with open(filename, 'r') as file:
+        data = json.load(file)
+
+    graph = nx.Graph()
+
+    for node_data in data['nodes']:
+        node_id, props = node_data
+        graph.add_node(node_id, **props)
+
+    for edge_data in data['edges']:
+        source, target, props = edge_data
+        graph.add_edge(source, target, **props)
+
+    return read_graph(graph)
+
+
+def read_graph(
+    G,
+):
+    pos = nx.get_node_attributes(G, "pos")
+
+    try:
+        centers = pd.DataFrame(
+            [
+                {"z": v[0], "y": v[1], "x": v[2], "label": k}
+                for k, v in pos.items()
+            ]
+        )
+        out = centers[["z", "y", "x"]]
+    except IndexError:
+        centers = pd.DataFrame(
+            [{"y": v[0], "x": v[1], "label": k} for k, v in pos.items()]
+        )
+        out = centers[["y", "x"]]
+
+    lines = [[pos[i] for i in ids] for ids in list(G.edges)]
+    try:
+        weights = [0.2 * 1 * e[2]["weight"] for e in G.edges(data=True)]
+    except (IndexError, KeyError):
+        print("no weights")
+        weights = [1] * len(lines)
+
+    print(
+        f"{len(lines)} lines for {len(centers)}  positions recovered, rendering..."
+    )
+    return [
+        (
+            out,
+            {"name": "Centers", "properties": centers},
+            "points",
+        ),
+        (
+            lines,
+            {
+                "shape_type": "line",
+                "name": CNAME,
+                "edge_width": weights,
+                "metadata": {"graph": G},
+            },
+            "shapes",
+        ),
+    ]
+
+
+
+
+def reader_numpy(path):
+    """Take a path or list of paths and return a list of LayerData tuples.
+
+    Readers are expected to return data as a list of tuples, where each tuple
+    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
+    both optional.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    layer_data : list of tuples
+        A list of LayerData tuples where each tuple in the list contains
+        (data, metadata, layer_type), where data is a numpy array, metadata is
+        a dict of keyword arguments for the corresponding viewer.add_* method
+        in napari, and layer_type is a lower-case string naming the type of
+        layer.
+        Both "meta", and "layer_type" are optional. napari will default to
+        layer_type=="image" if not provided
+    """
+    # handle both a string and a list of strings
+    paths = [path] if isinstance(path, str) else path
+    # load all files into array
+    arrays = [np.load(_path, allow_pickle=True).item()["masks"] for _path in paths]
+    # stack arrays into single array
+    data = np.squeeze(np.stack(arrays))
+
+    # optional kwargs for the corresponding viewer.add_* method
+    add_kwargs = {}
+
+    layer_type = "labels"  # optional, default is "image"
+    return [(data, add_kwargs, layer_type)]
---
+++ packages/napari-griottes/napari_griottes-0.4.0.tar.gz/napari_griottes-0.4.0/src/napari_griottes/_writer.py
@@ -0,0 +1,107 @@
+"""
+This module is an example of a barebones writer plugin for napari.
+
+It implements the Writer specification.
+see: https://napari.org/plugins/stable/guides.html#writers
+
+Replace code below according to your needs.
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Sequence, Tuple, Union
+
+import networkx as nx
+import json
+
+if TYPE_CHECKING:
+    DataType = Union[Any, Sequence[Any]]
+    FullLayerData = Tuple[DataType, dict, str]
+
+import numpy as np
+
+def numpy_to_python(value):
+    if isinstance(value, np.generic):
+        return value.tolist()
+    return value
+
+def save_graph_to_json(graph, filename):
+    
+    with open(filename, 'w') as file:
+        json.dump({
+            "nodes": list(graph.nodes.data()),
+            "edges": list(graph.edges.data())
+        }, file, default=_ser)
+    return [filename]
+
+def _ser(o):
+    """convert types for json.dumps to work"""
+    try:
+        if isinstance(o, (int, np.int32, np.uint16, np.intc, np.int64)):
+            return int(o)
+        elif isinstance(o, (float, str)):
+            return o
+        else:
+            return list(o)
+    except TypeError as e:
+        print(o, type(o))
+        raise e
+
+def save_graph(path: str, data: FullLayerData, props: dict, **kwargs):
+    """Writes a single layer graph"""
+    try:
+        ppp = path if path.endswith(".json") else path + ".json"
+        out = save_graph_to_json(data.metadata["graph"], ppp)
+        print(f"Saved graph to {out}")
+    except KeyError:
+        print("Grapth not found, choose another layer")
+    return [out]
+
+
+# (
+#     '/home/aaristov/test',
+
+#     [
+#         <class 'numpy.ndarray'> (2, 2) float64,
+#         <class 'numpy.ndarray'> (2, 2) float64,
+#         <class 'numpy.ndarray'> (2, 2) float64
+#     ],
+
+#     {
+#         'name': 'Connections',
+#         'metadata':
+#         {
+#             'graph': <networkx.classes.graph.Graph object at 0x7f39a5a71d60>
+#         },
+#         'scale': [1.0, 1.0],
+#         'translate': [0.0, 0.0],
+#         'rotate': [[1.0, 0.0], [0.0, 1.0]],
+#         'shear': [0.0],
+#         'opacity': 0.7,
+#         'blending': 'translucent',
+#         'visible': True,
+#         'experimental_clipping_planes': [],
+#         'ndim': 2,
+#         'properties': {},
+#         'property_choices': {},
+#         'text': {'values': <class 'numpy.ndarray'> (0,) <U32,
+#         'visible': True,
+#         'size': 12,
+#         'color': <class 'numpy.ndarray'> (4,) float64,
+#         'blending': <Blending.TRANSLUCENT: 'translucent'>,
+#         'anchor': <Anchor.CENTER: 'center'>,
+#         'translation': 0, 'rotation': 0},
+#         'shape_type': ['line', 'line', 'line'],
+#         'z_index': [0, 0, 0],
+#         'edge_width': [1, 1, 1],
+#         'face_color': <class 'numpy.ndarray'> (3, 4) float64,
+#         'face_color_cycle': <class 'numpy.ndarray'> (2, 4) float32,
+#         'face_colormap': 'viridis', 'face_contrast_limits': None,
+#         'edge_color': <class 'numpy.ndarray'> (3, 4) float64,
+#         'edge_color_cycle': <class 'numpy.ndarray'> (2, 4) float32,
+#         'edge_colormap': 'viridis',
+#         'edge_contrast_limits': None,
+#         'features': Empty DataFrame
+#     Columns: []
+#     Index: [0, 1, 2]
+#     }
+# )
