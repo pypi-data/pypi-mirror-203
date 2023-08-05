d23dc8754b1865f5df194d1da76f3d38d1621077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:57 +0000

    Add package HACNet/1.3.3/HACNet-1.3.3.tar.gz

---
+++ packages/HACNet/HACNet-1.3.3.tar.gz/HACNet-1.3.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'HACNet',
+    packages = ['HACNet'],
+    version = '1.3.3',
+    description = 'Python package',
+    author = 'Gregory W. Kyro',
+    author_email = 'gregory.kyro@yale.edu',
+    url = 'https://github.com/gregory-kyro/HAC-Net/tree/main/HACNet',
+    download_url = 'https://github.com/gregory-kyro/HAC-Net/archive/refs/tags/v1.3.2.tar.gz',
+    keywords = [],
+    classifiers = []
+)
