1cee7e3dcab21b9d761790ef929dc624f18a8204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:00 +0000

    Add package ylin-distributions/0.1/ylin_distributions-0.1.tar.gz

---
+++ packages/ylin-distributions/ylin_distributions-0.1.tar.gz/ylin_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='ylin_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['ylin_distributions'],
+      author = 'Yu-Huai Lin',
+      author_email = 'yuhuailin0323@gmail.com',
+      zip_safe=False)
