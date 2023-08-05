654c68f5f4e08069f52755a555e9511b8e4ee8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:52 +0000

    Add package discord-advert-framework/2.5.5/discord_advert_framework-2.5.5-py3-none-any.whl

---
+++ packages/discord-advert-framework/discord_advert_framework-2.5.5-py3-none-any.whl/daf/__init__.py
@@ -0,0 +1,18 @@
+"""
+Discord Advertisement Framework
+"""
+import _discord as discord
+from .client import *
+from .core import *
+from .dtypes import *
+from .guild import *
+from .message import *
+from .logging import *
+from .web import *
+
+from .misc import DOCUMENTATION_MODE
+if DOCUMENTATION_MODE:
+    from .misc import *
+
+
+VERSION = "v2.5.5"
