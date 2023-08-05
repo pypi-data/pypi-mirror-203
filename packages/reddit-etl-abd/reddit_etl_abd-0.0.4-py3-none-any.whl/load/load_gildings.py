4a406d2c1d3952d2cdd0a9b898d5f41fea7377dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:12 +0000

    Add package reddit-etl-abd/0.0.4/reddit_etl_abd-0.0.4-py3-none-any.whl

---
+++ packages/reddit-etl-abd/reddit_etl_abd-0.0.4-py3-none-any.whl/load/load_gildings.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from includes.vars import *
+from datetime import date
+from includes.functions import loadConfigs
+
+def load_gildings():
+    spark = SparkSession.builder \
+        .config("spark.hadoop.fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
+        .config("spark.jars", "/jars/postgresql-42.2.5.jar") \
+        .getOrCreate()
+    loadConfigs(spark.sparkContext)
+
+    today = date.today().strftime('%Y%m%d')
+    today = 20230326
+
+    input_folder = "gildings"
+    input_file = "gildings"
+    table_name = "staging.gildings"
+    mode = "append"
+
+    df = spark.read.option("header", "true") \
+        .parquet(f"s3a://{minio_bucket}/processed/{input_folder}/{input_file}_{today}")
+
+    properties = {"user": postgres_user, "password": postgres_pass}
+    df.write.jdbc(url=postgres_url, table=table_name, mode=mode, properties=properties)
