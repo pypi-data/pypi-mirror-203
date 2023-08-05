059314fea36f3c7cfdcb72d7ad31bbbccd8d2737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:06 +0000

    Add package jkh011120-randmat/0.0.1/jkh011120_randmat-0.0.1-py3-none-any.whl

---
+++ packages/jkh011120-randmat/jkh011120_randmat-0.0.1-py3-none-any.whl/randmat/randmat.py
@@ -0,0 +1,13 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+def randhist(n_bins:int, around:int)->None:
+    x = np.random.randn(around)
+    plt.hist(x, n_bins, histtype='bar', color= "red");
+    plt.show()
+
+def randscatter(start_x:int,end_x:int,y:int)->None:
+    xData = np.arange(start_x, end_x) 
+    yData = xData + 2*np.random.randn(y)
+    plt.scatter(xData, yData) 
+    plt.show()
