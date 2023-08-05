fbbf008acb5adfef9b879577b1f0a08082c5a77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:07 +0000

    Add package netbox-kea-dhcp/0.0.1a4/netbox_kea_dhcp-0.0.1a4.tar.gz

---
+++ packages/netbox-kea-dhcp/netbox_kea_dhcp-0.0.1a4.tar.gz/netbox_kea_dhcp-0.0.1a4/netboxkea/entry_point.py
@@ -0,0 +1,39 @@
+import logging
+
+from .config import get_config
+from .connector import Connector
+from .kea.app import DHCP4App
+from .listener import WebhookListener
+from .logger import init_logger
+from .netbox import NetboxApp
+
+
+def run():
+    conf = get_config()
+    init_logger(conf.log_level, conf.ext_log_level, conf.syslog_level_prefix)
+
+    # Instanciate interfaces with source and sink
+    logging.info(f'netbox: {conf.netbox_url}, kea: {conf.kea_url}')
+    nb = NetboxApp(
+        conf.netbox_url, conf.netbox_token, prefix_filter=conf.prefix_filter,
+        iprange_filter=conf.iprange_filter,
+        ipaddress_filter=conf.ipaddress_filter)
+    kea = DHCP4App(conf.kea_url)
+    conn = Connector(
+        nb, kea, check=conf.check_only, prefix_dhcp_map=conf.prefix_dhcp_map)
+
+    if not conf.full_sync_at_startup and not conf.listen:
+        logging.warning('Neither full sync nor listen mode has been asked')
+
+    # Start a full synchronisation
+    if conf.full_sync_at_startup:
+        logging.info('Start full sync')
+        conn.sync_all()
+
+    # Start listening for events
+    if conf.listen:
+        logging.info(f'Listen for events on {conf.bind}:{conf.port}')
+        server = WebhookListener(
+            connector=conn, host=conf.bind, port=conf.port, secret=conf.secret,
+            secret_header=conf.secret_header)
+        server.run()
