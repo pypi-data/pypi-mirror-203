37df40c3d797c35b30be4f7b67df5c2c75cccc56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:38 +0000

    Add package streamlit-mermaid-component/0.0.5/streamlit-mermaid-component-0.0.5.tar.gz

---
+++ packages/streamlit-mermaid-component/streamlit-mermaid-component-0.0.5.tar.gz/streamlit-mermaid-component-0.0.5/mermaid_component/frontend/build/precache-manifest.c9b44d581d165a5cc353a57cfa2d3ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571ec1caf0487aa0a2b3117fa9968959",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e660c5069b45e79da6a8",
+    "url": "./static/js/2.0d0a8ff5.chunk.js"
+  },
+  {
+    "revision": "cea20375c1e694a681ac6662697447eb",
+    "url": "./static/js/2.0d0a8ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa6ca8cda0f898db730",
+    "url": "./static/js/main.bdfe0c11.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.bdfe0c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420a063b951fa0839922",
+    "url": "./static/js/runtime-main.71c5c8f7.js"
+  }
+]);
