b2207e6b4c4abb446f5f9b49ec9037754842d4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:21 +0000

    Add package pyxml3/0.0.1/pyxml3-0.0.1.tar.gz

---
+++ packages/pyxml3/pyxml3-0.0.1.tar.gz/pyxml3-0.0.1/pyxml/element.py
@@ -0,0 +1,156 @@
+"""
+XML Element/Node Definitions
+"""
+from typing import Dict, Optional, List, Iterator, Any
+from typing_extensions import Self
+
+#** Variables **#
+__all__ = [
+    'Element', 
+    'Comment', 
+    'Declaration',
+    'ProcessingInstruction',
+]
+
+#** Classes **#
+
+class Element:
+    """XML Element Object Definition"""
+
+    def __init__(self, tag, attrib=None, **extra):
+        self.tag = tag
+        self.attrib:   Dict[str, str]    = {**(attrib or {}), **extra}
+        self.parent:   Optional[Element] = None
+        self.children: List[Self]        = []
+        self.text:     Optional[str]     = None
+        self.tail:     Optional[str]     = None
+
+    def __repr__(self) -> str:
+        return 'Element(tag=%r, attrib=%r)' % (self.tag, self.attrib)
+    
+    def __iter__(self) -> Iterator[Self]:
+        return iter(self.children)
+
+    def __len__(self) -> int:
+        return len(self.children)
+
+    def __bool__(self):
+        raise NotImplementedError
+
+    def __getitem__(self, index: int) -> Self:
+        return self.children[index]
+
+    def __setitem__(self, index: int, element: Self):
+        self.children[index] = element
+
+    @classmethod
+    def makeelement(cls, tag, attrib):
+        return cls(tag, attrib)
+
+    def insert(self, index: int, element: Self):
+        self.children.insert(index, element)
+
+    def append(self, element: Self):
+        self.children.append(element)
+        element.parent = self
+
+    def extend(self, elements: Iterator[Self]):
+        self.children.extend(elements)
+        for elem in elements:
+            elem.parent = self
+
+    def remove(self, element: Self):
+        self.children.remove(element)
+        element.parent = None
+
+    def clear(self):
+        for elem in self.children:
+            elem.parent = None
+        self.children.clear()
+
+    def get(self, key: str, default: Any = None):
+        return self.attrib.get(key, default)
+
+    def set(self, key: str, value: str):
+        self.attrib[key] = value
+
+    def keys(self):
+        return self.attrib.keys()
+
+    def values(self):
+        return self.attrib.values()
+
+    def items(self):
+        return self.attrib.items()
+
+    def iter(self, tag: Optional[bytes] = None) -> Iterator[Self]:
+        """iterate all children recursively from parent"""
+        if tag is None or tag == self.tag:
+            yield self
+        for child in self.children:
+            yield from child.iter(tag)
+   
+    def itertext(self):
+        """iterate all elements with text in them and retreieve values"""
+        if self.text:
+            yield self.text
+        for child in self.children:
+            yield from child.itertext()
+
+    def find(self, path: str) -> Optional[Self]:
+        """retrieve single elmement matching xpath"""
+        return xpath.find(self, path)
+
+    def findall(self, path: str) -> List[Self]:
+        """retrieve all elements in list matching xpath"""
+        return xpath.findall(self, path)
+
+    def finditer(self, path: str) -> Iterator[Self]:
+        """iterate all elements matching xpath"""
+        return xpath.iterfind(self, path)
+
+    def findtext(self, path: str, default=None) -> Optional[str]:
+        """collect all text within elements matching xpath"""
+        return xpath.findtext(self, path, default)
+ 
+    def xpath(self, path: str) -> List[Self]:
+        """alias for findall for compatability with lxml"""
+        return self.findall(path)
+
+    def getparent(self) -> Optional[Self]:
+        """retrieve parent for compatability with lxml"""
+        return self.parent
+
+    def getchildren(self) -> List[Self]:
+        """retrieve children for compatability with lxml"""
+        return self.children
+
+class _Special(Element):
+    """Baseclass for special elements such as Comments and PI"""
+
+    def __init__(self, text: str):
+        super().__init__(self.__class__)
+        self.text   = text
+        self._cname = self.__class__.__name__
+    
+    def __repr__(self) -> str:
+        return f'{self._cname}(text={self.text})'
+
+    def itertext(self):
+        return
+
+class Comment(_Special):
+    pass
+
+class Declaration(_Special):
+    pass
+
+class ProcessingInstruction(_Special):
+    
+    def __init__(self, target: str, value: str):
+        super().__init__(f'{target} {value}')
+        self.target = target
+        self.value  = value
+
+#** Imports **#
+from . import xpath
---
+++ packages/pyxml3/pyxml3-0.0.1.tar.gz/pyxml3-0.0.1/pyxml/xpath/engine.py
@@ -0,0 +1,144 @@
+"""
+XPATH Processing Engine
+"""
+import re
+from typing import Iterator, List, Optional, Tuple
+
+from .lexer import XToken, XLexer, EToken, ELexer
+from .functions import *
+from ..element import Element
+from .._tokenize import Result
+
+#** Variables **#
+__all__ = ['iter_xpath', 'find_xpath', 'list_xpath']
+
+#: type hint for list of argument getters
+Args = List[ArgGetter]
+
+#: regex expression to match variable string
+re_var = re.compile(r'^@\w+$')
+
+#** Functions **#
+
+def filter_tag(elements: Iterator[Element], tag: str) -> Iterator[Element]:
+    """only return element if it matches the specified tag"""
+    for elem in elements:
+        if elem.tag == tag:
+            yield elem
+
+def get_parent(element: Element, parents: int) -> Optional[Element]:
+    """retrieve parent elements from orignal element"""
+    for _ in range(0, parents):
+        if element.parent is None:
+            return
+        element = element.parent
+    return element
+
+def compile_expr(expr: bytes) -> Tuple[Args, Optional[Result], EvalExpr]:
+    """compile a valid xpath filter expression"""
+    # generate context for compiling expression
+    lexer = ELexer(iter(expr))
+    args: Args = []
+    action: Optional[Result] = None
+    compiled: EvalExpr = lambda _: False
+    # modify action for special behaviors
+    if expr.isdigit():
+        action = Result(EToken.FUNCTION, b'index', 0, 0)
+    if re_var.match(expr.decode()):
+        action = Result(EToken.FUNCTION, b'isempty', 0, 0)
+    # parse expression according to lexer bytes
+    while True:
+        # retrieve next action in expression
+        result = lexer.next()
+        if result is None:
+            break
+        # handle according to token
+        token, value, _, _ = result
+        if token >= EToken.EQUALS:
+            action = result
+            continue
+        elif token <= EToken.VARIABLE:
+            arg = compile_argument(result)
+            args.append(arg)
+        elif token == EToken.EXPRESSION:
+            expr_args = compile_expr_args(value)
+            args.extend(expr_args)
+        elif token == EToken.COMMA:
+            pass
+        else:
+            raise ValueError('unsupported action?', result)
+        # process action when specified
+        if action:
+            # compile expression function and reset args/action state
+            compiled = compile_action(action, args)
+            args     = [wrap_expr(action, compiled)]
+            action   = None
+    # ensure no args are left hanging
+    return (args, action, compiled)
+
+def compile_expr_args(expr: bytes) -> Args:
+    """compile a partial filter expression only to collect arguments"""
+    args, action, _ = compile_expr(expr)
+    if action:
+        raise ValueError('invalid arguments', action, args)
+    return args 
+
+def compile_expr_func(expr: bytes) -> EvalExpr:
+    """compile a complete filter expression into a single function"""
+    args, action, compiled = compile_expr(expr)
+    if action and args:
+        raise ValueError('incomplete expression', action, args)
+    return compiled
+
+def iter_xpath(xpath: bytes, elements: Iterator[Element]) -> Iterator[Element]:
+    """
+    iterate parse and evaluate xpath to find and filter elements
+
+    :param xpath:    raw xpath expression
+    :param elements: elements to search for xpath components
+    :return:         iterator of elements matching xpath criteria
+    """
+    lexer = XLexer(iter(xpath))
+    for action in lexer.iter():
+        # process action according to token-type
+        token, value, _, _ = action
+        if token == XToken.CHILD:
+            elements = (c for e in elements for c in e)
+        elif token == XToken.DECENDANT:
+            elements = (c for e in elements for c in e.iter())
+        elif token == XToken.NODE:
+            elements = (e for e in filter_tag(elements, value.decode()))
+        elif token in (XToken.WILDCARD, XToken.SELF):
+            continue
+        elif token == XToken.PARENT:
+            parents  = (get_parent(e, len(value)) for e in elements)
+            elements = (p for p in parents if p is not None)  
+        elif token == XToken.FILTER:
+            expr     = compile_expr_func(value)
+            elements = (e for e in elements if expr(e))
+        else:
+            raise ValueError('unsupported token', action)
+    return elements
+
+def find_xpath(xpath: bytes, elements: Iterator[Element]) -> Optional[Element]:
+    """
+    find first matching element associated w/ xpath
+
+    :param xpath:    raw xpath expression
+    :param elements: elements to search for xpath components
+    :return:         first element found matching criteria
+    """
+    try:
+        return next(iter_xpath(xpath, elements))
+    except StopIteration:
+        return
+
+def list_xpath(xpath: bytes, elements: List[Element]) -> List[Element]:
+    """
+    list parse and evaluate xpath to find and filter elements
+
+    :param xpath:    raw xpath expression
+    :param elements: elements to search for xpath components
+    :return:         list of elements matching xpath criteria
+    """
+    return list(iter_xpath(xpath, (e for e in elements)))
