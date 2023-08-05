211277167391b5c465f388d7d8170e9632826c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:09 +0000

    Add package folio-migration-tools/1.7.10/folio_migration_tools-1.7.10-py3-none-any.whl

---
+++ packages/folio-migration-tools/folio_migration_tools-1.7.10-py3-none-any.whl/folio_migration_tools/library_configuration.py
@@ -0,0 +1,81 @@
+from enum import Enum
+from typing import Annotated
+from typing import Optional
+
+from pydantic import BaseModel
+from pydantic import Field
+from pydantic.types import DirectoryPath
+
+
+class HridHandling(str, Enum):
+    """Enum determining how the HRID generation should be handled.
+        - default: Enumerates the HRID, building on the current value in the HRID settings
+        - preserve001: Takes the 001 and uses this as the HRID.
+
+    Args:
+        str (_type_): _description_
+        Enum (_type_): _description_
+    """
+
+    default = "default"
+    preserve001 = "preserve001"
+
+
+class FileDefinition(BaseModel):
+    file_name: str
+    suppressed: Optional[bool] = False
+    staff_suppressed: Optional[bool] = False
+    service_point_id: Optional[str] = ""
+
+
+class IlsFlavour(str, Enum):
+    """ """
+
+    aleph = "aleph"
+    voyager = "voyager"
+    sierra = "sierra"
+    millennium = "millennium"
+    koha = "koha"
+    tag907y = "tag907y"
+    tag001 = "tag001"
+    tagf990a = "tagf990a"
+    custom = "custom"
+    none = "none"
+
+
+class FolioRelease(str, Enum):
+    lotus = "lotus"
+    morning_glory = "morning-glory"
+    nolana = "nolana"
+    orchid = "orchid"
+
+
+class LibraryConfiguration(BaseModel):
+    okapi_url: str
+    tenant_id: str
+    okapi_username: str
+    okapi_password: str
+    base_folder: DirectoryPath = Field(
+        description=(
+            "The base folder for migration. "
+            "Should ideally be a github clone of the migration_repo_template"
+        )
+    )
+    multi_field_delimiter: Optional[str] = "<delimiter>"
+    failed_records_threshold: Annotated[
+        int, Field(description=("Number of failed records until the process shuts down"))
+    ] = 5000
+    failed_percentage_threshold: Annotated[
+        int, Field(description=("Percentage of failed records until the process shuts down"))
+    ] = 20
+    library_name: str
+    log_level_debug: bool
+    folio_release: FolioRelease = Field(
+        description=(
+            "The Flavour of the ILS you are migrating from. This choice is "
+            "maninly tied to the handling of legacy identifiers and thereby the "
+            "deterministic UUIDs generated from them."
+        )
+    )
+    iteration_identifier: str
+    add_time_stamp_to_file_names: Optional[bool] = False
---
+++ packages/folio-migration-tools/folio_migration_tools-1.7.10-py3-none-any.whl/folio_migration_tools/migration_tasks/bibs_transformer.py
@@ -0,0 +1,118 @@
+import logging
+from typing import Annotated
+from typing import List
+from typing import Optional
+
+from folio_uuid.folio_namespaces import FOLIONamespaces
+from pydantic import Field
+
+from folio_migration_tools.helper import Helper
+from folio_migration_tools.library_configuration import FileDefinition
+from folio_migration_tools.library_configuration import HridHandling
+from folio_migration_tools.library_configuration import IlsFlavour
+from folio_migration_tools.library_configuration import LibraryConfiguration
+from folio_migration_tools.marc_rules_transformation.marc_file_processor import (
+    MarcFileProcessor,
+)
+from folio_migration_tools.marc_rules_transformation.rules_mapper_bibs import (
+    BibsRulesMapper,
+)
+from folio_migration_tools.migration_tasks.migration_task_base import MigrationTaskBase
+from folio_migration_tools.task_configuration import AbstractTaskConfiguration
+
+
+class BibsTransformer(MigrationTaskBase):
+    class TaskConfiguration(AbstractTaskConfiguration):
+        name: str
+        add_administrative_notes_with_legacy_ids: Annotated[
+            bool,
+            Field(
+                description=(
+                    "If set to true, an Administrative note will be added to the records "
+                    "containing the legacy ID. Use this in order to protect the values from "
+                    "getting overwritten by overlays,"
+                ),
+            ),
+        ] = True
+        migration_task_type: str
+        hrid_handling: Optional[HridHandling] = HridHandling.default
+        deactivate035_from001: Optional[bool] = False
+        files: List[FileDefinition]
+        ils_flavour: IlsFlavour
+        custom_bib_id_field: Annotated[
+            str,
+            Field(
+                description=(
+                    'A string representing a MARC field with optional subfield indicated by a "$" '
+                    '(eg. "991$a") from which to draw legacy Bib ID. Use this in combination '
+                    'with `ilsFlavour: "custom"`. Defaults to "001", and is ignored for all other '
+                    "ilsFlavours."
+                )
+            ),
+        ] = "001"
+        tags_to_delete: Optional[List[str]] = []
+        reset_hrid_settings: Optional[bool] = False
+        never_update_hrid_settings: Optional[bool] = False
+        create_source_records: Annotated[
+            bool, Field(description="Controls wheter or not to retain the MARC records in SRS.")
+        ] = True
+        parse_cataloged_date: Annotated[
+            bool,
+            Field(
+                title="Parse cataloged date",
+                description=(
+                    "Parse fields mapped to catalogedDate into a FOLIO accepted date string using "
+                    "dateutil.parser. Verify results carefully when using"
+                ),
+            ),
+        ] = False
+
+    @staticmethod
+    def get_object_type() -> FOLIONamespaces:
+        return FOLIONamespaces.instances
+
+    def __init__(
+        self,
+        task_config: TaskConfiguration,
+        library_config: LibraryConfiguration,
+        use_logging: bool = True,
+    ):
+        super().__init__(library_config, task_config, use_logging)
+        self.processor: MarcFileProcessor
+        self.check_source_files(
+            self.folder_structure.legacy_records_folder, self.task_configuration.files
+        )
+        self.mapper = BibsRulesMapper(self.folio_client, library_config, self.task_configuration)
+        self.bib_ids: set = set()
+        if (
+            self.task_configuration.reset_hrid_settings
+            and not self.task_configuration.never_update_hrid_settings
+        ):
+            self.mapper.hrid_handler.reset_instance_hrid_counter()
+        logging.info("Init done")
+
+    def do_work(self):
+        self.do_work_marc_transformer()
+
+    def wrap_up(self):
+        logging.info("Done. Transformer wrapping up...")
+        self.extradata_writer.flush()
+        self.processor.wrap_up()
+        with open(self.folder_structure.migration_reports_file, "w+") as report_file:
+            self.mapper.migration_report.write_migration_report(
+                "Bibliographic records transformation report",
+                report_file,
+                self.start_datetime,
+            )
+            Helper.print_mapping_report(
+                report_file,
+                self.mapper.parsed_records,
+                self.mapper.mapped_folio_fields,
+                self.mapper.mapped_legacy_fields,
+            )
+
+        logging.info(
+            "Done. Transformation report written to %s",
+            self.folder_structure.migration_reports_file.name,
+        )
+        self.clean_out_empty_logs()
