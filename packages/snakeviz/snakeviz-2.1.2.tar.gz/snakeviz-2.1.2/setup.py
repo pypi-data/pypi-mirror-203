46ca14310c969fd042a8731248a7cea8f95d0ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:15 +0000

    Add package snakeviz/2.1.2/snakeviz-2.1.2.tar.gz

---
+++ packages/snakeviz/snakeviz-2.1.2.tar.gz/snakeviz-2.1.2/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+import os
+
+
+NAME = 'snakeviz'
+VERSION = '2.1.2'
+
+
+# Create a simple version.py module; less trouble than hard-coding the version
+with open(os.path.join('snakeviz', 'version.py'), 'w') as f:
+    f.write('__version__ = version = %r' % VERSION)
+
+# Load up the description from README.rst
+with open('README.rst') as f:
+    DESCRIPTION = f.read()
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    author='Matt Davis',
+    author_email='jiffyclub@gmail.com',
+    url='https://github.com/jiffyclub/snakeviz',
+    description='A web-based viewer for Python profiler output',
+    long_description=DESCRIPTION,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: JavaScript',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Software Development'
+    ],
+    packages=['snakeviz'],
+    package_data={
+        'snakeviz': ['static/*.ico',
+                     'static/*.js',
+                     'static/*.css',
+                     'static/vendor/*.js',
+                     'static/vendor/*.css',
+                     'static/images/*.png',
+                     'templates/*.html']
+    },
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
+    install_requires=['tornado>=2.0'],
+    entry_points={
+        'console_scripts': ['snakeviz = snakeviz.cli:main']
+    }
+)
