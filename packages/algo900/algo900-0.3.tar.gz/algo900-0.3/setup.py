ad25aee6371e299885ba28058d75006d954f96a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:34 +0000

    Add package algo900/0.3/algo900-0.3.tar.gz

---
+++ packages/algo900/algo900-0.3.tar.gz/algo900-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="algo900",
+	version="0.3",
+	description="algo",
+	author="algo",
+	license='MIT',
+	keywords="algo",
+	packages=find_packages()
+)
