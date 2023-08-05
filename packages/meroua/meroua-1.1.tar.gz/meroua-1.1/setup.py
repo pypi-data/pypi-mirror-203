541add81d86e161600431da528a2e23271af4990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:29 +0000

    Add package meroua/1.1/meroua-1.1.tar.gz

---
+++ packages/meroua/meroua-1.1.tar.gz/meroua-1.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='meroua',
+    version='1.1',
+    description='A module that defines a constant type in Python using Jython',
+    author='Sahraoui Tarek Ziad',
+    author_email='tziad2027@gmail.com',
+    packages=setuptools.find_packages(),
+    py_modules=['mymodule'],
+
+)
