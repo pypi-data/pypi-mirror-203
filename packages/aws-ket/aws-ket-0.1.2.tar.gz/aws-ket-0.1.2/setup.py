44aa7ac71e80500b8c5b13b5646e9cec7b67a4db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:22 +0000

    Add package aws-ket/0.1.2/aws-ket-0.1.2.tar.gz

---
+++ packages/aws-ket/aws-ket-0.1.2.tar.gz/aws-ket-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+VERSION = "0.1.2"
+setup(
+    name             = 'aws-ket',
+    author           = 'Sayef Iqbal',
+    author_email     = 's2400@columbia.edu',
+    maintainer       = 'Sayef Iqbal',
+    url              = 'https://github.com/sayefiqb/aws-ket',
+    description      = 'Utility tool to encrypt data using AWS KMS and store it in preferred backend.',
+    version          = VERSION,
+    include_package_data= True,
+    # entry_points = {
+    #     'console_scripts': ['awsket=awsket.ket']
+    # }
+)
