f21486f0a959686542f62c31a9b09f556fed91d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:35 +0000

    Add package Geode-Implicit/2.0.0/Geode_Implicit-2.0.0-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/Geode-Implicit/Geode_Implicit-2.0.0-cp37-cp37m-manylinux2014_x86_64.whl/geode_implicit/explicitation.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode_geosciences
+from geode_conversion import model
+from geode_background import bmesh_2d
+
+from .lib64.geode_implicit_py_explicitation import *
+ImplicitExplicitationLibrary.initialize()
