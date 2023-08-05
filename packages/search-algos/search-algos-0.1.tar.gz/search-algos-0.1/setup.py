0beb0859af045ff4f81ce4b741c3adf028a3f32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:53 +0000

    Add package search-algos/0.1/search-algos-0.1.tar.gz

---
+++ packages/search-algos/search-algos-0.1.tar.gz/search-algos-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='search-algos',
+    version='0.1',
+    description='A collection of searching algorithms for lists and dictionaries',
+    author='Arpan Adhikari',
+    author_email='codewitharpan@gmail.com',
+    packages=find_packages()
+)
