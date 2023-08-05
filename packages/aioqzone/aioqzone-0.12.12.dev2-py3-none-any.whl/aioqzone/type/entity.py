ef0dcffb403299e45c181f670f22904688de1ba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:25 +0000

    Add package aioqzone/0.12.12.dev2/aioqzone-0.12.12.dev2-py3-none-any.whl

---
+++ packages/aioqzone/aioqzone-0.12.12.dev2-py3-none-any.whl/aioqzone/type/entity.py
@@ -0,0 +1,60 @@
+"""This module defines feed text entities. Entities take place of "contents" since this simplifies
+and details the works of rich-text stringify.
+
+.. versionadded:: 0.5.0.dev0
+"""
+
+from typing import List, Optional
+
+from pydantic import BaseModel, Field
+
+
+class HasContent(BaseModel):
+    content: str = ""
+
+
+class ConEntity(BaseModel):
+    """Base class for all content entities. All entities has a numeric :obj:`type`."""
+
+    pass
+
+
+class TextEntity(ConEntity):
+    """Text.
+
+    Two objects of this class are considered equal, if their :obj:`con` is equal.
+    """
+
+    con: str = ""
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, TextEntity) and self.con == other.con
+
+
+class AtEntity(ConEntity):
+    """Mention. like: ``@{uin:123456,nick:foobar}``, ``@foobar``
+
+    Two objects of this class are considered equal, if their :obj:`uin` is equal.
+    """
+
+    uin: int
+    nick: str = ""
+
+    def __eq__(self, other) -> bool:
+        return isinstance(other, AtEntity) and self.uin == other.uin
+
+
+class EmEntity(ConEntity):
+    """Emoji. like: ``[em]e100[/em]``
+
+    Two objects of this class are considered equal, if their :obj:`eid` is equal.
+    """
+
+    eid: int
+
+    def __eq__(self, other):
+        return isinstance(other, EmEntity) and self.eid == other.eid
+
+
+class HasConEntity(HasContent):
+    entities: Optional[List[ConEntity]] = Field(default=None, alias="conlist")
