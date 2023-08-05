13cbb0342cd59c82e07d4f65ba8191f2edf45291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:07 +0000

    Add package pagla-chacha/0.2/pagla-chacha-0.2.tar.gz

---
+++ packages/pagla-chacha/pagla-chacha-0.2.tar.gz/pagla-chacha-0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pagla-chacha',
+    version='0.2',
+    packages=find_packages(),
+    description='A package that contains multiple data structures',
+    author='Touhidul-Afif',
+    author_email='your.email@example.com',
+    url='https://github.com/Afif2K/338-project.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
