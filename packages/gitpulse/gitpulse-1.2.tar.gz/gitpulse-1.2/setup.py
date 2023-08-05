2facba6161677e61238603a8233eb4d523ca4ccc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:05 +0000

    Add package gitpulse/1.2/gitpulse-1.2.tar.gz

---
+++ packages/gitpulse/gitpulse-1.2.tar.gz/gitpulse-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+# Read the content of the README.md file
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='gitpulse',
+    version='1.2',
+    packages=find_packages(),
+    install_requires=[
+        # Add your package dependencies here:
+        'gitpython',
+        'jinja2',
+        'python-dateutil',
+    ],
+    entry_points={
+        'console_scripts': [
+            'gitpulse = gitpulse.gitpulse_cli:main',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
