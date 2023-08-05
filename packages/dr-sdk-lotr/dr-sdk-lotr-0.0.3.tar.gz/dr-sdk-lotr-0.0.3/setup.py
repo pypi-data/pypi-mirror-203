dee0a5de1e0b02a4f880fa58a3c2b6d1d13a86cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:59 +0000

    Add package dr-sdk-lotr/0.0.3/dr-sdk-lotr-0.0.3.tar.gz

---
+++ packages/dr-sdk-lotr/dr-sdk-lotr-0.0.3.tar.gz/dr-sdk-lotr-0.0.3/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='dr-sdk-lotr',
+    version='0.0.3',
+    description='SDK for interacting with the Lord of the Rings API',
+    author='Daniel Reliford',
+    author_email='dreliford@gmail.com',
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
---
+++ packages/dr-sdk-lotr/dr-sdk-lotr-0.0.3.tar.gz/dr-sdk-lotr-0.0.3/tests/test_characters.unittest.py
@@ -0,0 +1,38 @@
+import unittest
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from dr_sdk_lotr.characters import Characters
+from config import access_token
+
+
+class TestCharacters(unittest.TestCase):
+
+    def test_get_characters(self):
+        characters = Characters(self.api_key)
+        test_get_characters = characters.get_all_characters()
+        self.assertEqual(len(test_get_characters), 933)
+
+    def test_get_character_by_id(self):
+        characters = Characters(self.api_key)
+        test_get_character_by_id = characters.get_character_by_id('5cd99d4bde30eff6ebccfbed')
+        self.assertEqual(test_get_character_by_id['name'], "Aranuir")
+        self.assertEqual(test_get_character_by_id['race'], "Human")
+        self.assertEqual(test_get_character_by_id['_id'], "5cd99d4bde30eff6ebccfbed")
+
+    def test_get_quotes_by_character_id(self):
+        characters = Characters(self.api_key)
+        test_get_quotes_by_character_id = characters.get_quotes_by_character_id('5cdbdf477ed9587226e7949b')
+        self.assertEqual(test_get_quotes_by_character_id[1]['dialog'], "Faramir! Orcs have taken the eastern shore. Their numbers are too great. By nightfall we will be overrun.")
+        self.assertEqual(len(test_get_quotes_by_character_id), 7)
+
+    def test_get_character_by_name(self):
+        characters = Characters(self.api_key)
+        test_get_character_by_name = characters.get_character_by_name('Gandalf')
+        self.assertEqual(test_get_character_by_name['name'], 'Gandalf')
+        
+
+if __name__ == '__main__':
+    TestCharacters.api_key = access_token
+    unittest.main()
