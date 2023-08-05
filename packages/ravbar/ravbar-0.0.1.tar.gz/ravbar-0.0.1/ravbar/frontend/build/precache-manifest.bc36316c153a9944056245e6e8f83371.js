5b84f4bdf0880aec6ff3a4ede1e727d2c749c6b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:31 +0000

    Add package ravbar/0.0.1/ravbar-0.0.1.tar.gz

---
+++ packages/ravbar/ravbar-0.0.1.tar.gz/ravbar-0.0.1/ravbar/frontend/build/precache-manifest.bc36316c153a9944056245e6e8f83371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f8e25185afc24555eb8f129f5038de",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a99490e2c5b3900f12af",
+    "url": "./static/css/main.d9e4b44e.chunk.css"
+  },
+  {
+    "revision": "21f7a5692533b08dd7b8",
+    "url": "./static/js/2.5c88d1c5.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.5c88d1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99490e2c5b3900f12af",
+    "url": "./static/js/main.46c6351c.chunk.js"
+  },
+  {
+    "revision": "03ccc849c42a2bb4f5bf",
+    "url": "./static/js/runtime-main.4bd7b106.js"
+  }
+]);
