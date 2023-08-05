3681460fe8e3ae6f302957f1020de37397c0957e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:57 +0000

    Add package osintbuddy/0.0.2rc15.post1/osintbuddy-0.0.2rc15.post1.tar.gz

---
+++ packages/osintbuddy/osintbuddy-0.0.2rc15.post1.tar.gz/osintbuddy-0.0.2rc15.post1/src/osintbuddy/plugins.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+import hashlib
+from typing import List
+from osintbuddy.utils import slugify
+from fastapi import APIRouter
+
+# @todo
+
+
+class BasePlugin(object):
+    def __init__(self) -> None:
+        self.transforms: dict = None
+        self.node: dict = None
+        self.node_name: str = None
+        self.settings: dict = None
+        self.repository: str = None
+        self.authors = List[str]
+        self._base_plugin_endpoint = slugify(self.node_name)
+        self._endpoints = self.generate_endpoints()
+        self._router: APIRouter = APIRouter(prefix=self._base_plugin_endpoint)
+
+    def request(self, transform: str, **kwargs):
+        return self.transforms[transform](**kwargs)
+
+    def _generate_endpoints(self):
+        if self.transforms is None:
+            return
+        endpoints = []
+        for transform in self.transforms.keys():
+            route = str.encode(
+                f"{self._base_plugin_endpoint}{transform}{self.repository}"
+            )
+            endpoint_route = hashlib.md5(route).hexdigest()
+            self._router.add_api_route(
+                endpoint_route,
+                endpoint=self.transforms[transform]
+            )
+        return endpoints
+
+
+class Plugin(BasePlugin):
+    def __init__(self):
+        pass
