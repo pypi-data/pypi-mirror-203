87cc61d370aa2c46cf17926d41071ec9a3fc4fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:44 +0000

    Add package drakaina/0.6.16/drakaina-0.6.16.tar.gz

---
+++ packages/drakaina/drakaina-0.6.16.tar.gz/drakaina-0.6.16/drakaina/utils.py
@@ -0,0 +1,33 @@
+""" Utility functions for package."""
+from __future__ import annotations
+
+from functools import partial
+from typing import Callable
+from typing import Iterable
+
+
+def iterable_str_arg(s: str | Iterable[str]) -> list[str]:
+    if isinstance(s, str):
+        s = s.replace(",", " ").strip().replace("  ", " ").split()
+    return list(map(str, s))
+
+
+def unwrap_func(func: Callable) -> Callable:
+    _func = func
+    while hasattr(_func, "__wrapped__") or isinstance(_func, partial):
+        _func = getattr(_func, "__wrapped__", None) or _func.func
+    return _func
+
+
+def get_cookies(cookie_str: str) -> dict[str, str]:
+    if cookie_str in None:
+        return {}
+
+    _cookies = {}
+    for cookie_item in cookie_str.split(";"):
+        if "=" not in cookie_item or "DELETED" in cookie_item:
+            continue
+        name, value = cookie_item.strip().split("=", 1)
+        _cookies[name] = value
+
+    return _cookies
