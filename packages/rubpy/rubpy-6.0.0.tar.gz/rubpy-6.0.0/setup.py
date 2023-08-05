b69e11b21c869269e264c2a2394cedd4885e39e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:45 +0000

    Add package rubpy/6.0.0/rubpy-6.0.0.tar.gz

---
+++ packages/rubpy/rubpy-6.0.0.tar.gz/rubpy-6.0.0/setup.py
@@ -0,0 +1,40 @@
+import re
+from setuptools import find_packages, setup
+
+with open('rubpy/__init__.py', 'r') as f:
+    version = re.search(r'__version__\s*=\s*\'(\S+)\'', f.read()).group(1)
+
+with open("README.md", encoding="UTF-8") as f:
+    readme = f.read()
+
+setup(
+    name='rubpy',
+    version=version,
+    author='Shayan Heidari',
+    url='https://github.com/shayanheidari01/rubika',
+    author_email = 'contact@shayanheidari.info',
+    description='This is an unofficial library and fastest library for deploying robots on Rubika accounts.',
+    python_requires='>=3.7',
+    long_description=readme,
+    long_description_content_type = 'text/markdown',
+    packages=find_packages(exclude=['rubpy*']),
+    install_requires=['aiohttp', 'pycryptodome'],
+    extras_require={
+        'opencv-python': ['opencv-python']
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Topic :: Internet',
+        'Topic :: Communications',
+        'Topic :: Communications :: Chat',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Application Frameworks'
+    ],
+)
