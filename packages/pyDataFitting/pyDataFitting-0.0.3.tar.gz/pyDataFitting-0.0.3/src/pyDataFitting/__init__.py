9e4ad8ba7aa128471185ef3e0e79ce1c0008c080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:26 +0000

    Add package pyDataFitting/0.0.3/pyDataFitting-0.0.3.tar.gz

---
+++ packages/pyDataFitting/pyDataFitting-0.0.3.tar.gz/pyDataFitting-0.0.3/src/pyDataFitting/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from . import linear_regression
+from . import multivariate_regression
+from . import nonlinear_regression
+from . import polynomial_regression
+from .model_tools import model_tools
