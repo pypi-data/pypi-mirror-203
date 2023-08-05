006f0fedd6d4ceb7d9f36ae04a1dcfe3c4da9500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:36 +0000

    Add package algoxyz/0.31/algoxyz-0.31.tar.gz

---
+++ packages/algoxyz/algoxyz-0.31.tar.gz/algoxyz-0.31/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="algoxyz",
+	version="0.31",
+	description="algo",
+	author="algo",
+	license='MIT',
+	keywords="algo",
+	packages=find_packages()
+)
