5969cee79ee5d568220de8151d342e7139e141fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:47 +0000

    Add package dataroid/0.0.3/dataroid-0.0.3.tar.gz

---
+++ packages/dataroid/dataroid-0.0.3.tar.gz/dataroid-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A Simple Wrapper For Synthetic Data Generation'
+LONG_DESCRIPTION = (this_directory / "dataroid/README.md").read_text()
+
+
+# Setting up
+setup(
+    name="dataroid",
+    version=VERSION,
+    author="torchd3v (Burak Egeli)",
+    author_email="<burak96egeli@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=['pandas==1.5.3', 'ctgan==0.7.1'],
+    keywords=['python', 'data', 'generate', 'synthetic', 'deep learning', 'model'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
