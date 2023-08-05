491baf3734d2b57273e1af8151dcb64c3f10818f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:32 +0000

    Add package hagis/0.2.2/hagis-0.2.2.tar.gz

---
+++ packages/hagis/hagis-0.2.2.tar.gz/hagis-0.2.2/hagis/hagis.py
@@ -0,0 +1,194 @@
+from datetime import datetime
+from json import dumps, loads
+from requests import post
+from types import SimpleNamespace
+from typing import Any, Dict, Generic, Iterable, List, Optional, Tuple, Type, TypeVar, Union
+
+T = TypeVar("T")
+
+class Layer(Generic[T]):
+    def __init__(self, layer_url: str, model: Type[T] = SimpleNamespace, oid_field: str = "objectid", shape_property_name: str = "shape", **mapping: str) -> None:
+        """ Creates a new instance of the Layer class.
+
+        Args:
+            layer_url (str): Layer url (e.g. .../FeatureServer/0).
+            model (Type[T], optional): Model to map to.  Defaults to SimpleNamespace.
+            oid_field (str, optional): Name of the Object ID field.  Defaults to "objectid".
+            shape_property_name (str, optional): Name of the shape property.  Defaults to "shape".
+        """
+        self._layer_url = layer_url
+        self._model = model
+        self._oid_field = oid_field
+        self._shape_property_name = shape_property_name
+        self._shape_property_type = None
+        self._unknown_shape_types = [Any, object, SimpleNamespace]
+        self._fields: Dict[str, str] = {}
+        self._is_dynamic = model == SimpleNamespace
+
+        if self._is_dynamic:
+            return
+        
+        # List of custom mapping properties that have been handled.
+        mapped: List[str] = []
+
+        for type in reversed(model.__mro__):
+            if hasattr(type, "__annotations__"):
+                for property_name, property_type in type.__annotations__.items():
+                    key = property_name.lower()
+
+                    if property_name in mapping:
+                        self._fields[key] = mapping[property_name]
+                        mapped.append(property_name)
+                    else:
+                        self._fields[key] = property_name
+
+                    if key == shape_property_name.lower():
+                        self._shape_property_name = property_name
+                        self._shape_property_type = property_type
+
+        # Add custom properties that have not been handled as dynamically handled propeties.
+        for property, field in mapping.items():
+            if property not in mapped:
+                self._fields[property.lower()] = field
+
+    def query(self, where_clause: str = "1=1", **kwargs: Any) -> Iterable[T]:
+        if self._is_dynamic:
+            # If dynamic, request all fields.
+            fields = "*"
+        else:
+            # Otherwise, request only what is used by the model.
+            fields = ",".join([f for (_, f) in self._fields.items() if f != self._shape_property_name.lower()])
+
+            if not self._shape_property_name:
+                kwargs["returnGeometry"] = False
+
+        for row in self._query(where_clause, fields, **kwargs):
+            if self._is_dynamic:
+                yield row  # type: ignore
+            else:                
+                dictionary = {property: getattr(row, field) for (property, field) in self._fields.items()}
+                if hasattr(self._model, "__dataclass_fields__"):
+                    # Support for dataclasses.
+                    item = self._model(*dictionary.values())
+                else:
+                    # Normal classes require the parameterless constructor.
+                    item = self._model()
+                    for property_name, property_value in dictionary.items():
+                        setattr(item, property_name, property_value)
+                yield item
+
+    def count(self, where_clause: str = "1=1") -> int:
+        obj = self._post("query", where=where_clause, returnCountOnly=True, f="json")
+        return obj.count
+
+    def find(self, oid: int, **kwargs: Any) -> Optional[T]:
+        items = [item for item in self.query(f"{self._oid_field}={oid}", **kwargs)]
+        if items:
+            return items[0]
+
+    def apply_edits(self, adds: Optional[List[T]] = None, updates: Optional[List[T]] = None, deletes: Union[List[int], List[str], None] = None, **kwargs: Any) -> SimpleNamespace:
+        adds_json = "" if adds is None else dumps([self._to_dict(x) for x in adds])
+        updates_json = "" if updates is None else dumps([self._to_dict(x) for x in updates])
+        deletes_json = "" if deletes is None else dumps([x for x in deletes])
+
+        result = self._post("applyEdits", adds=adds_json, updates=updates_json, deletes=deletes_json, f="json", **kwargs)
+        return result
+
+    def insert(self, items: List[T], **kwargs: Any) -> List[int]:
+        result = self.apply_edits(adds=items, **kwargs)
+        return [x.objectId for x in result.addResults]
+
+    def update(self, items: List[T], **kwargs: Any) -> None:
+        self.apply_edits(updates=items, **kwargs)
+
+    def delete(self, where_clause: str, **kwargs: Any) -> None:
+        self._post("deleteFeatures", where=where_clause, f="json", **kwargs)
+    
+    def _to_dict(self, item: T) -> Dict[str, Any]:
+        dictionary: Dict[str, Any] = {}
+        attributes: Dict[str, Any] = {}
+        dictionary["attributes"] = attributes
+
+        for key, value in item.__dict__.items():
+            property = key.lower()
+            field = self._fields[property]
+            if property == self._shape_property_name:
+                dictionary["geometry"] = value.__dict__
+            elif isinstance(value, datetime):
+                attributes[field] = int((value - datetime.utcfromtimestamp(0)).total_seconds() * 1000)
+            else:
+                attributes[field] = value
+
+        return dictionary
+
+    def _post(self, method: str, **kwargs: Any) -> SimpleNamespace:
+        response = post(f"{self._layer_url}/{method}", kwargs)
+
+        # Map it to a dynamic object.
+        obj = loads(response.text, object_hook=lambda x: SimpleNamespace(**x))
+
+        # If this is an error response, throw an exception.
+        if hasattr(obj, "error"):
+            raise Exception(obj.error.message)
+
+        return obj
+
+    def _get_rows(self, where_clause: str, fields: str, **kwargs: Any) -> Tuple[List[SimpleNamespace], bool]:
+        obj = self._post("query", where=where_clause, outFields=fields, f="json", **kwargs)
+
+        date_fields = [f.name for f in obj.fields if f.type == "esriFieldTypeDate"] if hasattr(obj, "fields") else []
+
+        if date_fields:
+            for f in obj.features:
+                for key, value in f.attributes.__dict__.items():
+                    if key in date_fields and value:
+                        f.attributes.__dict__[key] = datetime.fromtimestamp(value / 1000)
+
+        return (obj.features, obj.exceededTransferLimit if hasattr(obj, "exceededTransferLimit") else False)
+
+    def _get_oids(self, where_clause: str) -> List[int]:
+        obj = self._post("query", where=where_clause, returnIdsOnly="true", f="json")
+        return obj.objectIds
+
+    def _map(self, row: SimpleNamespace) -> SimpleNamespace:
+        if not hasattr(row, "geometry"):
+            return row.attributes
+
+        if self._shape_property_type is None or self._shape_property_type in self._unknown_shape_types:
+            shape = row.geometry
+        else:
+            shape = self._shape_property_type()
+            shape.__dict__ = row.geometry.__dict__
+        return SimpleNamespace(**row.attributes.__dict__, **{self._shape_property_name: shape})
+
+    def _query(self, where_clause: str, fields: str, **kwargs: Any) -> Iterable[SimpleNamespace]:
+        def get_rows(where_clause: str):
+            return self._get_rows(where_clause, fields, **kwargs)
+
+        rows, exceededTransferLimit = get_rows(where_clause)
+
+        for row in rows:
+            yield self._map(row)
+
+        if exceededTransferLimit:
+            size = len(rows)
+            oids = self._get_oids(where_clause)
+            for n in range(size, len(oids), size):
+                more_where_clause = f"{self._oid_field} IN ({','.join(map(str, oids[n:n+size]))})"
+                more_rows, _ = get_rows(more_where_clause)
+                for row in more_rows:
+                    yield self._map(row)
+
+
+class Point:
+    x: float
+    y: float
+
+class Multipoint:
+    points: List[List[float]]
+
+class Polyline:
+    paths: List[List[List[float]]]
+
+class Polygon:
+    rings: List[List[List[float]]]
