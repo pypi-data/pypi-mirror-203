84d9daddb225fcb54060715072d6eb2b0527cf86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:21 +0000

    Add package chat-spiral/0.0.4/chat-spiral-0.0.4.tar.gz

---
+++ packages/chat-spiral/chat-spiral-0.0.4.tar.gz/chat-spiral-0.0.4/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="chat-spiral",
+    version="0.0.4",
+    author="Travis Hammond",
+    description="A framework for creating chat spirals for Large Language Models finetuned for conversations. Currently work-in-progress.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Tiger767/spiral",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "regex",
+        "openai",
+        "pandas",
+        "tiktoken",
+        "faiss"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    license="MIT License",
+)
