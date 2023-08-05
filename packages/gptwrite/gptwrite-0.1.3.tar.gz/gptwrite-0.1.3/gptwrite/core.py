5ca6f6af2c2a08a8acd5a43a2e11d95b7fb78e80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:25 +0000

    Add package gptwrite/0.1.3/gptwrite-0.1.3.tar.gz

---
+++ packages/gptwrite/gptwrite-0.1.3.tar.gz/gptwrite-0.1.3/gptwrite/core.py
@@ -0,0 +1,141 @@
+import re
+import logging
+from typing import List, Callable, Optional, Dict
+import i18n
+import openai
+import questionary
+from gptwrite.prompts import build_prompt_generate_topics, build_prompt_generate_texts, build_prompt_rewrite_texts
+
+
+OpenAIMessages = List[Dict[str, str]]
+GenerateFunc = Callable[[OpenAIMessages, str, Optional[str]], str]
+
+ACTIONS = ["Thought", "Consult", "Write"]
+
+logger = logging.getLogger(__name__)
+
+
+def generate_text_by_llm(
+        messages: OpenAIMessages,
+        model_name: str = "gpt-3.5-turbo",
+        stop: Optional[str] = None
+    ) -> str:
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=messages,
+        stop=stop)
+    content = response.choices[0].message.content
+    return content
+
+def select_topics(
+        topics: List[str],
+        lang_for_description: str,
+    ) -> List[str]:
+    selected_topics = questionary.checkbox(
+        i18n.t("messages.want_topic", locale=lang_for_description),
+        choices=topics).unsafe_ask()
+    return selected_topics
+
+def generate_topics(
+        theme: str,
+        lang_for_generating: str,
+        nuance: Optional[str] = None,
+        generate_text_func: GenerateFunc = generate_text_by_llm,
+    ) -> List[str]:
+    prompt = build_prompt_generate_topics(theme=theme, language=lang_for_generating, nuance=nuance)
+    messages = [{
+        "role": "user",
+        "content": prompt,
+    }]
+    content = generate_text_func(messages=messages)
+    return put_generated_text_into_list(content)
+
+def put_generated_text_into_list(text: str) -> List[str]:
+    topics = []
+    for line in text.split("\n"):
+        if line.startswith("- "):
+            topics.append(line[2:].strip())
+            continue
+        if line.startswith("・ "):
+            topics.append(line[2:].strip())
+            continue
+        if line.startswith("* "):
+            topics.append(line[2:].strip)
+            continue
+        num_list_match = re.search(r"^(\d+)\. ", line)
+        if num_list_match:
+            topics.append(line[len(num_list_match.groups()[0]):].strip())
+            continue
+    return topics
+
+def generate_texts(
+        theme: str,
+        topics: List[str],
+        nuance: str,
+        lang_for_generating: str,
+        lang_for_description: str,
+        generate_text_func: GenerateFunc  = generate_text_by_llm,
+    ) -> str:
+    prompt = build_prompt_generate_texts(theme=theme, topics=topics, nuance=nuance, language=lang_for_generating)
+    messages = [{
+        "role": "user",
+        "content": prompt,
+    }]
+
+    writes = []
+
+    while True:
+        content = generate_text_func(messages=messages, stop="\n\n")
+        print(content)
+
+        if content.startswith("[Consult]"):
+            comprementary = questionary.text(i18n.t("messages.want_complementary", locale=lang_for_description)).unsafe_ask()
+            messages.append({
+                "role": "user",
+                "content": f"[Complement]{comprementary}",
+            })
+            continue
+
+        if content.startswith("[Thought]"):
+            messages.append({
+                "role": "assistant",
+                "content": content,
+            })
+            continue
+        
+        if content.startswith("[Write]"):
+            writes.append(content[7:].strip())
+            if not questionary.confirm(i18n.t("messages.confirm_want_more", locale=lang_for_description)).unsafe_ask():
+                break
+            messages.append({
+                "role": "assistant",
+                "content": f"[Request]{i18n.t('messages.want_more', locale=lang_for_description)}",
+            }) 
+            continue
+
+        messages.append({"role": "system", "content": i18n.t("messages.", locale=lang_for_description)})
+
+    return rewrite_texts(writes, lang_for_generating, lang_for_description, generate_text_func)
+
+def rewrite_texts(
+        sentences: List[str],
+        lang_for_generating: str,
+        lang_for_description: str,
+        generate_text_func: GenerateFunc = generate_text_by_llm,
+    ) -> str:
+    print(i18n.t("messages.rewrite", locale=lang_for_description))
+    prompt = build_prompt_rewrite_texts(sentences=sentences, language=lang_for_generating)
+    messages = [{
+        "role": "user",
+        "content": prompt,
+    }]
+
+    result_texts = ""
+    while True:
+        content = generate_text_func(messages)
+        print(content)
+        result_texts += content
+        if not questionary.confirm(i18n.t("messages.want_more", locale=lang_for_description)).unsafe_ask():
+            break
+        messages.append({"role": "user", "content": f"[Request]{i18n.t('messages.want_more', locale=lang_for_description)}"})
+    return result_texts
