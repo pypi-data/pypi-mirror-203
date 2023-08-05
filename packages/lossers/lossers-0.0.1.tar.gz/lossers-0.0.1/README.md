75c26b996d26331653c2fc78da39a294553fd361
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package lossers/0.0.1/lossers-0.0.1.tar.gz

---
+++ packages/lossers/lossers-0.0.1.tar.gz/lossers-0.0.1/README.md
@@ -0,0 +1,4 @@
+
+```shell
+pip install git+https://github.com/JiauZhang/lossers.git
+```
---
+++ packages/lossers/lossers-0.0.1.tar.gz/lossers-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'lossers',
+  packages = find_packages(),
+  version = '0.0.1',
+  license='MIT',
+  description = 'ML Loss Function',
+  author = 'JiauZhang',
+  author_email = 'jiauzhang@163.com',
+  url = 'https://github.com/JiauZhang/lossers',
+  long_description_content_type = 'text/markdown',
+  keywords = [
+    'artificial intelligence',
+    'loss function',
+    'deep learning',
+  ],
+  install_requires=[
+  ],
+  classifiers=[
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
