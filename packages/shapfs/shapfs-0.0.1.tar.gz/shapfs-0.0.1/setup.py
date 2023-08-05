16dcb6ddad36f8d8da43a407bc88ed60fe3fc582
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:02 +0000

    Add package shapfs/0.0.1/shapfs-0.0.1.tar.gz

---
+++ packages/shapfs/shapfs-0.0.1.tar.gz/shapfs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shapfs',
+    version='0.0.1',
+    author='Jianyang Hu',
+    author_email='meegohjy@gmail.com',
+    description='Shapley value is used for feature selection',
+    packages=find_packages(),
+
+)
