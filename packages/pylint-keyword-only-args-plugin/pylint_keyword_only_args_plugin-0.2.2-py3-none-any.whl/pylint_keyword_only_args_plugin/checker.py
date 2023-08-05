7b06fc1d26c68432924266621507347d16203585
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:58 +0000

    Add package pylint-keyword-only-args-plugin/0.2.2/pylint_keyword_only_args_plugin-0.2.2-py3-none-any.whl

---
+++ packages/pylint-keyword-only-args-plugin/pylint_keyword_only_args_plugin-0.2.2-py3-none-any.whl/pylint_keyword_only_args_plugin/checker.py
@@ -0,0 +1,46 @@
+import builtins
+from typing import Iterable
+
+from astroid.nodes import Call, Assign, Tuple, Attribute
+from pylint.checkers import BaseChecker
+
+
+class KeywordOnlyArgsChecker(BaseChecker):
+    name = "keyword-only-args"
+    priority = -100
+    msgs = {
+        "C0004": (
+            "Only keyword arguments allowed to callable.",
+            "keyword-only-args",
+            "Positional arguments to callable not allowed. "
+            "Example: func_call(a=1, b=2) - good, func_call(a, b) - bad, func_call(a, b=2) - bad.",
+        ),
+    }
+    options = (
+        (
+            "skip-names-list",
+            {"default": "", "type": "string", "help": "Comma separated list of callable names to skip"}
+        ),
+    )
+
+    def visit_call(self, node: Call) -> None:
+        nodes: Iterable = [node]
+        if isinstance(node, Assign):
+            if isinstance(node.value, Call):
+                nodes = [node.value]
+
+            if isinstance(node.value, Tuple):
+                nodes = node.value.elts
+
+        skip_names_list = [*dir(builtins), *self.linter.config.skip_names_list.split(","), "Path"]
+        for _node in nodes:
+            if isinstance(_node.func, Attribute):
+                node_name = _node.func.attrname
+            else:
+                node_name = _node.func.name
+
+            if node_name in skip_names_list:
+                return
+
+            if _node.args:
+                self.add_message("keyword-only-args", node=_node)
