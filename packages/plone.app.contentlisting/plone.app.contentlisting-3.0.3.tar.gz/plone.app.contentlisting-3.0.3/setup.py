38a06b7af4d940f749fd7304fcd0c96138a71333
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:01 +0000

    Add package plone.app.contentlisting/3.0.3/plone.app.contentlisting-3.0.3.tar.gz

---
+++ packages/plone.app.contentlisting/plone.app.contentlisting-3.0.3.tar.gz/plone.app.contentlisting-3.0.3/setup.py
@@ -0,0 +1,64 @@
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+version = "3.0.3"
+
+long_description = "{}\n\n{}".format(read("README.rst"), read("CHANGES.rst"))
+
+setup(
+    name="plone.app.contentlisting",
+    version=version,
+    description="Listing of content for the Plone CMS",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Plone",
+        "Framework :: Plone :: Core",
+        "Framework :: Plone :: 6.0",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="content list Plone",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.app.contentlisting",
+    license="GPL version 2",
+    packages=find_packages(),
+    namespace_packages=["plone", "plone.app"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "plone.base",
+        "Products.MimetypesRegistry",
+        "Products.ZCatalog",
+        "plone.i18n",
+        "plone.registry",
+        "plone.rfc822",
+        "plone.uuid",
+    ],
+    extras_require={
+        "test": [
+            "plone.app.contenttypes[test]",
+            "plone.app.testing",
+            "plone.batching",
+            "plone.testing",
+        ],
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
