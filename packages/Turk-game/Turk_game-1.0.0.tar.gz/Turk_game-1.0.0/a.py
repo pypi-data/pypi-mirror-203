5ef4776c6255a344bcf0fe67ebbe3a524a8f4ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:25 +0000

    Add package Turk-game/1.0.0/Turk_game-1.0.0.tar.gz

---
+++ packages/Turk-game/Turk_game-1.0.0.tar.gz/Turk_game-1.0.0/a.py
@@ -0,0 +1,4 @@
+import Turk_game as Tg
+
+ekran = Tg.EKRAN(600,600,"pattez","a.png",False)
+ekran.dongu([[True,print,"hello"],[True,ekran.yerlestir,ekran.ikon,0,0]],[],[],ekran.update)
---
+++ packages/Turk-game/Turk_game-1.0.0.tar.gz/Turk_game-1.0.0/setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name = 'Turk_game',
+    packages = ['.'],
+    version = '1.0.0',
+    description = 'Türk profesyoneller ve acemiler için basitleştirilmiş ve kısaltılmış pygame',
+    author = 'Eymen',
+    author_email = 'taneymen2009@gmail.com',
+    url = '',
+    keywords = [],
+    classifiers = [],
+)
