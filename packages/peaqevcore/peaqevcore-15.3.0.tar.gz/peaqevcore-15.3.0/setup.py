957198a81c7e3c59486591ec855573bf0be521d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:38 +0000

    Add package peaqevcore/15.3.0/peaqevcore-15.3.0.tar.gz

---
+++ packages/peaqevcore/peaqevcore-15.3.0.tar.gz/peaqevcore-15.3.0/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+
+setuptools.setup(
+    name="peaqevcore",
+    version="15.3.0",
+    author="Magnus Eldén",
+    description="Core types for peaqev car charging",
+    url="https://github.com/elden1337/peaqev-core",
+    license="CC-NC-ND",
+    packages=[
+        "peaqevcore", 
+        "peaqevcore.services.hourselection",
+        "peaqevcore.services.hourselection.hourselectionservice",
+        "peaqevcore.services.hourselection.initializers",
+        "peaqevcore.models.hourselection", 
+        "peaqevcore.models.hourselection.hourobjects", 
+        "peaqevcore.services.chargertype",
+        "peaqevcore.models.chargertype",
+        "peaqevcore.models", 
+        "peaqevcore.models.hub", 
+        "peaqevcore.hub", 
+        "peaqevcore.services.prediction", 
+        "peaqevcore.services.production", 
+        "peaqevcore.services.session",
+        "peaqevcore.services.threshold", 
+        "peaqevcore.services.locale", 
+        "peaqevcore.services.locale.countries", 
+        "peaqevcore.services.locale.querytypes", 
+        "peaqevcore.models.locale",
+        "peaqevcore.models.locale.enums",
+        "peaqevcore.models.locale.price",
+        "peaqevcore.models.locale.price.models",
+        "peaqevcore.services.scheduler",
+        "peaqevcore.services.timer"
+        ],
+    test_requires=['pytest']
+)   
