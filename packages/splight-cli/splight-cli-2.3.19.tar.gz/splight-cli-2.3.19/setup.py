e4ca33738d811bee720a80348d1ca7ffac888362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:09 +0000

    Add package splight-cli/2.3.19/splight-cli-2.3.19.tar.gz

---
+++ packages/splight-cli/splight-cli-2.3.19.tar.gz/splight-cli-2.3.19/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+from version import __version__
+
+with open("requirements.txt") as fp:
+    install_requires = fp.readlines()
+
+dependency_links = [
+    # External repositories different from pypi
+]
+
+setup(
+    name="splight-cli",
+    version=__version__,
+    author="Splight",
+    author_email="factory@splight-ae.com",
+    description="CLI tool to build and run Splight components",
+    py_modules=["splight"],
+    install_requires=install_requires,
+    packages=find_packages(),
+    package_data={
+        "cli": [
+            "component/templates/*",
+            "component/templates/.splightignore",
+            "datalake/dump_example.csv",
+        ],
+    },
+    include_package_data=True,
+    dependency_links=dependency_links,
+    entry_points={
+        "console_scripts": [
+            "splight = cli.cli:app",
+        ],
+    },
+)
