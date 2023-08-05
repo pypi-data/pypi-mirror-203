81a07fcc904f0bd72a6c3be6a2a4709d8a01a894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package pyapikey/0.0.6/pyapikey-0.0.6.tar.gz

---
+++ packages/pyapikey/pyapikey-0.0.6.tar.gz/pyapikey-0.0.6/pyapikey/core.py
@@ -0,0 +1,48 @@
+import os
+from typing import Any
+import datetime
+import json
+
+
+def get_key(domain: str) -> str:
+    filename = os.path.expanduser("~/.config/pyapikey.json")
+    with open(filename, "rt") as file_handle:
+        keys = json.load(file_handle)
+        return keys[domain]
+
+
+def default(obj):
+    if isinstance(obj, datetime.datetime):
+        return {'_isoformat': obj.isoformat()}
+    return str(obj)
+
+
+def object_hook(obj):
+    _isoformat = obj.get('_isoformat')
+    if _isoformat is not None:
+        return datetime.datetime.fromisoformat(_isoformat)
+    return obj
+
+
+class TempStore:
+    def __init__(self):
+        filename = os.path.expanduser("~/.config/pyapikey.temp.json")
+        if os.path.isfile(filename):
+            with open(filename) as file_handle:
+                self.data = json.load(file_handle, object_hook=object_hook)
+        else:
+            self.data = {}
+
+    def get(self, key: str) -> Any:
+        return self.data[key]
+
+    def set(self, key: str, value: Any) -> None:
+        self.data[key] = value
+
+    def has(self, key: str) -> bool:
+        return key in self.data
+
+    def save(self):
+        filename = os.path.expanduser("~/.config/pyapikey.temp.json")
+        with open(filename, 'w') as file_handle:
+            json.dump(fp=file_handle, obj=self.data, default=default)
