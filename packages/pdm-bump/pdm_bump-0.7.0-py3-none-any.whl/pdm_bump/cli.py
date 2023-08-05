03975dad9aa82f0585d93dbb86d6f8a879840347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:38 +0000

    Add package pdm-bump/0.7.0/pdm_bump-0.7.0-py3-none-any.whl

---
+++ packages/pdm-bump/pdm_bump-0.7.0-py3-none-any.whl/pdm_bump/cli.py
@@ -0,0 +1,34 @@
+#
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021-2023 Carsten Igel.
+#
+# This file is part of pdm-bump
+# (see https://github.com/carstencodes/pdm-bump).
+#
+# This file is published using the MIT license.
+# Refer to LICENSE for more information
+#
+from typing import Optional, Protocol
+
+# MyPy cannot resolve this during pull request
+from pdm.project.config import ConfigItem as _ConfigItem  # type: ignore
+
+from .plugin import BumpCommand as _Command
+
+
+class _CoreLike(Protocol):
+    def register_command(
+        self, command: type[_Command], name: Optional[str] = None
+    ) -> None:
+        # Method empty: Only a protocol stub
+        pass
+
+    @staticmethod
+    def add_config(name: str, config_item: _ConfigItem) -> None:
+        # Method empty: Only a protocol stub
+        pass
+
+
+def main(core: _CoreLike) -> None:
+    core.register_command(_Command)
