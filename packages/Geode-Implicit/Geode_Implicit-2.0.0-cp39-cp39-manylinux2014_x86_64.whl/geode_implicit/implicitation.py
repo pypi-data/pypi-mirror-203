8e19966b90ac55ca43c6308b98841256e666ec96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:34 +0000

    Add package Geode-Implicit/2.0.0/Geode_Implicit-2.0.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/Geode-Implicit/Geode_Implicit-2.0.0-cp39-cp39-manylinux2014_x86_64.whl/geode_implicit/implicitation.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+from geode_numerics import scalar_function
+
+from .lib64.geode_implicit_py_implicitation import *
+ImplicitImplicitationLibrary.initialize()
