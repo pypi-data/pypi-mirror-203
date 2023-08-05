48c5053fae2ef489508ea336f11831287ebf5d97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:05 +0000

    Add package nazo-rand/0.0.2/nazo_rand-0.0.2.tar.gz

---
+++ packages/nazo-rand/nazo_rand-0.0.2.tar.gz/nazo_rand-0.0.2/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="nazo_rand",
+    ext_modules=cythonize(
+        Extension(
+            name="",
+            sources=["rand/nazo_rand.pyx"],
+            language=["c++"],
+            extra_compile_args=["-std=c++17", "-O3"],
+            extra_link_args=["-O3"],
+        ),
+        compiler_directives={
+            "language_level": 3,
+            "boundscheck": False,
+            "wraparound": False,
+            "binding": True,
+            "cdivision": True,
+        },
+    ),
+    author="bymoye",
+    author_email="s3moye@gmail.com",
+    version="0.0.2",
+    description="A fast random number generator for python",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    license="Free for non-commercial use",
+    package_data={
+        "": [
+            "rand/nazo_rand.pyi",
+            "rand/nazo_rand.pyx",
+            "rand/nazo_rand.hpp",
+            "rand/nazo_rand.pxd",
+        ]
+    },
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Cython",
+        "Programming Language :: C++",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS :: MacOS X",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.8",
+    packages=["rand"],
+)
