f8dbada5aaf25b2f7da4eb0408bfb2846c37d5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:12 +0000

    Add package wield.declarative/1.5.0/wield.declarative-1.5.0-py3-none-any.whl

---
+++ packages/wield.declarative/wield.declarative-1.5.0-py3-none-any.whl/wield/declarative/argparse/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2021 Massachusetts Institute of Technology.
+# SPDX-FileCopyrightText: © 2021 Lee McCuller <mcculler@caltech.edu>
+# NOTICE: authors should document their contributions in concisely in NOTICE
+# with details inline in source files, comments, and docstrings.
+"""
+"""
+from .oo_argparse import (
+    OOArgParse,
+)
+
+from .annotations import (
+    store_true,
+    store_false,
+    argument,
+    group,
+    command,
+)
---
+++ packages/wield.declarative/wield.declarative-1.5.0-py3-none-any.whl/wield/declarative/callbacks/__init__.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2021 Massachusetts Institute of Technology.
+# SPDX-FileCopyrightText: © 2021 Lee McCuller <mcculler@caltech.edu>
+# NOTICE: authors should document their contributions in concisely in NOTICE
+# with details inline in source files, comments, and docstrings.
+"""
+"""
+from .callbacks import (
+    Callback,
+    callbackmethod,
+    callbackstaticmethod,
+    singlecallbackmethod,
+    SingleCallback,
+)
+
+from .relay import (
+    RelayValue,
+    RelayValueRejected,
+    RelayValueCoerced,
+)
+
+from .validators import (
+    min_max_validator_int,
+    min_max_validator,
+)
+
+from .state_booleans import (
+    RelayBool,
+    RelayBoolNot,
+    RelayBoolAll,
+    RelayBoolAny,
+    RelayBoolNotAll,
+    RelayBoolNotAny,
+)
