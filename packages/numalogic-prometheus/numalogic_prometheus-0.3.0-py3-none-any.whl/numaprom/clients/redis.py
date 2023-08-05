602457d584c49b7d8007dffeb0fa59e5dfdbf8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:51 +0000

    Add package numalogic-prometheus/0.3.0/numalogic_prometheus-0.3.0-py3-none-any.whl

---
+++ packages/numalogic-prometheus/numalogic_prometheus-0.3.0-py3-none-any.whl/numaprom/clients/redis.py
@@ -0,0 +1,35 @@
+import json
+from typing import Optional
+
+from redis.cluster import RedisCluster
+
+from numaprom import get_logger
+from numaprom.tools import is_host_reachable
+
+_LOGGER = get_logger(__name__)
+redis_client: Optional[RedisCluster] = None
+
+
+def get_redis_client(
+    host: str, port: str, password: str = None, decode_responses: bool = True, recreate=False
+) -> RedisCluster:
+    global redis_client
+
+    if not recreate and redis_client:
+        return redis_client
+
+    redis_params = {
+        "host": host,
+        "port": port,
+        "password": password,
+        "decode_responses": decode_responses,
+        "skip_full_coverage_check": True,
+        "dynamic_startup_nodes": False,
+    }
+    _LOGGER.info("Redis params: %s", json.dumps(redis_params, indent=4))
+
+    if not is_host_reachable(host, port):
+        _LOGGER.error("Redis Cluster is unreachable after retries!")
+
+    redis_client = RedisCluster(**redis_params)
+    return redis_client
