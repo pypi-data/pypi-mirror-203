9a6b664b6e67b2e07a79b917b6271e95922560f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:15 +0000

    Add package nsj-rest-lib/0.1.13/nsj_rest_lib-0.1.13.tar.gz

---
+++ packages/nsj-rest-lib/nsj_rest_lib-0.1.13.tar.gz/nsj_rest_lib-0.1.13/src/nsj_rest_lib/entity/entity_base.py
@@ -0,0 +1,34 @@
+import abc
+
+from typing import List
+
+
+class EMPTY:
+    pass
+
+
+class EntityBase(abc.ABC):
+
+    def get_pk_column_name(self) -> str:
+        return 'id'
+
+    @abc.abstractmethod
+    def get_table_name(self) -> str:
+        pass
+
+    @abc.abstractmethod
+    def get_default_order_fields(self) -> List[str]:
+        pass
+
+    @abc.abstractmethod
+    def get_pk_field(self) -> str:
+        pass
+
+    def get_insert_returning_fields(self) -> List[str]:
+        return None
+
+    def get_update_returning_fields(self) -> List[str]:
+        return None
+    
+    def get_const_fields(self) -> List[str]:
+        return ['criado_em','criado_por']
