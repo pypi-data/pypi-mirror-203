634fde6096239f2953234d73311537a47748f3ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:06 +0000

    Add package wield.declarative/1.5.1/wield.declarative-1.5.1-py3-none-any.whl

---
+++ packages/wield.declarative/wield.declarative-1.5.1-py3-none-any.whl/wield/declarative/callbacks/validators.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2021 Massachusetts Institute of Technology.
+# SPDX-FileCopyrightText: © 2021 Lee McCuller <mcculler@caltech.edu>
+# NOTICE: authors should document their contributions in concisely in NOTICE
+# with details inline in source files, comments, and docstrings.
+"""
+"""
+import math
+
+from .relay import (
+    RelayValueCoerced,
+    RelayValueRejected,
+)
+
+
+def min_max_validator_int(min_val, max_val):
+    def validator(val):
+        # put limits on motion
+        try:
+            val = int(val)
+            if val < min_val:
+                val = min_val
+            elif val > max_val:
+                val = max_val
+            if val != val:
+                raise RelayValueCoerced(val)
+            return
+        # catch NaN's hitting the int
+        except ValueError:
+            raise RelayValueRejected()
+        return val
+
+    return validator
+
+
+def min_max_validator(min_val, max_val, min_ne=False, max_ne=False, allow_nan=False):
+    if not min_ne:
+        if not max_ne:
+
+            def validator(val):
+                if not math.isfinite(val):
+                    if allow_nan:
+                        return
+                    else:
+                        raise RelayValueRejected()
+                # put limits on motion
+                if val < min_val:
+                    raise RelayValueCoerced(min_val)
+                elif val > max_val:
+                    raise RelayValueCoerced(max_val)
+                return val
+
+        else:
+
+            def validator(val):
+                if not math.isfinite(val):
+                    if allow_nan:
+                        return
+                    else:
+                        raise RelayValueRejected()
+                # put limits on motion
+                if val < min_val:
+                    raise RelayValueCoerced(min_val)
+                elif val >= max_val:
+                    raise RelayValueRejected()
+                return val
+
+    else:
+        if not max_ne:
+
+            def validator(val):
+                if not math.isfinite(val):
+                    if allow_nan:
+                        return
+                    else:
+                        raise RelayValueRejected()
+                # put limits on motion
+                if val <= min_val:
+                    raise RelayValueRejected()
+                elif val > max_val:
+                    raise RelayValueCoerced(max_val)
+                return val
+
+        else:
+
+            def validator(val):
+                if not math.isfinite(val):
+                    if allow_nan:
+                        return
+                    else:
+                        raise RelayValueRejected()
+                # put limits on motion
+                if val <= min_val:
+                    raise RelayValueRejected()
+                elif val >= max_val:
+                    raise RelayValueRejected()
+                return val
+
+    return validator
---
+++ packages/wield.declarative/wield.declarative-1.5.1-py3-none-any.whl/wield/declarative/properties/__init__.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2021 Massachusetts Institute of Technology.
+# SPDX-FileCopyrightText: © 2021 Lee McCuller <mcculler@caltech.edu>
+# NOTICE: authors should document their contributions in concisely in NOTICE
+# with details inline in source files, comments, and docstrings.
+"""
+"""
+
+from .bases import (
+    PropertyTransforming,
+    HasDeclaritiveAttributes,
+    InnerException,
+    PropertyAttributeError,
+)
+
+from .memoized import (
+    memoized_class_property,
+    mproperty,
+    dproperty,
+    mproperty_plain,
+    dproperty_plain,
+    mproperty_fns,
+    dproperty_fns,
+    mfunction,
+)
+
+from .memoized_adv import (
+    mproperty_adv,
+    dproperty_adv,
+)
+
+from .memoized_adv_group import (
+    dproperty_adv_group,
+    mproperty_adv_group,
+    group_mproperty,
+    group_dproperty,
+)
+
+# because this is the critical unique object
+from ..utilities.unique import (
+    NOARG,
+)
