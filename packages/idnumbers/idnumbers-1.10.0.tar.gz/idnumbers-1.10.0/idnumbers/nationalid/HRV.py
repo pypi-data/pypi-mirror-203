e4d4ec8fd1dba136bd9bfa9117d19774e7230e24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:48 +0000

    Add package idnumbers/1.10.0/idnumbers-1.10.0.tar.gz

---
+++ packages/idnumbers/idnumbers-1.10.0.tar.gz/idnumbers-1.10.0/idnumbers/nationalid/HRV.py
@@ -0,0 +1,12 @@
+from .hrv.personal_id import PersonalID
+from .util import alias_of
+
+
+NationalID = alias_of(PersonalID)
+"""
+alias of PersonalID
+"""
+OIB = alias_of(PersonalID)
+"""
+alias of PersonalID
+"""
