5f54596ce4b0ca023fbc61e9e9bb3e4fa361b9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:39 +0000

    Add package symdexer/0.2.0/symdexer-0.2.0-py3-none-any.whl

---
+++ packages/symdexer/symdexer-0.2.0-py3-none-any.whl/symdexer/modules.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from typing import Generator
+from dataclasses import dataclass
+from functools import cached_property
+
+
+@dataclass
+class Module:
+    path: Path
+    name: str
+
+    @cached_property
+    def mtime(self):
+        return int(self.path.stat().st_mtime)
+
+
+def walk_modules(path: Path, prefix: str = "") -> Generator[Module, None, None]:
+    if not prefix:
+        prefix = path.stem
+
+    if path.is_file():
+        if path.suffix == ".py":
+            yield Module(path, prefix)
+        return
+
+    for subpath in path.iterdir():
+        yield from walk_modules(
+            subpath,
+            prefix if subpath.stem == "__init__" else f"{prefix}.{subpath.stem}",
+        )
