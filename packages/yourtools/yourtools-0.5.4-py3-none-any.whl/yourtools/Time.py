35879aa8c45ea13f9523ce067be0032d5c8d6109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:19 +0000

    Add package yourtools/0.5.4/yourtools-0.5.4-py3-none-any.whl

---
+++ packages/yourtools/yourtools-0.5.4-py3-none-any.whl/yourtools/Time.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+@version    : v1.0
+@author     : fangzheng
+@contact    : fangzheng@rp-pet.cn
+@software   : PyCharm
+@filename   : Time.py
+@create time: 2023/4/13 3:35 PM
+@modify time: 2023/4/13 3:35 PM
+@describe   : 
+-------------------------------------------------
+"""
+from time import strftime, localtime
+
+
+class Time:
+    def __init__(self):
+        self.time_format = "%Y-%m-%d %H:%M:%S"
+
+    def curr_time(self, time_format=None):
+        if time_format:
+            return strftime(time_format, localtime())
+        else:
+            return strftime(self.time_format, localtime())
+
+    def curr_time_by_number(self):
+        return self.curr_time("%Y%m%d%H%M%S")
