816524ecd42f81e928f1303252404e9c88bc18d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:01 +0000

    Add package apyproxy/0.1.0/apyproxy-0.1.0.tar.gz

---
+++ packages/apyproxy/apyproxy-0.1.0.tar.gz/apyproxy-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['apyproxy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'apyproxy',
+    'version': '0.1.0',
+    'description': 'Python REST client library inspired by xmlrpclib',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
