db7797fa5b437478c4e70f79324cc2a8e887acc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:19 +0000

    Add package devgoldyutils/2.4.3/devgoldyutils-2.4.3-py3-none-any.whl

---
+++ packages/devgoldyutils/devgoldyutils-2.4.3-py3-none-any.whl/devgoldyutils/__init__.py
@@ -0,0 +1,14 @@
+from .console import Console
+
+from .colours import Colours
+from .dataclasses import DictDataclass
+from .errors import DevGoldyUtilsError
+from .logging import add_custom_handler, LoggerAdapter
+from .file_configs import JSONConfig
+
+# For those Americans. Imagine not using English UK.
+Colors = Colours
+
+# Utils from other libraries
+from prettyprinter import cpprint as pprint, install_extras
+install_extras(include=["dataclasses"])
