ba24426fbd99e9015667fc7fff76336ebf3f7086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package peaksjs-widget/0.0.32/peaksjs_widget-0.0.32.tar.gz

---
+++ packages/peaksjs-widget/peaksjs_widget-0.0.32.tar.gz/peaksjs_widget-0.0.32/peaksjs_widget/_version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) AntoineDaurat.
+# Distributed under the terms of the Modified BSD License.
+
+version_info = (0, 0, 32)
+__version__ = ".".join(map(str, version_info))
