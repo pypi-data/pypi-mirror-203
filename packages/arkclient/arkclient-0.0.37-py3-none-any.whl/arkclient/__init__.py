1da4f9334b93521d8fb3173ac85bc999a14935df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:15 +0000

    Add package arkclient/0.0.37/arkclient-0.0.37-py3-none-any.whl

---
+++ packages/arkclient/arkclient-0.0.37-py3-none-any.whl/arkclient/__init__.py
@@ -0,0 +1,4 @@
+from arkclient.client import GameBotClient, APIClient
+from arkclient.main import test
+
+__all__ = ['GameBotClient', 'APIClient', 'test']
---
+++ packages/arkclient/arkclient-0.0.37-py3-none-any.whl/arkclient/client/__init__.py
@@ -0,0 +1,5 @@
+from arkclient.client.game_bot_client import GameBotClient
+from arkclient.client.api_client import APIClient
+
+
+__all__ = ['GameBotClient', 'APIClient']
