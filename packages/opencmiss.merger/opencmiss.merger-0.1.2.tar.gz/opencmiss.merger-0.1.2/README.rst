322a1ebed2a6bc28c5f67a46e8c32a9b0504dfee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:39 +0000

    Add package opencmiss.merger/0.1.2/opencmiss.merger-0.1.2.tar.gz

---
+++ packages/opencmiss.merger/opencmiss.merger-0.1.2.tar.gz/opencmiss.merger-0.1.2/README.rst
@@ -0,0 +1,9 @@
+
+================
+OpenCMISS Merger
+================
+
+This package has been renamed. Use :code:`pip install cmlibs.merger` instead.
+New package: https://pypi.org/project/cmlibs.merger/
+
+See `OpenCMISS2CMLibs rename tool <https://pypi.org/project/opencmiss2cmlibs/>`_ for help with renaming your own code.
---
+++ packages/opencmiss.merger/opencmiss.merger-0.1.2.tar.gz/opencmiss.merger-0.1.2/setup.py
@@ -0,0 +1,27 @@
+import io
+
+from setuptools import setup
+
+
+def readfile(filename, split=False):
+    with io.open(filename, encoding="utf-8") as stream:
+        if split:
+            return stream.read().split("\n")
+        return stream.read()
+
+
+readme = readfile("README.rst", split=True)
+
+requires = ['cmlibs.merger']
+
+setup(
+    name='opencmiss.merger',
+    version="0.1.2",
+    description='OpenCMISS Merger functions.',
+    long_description='\n'.join(readme),
+    long_description_content_type='text/x-rst',
+    classifiers=["Development Status :: 7 - Inactive"],
+    license='Apache Software License',
+    license_files=("LICENSE",),
+    install_requires=requires,
+)
