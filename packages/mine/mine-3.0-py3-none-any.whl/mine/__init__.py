8e4b153b301d8f951d81f7d8fcf3716ca51bd96c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:31 +0000

    Add package mine/3.0/mine-3.0-py3-none-any.whl

---
+++ packages/mine/mine-3.0-py3-none-any.whl/mine/__init__.py
@@ -0,0 +1,7 @@
+"""Package for mine."""
+
+from pkg_resources import get_distribution
+
+CLI = "mine"
+VERSION = "mine v{}".format(get_distribution("mine").version)
+DESCRIPTION = "Share application state across computers using Dropbox."
