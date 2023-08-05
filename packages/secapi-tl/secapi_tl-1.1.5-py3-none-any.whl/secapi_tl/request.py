59a9fd71d9b628c70e06b2ca30c325abe4f428ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:54 +0000

    Add package secapi-tl/1.1.5/secapi_tl-1.1.5-py3-none-any.whl

---
+++ packages/secapi-tl/secapi_tl-1.1.5-py3-none-any.whl/secapi_tl/request.py
@@ -0,0 +1,37 @@
+"""
+This module provides the sec_request function which must be used
+for all requests to the  sec.gov domain to ensure that the amount
+of requests stays in the boundaries set by the sec.
+
+The method was implemented as a static method of a class because
+an implementation as a normal function caused some issues
+by exceeding the request limits when used with threading.
+These issues disappeared with the implementation as a static method.
+"""
+import time
+import math
+import requests
+from ratelimit import limits, sleep_and_retry
+
+SEC_REQUEST_COUNT = 1
+SEC_PERIOD = 0.1
+SEC_HEADER = {'User-Agent': "myUserAgent"}
+
+
+class Request:
+
+    @staticmethod
+    @sleep_and_retry
+    @limits(calls=SEC_REQUEST_COUNT, period=SEC_PERIOD)
+    def sec_request(url: str, header: dict = None, retries: int=5):
+        if header is None:
+            header = SEC_HEADER
+        response = requests.get(url=url, headers=header)
+        if response.status_code != 200 and retries > 0:
+            time.sleep(20 * (1 / math.exp(retries-1)))
+            response = Request.sec_request(url=url, header=header, retries=retries-1)
+
+        # raise Connection error on deepest function call if there was no successful response for all tries
+        if response.status_code != 200 and retries == 0:
+            raise ConnectionError(f"invalid response statuscode: {response.status_code}")
+        return response
