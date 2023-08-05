d2a13968e9f7e8b3ff67a5d99da2a7edb98220ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:13 +0000

    Add package randomread/0.2.0/randomread-0.2.0.tar.gz

---
+++ packages/randomread/randomread-0.2.0.tar.gz/randomread-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['randomread']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'randomread',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'ninpnin',
+    'author_email': 'vainoyrjanainen@icloud.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
