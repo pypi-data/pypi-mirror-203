987815c637a34b0836e33a27db1003b79c505858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:48 +0000

    Add package PyUPC-EAN/2.9.4/PyUPC-EAN-2.9.4.tar.gz

---
+++ packages/PyUPC-EAN/PyUPC-EAN-2.9.4.tar.gz/PyUPC-EAN-2.9.4/upcean/encode/getsfname.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+'''
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the Revised BSD License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Revised BSD License for more details.
+
+    Copyright 2011-2023 Game Maker 2k - https://github.com/GameMaker2k
+    Copyright 2011-2023 Kazuki Przyborowski - https://github.com/KazukiPrzyborowski
+
+    $FileInfo: getsfname.py - Last Update: 4/14/2023 Ver. 2.9.4 RC 1 - Author: cooldude2k $
+'''
+
+from __future__ import absolute_import, division, print_function, unicode_literals;
+import sys, os, re, upcean.support;
+pilsupport = upcean.support.check_for_pil();
+cairosupport = upcean.support.check_for_cairo();
+from upcean.encode.predraw import *;
+if(pilsupport):
+ import upcean.encode.prepil;
+if(cairosupport):
+ import upcean.encode.precairo;
+
+def get_save_filename(outfile,imageoutlib="pillow"):
+ imageoutlib = imageoutlib.lower();
+ if(not pilsupport and imageoutlib=="pillow"):
+  imageoutlib = "cairo";
+ if(not cairosupport and (imageoutlib=="cairo" or imageoutlib=="cairosvg")):
+  imageoutlib = "pillow";
+ if(not cairosupport and imageoutlib=="cairosvg"):
+  imageoutlib = "pillow";
+ if(imageoutlib!="pillow" and imageoutlib!="cairo" and imageoutlib!="cairosvg"):
+  imageoutlib = "pillow";
+ if(not pilsupport and not cairosupport):
+  return False;
+ if(sys.version[0]=="2"):
+  if(isinstance(outfile, str) or isinstance(outfile, unicode)):
+   oldoutfile = outfile[:];
+ if(sys.version[0]>="3"):
+  if(isinstance(outfile, str)):
+   oldoutfile = outfile[:];
+ if(isinstance(outfile, tuple)):
+  oldoutfile = tuple(outfile[:]);
+ if(isinstance(outfile, list)):
+  oldoutfile = list(outfile[:]);
+ if(outfile is None or isinstance(outfile, bool)):
+  oldoutfile = None;
+ if(sys.version[0]=="2"):
+  if(isinstance(oldoutfile, str) or isinstance(outfile, unicode)):
+   if(outfile!="-" and outfile!="" and outfile!=" "):
+    if(len(re.findall("^\.([A-Za-z]+)$", os.path.splitext(oldoutfile)[1]))>0):
+     outfileext = re.findall("^\.([A-Za-z]+)", os.path.splitext(outfile)[1])[0].upper();
+    if(len(re.findall("^\.([A-Za-z]+)$", os.path.splitext(oldoutfile)[1]))==0 and len(re.findall("(.*)\:([a-zA-Z]+)", oldoutfile))>0):
+     tmpoutfile = re.findall("(.*)\:([a-zA-Z]+)", oldoutfile);
+     del(outfile);
+     outfile = tmpoutfile[0][0];
+     outfileext = tmpoutfile[0][1].upper();
+    if(len(re.findall("^\.([A-Za-z]+)$", os.path.splitext(oldoutfile)[1]))==0 and len(re.findall("(.*)\:([a-zA-Z]+)", oldoutfile))==0):
+     outfileext = "PNG";
+   if(imageoutlib=="pillow"):
+    if(outfileext=="BYTES"):
+     outfileext = "BYTES";
+    else:
+     outfileext = Image.registered_extensions().get("."+outfileext.lower(), "PNG");
+   else:
+    if(outfileext=="BYTES"):
+     outfileext = "BYTES";
+    elif(outfileext=="SVG"):
+     outfileext = "SVG";
+    elif(outfileext=="PDF"):
+     outfileext = "PDF";
+    elif(outfileext=="PS"):
+     outfileext = "PS";
+    elif(outfileext=="EPS"):
+     outfileext = "EPS";
+    else:
+     outfileext = "PNG";
+   return (outfile, outfileext.upper());
+ if(sys.version[0]>="3"):
+  if(isinstance(oldoutfile, str)):
+   if(outfile!="-" and outfile!="" and outfile!=" "):
+    if(len(re.findall("^\.([A-Za-z]+)$", os.path.splitext(oldoutfile)[1]))>0):
+     outfileext = re.findall("^\.([A-Za-z]+)", os.path.splitext(outfile)[1])[0].upper();
+    if(len(re.findall("^\.([A-Za-z]+)$", os.path.splitext(oldoutfile)[1]))==0 and len(re.findall("(.*)\:([a-zA-Z]+)", oldoutfile))>0):
+     tmpoutfile = re.findall("(.*)\:([a-zA-Z]+)", oldoutfile);
+     del(outfile);
+     outfile = tmpoutfile[0][0];
+     outfileext = tmpoutfile[0][1].upper();
+    if(len(re.findall("^\.([A-Za-z]+)$", os.path.splitext(oldoutfile)[1]))==0 and len(re.findall("(.*)\:([a-zA-Z]+)", oldoutfile))==0):
+     outfileext = "PNG";
+   if(imageoutlib=="pillow"):
+    if(outfileext=="BYTES"):
+     outfileext = "BYTES";
+    else:
+     outfileext = Image.registered_extensions().get("."+outfileext.lower(), "PNG");
+   else:
+    if(outfileext=="BYTES"):
+     outfileext = "BYTES";
+    elif(outfileext=="SVG"):
+     outfileext = "SVG";
+    elif(outfileext=="PDF"):
+     outfileext = "PDF";
+    elif(outfileext=="PS"):
+     outfileext = "PS";
+    elif(outfileext=="EPS"):
+     outfileext = "EPS";
+    else:
+     outfileext = "PNG";
+   return (outfile, outfileext.upper());
+ if(isinstance(oldoutfile, tuple) or isinstance(oldoutfile, list)):
+  del(outfile);
+  outfile = oldoutfile[0];
+  outfileext = oldoutfile[1];
+  return (outfile, outfileext.upper());
+ if(outfile is None or isinstance(outfile, bool) or isinstance(outfile, file)):
+  return outfile;
