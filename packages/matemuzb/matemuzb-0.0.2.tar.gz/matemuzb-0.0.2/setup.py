284e39b0cdab3ba9916b44536e186d9b9a624568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:57 +0000

    Add package matemuzb/0.0.2/matemuzb-0.0.2.tar.gz

---
+++ packages/matemuzb/matemuzb-0.0.2.tar.gz/matemuzb-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='matemuzb',
+    version='0.0.2',
+    author='Sarvar',
+    author_email='sarvarsaydullayev185@gmail.com',
+    description='Bu kutubxona ishlatilmaydi'
+)
