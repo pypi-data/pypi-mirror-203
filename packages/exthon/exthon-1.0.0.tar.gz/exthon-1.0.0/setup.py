05dbf275aba2a2d2571e1590b9d9f7b92e69ba54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:37 +0000

    Add package exthon/1.0.0/exthon-1.0.0.tar.gz

---
+++ packages/exthon/exthon-1.0.0.tar.gz/exthon-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='exthon',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='DogEgg',
+    author_email='',
+    description=''
+)
