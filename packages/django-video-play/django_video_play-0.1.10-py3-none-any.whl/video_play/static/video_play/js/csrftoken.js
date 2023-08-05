41e9a17414ea233f66c3bcbdb755913bef21518a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:10 +0000

    Add package django-video-play/0.1.10/django_video_play-0.1.10-py3-none-any.whl

---
+++ packages/django-video-play/django_video_play-0.1.10-py3-none-any.whl/video_play/static/video_play/js/csrftoken.js
@@ -0,0 +1,37 @@
+﻿// 使用 jQuery 设置 csrftoken
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // cookie 的名称以指定字符串开头吗？
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function csrfSafeMethod(method) {
+    // 这些 HTTP 方法不需要 CSRF 防护
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+var csrftoken = getCookie('csrftoken');
+
+// var page_title = $(document).attr("title");
+// 这将使用适当的标头设置每个ajax调用。
+$.ajaxSetup({
+    beforeSend: function (xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+
+
+
