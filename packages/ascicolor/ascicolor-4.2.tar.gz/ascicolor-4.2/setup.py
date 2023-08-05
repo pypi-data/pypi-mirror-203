f8c457bdcec9bad6423fd91f5a7d008a5376195e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:01 +0000

    Add package ascicolor/4.2/ascicolor-4.2.tar.gz

---
+++ packages/ascicolor/ascicolor-4.2.tar.gz/ascicolor-4.2/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+setup(
+    name="ascicolor",
+    version='4.2',
+    license='Eclipse Public License 2.0',
+    authors=["smithdevsec"],
+    author_email="ascdev@gmail.com",
+    description="Make Your Code colrful",
+    long_description='Documentation: https://github.com/smithdevsec/ascicolor',
+    packages=['ascicolor']
+)
+
