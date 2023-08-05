5f8f032ebd8c07f5d003546a4a88569dbb0d82af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:17 +0000

    Add package statistics-core/0.0.7/statistics_core-0.0.7.tar.gz

---
+++ packages/statistics-core/statistics_core-0.0.7.tar.gz/statistics_core-0.0.7/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "statistics_core",
+    version = "0.0.7",
+    author = "LokasWiki",
+    # author_email = "author@example.com",
+    description = "short package description",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/LokasWiki/statistics-core",
+    project_urls = {
+        "Bug Tracker": "https://github.com/LokasWiki/statistics-core/issues",
+    },
+    classifiers = [
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6"
+)
