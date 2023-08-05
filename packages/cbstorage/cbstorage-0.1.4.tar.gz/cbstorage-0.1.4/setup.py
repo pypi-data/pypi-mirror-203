ce8ab85387e9724d4bc9b812551486cca8428e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:37 +0000

    Add package cbstorage/0.1.4/cbstorage-0.1.4.tar.gz

---
+++ packages/cbstorage/cbstorage-0.1.4.tar.gz/cbstorage-0.1.4/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cbstorage']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.26.113,<2.0.0']
+
+setup_kwargs = {
+    'name': 'cbstorage',
+    'version': '0.1.4',
+    'description': 'The code is a Python package for managing file storage on either AWS S3 or a local machine. It provides methods for listing, searching, and storing files. The package includes two classes, Aws and Local, which implement the same set of methods for accessing and manipulating files in S3 and a local directory.',
+    'long_description': None,
+    'author': 'germangerken',
+    'author_email': 'germangerken@yandex.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
