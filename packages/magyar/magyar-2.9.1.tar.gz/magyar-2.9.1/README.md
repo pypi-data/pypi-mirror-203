b81e1c3ceb06eadf8431b59d01c7a56207876be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:40 +0000

    Add package magyar/2.9.1/magyar-2.9.1.tar.gz

---
+++ packages/magyar/magyar-2.9.1.tar.gz/magyar-2.9.1/README.md
@@ -0,0 +1,64 @@
+# magyar
+
+
+## Leírás
+
+Magyar listák gyűjteménye - Collection of Hungarian lists.
+
+Az alábbi listákat találod :
+1. vezetéknevek   =  magyar.vezeteknev
+2. női keresztnevek  = magyar.keresztnev_n
+3. férfi keresztnevek = magyar.keresztnev_f
+4. utcanevek = magyar.utca
+5. telelpülésnevek= magyar.telepules
+6. vármegyék nevei = magyar.megye
+7. folyók nevei = magyar.folyo
+8. a hét napjai = magyar.nap
+9. az év hónapjai = magyar.honap
+10. gyümölcsok = magyar.gyumolcs
+11. zöldségek = magyar.zoldseg
+
+Szótárak  (dictionary): 
+1. Királyok és uralkodásuk ideje  = magyar.kiraly
+
+## Description
+1. last names =  magyar.vezeteknev
+2. female first names = magyar.keresztnev_n
+3. male first names  = magyar.keresztnev_f
+4. street names = magyar.utca
+5. city names = magyar.telepules
+6. names of counties = magyar.megye
+7. names of rivers = magyar.folyo
+8. he days of the week = magyar.nap
+9. the months of the year = magyar.honap
+10. fruits = magyar.gyumolcs
+11. vegetables = magyar.zoldseg
+
+Dictionary:
+1. Hungarian Kings and Reigns = magyar.kiraly
+
+## Listák használat:
+
+ Főként véletlengenerátorok kiegészítőjeként ajánlom a listákat
+ 
+I recommend it mainly as a supplement to random number generators. 
+       
+             random.choice
+telepulesek = random.choice(magyar.telepules)
+
+## Szótárak:
+Több adatot tartalmaznak.
+
+magyar.kiraly tartalma :
+
+kiraly = {'király neve' : (tól, ig)}
+
+
+## Szerző
+
+* Név: Nagy BÉLa
+* E-mail:nagy.bela.budapest@gmail.com
+
+## Licenc
+
+Oktatási célra készült, szabadon használható
---
+++ packages/magyar/magyar-2.9.1.tar.gz/magyar-2.9.1/magyar.py
@@ -0,0 +1,124 @@
+# Magyar nevek listái
+
+vezeteknev = ['Kovács', 'Nagy', 'Kiss', 'Tóth', 'Varga', 'Farkas', 'Horváth', 'Szabó', 'Molnár', 'Balázs',
+              'Lakatos', 'Takács', 'Papp', 'Katona', 'Szilágyi', 'Balogh', 'Gulyás', 'Szőke', 'Simon', 'Fehér',
+              'Barta', 'Császár', 'Sándor', 'Fodor', 'Bakos', 'László', 'Kerekes', 'Fekete', 'Török', 'Pintér',
+              'Juhász', 'Gáspár', 'Kis', 'Bognár', 'Hajdu', 'Kónya', 'Kozma', 'Borbély', 'Budai', 'Hegedűs',
+              'Ács', 'Bálint', 'Rácz', 'Virág', 'Sipos', 'Kelemen', 'Gál', 'Szőllősi', 'Lengyel', 'Füredi',
+              'Magyar', 'Fazekas', 'Antal', 'Bérczes', 'Temesvári', 'Oláh', 'Pál', 'Barna', 'Bajnai', 'Balog',
+              'Kocsis', 'Halász', 'Kertész', 'Boros', 'Fekete', 'Bérczi', 'Vincze', 'Dudás', 'Mészáros',
+              'Gombos', 'Árkosi', 'Pánczél', 'Karádi', 'Kulcsár', 'Ágoston', 'Pap', 'Pálinkás', 'Csák', 'Bódi',
+              'Kerekes', 'Fischer', 'Győri', 'Magyar', 'Péter', 'Fenyő', 'Kocsis', 'Fazekas', 'Kis-Benedek',
+              'Bán', 'Bánhidi', 'Kovácsik', 'Kincses', 'Iványi', 'Bertalan', 'Molnár', 'Papp', 'Benedek',
+              'Huszár', 'Kis-Gál', 'Németh', 'Somogyi', 'Boda', 'Budai', 'Székely', 'Borsos', 'Czeglédi',
+              'Árvai', 'Miklós', 'Füzesi', 'Pap', 'Kisfaludi', 'Pongrácz', 'Tóth', 'Budavári', 'Sajó',
+              'Mészáros', 'Lévai', 'Kéri', 'Czakó', 'Csordás', 'Mészáros', 'Bodó', 'Kántor', 'Nagy-Balogh',
+              'Szekeres', 'Takács', 'Jakab', 'Herczeg', 'Vajda', 'Gyuris', 'Kincs', 'Végh', 'Lukács', 'Erdélyi',
+              'Simon', 'Bárdi', 'Szabó-Balog', 'Szubin']
+
+keresztnev_n = ['Anna', 'Cecília', 'Erika', 'Hanna', 'Katalin', 'Mária', 'Nóra', 'Orsolya', 'Réka', 'Szilvia',
+                'Tünde', 'Viktória', 'Dóra', 'Eszter', 'Flóra', 'Hajnalka', 'Izabella', 'Jázmin', 'Kamilla', 'Nikolett',
+                'Panna', 'Rita', 'Sarolta', 'Zsófia', 'Ágota', 'Csilla', 'Lilla', 'Orsolya', 'Panna',
+                'Szandra', 'Tamara', 'Ágnes', 'Júlia', 'Kincső', 'Babett', 'Barbara', 'Beáta', 'Bianka',
+                'Blanka', 'Boglárka', 'Borbála', 'Boróka', 'Brigitta', 'Bernadett', 'Bettina', 'Berta', 'Betti',
+                'Bianka', 'Cecília', 'Cintia', 'Csenge', 'Csilla', 'Detti', 'Diana', 'Dóra', 'Dalma', 'Daniella',
+                'Debóra', 'Dolli', 'Dominika']
+
+keresztnev_f = ['Ábel', 'Ádám', 'Ákos', 'Aladár', 'Albert', 'Albin', 'Alekszander', 'Alfréd', 'Ambrus', 'Andor',
+                'András', 'Áron', 'Árpád', 'Attila', 'Aurél', 'Bálint', 'Barna', 'Barnabás', 'Béla', 'Benedek',
+                'Benjámin', 'Bernát', 'Botond', 'Csongor', 'Dénes', 'Dániel', 'Dávid', 'Dezső', 'Dominik', 'Donát',
+                'Edgár', 'Edmund', 'Ede', 'Eduárd', 'Endre', 'Erik', 'Ernő', 'Fábián', 'Farkas', 'Ferenc', 'Flórián',
+                'Gábor', 'Géza', 'Gergely', 'Gusztáv', 'György', 'Győző', 'Hunor', 'Igor', 'Imre', 'István', 'János',
+                'József', 'Kálmán', 'Károly', 'Kázmér', 'Kelemen', 'Kende', 'Kornél', 'Kristóf',
+                'Lajos', 'László', 'Levente', 'Lóránt', 'Lóránd', 'Lukács', 'Márió', 'Márk', 'Márton', 'Mátyás',
+                'Miklós', 'Milán', 'Mózes', 'Nándor', 'Nikolasz', 'Noé', 'Norbert', 'Olivér', 'Ödön', 'Oszkár',
+                'Ottó', 'Pál', 'Patrik', 'Péter', 'Róbert', 'Rudolf', 'Sámuel', 'Sándor', 'Sebestyén', 'Soma',
+                'Szabolcs', 'Szilárd', 'Szilveszter', 'Tamás', 'Tihamér', 'Tibor', 'Tivadar', 'Tódor', 'Tóbiás',
+                'Vencel', 'Viktor', 'Vilmos', 'Vince', 'Zalán', 'Zétény', 'Zoltán', 'Zsigmond', 'Zsolt']
+
+utca = ['Ady Endre utca', 'Akácfa utca', 'Alkotmány utca', 'Almássy tér', 'Andrássy út', 'Apáczai Csere János utca',
+        'Aradi utca', 'Árpád fejedelem útja', 'Attila utca', 'Bajcsy-Zsilinszky út', 'Balassi Bálint utca',
+        'Baross utca', 'Bartók Béla út', 'Batthyány tér', 'Bécsi utca', 'Belgrád rakpart', 'Bethlen Gábor tér',
+        'Béke tér', 'Bem rakpart', 'Benczúr utca', 'Bérkocsis utca', 'Berzsenyi utca', 'Bihari utca', 'Bocskai utca',
+        'Budaörsi út', 'Csarnok tér', 'Csévi utca', 'Csillaghegyi út', 'Csokonai utca', 'Csömöri utca', 'Dandár utca',
+        'Darázs utca', 'Deák Ferenc utca', 'Dózsa György út', 'Egyetem tér', 'Erzsébet körút', 'Erzsébet tér',
+        'Fadrusz utca', 'Fehérvári út', 'Kőrösi Csoma Sándor út', 'Ferenciek tere', 'Ferenc körút', 'Ferenc tér',
+        'Feszty Árpád utca', 'Fő utca', 'Frankel Leó utca', 'Gát utca', 'Gönczy Pál utca', 'Gyömrői út',
+        'Gyulai Pál utca', 'Hajós utca', 'Haller utca', 'Hársfa utca', 'Hattyú utca', 'Hegedűs Gyula utca',
+        'Hegyalja út', 'Heltai Jenő utca', 'Hercegprímás utca', 'Hidegkúti út', 'Hűvösvölgyi út', 'Jókai utca',
+        'József Attila utca', 'Kálmán Imre utca', 'Kálvin tér', 'Károly körút', 'Kassák Lajos utca',
+        'Katona József utca', 'Káva utca', 'Kazinczy utca', 'Király utca',  'Klauzál tér', 'Klauzál utca',
+        'Köztársaság tér', 'Kőbányai út', 'Közraktár utca', 'Klapka György út', 'Mártirok útja'
+        'Krisztina körút', 'Kun utca', 'Váci út', 'Lajos utca', 'Lánchíd utca', 'Üllői út']
+
+telepules = [
+    'Abony', 'Ajka', 'Albertirsa', 'Apagy', 'Aszód', 'Baja', 'Balassagyarmat', 'Balatonalmádi',
+    'Balatonfüred', 'Balmazújváros', 'Barcs', 'Bátaszék', 'Békéscsaba', 'Bélapátfalva', 'Berettyóújfalu',
+    'Biatorbágy', 'Bicske', 'Bóly', 'Budakeszi', 'Budaörs', 'Budapest', 'Cegléd', 'Celldömölk',
+    'Cigánd', 'Csongrád', 'Csorna', 'Csurgó', 'Dabas', 'Debrecen', 'Dég', 'Dombóvár', 'Dunaharaszti',
+    'Dunaújváros', 'Dunakeszi', 'Dunapataj', 'Dunaszentmiklós', 'Edelény', 'Eger', 'Encs', 'Érd',
+    'Érpatak', 'Esztár', 'Fehérgyarmat', 'Fót', 'Gárdony', 'Göd', 'Gödöllő', 'Gyál', 'Gyomaendrőd',
+    'Gyömrő', 'Gyöngyös', 'Gyula', 'Hajdúböszörmény', 'Hajdúhadház', 'Hajdúnánás', 'Halásztelek',
+    'Harkány', 'Hatvan', 'Hévíz', 'Hódmezővásárhely', 'Ibrány', 'Isaszeg', 'Jánossomorja', 'Jászberény',
+    'Kaba', 'Kalocsa', 'Kapuvár', 'Karcag', 'Kápolna', 'Kaposvár', 'Kapuvár', 'Karcag', 'Kazincbarcika',
+    'Kecel', 'Kecskemét', 'Kemecse', 'Keszthely', 'Kisbér', 'Kiskőrös', 'Kiskunfélegyháza',
+    'Kiskunhalas', 'Kiskunlacháza', 'Kisújszállás', 'Komárom', 'Komló', 'Környe', 'Kőszeg',
+    'Kunhegyes', 'Lábatlan', 'Lajosmizse', 'Lenti', 'Létavértes', 'Levél', 'Maglód', 'Magyarbánhegyes',
+    'Makó', 'Marcali', 'Martfű', 'Mátészalka', 'Mátraháza', 'Mátraverebély', 'Mátyásdomb',
+    'Medgyesegyháza', 'Mezőberény', 'Mezőcsát', 'Mezőkövesd', 'Mezőtúr',
+    'Miskolc', 'Mohács', 'Monor', 'Monorierdő', 'Mór', 'Mosonmagyaróvár', 'Múcsony',
+    'Nagyatád', 'Nagybajom', 'Nagybátony', 'Nagyecsed', 'Nagyhalász', 'Nagykálló', 'Nagykáta', 'Nagykőrös',
+    'Nagykovácsi', 'Nagymányok', 'Nagymaros', 'Nagymező', 'Nagyrábé', 'Nagytarcsa', 'Nemesgulács',
+    'Nemesvámos', 'Nógrád', 'Nyírbátor', 'Nyíregyháza', 'Ócsa', 'Orosháza', 'Oroszlány', 'Oroszi',
+    'Őrbottyán', 'Paks', 'Pálháza', 'Pápa', 'Pásztó', 'Pécs', 'Pécsvárad', 'Pellérd', 'Pétfürdő',
+    'Pilis', 'Pilisvörösvár', 'Polgár', 'Pomáz', 'Püspökladány', 'Putnok', 'Ráckeve', 'Rákoscsaba', 'Rátka',
+    'Recsk', 'Répcelak', 'Rétság', 'Rigács', 'Romhány', 'Rozsály', 'Ságvár', 'Sajószentpéter', 'Salgótarján',
+    'Sárbogárd', 'Sáregres', 'Sárisáp', 'Sárospatak', 'Sárvár', 'Sásd', 'Sátoraljaújhely', 'Sellye',
+    'Siófok', 'Solt', 'Somlóvásárhely', 'Sopron', 'Sümeg', 'Szabadbattyán', 'Szabadszállás', 'Szajol',
+    'Szakoly', 'Szakonyfalu', 'Szarvas', 'Szatmárcseke', 'Szatymaz', 'Szebény', 'Szekszárd', 'Szentendre',
+    'Szentgotthárd', 'Szentkirályszabadja', 'Szentlőrinckáta', 'Szentmártonkáta', 'Szenttamás', 'Szigetbecse',
+    'Szigethalom', 'Szigetszentmárton', 'Szigetszentmiklós', 'Szigetvár', 'Szilvásvárad', 'Szirák', 'Szolnok',
+    'Szombathely', 'Tab', 'Táborfalva', 'Tápiószecső', 'Tapolca', 'Tárnok']
+
+megye = ['Bács-Kiskun', 'Baranya', 'Békés', 'Borsod-Abaúj-Zemplén', 'Csongrád-Csanád', 'Fejér',
+         'Győr-Moson-Sopron', 'Hajdú-Bihar', 'Heves', 'Jász-Nagykun-Szolnok', 'Komárom-Esztergom',
+         'Nógrád', 'Pest', 'Somogy', 'Szabolcs-Szatmár-Bereg', 'Tolna', 'Vas', 'Veszprém', 'Zala']
+
+folyo = ['Duna', 'Tisza', 'Dráva', 'Rába', 'Marcal', 'Szamos', 'Körös', 'Zagyva', 'Sajó', 'Ipoly',
+         'Driszavica', 'Bodrog', 'Latorica', 'Zala', 'Hernád', 'Rábca', 'Lajta',
+         'Kis-Duna', 'Fekete-Körös', 'Kis-Sajó', 'Torna', 'Kraszna', 'Mura', 'Séd', 'Kapos', 'Galga',
+         'Cserna', 'Ivacs-patak', 'Bükkös-patak', 'Pilinka', 'Bodva', 'Hármas-Körös', 'Sajgó']
+
+nap = ['hétfő', 'kedd', 'szerda', 'csütörtök', 'péntek', 'szombat', 'vasárnap']
+
+honap = ['január', 'február', 'március', 'április', 'május', 'június', 'július',
+         'augusztus', 'szeptember', 'október', 'november', 'december']
+
+gyumolcs= ['alma', 'ananász',  'banán', 'barack', 'birs', 'cseresznye', 'datolya', 'dinnye', 'eper', 'füge',
+            'gránátalma', 'kajszibarack', 'kivi', 'málna', 'meggy', 'narancs', 'őszibarack', 'ribizli',
+            'sárgabarack', 'szeder', 'szilva', 'egres', 'vörösszőlő', 'szeder', 'körte', 'meggy', 'áfonya',
+           'sárgadinnye', 'erdei szamóca', 'mangó']
+zoldseg = ['articsóka', 'bab', 'borsó', 'brokkoli', 'burgonya', 'cékla', 'csicsóka', 'csiperkegomba', 'fokhagyma',
+           'hagyma', 'karalábé', 'karfiol', 'káposzta', 'kelbimbó', 'kukorica', 'fejes sajáta', 'lilahagyma',
+           'lilakáposzta', 'padlizsán', 'paprika', 'paszternák', 'retek', 'sárgarépa', 'spárga', 'spenót',
+           'sütőtök', 'tök', 'uborka', 'zeller', 'sárgarépa', 'feketeretek','zeller', 'kínai kel', 'retek',
+           'jégcsapretek', 'fokhagyma', 'újhagyma', 'szárzeller']
+
+
+
+kiraly = {'Árpád': (895, 907), 'Zoltán': (907, 947), 'Fajsz': (947, 955), 'Taksony': (955, 972), 'Géza': (972, 997),
+    'I.István': (997, 1038),'I. Péter': (1038, 1041), 'Sámuel': (1041, 1044), 'Péter-2': (1044, 1045), 'I.András': (1041, 1060),
+    'I. Béla': (1060, 1063), 'Salamon': (1063, 1074), 'I. Géza': (1074, 1077), 'I. László': (1077, 1095),
+    'Kálmán': (1095, 1116),'II. István': (1116, 1131), 'II. Béla': (1131, 1141), 'II. Géza': (1141,1162),
+    'III. István': (1162, 1172), 'III. Béla': (1172, 1196), 'Imre':(1196, 1204), 'III. László': (1204, 1205),
+    'II. András': (1205, 1235),'IV. Béla': (1235, 1270), 'V. István': (1270, 1272), 'IV. László': (1272, 1290),
+    'III. András': (1290, 1301), 'Vencel': (1301, 1305), 'Ottó': (1305, 1307), 'I. Károly': (1308, 1342),
+    'I.Lajos': (1342, 1382), 'Mária': (1382, 1385), 'II. Károly': (1385, 1386), 'Mária-2':(1386, 1395),
+    'Zsigmond' : (1387, 1437), 'Albert': (1437, 1439), 'V. László': (1440, 1457), 'I. Ulászló': (1440, 1444),
+    'I. Mátyás': (1458, 1490), 'II. Ulászló': (1490, 1516), 'II. Lajos': (1516, 1526), 'I.János': (1526, 1540),
+    'II. János' : (1540, 1571), 'I. Ferdinánd': (1526, 1564), 'Miksa': (1564, 1576), 'Rudolf': (1576, 1608),
+    'II. Mátyás': (1608, 1619), 'II. Ferdinánd': (1618, 1637), 'III. Ferdinánd': (1637,1657), 'IV. Ferdinánd': (1647,1654),
+    'I. Lipót' : (1657, 1705),  'I. József': (1705,1711), 'III. Károly': (1711,1740), 'Mária Terézia': (1740, 1780),
+    'II. József':(1780, 1790), 'II. Lipót': (1790, 1792), 'I. Ferenc': (1790, 1835), 'V. Ferdinánd' : (1835,1848),
+    'I. Ferenc József' : (1848,1916), 'IV. Károly':(1916,1918)
+}
