3247ee7e6b2ac603e98c1e600b2e6515ec2b6e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:26 +0000

    Add package apd/0.5.0/apd-0.5.0-py3-none-any.whl

---
+++ packages/apd/apd-0.5.0-py3-none-any.whl/apd/__init__.py
@@ -0,0 +1,29 @@
+###############################################################################
+# (c) Copyright 2021 CERN for the benefit of the LHCb Collaboration           #
+#                                                                             #
+# This software is distributed under the terms of the GNU General Public      #
+# Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   #
+#                                                                             #
+# In applying this licence, CERN does not waive the privileges and immunities #
+# granted to it by virtue of its status as an Intergovernmental Organization  #
+# or submit itself to any jurisdiction.                                       #
+###############################################################################
+__all__ = [
+    "AnalysisData",
+    "get_analysis_data",
+    "fetch_ap_info",
+    "load_ap_info_from_single_file",
+    "SampleCollection",
+    "cache_ap_info",
+    "auth",
+    "authw",
+]
+
+from .analysis_data import AnalysisData, get_analysis_data
+from .ap_info import (
+    SampleCollection,
+    cache_ap_info,
+    fetch_ap_info,
+    load_ap_info_from_single_file,
+)
+from .eos import auth, authw
