40df62ac72d44d687a43031091dbd438cdaf7ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:09 +0000

    Add package revchatgptauth/2023.4.16/revchatgptauth-2023.4.16-py3-none-any.whl

---
+++ packages/revchatgptauth/revchatgptauth-2023.4.16-py3-none-any.whl/revChatGPTAuth/openai_cookie_parser.py
@@ -0,0 +1,31 @@
+from http.cookiejar import Cookie
+from typing import Any, Optional
+
+from revChatGPTAuth.supported_browser import SupportedBrowser
+from revChatGPTAuth.utils import load_cookies
+
+
+class OpenAICookieParser:
+
+    SUPPORTED_BROWSERS: list[str] = [browser.value for browser in SupportedBrowser]
+
+    def __init__(self, browser_name: SupportedBrowser):
+        self.BROWSER_NAME = browser_name
+
+    def parse_cookie(self):
+        all_cookies = load_cookies(self.BROWSER_NAME)
+        openai_cookies = self._get_openai_cookies(all_cookies.__dict__)
+        openai_cookies_dict: dict[str, Optional[str]] = {key: cookie.value for key, cookie in openai_cookies.items()}
+        return openai_cookies_dict
+
+    def _get_openai_cookies(self, all_cookies_dict: dict[str, Any]):
+        try:
+            chat_openai_cookies: dict[str, Cookie] = all_cookies_dict['_cookies']['chat.openai.com']['/']
+            dot_chat_openai_cookies: dict[str, Cookie] = all_cookies_dict['_cookies']['.chat.openai.com']['/']
+            dot_openai_cookies: dict[str, Cookie] = all_cookies_dict['_cookies']['.openai.com']['/']
+            openai_cookies = {**chat_openai_cookies, **dot_chat_openai_cookies, **dot_openai_cookies}
+            return openai_cookies
+        except KeyError as error:
+            raise ValueError(
+                f'OpenAI cookies not found. Please log in to OpenAI using your browser {self.BROWSER_NAME} to proceed.'
+            ) from error
---
+++ packages/revchatgptauth/revchatgptauth-2023.4.16-py3-none-any.whl/revChatGPTAuth/test/test_chatgpt_access_token_parser.py
@@ -0,0 +1,54 @@
+from unittest.mock import MagicMock, patch
+
+from revChatGPTAuth.chatgpt_access_token_parser import ChatGPTAccessTokenParser, get_access_token
+from revChatGPTAuth.supported_browser import SupportedBrowser
+
+
+class TestChatGPTAccessTokenParser:
+
+    @patch('revChatGPTAuth.chatgpt_access_token_parser.OpenAICookieParser')
+    @patch('httpx.get')
+    def test_get_openai_chatgpt_access_token(
+        self,
+        mock_httpx_get: MagicMock,
+        mock_openai_cookie_parser: MagicMock,
+    ) -> None:
+        # given
+        ## mock cookies
+        mock_cookie_parser = MagicMock()
+        mock_cookie_parser.parse_cookie.return_value = {'cookie_key': 'cookie_value'}
+        mock_openai_cookie_parser.return_value = mock_cookie_parser
+
+        ## mock access token response
+        expected_access_token = 'access_token'
+        mock_response = MagicMock()
+        mock_response.json.return_value = {'accessToken': expected_access_token}
+        mock_httpx_get.return_value = mock_response
+
+        chatgpt_access_token_parser = ChatGPTAccessTokenParser(SupportedBrowser.CHROME)
+
+        # when
+        access_token = chatgpt_access_token_parser.get_access_token()
+
+        # then
+        mock_cookie_parser.parse_cookie.assert_called_once()
+        mock_httpx_get.assert_called_once_with(
+            'https://chat.openai.com/api/auth/session',
+            headers={'cookie': 'cookie_key=cookie_value'},
+        )
+        mock_response.raise_for_status.assert_called_once()
+        assert access_token == expected_access_token
+
+
+@patch('revChatGPTAuth.chatgpt_access_token_parser.ChatGPTAccessTokenParser')
+def test_get_access_token(mock_chatgpt_access_token_parser: MagicMock) -> None:
+    # given
+    expected_access_token = 'access_token'
+    mock_chatgpt_access_token_parser.return_value.get_access_token.return_value = expected_access_token
+
+    # when
+    access_token = get_access_token(SupportedBrowser.CHROME)
+
+    # then
+    mock_chatgpt_access_token_parser.assert_called_once_with(SupportedBrowser.CHROME)
+    assert access_token == expected_access_token
