0675ca306a45820b26da994c194d10c47daee901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:06 +0000

    Add package paralleltd/0.0.1/paralleltd-0.0.1.tar.gz

---
+++ packages/paralleltd/paralleltd-0.0.1.tar.gz/paralleltd-0.0.1/paralleltd/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2023/4/15 11:40
+# @Author   : Leslie Pan
+# @Site     : 
+# @File     : __init__.py.py
+# @Software : PyCharm
+
+from .Mps import Mps
---
+++ packages/paralleltd/paralleltd-0.0.1.tar.gz/paralleltd-0.0.1/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time     : 2023/4/15 11:37
+# @Author   : Leslie Pan
+# @Site     : 
+# @File     : setup.py
+# @Software : PyCharm
+
+from setuptools import setup
+
+# setup(
+#     name = 'paralleltd',
+#     version = '0.0',
+#     description = 'run your code parallely',
+#     packages = ['paralleltd'],
+#     author_email = 'pankyfk@163.com',
+#     zip_save = False
+# )
+
+if __name__ == '__main__':
+    setup()
