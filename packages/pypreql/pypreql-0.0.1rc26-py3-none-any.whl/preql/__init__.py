677eaea0c79fc42da924a0526c053ea94d85db42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:39 +0000

    Add package pypreql/0.0.1rc26/pypreql-0.0.1rc26-py3-none-any.whl

---
+++ packages/pypreql/pypreql-0.0.1rc26-py3-none-any.whl/preql/__init__.py
@@ -0,0 +1,7 @@
+from preql.core.models import Environment
+from preql.executor import Executor, Dialects
+from preql.parser import parse
+
+__version__ = "0.0.1-rc.26"
+
+__all__ = ["parse", "Executor", "Dialects", "Environment"]
