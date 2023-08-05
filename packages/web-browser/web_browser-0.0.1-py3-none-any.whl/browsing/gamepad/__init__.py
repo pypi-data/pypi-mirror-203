dcc1e91c5c39d75c99ca2067a31acb80673ec667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:14 +0000

    Add package web-browser/0.0.1/web_browser-0.0.1-py3-none-any.whl

---
+++ packages/web-browser/web_browser-0.0.1-py3-none-any.whl/browsing/gamepad/__init__.py
@@ -0,0 +1,2 @@
+from web_browser.gamepad.gamepad import Gamepad
+
---
+++ packages/web-browser/web_browser-0.0.1-py3-none-any.whl/browsing/main.py
@@ -0,0 +1,34 @@
+from web_browser.chrome import Chrome
+from web_browser import Driver
+import os
+from web_browser.gamepad import Gamepad
+
+
+def main(driver: Driver):
+    print("Navigating to url")
+    driver.get('https://gamepad-tester.com/')
+    print("waiting for page to load")
+    driver.pause(4)
+    print("starting gamepad")
+    gamepad = Gamepad(driver)
+    print("Finished initializing gamepad")
+    print("connecting to controller")
+    gamepad.connect_controller()
+    print("controller connected")
+    print("sending buttons")
+    gamepad.send_buttons('a', 'x', 'a', 'x', hold=5)
+
+
+if __name__ == '__main__':
+    c = Chrome(os.getenv('CHROME_DRIVER'))
+    c.change_window(pos_x=500, width=1300, height=1000)
+    c.add_extension('tests/extensions/Dark-Reader.crx')
+
+    try:
+        d = Driver(c)
+        main(d)
+        input("Press enter to quit")
+        c.quit()
+    except Exception as e:
+        c.quit()
+        raise e
