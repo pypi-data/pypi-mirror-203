2cc7e833134ef26187faec8bd060d7ed9c40688b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:41 +0000

    Add package abrain/1.0rc0.post3/abrain-1.0rc0.post3-cp310-cp310-win32.whl

---
+++ packages/abrain/abrain-1.0rc0.post3-cp310-cp310-win32.whl/abrain/_cpp/__init__.pyi
@@ -0,0 +1,14 @@
+"""Docstring main module"""
+from __future__ import annotations
+import _cpp
+import typing
+
+__all__ = [
+    "config",
+    "genotype",
+    "misc",
+    "phenotype"
+]
+
+
+__version__ = '1.0rc-post3'
---
+++ packages/abrain/abrain-1.0rc0.post3-cp310-cp310-win32.whl/abrain/_cpp/misc/__init__.pyi
@@ -0,0 +1,10 @@
+"""Docstring for phenotype submodule"""
+from __future__ import annotations
+import _cpp.misc
+import typing
+
+__all__ = [
+    
+]
+
+
