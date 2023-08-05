01c03d16a2c77673c382eacb1c0f768c099652a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:43 +0000

    Add package luau/0.2.5/luau-0.2.5.tar.gz

---
+++ packages/luau/luau-0.2.5.tar.gz/luau-0.2.5/src/luau/roblox/__init__.py
@@ -0,0 +1,30 @@
+import os
+from .tool import get_tool_name
+
+def format(path: str):
+	stylua_tool_name = get_tool_name("JohnnyMorganz/StyLua", "0.15.3")
+	os.system(f"{stylua_tool_name} {path}")
+
+def write_script(build_path: str, content: str, write_as_directory: bool=False):
+	dir_name, file_name = os.path.split(build_path)
+	if not os.path.exists(dir_name):
+		os.makedirs(dir_name)
+	elif os.path.exists(build_path):
+		os.remove(build_path)
+
+	if write_as_directory:
+		full_dir_path = build_path.split(".")[0]
+		os.makedirs(full_dir_path)
+		full_ext = ".".join(build_path.split(".")[1:])
+		init_file_path = dir_name+"/init."+full_ext
+		out_file = open(init_file_path, "w")
+		out_file.write(content)
+		out_file.close()
+		format(init_file_path)
+
+	else:
+		out_file = open(build_path, "w")
+		out_file.write(content)
+		out_file.close()
+		format(build_path)
+
