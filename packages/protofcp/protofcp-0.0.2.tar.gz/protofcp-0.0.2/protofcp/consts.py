27db1b3f2e28f83e779bc161b6f6bff12dcf3cdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:10 +0000

    Add package protofcp/0.0.2/protofcp-0.0.2.tar.gz

---
+++ packages/protofcp/protofcp-0.0.2.tar.gz/protofcp-0.0.2/protofcp/consts.py
@@ -0,0 +1,22 @@
+
+# FCP TAG
+
+FCP =       [
+            ord("f"),
+            ord("c"), 
+            ord("p")
+            ]
+
+# CHANNEL CONSTANTS
+
+class Channels:
+    CHANNEL_01 =      0x3e8
+    CHANNEL_02 =      0x3e9
+    CHANNEL_03 =      0x3ea
+    CHANNEL_04 =      0x3eb
+    CHANNEL_05 =      0x3ec
+    CHANNEL_06 =      0x3ed
+    CHANNEL_07 =      0x3ee
+    CHANNEL_08 =      0x3ef
+    CHANNEL_09 =      0x3f0
+    CHANNEL_10 =      0x3f1
