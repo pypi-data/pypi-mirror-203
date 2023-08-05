b489dd5a89263f43f9546cb99ccd4e57f933e61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:23 +0000

    Add package st-text-annotator/0.1.1/st_text_annotator-0.1.1.tar.gz

---
+++ packages/st-text-annotator/st_text_annotator-0.1.1.tar.gz/st_text_annotator-0.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="streamlit-text-annotator",
+    version="0.1.1",
+    author="Robin Marquet",
+    author_email="robin.marquet3@gmail.com",
+    description="Component for annotating text for NLP resolution",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rmarquet21/steamlit-text-annotator",
+    package_dir = {"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "streamlit >= 1.0.0",
+    ],
+)
