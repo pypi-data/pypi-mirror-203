ca3228d9f728b76afb590266c8fc6189cd8cdec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:49 +0000

    Add package drs-compliance-test/1.0.1/drs-compliance-test-1.0.1.tar.gz

---
+++ packages/drs-compliance-test/drs-compliance-test-1.0.1.tar.gz/drs-compliance-test-1.0.1/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+# from supported_drs_versions import SUPPORTED_DRS_VERSIONS
+
+SUPPORTED_DRS_VERSIONS = ["1.2.0"]
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="drs-compliance-test",
+    version="1.0.1",
+    author="Yash Puligundla",
+    author_email="yasasvini.puligundla@ga4gh.org",
+    packages=["compliance_suite"],
+    package_data={'compliance_suite': ['config/*', 'schemas/*', 'schemas/v1.2.0/*']},
+    description="A compliance utility reporting system for GA4GH DRS server implementations. "
+                "Supports GA4GH DRS versions - " + ",".join(SUPPORTED_DRS_VERSIONS),
+    long_description="test",
+    long_description_content_type="text/markdown",
+    url="https://github.com/ga4gh/drs-compliance-suite",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=['python-json-logger==2.0.4',
+                      'structlog==21.5.0',
+                      'requests==2.28.1',
+                      'Flask==2.2.0',
+                      'Flask-HTTPAuth==4.7.0',
+                      'pytest==7.1.2',
+                      'pytest-cov==3.0.0',
+                      'ga4gh-testbed-lib==0.2.0'],
+    entry_points='''
+        [console_scripts]
+        drs-compliance=compliance_suite.report_runner:main
+    '''
+
+)
