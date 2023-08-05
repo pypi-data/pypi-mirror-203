c17f848861a7ed43a3c12bededc61501afc654cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:26 +0000

    Add package tyba-cvxpy/1.4.2/tyba_cvxpy-1.4.2-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/tyba-cvxpy/tyba_cvxpy-1.4.2-cp37-cp37m-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.4.2'
+version = '1.4.2'
+full_version = '1.4.2.dev0+0.c5c536e'
+git_revision = 'c5c536e'
+commit_count = '0'
+release = False
+if not release:
+    version = full_version
