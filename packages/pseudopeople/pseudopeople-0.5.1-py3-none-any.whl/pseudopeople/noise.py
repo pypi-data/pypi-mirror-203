61e06a41e2757f0a147c88f4a1ba13c9b0046a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:33 +0000

    Add package pseudopeople/0.5.1/pseudopeople-0.5.1-py3-none-any.whl

---
+++ packages/pseudopeople/pseudopeople-0.5.1-py3-none-any.whl/pseudopeople/noise.py
@@ -0,0 +1,87 @@
+"""
+================
+   Noise Form
+================
+
+A function that will noise data based on a user's configuration.
+
+This function is the core of Pseudopeople, which takes in a pd.DataFrame, or form
+data and will add noise to both rows and columns based on the configured values
+provided by the user.  First, the Form will be noised for missing data and have
+rows in each columns changed to null values.  Then, the form data will be noised
+by column and row for each type of additional noise type.
+"""
+import pandas as pd
+from tqdm import tqdm
+from vivarium import ConfigTree
+
+from pseudopeople.configuration import Keys
+from pseudopeople.entity_types import ColumnNoiseType, RowNoiseType
+from pseudopeople.noise_entities import NOISE_TYPES
+from pseudopeople.schema_entities import Form
+from pseudopeople.utilities import get_randomness_stream
+
+
+def noise_form(
+    form: Form,
+    form_data: pd.DataFrame,
+    configuration: ConfigTree,
+    seed: int,
+) -> pd.DataFrame:
+    """
+    Adds noise to the input form data. Noise functions are executed in the order
+    defined by :py:const: `.NOISE_TYPES`. Row noise functions are applied to the
+    whole DataFrame. Column noise functions will be applied to each column that
+    is pertinent to it.
+
+    Noise levels are determined by the noise_config.
+    :param form:
+        Form needing to be noised
+    :param form_data:
+        Clean data input which needs to be noised.
+    :param configuration:
+        Object to configure noise levels
+    :param seed:
+        Seed for controlling randomness
+    :return:
+        Noised form data
+    """
+    randomness = get_randomness_stream(form.name, seed)
+
+    noise_configuration = configuration[form.name]
+    for noise_type in tqdm(NOISE_TYPES, desc="Applying noise", unit="type"):
+        if isinstance(noise_type, RowNoiseType):
+            if (
+                Keys.ROW_NOISE in noise_configuration
+                and noise_type.name in noise_configuration.row_noise
+            ):
+                # Apply row noise
+                form_data = noise_type(
+                    form_data,
+                    noise_configuration.row_noise[noise_type.name],
+                    randomness,
+                )
+
+        elif isinstance(noise_type, ColumnNoiseType):
+            if Keys.COLUMN_NOISE in noise_configuration:
+                columns_to_noise = [
+                    col
+                    for col in noise_configuration.column_noise
+                    if col in form_data.columns
+                    and noise_type.name in noise_configuration.column_noise[col]
+                ]
+                # Apply column noise to each column as appropriate
+                for column in columns_to_noise:
+                    form_data[column] = noise_type(
+                        form_data[column],
+                        noise_configuration.column_noise[column][noise_type.name],
+                        randomness,
+                        column,
+                    )
+        else:
+            raise TypeError(
+                f"Invalid noise type. Allowed types are {RowNoiseType} and "
+                f"{ColumnNoiseType}. Provided {type(noise_type)}."
+            )
+
+    return form_data
---
+++ packages/pseudopeople/pseudopeople-0.5.1-py3-none-any.whl/pseudopeople/noise_entities.py
@@ -0,0 +1,82 @@
+from typing import NamedTuple
+
+from pseudopeople import noise_functions, utilities
+from pseudopeople.configuration import Keys
+from pseudopeople.entity_types import ColumnNoiseType, RowNoiseType
+
+
+class __NoiseTypes(NamedTuple):
+    """Container for all noise types in the order in which they should be applied:
+    omissions, duplications, missing data, incorrect selection, copy from w/in
+    household, month and day swaps, zip code miswriting, age miswriting,
+    numeric miswriting, nicknames, fake names, phonetic, OCR, typographic
+
+    NOTE: Any configuration tree overwrites in these objects are what ends up
+    in the "baseline" ConfigTree layer.
+    """
+
+    omission: RowNoiseType = RowNoiseType("omission", noise_functions.omit_rows)
+    # duplication: RowNoiseType = RowNoiseType("duplication", noise_functions.duplicate_rows,)
+    missing_data: ColumnNoiseType = ColumnNoiseType(
+        "missing_data",
+        noise_functions.generate_missing_data,
+        token_noise_level=None,
+    )
+    incorrect_selection: ColumnNoiseType = ColumnNoiseType(
+        "incorrect_selection",
+        noise_functions.generate_incorrect_selections,
+        noise_level_scaling_function=utilities.noise_scaling_incorrect_selection,
+        token_noise_level=None,
+    )
+    # copy_from_within_household: ColumnNoiseType = ColumnNoiseType(
+    #     "copy_from_within_household",
+    #     noise_functions.generate_within_household_copies,
+    # )
+    # month_day_swap: ColumnNoiseType = ColumnNoiseType(
+    #     "month_day_swap",
+    #     noise_functions.swap_months_and_days,
+    # )
+    zipcode_miswriting: ColumnNoiseType = ColumnNoiseType(
+        "zipcode_miswriting",
+        noise_functions.miswrite_zipcodes,
+        token_noise_level=None,
+        additional_parameters={
+            "first_two_digits_noise_level": 0.04,
+            "middle_digit_noise_level": 0.20,
+            "last_two_digits_noise_level": 0.36,
+        },
+    )
+    age_miswriting: ColumnNoiseType = ColumnNoiseType(
+        "age_miswriting",
+        noise_functions.miswrite_ages,
+        token_noise_level=None,
+        additional_parameters={Keys.AGE_MISWRITING_PERTURBATIONS: {-1: 0.5, 1: 0.5}},
+    )
+    numeric_miswriting: ColumnNoiseType = ColumnNoiseType(
+        "numeric_miswriting",
+        noise_functions.miswrite_numerics,
+    )
+    # nickname: ColumnNoiseType = ColumnNoiseType(
+    #     "nickname",
+    #     noise_functions.generate_nicknames,
+    # )
+    fake_name: ColumnNoiseType = ColumnNoiseType(
+        "fake_name",
+        noise_functions.generate_fake_names,
+    )
+    # phonetic: ColumnNoiseType = ColumnNoiseType(
+    #     "phonetic",
+    #     noise_functions.generate_phonetic_errors,
+    # )
+    # ocr: ColumnNoiseType = ColumnNoiseType(
+    #     "ocr",
+    #     noise_functions.generate_ocr_errors,
+    # )
+    typographic: ColumnNoiseType = ColumnNoiseType(
+        "typographic",
+        noise_functions.generate_typographical_errors,
+        additional_parameters={"include_original_token_level": 0.1},
+    )
+
+
+NOISE_TYPES = __NoiseTypes()
