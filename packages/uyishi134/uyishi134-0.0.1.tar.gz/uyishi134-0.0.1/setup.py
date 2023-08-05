8686583a1ea1f45c485c538bf653902a58b6e250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:40 +0000

    Add package uyishi134/0.0.1/uyishi134-0.0.1.tar.gz

---
+++ packages/uyishi134/uyishi134-0.0.1.tar.gz/uyishi134-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='uyishi134',
+    version='0.0.1',
+    author='Sarvar',
+    author_email='sarvarsaydullayev185@gmail.com',
+    description='Bu kutubxona ishlatilmaydi'
+)
