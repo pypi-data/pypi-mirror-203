e5a3cc212413bfd23da454bdaecd9c83d9698c59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:10 +0000

    Add package planetaryguides/0.1/planetaryguides-0.1.tar.gz

---
+++ packages/planetaryguides/planetaryguides-0.1.tar.gz/planetaryguides-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='planetaryguides',
+      version='0.1',
+      description='Random Target Coordinates Generator for Remote Viewing Sessions.',
+      packages=find_packages(['planetaryguides']),
+      zip_safe=False)
