a1abfe91cc510c1d64e955ab31dbdad3ded52c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:03 +0000

    Add package dftt-timecode/0.0.12/dftt_timecode-0.0.12-py3-none-any.whl

---
+++ packages/dftt-timecode/dftt_timecode-0.0.12-py3-none-any.whl/dftt_timecode/error.py
@@ -0,0 +1,27 @@
+class DFTTError(Exception):
+    pass
+
+
+class DFTTTimecodeValueError(DFTTError):
+    pass
+
+
+class DFTTTimecodeTypeError(DFTTError):
+    pass
+
+class DFTTTimecodeOperatorError(DFTTError):
+    pass
+
+class DFTTTimeRangeMethodError(DFTTError):
+    pass
+
+class DFTTTimeRangeValueError(DFTTError):
+    pass
+
+class DFTTTimeRangeTypeError(DFTTError):
+    pass
+
+class DFTTTimeRangeFPSError(DFTTError):
+    pass
+
+
