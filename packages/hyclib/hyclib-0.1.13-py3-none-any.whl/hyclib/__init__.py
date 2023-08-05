924afdc1ededa4daf1fbc7ef7074324dae7c66ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:51 +0000

    Add package hyclib/0.1.13/hyclib-0.1.13-py3-none-any.whl

---
+++ packages/hyclib/hyclib-0.1.13-py3-none-any.whl/hyclib/__init__.py
@@ -0,0 +1,36 @@
+from .core import (
+    pprint,
+    config,
+    exceptions,
+    argparse,
+    logging,
+    warnings,
+    timeit,
+    clstools,
+    functools,
+    itertools,
+    configurable,
+    random,
+)
+    
+import importlib
+
+modules = [
+    '.np',
+    '.pt',
+    '.sp',
+    '.io',
+    '.npf',
+    '.pd',
+    '.plot',
+]
+
+for module in modules:
+    try:
+        importlib.import_module(module, package='hyclib')
+    except ImportError as err:
+        pass
+    
+del importlib, modules, module
+
+cfg = config.load_package_config('hyclib')
