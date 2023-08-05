f86ac02584642e1bf490d8cdcb1e8a102dcfe403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:00 +0000

    Add package Scopul/1.0.3/Scopul-1.0.3.tar.gz

---
+++ packages/Scopul/Scopul-1.0.3.tar.gz/Scopul-1.0.3/src/Scopul/scopul_exception.py
@@ -0,0 +1,18 @@
+class InvalidFileFormatError(Exception):
+    def __init__(self, value) -> None:
+        self.value = value
+
+
+class InvalidMusicElementError(Exception):
+    def __init__(self, value) -> None:
+        self.value = value
+
+
+class NoMusePathError(Exception):
+    def __init__(self, value) -> None:
+        self.value = value
+
+
+class MeasureNotFoundException(Exception):
+    def __init__(self, value) -> None:
+        self.value = value
