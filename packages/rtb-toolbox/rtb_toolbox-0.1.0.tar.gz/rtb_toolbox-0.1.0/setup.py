e0b1757dedf1db7e7fdecb020ec7782ee0648455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:53 +0000

    Add package rtb-toolbox/0.1.0/rtb_toolbox-0.1.0.tar.gz

---
+++ packages/rtb-toolbox/rtb_toolbox-0.1.0.tar.gz/rtb_toolbox-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['rtb_toolbox',
+ 'rtb_toolbox.forward_dynamics',
+ 'rtb_toolbox.forward_kinematics',
+ 'rtb_toolbox.frame',
+ 'rtb_toolbox.inverse_kinematics',
+ 'rtb_toolbox.link',
+ 'rtb_toolbox.trajectory',
+ 'rtb_toolbox.utils']
+
+package_data = \
+{'': ['*'], 'rtb_toolbox': ['images/*']}
+
+setup_kwargs = {
+    'name': 'rtb-toolbox',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Miguel',
+    'author_email': 'miguellukas52@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
