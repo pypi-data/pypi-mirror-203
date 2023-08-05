abddb649047d7f62dcd8c2abddad2059eb1fac30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:58 +0000

    Add package ENSF-338-final-project/1.3/ENSF%20338%20final%20project-1.3.tar.gz

---
+++ packages/ENSF-338-final-project/ENSF%20338%20final%20project-1.3.tar.gz/ENSF 338 final project-1.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name = 'ENSF 338 final project',
+      version = '1.3',
+      description = 'Final project package for ENSF 338',
+      author = 'Oluwafisayo Adabs, Okibe Abang',
+      author_email= 'okibzyabang@gmail.com, fizzyadabs@gmail.com',
+      packages = find_packages(),
+      install_requires = [],
+      )
