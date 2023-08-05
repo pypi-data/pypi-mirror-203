47e8db4e5c0e1fd74b0a69ff6f36f27374a54e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:08 +0000

    Add package sharewifi/1.0.4/sharewifi-1.0.4.tar.gz

---
+++ packages/sharewifi/sharewifi-1.0.4.tar.gz/sharewifi-1.0.4/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sharewifi',
+    version='1.0.4',
+    description='Generate QR codes for sharing WiFi network credentials',
+    url='https://github.com/4NK1T/sharewifi',
+    author='Ankit Pandey',
+    author_email='ashuankitpandey@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'sharewifi=sharewifi.sharewifi:generateQR'
+        ]
+    },
+    install_requires=[
+        'qrcode',
+        'click',
+        'cffi',
+        'pycparser',
+        'pynacl'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Utilities'
+    ],
+)
