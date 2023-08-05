15fb7e3027b9bbd09b6df067fc11bf698d44b0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:18 +0000

    Add package django-pipelines/0.1.1/django_pipelines-0.1.1-py3-none-any.whl

---
+++ packages/django-pipelines/django_pipelines-0.1.1-py3-none-any.whl/pipelines/reporters/__init__.py
@@ -0,0 +1,4 @@
+from .base import PipelineReporter
+
+
+__all__ = ["PipelineReporter"]
---
+++ packages/django-pipelines/django_pipelines-0.1.1-py3-none-any.whl/pipelines/tasks/__init__.py
@@ -0,0 +1,5 @@
+from .base import ModelTask, Task, TaskConfig
+from .registry import task_registry
+
+
+__all__ = ["Task", "ModelTask", "task_registry", "TaskConfig"]
