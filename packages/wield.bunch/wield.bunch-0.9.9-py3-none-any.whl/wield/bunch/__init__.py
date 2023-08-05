cfc3c608e68bed8eaf1a7a66804fa13a78f5a081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:11 +0000

    Add package wield.bunch/0.9.9/wield.bunch-0.9.9-py3-none-any.whl

---
+++ packages/wield.bunch/wield.bunch-0.9.9-py3-none-any.whl/wield/bunch/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: © 2021 Massachusetts Institute of Technology.
+# SPDX-FileCopyrightText: © 2021 Lee McCuller <mcculler@caltech.edu>
+# NOTICE: authors should document their contributions in concisely in NOTICE
+# with details inline in source files, comments, and docstrings.
+"""
+"""
+
+from ._version import version, __version__, version_info
+
+from .bunch import Bunch, FrozenBunch
+
+from .deep_bunch import DeepBunch
+
+
+__all__ = [
+    "Bunch",
+    "FrozenBunch",
+    "DeepBunch",
+    "version",
+    "__version__",
+    "version_info",
+]
