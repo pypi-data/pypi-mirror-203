643958b227cd9cd7ee832eeff06ef8d976311888
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:09 +0000

    Add package artemis-common/0.2.4b0/artemis-common-0.2.4b0.tar.gz

---
+++ packages/artemis-common/artemis-common-0.2.4b0.tar.gz/artemis-common-0.2.4b0/artemis_common/models/server/logs.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from fastapi import Request
+from fastapi import Response
+from pydantic import BaseModel
+
+
+class LoggingCenterModel(BaseModel):
+    name: str
+    host: str
+    logs_port: int
+    stats_port: int
+
+
+__all__ = [
+    LoggingCenterModel.__name__,
+]
