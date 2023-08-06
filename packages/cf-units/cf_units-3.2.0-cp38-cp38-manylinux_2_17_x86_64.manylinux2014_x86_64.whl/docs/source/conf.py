9f07e7c906e8a9238befab157412ea104c2a365f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:41 +0000

    Add package cf-units/3.2.0/cf_units-3.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cf-units/cf_units-3.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/docs/source/conf.py
@@ -0,0 +1,32 @@
+# Copyright cf-units contributors
+#
+# This file is part of cf-units and is released under the BSD license.
+# See LICENSE in the root of the repository for full licensing details.
+
+from importlib import metadata
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.doctest",
+    "sphinx.ext.viewcode",
+]
+
+templates_path = ["_templates"]
+source_suffix = ".rst"
+master_doc = "index"
+
+project = "cf-units"
+copyright = "Copyright cf-units contributors"
+
+current_version = metadata.version("cf_units")
+version = current_version.split("+")[0]
+release = current_version
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme = "alabaster"
+
+# Example configuration for intersphinx: refer to the Python standard library.
+intersphinx_mapping = {"http://docs.python.org/": None}
