6f517c687ca93e2ce4fb2fa947147475fae2592f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:15 +0000

    Add package AliveHelper/1.0/AliveHelper-1.0.tar.gz

---
+++ packages/AliveHelper/AliveHelper-1.0.tar.gz/AliveHelper-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AliveHelper',
+    version='1.0',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['*.*']
+    },
+    install_requires=[
+        'flask'
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_package_name=AliveHelper'
+        ]
+    },
+    python_requires='>=3.9'
+)
