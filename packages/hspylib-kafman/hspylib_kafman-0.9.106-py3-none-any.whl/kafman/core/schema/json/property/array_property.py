ade4d9d39a73b7cc9d518bb0dd8443c6b502f9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:11 +0000

    Add package hspylib-kafman/0.9.106/hspylib_kafman-0.9.106-py3-none-any.whl

---
+++ packages/hspylib-kafman/hspylib_kafman-0.9.106-py3-none-any.whl/kafman/core/schema/json/property/array_property.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HSPyLib-Kafman
+   @package: kafman.core.schema.json.property
+      @file: array_property.py
+   @created: Fri, 1 Jul 2022
+    @author: "<B>H</B>ugo <B>S</B>aporetti <B>J</B>unior")"
+      @site: "https://github.com/yorevs/hspylib")
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2022, HSPyLib team
+"""
+
+from typing import List
+
+from PyQt5.QtWidgets import QWidget
+
+from kafman.core.schema.json.json_type import JsonType
+from kafman.core.schema.schema_field import SchemaField
+from kafman.core.schema.widget_utils import WidgetUtils
+
+
+class ArrayProperty(SchemaField):
+    def __init__(self, name: str, description: str, a_items: List[str], required: bool = True):
+        super().__init__(name, description, JsonType.ARRAY, required=required)
+
+        self.a_items = a_items
+
+    def create_input_widget(self) -> QWidget:
+        self.widget = WidgetUtils.get_widget_type(self.a_type.value)()
+        return WidgetUtils.setup_list(self.widget, self.a_items, self.doc)
