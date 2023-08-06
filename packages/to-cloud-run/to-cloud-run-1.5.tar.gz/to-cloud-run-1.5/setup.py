adc24e0a34f93002e71fc40d3bfadf3e2d632ebd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:57 +0000

    Add package to-cloud-run/1.5/to-cloud-run-1.5.tar.gz

---
+++ packages/to-cloud-run/to-cloud-run-1.5.tar.gz/to-cloud-run-1.5/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name="to-cloud-run",
+    version="1.5",
+    packages=find_packages(),
+    py_modules=['to_cloud_run'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'to_cloud_run = to_cloud_run:main',
+        ],
+    },
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',)
