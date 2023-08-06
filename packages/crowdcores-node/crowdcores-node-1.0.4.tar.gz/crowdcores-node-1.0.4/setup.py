2b789b431d5ad24cea64948cca76a2b769f96e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:08 +0000

    Add package crowdcores-node/1.0.4/crowdcores-node-1.0.4.tar.gz

---
+++ packages/crowdcores-node/crowdcores-node-1.0.4.tar.gz/crowdcores-node-1.0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crowdcores-node',
+    version='1.0.4',
+    packages=find_packages(),
+    install_requires=[
+        'websockets',
+        'torch',
+        'transformers'
+    ],
+    entry_points={
+        'console_scripts': [
+            'crowdcores-node = crowdcores_node.node:main'
+        ]
+    }
+)
