8ca62a070ccde96ff79f1a31bd6529a55d2d14c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:39 +0000

    Add package docarray/0.30.0.dev118/docarray-0.30.0.dev118-py3-none-any.whl

---
+++ packages/docarray/docarray-0.30.0.dev118-py3-none-any.whl/docarray/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.30.0.dev118'
+
+import logging
+
+from docarray.array import DocList, DocVec
+from docarray.base_doc.doc import BaseDoc
+
+__all__ = ['BaseDoc', 'DocList', 'DocVec']
+
+logger = logging.getLogger('docarray')
+
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(levelname)s - %(name)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
