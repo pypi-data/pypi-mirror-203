e35e82c7d24e5d4c8a65e64a1f30141a6745e42e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:23 +0000

    Add package poectrl/0.4.1/poectrl-0.4.1-py3-none-any.whl

---
+++ packages/poectrl/poectrl-0.4.1-py3-none-any.whl/poectrl/main.py
@@ -0,0 +1,56 @@
+#! /bin/env python
+"""CLI program to Control PoE on supported routers."""
+
+import typer
+
+from .cliapp import CLIApp
+
+app = typer.Typer(add_completion=False, no_args_is_help=True)
+cliapp = CLIApp()
+
+
+@app.command()
+def apply(
+    profile_name: str = typer.Argument(
+        ..., help="Profile to apply", show_default=False
+    )
+):
+    """Call the apply function on the specified profile."""
+    cliapp.apply(profile_name)
+
+
+@app.command()
+def show(
+    profile_name: str = typer.Argument(
+        ..., help="Profile to view", show_default=False
+    )
+):
+    """Show information for the specified profile."""
+    cliapp.show(profile_name)
+
+
+@app.command()
+def list():
+    """List all profiles."""
+    cliapp.list()
+
+
+@app.command()
+def serve(
+    reload: bool = typer.Option(
+        False,
+        "--reload",
+        help="Reload the server on code changes",
+        show_default=False,
+    ),
+    port: int = typer.Option(8000, help="Port to listen on"),
+    host: bool = typer.Option(
+        False, "--host", help="Listen on all hosts", show_default=False
+    ),
+):
+    """Run a server to provide API access."""
+    cliapp.serve(reload, port, host)
+
+
+if __name__ == "__main__":
+    app()
