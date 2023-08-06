1dba702796bcb56966cd744c3625a54a22fe7749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:46 +0000

    Add package lexica-arts/0.1.1/lexica_arts-0.1.1.tar.gz

---
+++ packages/lexica-arts/lexica_arts-0.1.1.tar.gz/lexica_arts-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='lexica_arts',
+    version='0.1.1',
+    description='A module for generating image in Lexica.arts',
+    long_description=open('README.md').read(),     long_description_content_type='text/markdown',
+    py_modules=['lexica_arts'],
+)
+
