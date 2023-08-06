4d2675d28e7d4d8e0f1e5ac4ab5b535ed2b4e5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:33 +0000

    Add package ternel/0.0.1/ternel-0.0.1.tar.gz

---
+++ packages/ternel/ternel-0.0.1.tar.gz/ternel-0.0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name='ternel',
+    version='0.0.1',
+    author='terry-fei',
+    author_email='i@terryfei.com',
+    url='https://ternelgpt.com',
+    description=u'sdk for ternel',
+    packages=[],
+    install_requires=[]
+)
