01910721db3790020d88aa90e8a907be82835946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:26 +0000

    Add package bemserver-api/0.17.2/bemserver_api-0.17.2-py3-none-any.whl

---
+++ packages/bemserver-api/bemserver_api-0.17.2-py3-none-any.whl/bemserver_api/settings.py
@@ -0,0 +1,17 @@
+"""Default configuration"""
+
+
+class Config:
+    """Default configuration"""
+
+    # API parameters
+    API_TITLE = "BEMServer API"
+    OPENAPI_JSON_PATH = "api-spec.json"
+    OPENAPI_URL_PREFIX = "/"
+    OPENAPI_RAPIDOC_PATH = "/"
+    OPENAPI_RAPIDOC_URL = "https://cdn.jsdelivr.net/npm/rapidoc/dist/rapidoc-min.js"
+    OPENAPI_RAPIDOC_CONFIG = {
+        "theme": "dark",
+        "show-header": "false",
+        "render-style": "focused",
+    }
