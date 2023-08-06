eef723feaae442720de7a01a7f136370389b8626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:28 +0000

    Add package wenyue-first-package/0.1.0/wenyue_first_package-0.1.0.tar.gz

---
+++ packages/wenyue-first-package/wenyue_first_package-0.1.0.tar.gz/wenyue_first_package-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='wenyue_first_package',
+    version='0.1.0',
+    description='A simple Python package',
+    author='Wen Yue',
+    author_email='13263889722@163.com',
+    packages=['wenyue_first_package'],
+)
