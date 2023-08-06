bdd11f6f94873fb05e749bb8afcc9fcbd66068e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:28 +0000

    Add package y-class-files/0.1/y_class_files-0.1.tar.gz

---
+++ packages/y-class-files/y_class_files-0.1.tar.gz/y_class_files-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='y_class_files',
+      version='0.1',
+      description='Class for working with files',
+      packages=['y_class_files'],
+      author_email='nospex28@gmail.com',
+      zip_safe=False)
