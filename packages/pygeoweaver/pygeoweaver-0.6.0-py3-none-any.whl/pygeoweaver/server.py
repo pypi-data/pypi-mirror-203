2b7f4c839ca3fa5c3312796a5a7e2a6bd4d439c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:50 +0000

    Add package pygeoweaver/0.6.0/pygeoweaver-0.6.0-py3-none-any.whl

---
+++ packages/pygeoweaver/pygeoweaver-0.6.0-py3-none-any.whl/pygeoweaver/server.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+from pygeoweaver.utils import get_root_dir
+
+"""
+This module provides function to start and stop Geoweaver server.
+If it detects the current environment is Jupyter notebook, it will 
+open Geoweaver GUI in the output cell (if gui is not disabld.)
+
+"""
+
+def download_geoweaver():
+    """
+    Download Geoweaver to user home directory
+    """
+    pass
+
+
+def start():
+    print("start Geoweaver instance..")
+    result = subprocess.run(['./start.sh'], cwd=f"{get_root_dir()}/")
+    
+
+
+def stop():
+    print("stop Geoweaver instance..")
+    result = subprocess.run(['./stop.sh'], cwd=f"{get_root_dir()}/", shell=True)
+    
+
+
+
+
