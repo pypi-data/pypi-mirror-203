d3c5f1f33432f3f76b7c30eaaad6df4661f2ecee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:44 +0000

    Add package pandasflow/0.1.7/pandasflow-0.1.7.tar.gz

---
+++ packages/pandasflow/pandasflow-0.1.7.tar.gz/pandasflow-0.1.7/pandasflow/__init__.py
@@ -0,0 +1,14 @@
+
+
+from pandasflow.get_import import get_import
+from pandasflow.reset_mi import reset_mi
+from pandasflow.mean_error import mean_error
+
+from pandasflow.train_test_split import train_test_split_
+from pandasflow.train_valid_test_split import train_valid_test_split
+
+
+__version__ = '0.1.7'
+
+requirements = ['pandas', 'scikit-learn']
+
