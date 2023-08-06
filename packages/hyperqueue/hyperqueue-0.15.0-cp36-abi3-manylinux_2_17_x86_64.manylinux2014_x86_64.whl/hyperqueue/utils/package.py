5126191ee9e3fbfe56ceca8acea3e3faa77ef0e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:25:34 +0000

    Add package hyperqueue/0.15.0/hyperqueue-0.15.0-cp36-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/hyperqueue/hyperqueue-0.15.0-cp36-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/hyperqueue/utils/package.py
@@ -0,0 +1,9 @@
+class MissingPackageException(BaseException):
+    def __init__(self, package: str):
+        self.package = package
+
+    def __str__(self):
+        return (
+            f"Unable to import `{self.package}`. You have to install the "
+            f"`{self.package}` package."
+        )
