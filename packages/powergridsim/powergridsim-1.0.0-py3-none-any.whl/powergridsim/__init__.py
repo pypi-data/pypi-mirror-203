f3994fe8dc9e5009b7e043d6095aa3cd53f7ee3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:06 +0000

    Add package powergridsim/1.0.0/powergridsim-1.0.0-py3-none-any.whl

---
+++ packages/powergridsim/powergridsim-1.0.0-py3-none-any.whl/powergridsim/__init__.py
@@ -0,0 +1,9 @@
+"""
+powergridsim
+
+An python library of efficient simulating grid operations
+"""
+
+__version__ = "1.0.0"
+__author__ = 'Junying (Alice) Fang'
+__credits__ = 'Princeton University'
