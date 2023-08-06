75343c02678ae9fc2d0dc1b2d979c88fd202d1aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:46 +0000

    Add package umjunsik/1.2/umjunsik-1.2.tar.gz

---
+++ packages/umjunsik/umjunsik-1.2.tar.gz/umjunsik-1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="umjunsik",
+  version='1.2',
+  description='Umjunsik programming language',
+  url='https://github.com/rycont/umjunsik-lang',
+  author='rycont',
+  author_email='rycont@outlook.kr',
+  license='MIT',
+  keywords='umjunsik esolang',
+  python_requires='>=3',
+  entry_points={
+    'console_scripts': [
+      'umjunsik = umjunsik:main',
+    ],
+  },
+)
