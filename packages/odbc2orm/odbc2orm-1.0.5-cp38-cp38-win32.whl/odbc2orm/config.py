f1ea3dcc45b5d979b67d8a8b58fc4ac1504c7b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:46 +0000

    Add package odbc2orm/1.0.5/odbc2orm-1.0.5-cp38-cp38-win32.whl

---
+++ packages/odbc2orm/odbc2orm-1.0.5-cp38-cp38-win32.whl/odbc2orm/config.py
@@ -0,0 +1,15 @@
+DEFAULT_DRIVER = "Microsoft Access Driver (*.mdb, *.accdb)"
+
+
+CONFIG = {
+    "template": {
+        "leadin": None,
+        "table_leadin": None,
+        "table_column": None,
+        "table_leadout": None,
+        "leadout": None
+    },
+    "driver": DEFAULT_DRIVER,
+    "verbose": False,
+    "include_dump": False,
+}
