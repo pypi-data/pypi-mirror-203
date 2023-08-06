db8e4fe859dc96f760fddc065374d1e1eeb3f618
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:01 +0000

    Add package zhcode/0.0.4/zhcode-0.0.4.tar.gz

---
+++ packages/zhcode/zhcode-0.0.4.tar.gz/zhcode-0.0.4/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from pathlib import Path
+
+directory = Path(__file__).parent
+readme = (directory/"README.md").read_text()
+
+setuptools.setup(
+        name="zhcode",
+        version="0.0.4",
+        author="lnjng",
+        description="Programming languages in Chinese.",
+        license='MIT',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        url='https://github.com/Injng/zhcode',
+        packages=["zhcode", "zhcode.python"]
+        )
