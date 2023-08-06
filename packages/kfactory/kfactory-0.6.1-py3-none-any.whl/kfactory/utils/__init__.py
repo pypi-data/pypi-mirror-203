25b0b1948134ae8c38e5edbb4e3489fb870104aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:58 +0000

    Add package kfactory/0.6.1/kfactory-0.6.1-py3-none-any.whl

---
+++ packages/kfactory/kfactory-0.6.1-py3-none-any.whl/kfactory/utils/__init__.py
@@ -0,0 +1,27 @@
+"""Utilities to provide geometrical, fill and DRC violation help.
+
+:py:class:~`Enclosures can automatically generate slab and excludes based on minkowski
+sums instead of only vector based sizing.
+
+:py:func:~`fill_tiled` provides a filling algorithm that can use the
+:py:class:~`klayout.db.TilingProcessor` to calculate the regions to fill.
+
+:py:func:~`fix_spacing` uses a region space check to calculate areas that violate
+min space violations. :py:func:~`fix_spacing_tiled` to fix it using a TilingProcessor.
+"""
+
+from . import geo, violations
+from .geo import Direction, Enclosure, extrude_path, extrude_path_dynamic
+from .geo.fill import fill_tiled
+from .violations import fix_spacing_tiled
+
+__all__ = [
+    "Enclosure",
+    "violations",
+    "Direction",
+    "geo",
+    "extrude_path",
+    "extrude_path_dynamic",
+    "fill_tiled",
+    "fix_spacing_tiled",
+]
