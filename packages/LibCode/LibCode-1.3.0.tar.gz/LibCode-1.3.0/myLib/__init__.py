9f58872ca10a956f5b5df35d5ef9c7c3917cde88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:31 +0000

    Add package LibCode/1.3.0/LibCode-1.3.0.tar.gz

---
+++ packages/LibCode/LibCode-1.3.0.tar.gz/LibCode-1.3.0/myLib/__init__.py
@@ -0,0 +1,17 @@
+from .src.datastructures.linear import (
+    CDLL,
+    CSLL,
+    DLL,
+    QueueLL,
+    SLL,
+    StackLL,
+)
+from .src.datastructures.trees import (
+    AVL,
+    BST,
+)
+from .src.datastructures.nodes import (
+    DNode,
+    TNode,
+    SNode
+)
---
+++ packages/LibCode/LibCode-1.3.0.tar.gz/LibCode-1.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setup(
+    name="LibCode",
+    version="1.3.0",
+    description="A data structures and algorithms library implemented with Python and tested with Pytest!",
+    author="Axel Sanchez and  Mariia Podgaietska",
+    packages=[
+        "myLib",
+    ],
+    install_requires=[
+        "pytest",
+    ],
+    license="MIT",
+    url="https://github.com/Axeloooo/LibCode",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
