7777bae1ac53037a8b53feee6a70fa0893fd9664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:52 +0000

    Add package datadog-api-client/2.12.0/datadog-api-client-2.12.0.tar.gz

---
+++ packages/datadog-api-client/datadog-api-client-2.12.0.tar.gz/datadog-api-client-2.12.0/examples/v1/downtimes/UpdateDowntime.py
@@ -0,0 +1,23 @@
+"""
+Update a downtime returns "OK" response
+"""
+
+from os import environ
+from datadog_api_client import ApiClient, Configuration
+from datadog_api_client.v1.api.downtimes_api import DowntimesApi
+from datadog_api_client.v1.model.downtime import Downtime
+
+# there is a valid "downtime" in the system
+DOWNTIME_ID = environ["DOWNTIME_ID"]
+
+body = Downtime(
+    message="Example-Downtime-updated",
+    mute_first_recovery_notification=True,
+)
+
+configuration = Configuration()
+with ApiClient(configuration) as api_client:
+    api_instance = DowntimesApi(api_client)
+    response = api_instance.update_downtime(downtime_id=int(DOWNTIME_ID), body=body)
+
+    print(response)
