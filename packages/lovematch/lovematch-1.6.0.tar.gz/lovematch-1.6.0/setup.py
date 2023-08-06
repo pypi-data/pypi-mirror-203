72b90319166534c227f345e7993ae4a5b2c13e84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:11 +0000

    Add package lovematch/1.6.0/lovematch-1.6.0.tar.gz

---
+++ packages/lovematch/lovematch-1.6.0.tar.gz/lovematch-1.6.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.6.0'
+DESCRIPTION = 'Love Match Calculator'
+LONG_DESCRIPTION = 'A Python package to calculate love match percentage between two names.'
+
+# Setting up
+setup(
+    name="lovematch",
+    version=VERSION,
+    author="Devendra Parihar",
+    author_email="devendraparihar340@gmail.com",
+    url="https://github.com/Devparihar5/lovematch",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'love', 'match', 'percentage', 'calculator', 'lovematch'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
