a7eb04ca6d67e82414076153d21970deef323010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:21 +0000

    Add package xia-engine-terraform/0.0.2/xia_engine_terraform-0.0.2-cp39-none-manylinux1_x86_64.whl

---
+++ packages/xia-engine-terraform/xia_engine_terraform-0.0.2-cp39-none-manylinux1_x86_64.whl/xia_engine_terraform/__init__.py
@@ -0,0 +1,8 @@
+from xia_engine_terraform.engine import TerraformEngine, TerraformConnectParam, TerraformClient
+
+
+__all__ = [
+    "TerraformEngine", "TerraformConnectParam", "TerraformClient"
+]
+
+__version__ = "0.0.2"
