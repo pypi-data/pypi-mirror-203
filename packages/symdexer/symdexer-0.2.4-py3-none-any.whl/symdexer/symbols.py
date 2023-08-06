4b95ce6bb94a56a311f2d3f88ff91267e2038dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:29 +0000

    Add package symdexer/0.2.4/symdexer-0.2.4-py3-none-any.whl

---
+++ packages/symdexer/symdexer-0.2.4-py3-none-any.whl/symdexer/symbols.py
@@ -0,0 +1,43 @@
+import ast
+from pathlib import Path
+
+
+IMPORTS = "imports"
+DEFINES = "defines"
+ASSIGNS = "assigns"
+SYM_TYPES = [IMPORTS, DEFINES, ASSIGNS]
+
+
+def _imports(node: ast.AST):
+    for alias in node.names:
+        yield alias.name, IMPORTS
+
+
+def _defines(node: ast.AST):
+    yield node.name, DEFINES
+
+
+def _assigns(node: ast.AST):
+    for target in node.targets:
+        if isinstance(target, ast.Name):
+            yield target.id, ASSIGNS
+
+
+SYM_MAP = {
+    ast.Assign: _assigns,
+    ast.FunctionDef: _defines,
+    ast.ClassDef: _defines,
+    ast.Import: _imports,
+    ast.ImportFrom: _imports,
+}
+
+
+def iter_symbols(path: Path):
+    try:
+        root = ast.parse(path.read_text("utf-8"))
+    except SyntaxError:
+        return
+
+    for node in ast.iter_child_nodes(root):
+        if func := SYM_MAP.get(type(node)):
+            yield from func(node)
