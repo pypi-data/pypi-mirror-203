110d8dc68664fd5f5316431698dc673b88cdff8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:08 +0000

    Add package DPG/0.0.1/DPG-0.0.1.tar.gz

---
+++ packages/DPG/DPG-0.0.1.tar.gz/DPG-0.0.1/DPG/dp_test.py
@@ -0,0 +1,2 @@
+def test():
+    print("Hello 贝贝")
---
+++ packages/DPG/DPG-0.0.1.tar.gz/DPG-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="DPG",
+    version="0.0.1",
+    author="Michael",
+    author_email="",
+    description="A tool for you to generate data without personal information leak with DP technique.",
+    packages=setuptools.find_packages()
+)
