051fc69bec5b4c7859a03566245ae9e0cb19752b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:23 +0000

    Add package fiduswriter/3.11.13/fiduswriter-3.11.13-py3-none-any.whl

---
+++ packages/fiduswriter/fiduswriter-3.11.13-py3-none-any.whl/fiduswriter/website/static/js/modules/website/menu.js
@@ -0,0 +1,13 @@
+export class MenuWebsite {
+    constructor(menu) {
+        this.menu = menu
+    }
+
+    init() {
+        const documentsItem = this.menu.navItems.find(item => item.id === "documents")
+        if (!documentsItem) {
+            return
+        }
+        documentsItem.url = "/documents/"
+    }
+}
