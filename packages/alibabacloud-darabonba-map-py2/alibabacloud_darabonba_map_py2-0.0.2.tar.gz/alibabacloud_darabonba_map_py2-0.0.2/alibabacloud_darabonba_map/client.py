05563e16df7f6f45efdcac5b444664ab45ef42db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:28 +0000

    Add package alibabacloud-darabonba-map-py2/0.0.2/alibabacloud_darabonba_map_py2-0.0.2.tar.gz

---
+++ packages/alibabacloud-darabonba-map-py2/alibabacloud_darabonba_map_py2-0.0.2.tar.gz/alibabacloud_darabonba_map_py2-0.0.2/alibabacloud_darabonba_map/client.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# This file is auto-generated, don't edit it. Thanks.
+from typing import Dict, Any, List
+
+
+class Client:
+    """
+    This is a map module
+    """
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def size(raw):
+        return len(raw)
+
+    @staticmethod
+    def key_set(raw):
+        return raw.keys()
