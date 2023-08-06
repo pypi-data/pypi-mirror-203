ff0da19a10e2d9877da3a087f5f6aa0b92935553
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:10 +0000

    Add package auraboros/0.14.0a0/auraboros-0.14.0a0.tar.gz

---
+++ packages/auraboros/auraboros-0.14.0a0.tar.gz/auraboros-0.14.0a0/src/auraboros/global_.py
@@ -0,0 +1,29 @@
+"""
+Use to define global variables common use in the modules.
+"""
+import pygame
+
+pygame.init()
+
+
+screen: pygame.surface.Surface
+TARGET_FPS: int
+w_size_unscaled: tuple[int, int]
+w_size: tuple[int, int]
+
+
+def init(window_size=(960, 640), caption="", icon_filepath=None,
+         pixel_scale=2, set_mode_flags=0):
+    """This function initialize pygame and game engine.
+    Where to configure settings of game system is here."""
+    from . import global_
+    global_.TARGET_FPS = 60
+    pixel_scale = pixel_scale
+    global_.w_size_unscaled = window_size
+    global_.w_size = tuple([length // pixel_scale for length in window_size])
+    pygame.display.set_mode(global_.w_size_unscaled, set_mode_flags)
+    global_.screen = pygame.Surface(global_.w_size)
+    pygame.display.set_caption(caption)
+    if icon_filepath:
+        icon_surf = pygame.image.load(icon_filepath)
+        pygame.display.set_icon(icon_surf)
