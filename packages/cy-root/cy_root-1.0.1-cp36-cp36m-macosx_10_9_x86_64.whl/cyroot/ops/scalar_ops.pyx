50ec767bdb83308608e172d6eab39e642078e8b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:41 +0000

    Add package cy-root/1.0.1/cy_root-1.0.1-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/cy-root/cy_root-1.0.1-cp36-cp36m-macosx_10_9_x86_64.whl/cyroot/ops/scalar_ops.pyx
@@ -0,0 +1,15 @@
+# distutils: language=c++
+# cython: cdivision = True
+# cython: initializedcheck = False
+# cython: boundscheck = False
+# cython: profile = False
+
+from libc cimport math
+
+cdef inline bint isclose(double a, double b, double rtol=1e-5, double atol=1e-8) nogil:
+    if math.isinf(b) or math.isinf(a):
+        return math.isinf(a) and math.isinf(b)
+    return math.fabs(a - b) <= atol + rtol * math.fabs(b)
+
+cdef inline bint cisclose(double complex a, double complex b, double rtol=1e-5, double atol=1e-8) nogil:
+    return isclose(a.real, b.real, rtol, atol) and isclose(a.imag, b.imag, rtol, atol)
