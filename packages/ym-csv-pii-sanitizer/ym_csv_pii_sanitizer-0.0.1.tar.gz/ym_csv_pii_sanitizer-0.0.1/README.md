da649dc47dce60f13041f983edf0f260ab2bcabb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:29 +0000

    Add package ym-csv-pii-sanitizer/0.0.1/ym_csv_pii_sanitizer-0.0.1.tar.gz

---
+++ packages/ym-csv-pii-sanitizer/ym_csv_pii_sanitizer-0.0.1.tar.gz/ym_csv_pii_sanitizer-0.0.1/README.md
@@ -0,0 +1,53 @@
+# csv pii sanitizer
+
+## Installation
+
+### Step 1
+If you have git installed, clone this repository. If not then download this folder and unzip it.
+
+### Step 2
+Install python 3 if you have not already done so.
+
+### Step 3
+Create a Python virtual environment in the directory.
+While your terminal is in this directory run:
+
+Linux / macOS / WSL:
+```
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+On Windows (not WSL) run:
+```
+python -m venv .venv
+.venv\Scripts\activate.bat
+```
+
+### Step 4
+
+Install the dependencies. Run:
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run:
+```
+python3 main.py INPUT OUTPUT
+```
+INPUT - Path to csv file to sanitize.
+OUTPUT - Optional output path for result csv file.
+
+## Publishing to pypi
+
+Run to build:
+```
+python -m build
+```
+
+Run to publish:
+```
+twine upload dist/* -u __token__ -p TOKEN
+```
---
+++ packages/ym-csv-pii-sanitizer/ym_csv_pii_sanitizer-0.0.1.tar.gz/ym_csv_pii_sanitizer-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Redact a PHI (personal health information) data fron a CSV file.'
+LONG_DESCRIPTION = """## Installation\n\n```pip install ym_csv_pii_sanitizer```\n\n```python -m spacy download en_core_web_sm```\n\n## Usage\n\n```ym_csv_pii_sanitizer PATH_TO_CSV_FILE```"""
+
+setup(
+    name="ym_csv_pii_sanitizer",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author="Alex Ilin",
+    author_email="alex.ilin@ymeadows.com",
+    packages=find_packages(),
+    install_requires=[
+        'typer',
+        'spacy',
+        'phonenumbers'
+    ],
+    classifiers= [
+        "Programming Language :: Python :: 3"
+    ],
+    entry_points={
+        "console_scripts": ["ym_csv_pii_sanitizer = ym_csv_pii_sanitizer.main:run"],
+    },
+)
