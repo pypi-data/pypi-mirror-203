fc4d900e3302c6aab93b8494567ed4f2d6f2cb51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:22 +0000

    Add package nmodl-preprocessor/1.0.0/nmodl_preprocessor-1.0.0-py3-none-any.whl

---
+++ packages/nmodl-preprocessor/nmodl_preprocessor-1.0.0-py3-none-any.whl/nmodl_preprocessor/nmodl_to_python.py
@@ -0,0 +1,82 @@
+"""
+Adapted from the tutorial:
+https://bluebrain.github.io/nmodl/html/notebooks/nmodl-python-tutorial.html#Easy-code-generation-using-AST-visitors
+"""
+import textwrap
+import nmodl.dsl
+
+class VerbatimError(ValueError): pass
+
+class ComplexityError(ValueError): pass
+
+
+class PyGenerator(nmodl.dsl.visitor.AstVisitor):
+    def __init__(self):
+        super().__init__()
+        self.code_stack = []
+        self.pycode = ""
+
+    def push_block(self):
+        self.code_stack.append(self.pycode)
+        self.pycode = ""
+
+    def pop_block(self):
+        parent_block = self.code_stack.pop()
+        if parent_block.rstrip().endswith(':'):
+            self.pycode = textwrap.indent(self.pycode, '    ')
+        self.pycode = parent_block + self.pycode
+
+    def visit_statement_block(self, node):
+        self.push_block()
+        node.visit_children(self)
+        self.pop_block()
+
+    def visit_expression_statement(self, node):
+        node.visit_children(self)
+        self.pycode += "\n"
+
+    def visit_wrapped_expression(self, node):
+        self.pycode += '('
+        node.visit_children(self)
+        self.pycode += ')'
+
+    def visit_binary_expression(self, node):
+        lhs = node.lhs
+        rhs = node.rhs
+        op = node.op.eval()
+        if op == "^":
+            op = '**'
+        lhs.accept(self)
+        self.pycode += f" {op} "
+        rhs.accept(self)
+
+    def visit_var_name(self, node):
+        self.pycode += node.name.get_node_name()
+
+    def visit_integer(self, node):
+        self.pycode += nmodl.to_nmodl(node)
+
+    def visit_double(self, node):
+        self.pycode += nmodl.to_nmodl(node)
+
+    def visit_verbatim(self, node):
+        raise VerbatimError()
+
+    def visit_if_statement(self, node):
+        self.pycode += "if "
+        node.condition.accept(self)
+        self.pycode += ":\n"
+        node.statement_block.accept(self)
+        for elif_node in node.elseifs:
+            self.pycode += "elif "
+            elif_node.condition.accept(self)
+            self.pycode += ":\n"
+            elif_node.statement_block.accept(self)
+        if else_node := node.elses:
+            self.pycode += "else:\n"
+            else_node.statement_block.accept(self)
+
+    def visit_while_statement(self, node):
+        # Can not guarantee correct results BC the condition might reference unknown values.
+        raise ComplexityError()
+
