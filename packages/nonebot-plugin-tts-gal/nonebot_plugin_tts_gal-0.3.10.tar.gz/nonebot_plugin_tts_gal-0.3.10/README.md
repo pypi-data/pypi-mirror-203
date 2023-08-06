6023e3af5790da226f6f6f61c6fb3baf8ba52573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:06 +0000

    Add package nonebot-plugin-tts-gal/0.3.10/nonebot_plugin_tts_gal-0.3.10.tar.gz

---
+++ packages/nonebot-plugin-tts-gal/nonebot_plugin_tts_gal-0.3.10.tar.gz/nonebot_plugin_tts_gal-0.3.10/README.md
@@ -0,0 +1,259 @@
+
+<p align="center">
+  <a href="https://v2.nonebot.dev/"><img src="https://v2.nonebot.dev/logo.png" width="200" height="200" alt="nonebot"></a>
+</p>
+
+<div align="center">
+
+# nonebot-plugin-tts-gal
+
+_✨ [Nonebot2](https://github.com/nonebot/nonebot2) 基于nonebot2和vits的本地化角色语音合成插件 ✨_
+
+<p align="center">
+  <a href="https://github.com/dpm12345/nonebot_plugin_tts_gal/blob/master/LICENSE">
+    <img src="https://img.shields.io/github/license/dpm12345/nonebot_plugin_tts_gal.svg" alt="license">
+  </a>
+  <img src="https://img.shields.io/badge/python-3.8+-blue.svg" alt="Python">
+  <img src="https://img.shields.io/badge/nonebot-2.0.0b4+-red.svg" alt="NoneBot">
+  <a href="https://pypi.org/project/nonebot-plugin-tts-gal">
+    <img src="https://badgen.net/pypi/v/nonebot-plugin-tts-gal" alt="pypi">
+  </a>
+</p>
+
+</div>
+
+## 💬 前言
+
+平时因为学业问题，有时可能会回复得比较慢，请见谅。
+
+## 📖 介绍
+
+vits的nonebot的本地化插件，使用已训练好的模型，按照配置要求，使机器人发送文本对应的语音
+
+(**PS: 语音生成比较吃配置，生成过程花费时间可能会比较长，且大文本量会占用较大内存，过大会导致机器人进程 killed ，请留意**)
+
+## 💿 安装
+
+<details>
+<summary>nb-cli安装</summary>
+在 nonebot2 项目的根目录下打开命令行, 输入以下指令即可安装
+
+    nb plugin install nonebot-plugin-tts-gal
+
+</details>
+
+<details>
+<summary>pip安装</summary>
+
+    pip install nonebot_plugin_tts_gal
+
+</details>
+
+## 🍰 资源文件
+
+`data`文件夹中的`nonebot_plugin_tts_gal`会存储与插件有关的文件
+
+可以安装完插件后运行一次自动建立再退出
+
+也可以按照该页面的`data`文件夹进行手动建立
+
+具体的资源下载示例请查看[例子示例](https://github.com/dpm12345/nonebot_plugin_tts_gal/blob/master/Usage.md#%E4%BE%8B%E5%AD%90%E7%A4%BA%E4%BE%8B)
+
+## 💻 相关依赖
+
+<details>
+<summary>ffmpeg的安装</summary> 
+
+**Windows**
+
+在ffmpeg官网[下载](https://github.com/BtbN/FFmpeg-Builds/releases),选择对应的版本，下载后解压，并将位于`bin`目录添加到环境变量中
+
+其他具体细节可自行搜索
+
+**Linux**
+
+Ubuntu下
+
+```
+apt-get install ffmpeg
+```
+
+或者下载源码安装(具体可搜索相关教程)
+
+</details>
+
+## ⚙️ 配置
+
+| 配置项  | 必填 | 类型 | 默认值 |  说明  |
+| :-----: | :--: | :----: | :----: | :-----: |
+| tts_gal |  是  | Dict[Tuble[str],List[str]] | {():[""]} | 生成指定角色语音的关键配置，具体可见[tts_gal的配置要求](https://github.com/dpm12345/nonebot_plugin_tts_gal/blob/master/Usage.md#%E6%9C%BA%E5%99%A8%E4%BA%BA%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6%E8%A6%81%E6%B1%82) |
+| auto_delete_voice | 否 | Bool | True | 是否自动删除生成语音，`True`为是，`False`为否 |
+| decibel | 否 | int | -10 | 发送语音相对生成的分贝数<br>(原生成的音频音量可能比较大，因此通过此项来降低，负数为降，整数位升) |
+| tts_gal_is_at | 否 | Bool | True | 使用该功能是否需要@机器人 |
+| tts_gal_prefix | 否 | str | ""(空) | 使用该插件时的触发匹配前缀，减少冲突 |
+| tts_gal_priority | 否 | int | 3 | 该插件的优先级大小 |
+| tts_gal_tran_type | 否 | List[str] | ["youdao"] | 文本翻译使用项，默认为有道翻译("youdao")，可支持百度翻译("baidu")和腾讯翻译("tencent")，填写顺序为翻译调用优先级，若填写百度和腾讯翻译后需填写对应的配置项 |
+| baidu_tran_appid | 否 | str | ""(空) | 百度翻译接口对应的appid，可在[百度翻译控制台](https://fanyi-api.baidu.com/manage/developer)获取 |
+| baidu_tran_apikey | 否 | str | ""(空) | 百度翻译接口对应的apikey，可在[百度翻译控制台](https://fanyi-api.baidu.com/manage/developer)获取 |
+| tencent_tran_region | 否 | str | "ap-shanghai" | 腾讯翻译接口对应的地域，可在[地域列表](https://cloud.tencent.com/document/api/551/15615#.E5.9C.B0.E5.9F.9F.E5.88.97.E8.A1.A8)获取 |
+| tencent_tran_secretid | 否 | str | ""(空) | 腾讯翻译接口对应secretid，可在[云API密钥](https://console.cloud.tencent.com/capi)获取 |
+| tencent_tran_secretkey | 否 | str | ""(空) | 腾讯翻译接口对应secretkey，可在[云API密钥](https://console.cloud.tencent.com/capi)获取 |
+| tencent_tran_projectid | 否 | int | 0 | 腾讯翻译接口对应projectid，该项可以根据控制台-账号中心-项目管理中的配置填写，如无配置请填写默认项目ID:0或不填 |
+
+
+## 🎉 使用
+
+群聊和私聊仅有细微差别，其中下面语句中，`name`为合成语音的角色，`text`为转语音的文本内容(根据模型配置文件中的`language`会自动翻译为对应语言)，具体的相关功能解释可查看[这里](https://github.com/dpm12345/nonebot_plugin_tts_gal/blob/master/Usage.md#%E5%8A%9F%E8%83%BD%E8%A7%A3%E9%87%8A)
+
+| 指令 | 可使用者 |  说明   |
+| :---: | :----: | :-----: |
+| [name]说[text]<br>或<br>[name]发送[text] | 所有人 | 生成语音，<br>若配置了`tts_gal_is_at`为`true`和`tts_gal_prefix`不为空，<br>那么分别要@机器人和在`name`前添加配置的前缀，<br>如设置前缀`tts`，那么使用时为 tts[name]说[text] |
+| 禁用翻译 xxx | 超级用户(SUPERUSER) | 禁用名为xxx的翻译项，在启用前都不会使用xxx的翻译 |
+| 启用翻译 xxx | 超级用户(SUPERUSER) | 启用名为xxx的翻译项，将会使用xxx的翻译(按原来配置设置的优先级) |
+| 查看翻译 | 超级用户(SUPERUSER) | 查看插件目前调用的翻译项 |
+| 查看禁用翻译 | 超级用户(SUPERUSER) | 查看插件目前禁用的翻译项 |
+
+例如：
++ 宁宁说おはようございます.
++ 禁用翻译 baidu
++ 启用翻译 baidu
++ 查看翻译
++ 查看禁用翻译
+
+## ✏️ 今后
+
+添加更多的模型
+
+添加更多的翻译选项
+
+
+## ❔ Q&A
+
+<details>
+<summary>安装pyopenjtalk</summary> 
+
+如果出现如下错误
+
+<img src="./images/nonebot_plugin_tts_gal_3.jpg" alt="图3" style="zoom:80%;" />
+
+可能由于缺少cmake和MSVC造成的，需要在本机安装
+
+## Windows
+
+### 第一步 安装Visual Studio
+
+在Visual Studio的官网下载安装器,[VS2022点击此处下载](https://visualstudio.microsoft.com/zh-hans/free-developer-offers/)
+
+下载社区版，即`Visual Studio Community`
+
+### 第二步 下载相关工具
+
+下载后点击，进入如图所示先点击修改，然后选择如图所示的项目
+
+<img src="./images/nonebot_plugin_tts_gal_1.jpg" alt="图1" style="zoom:50%;" />
+
+<img src="./images/nonebot_plugin_tts_gal_2.jpg" alt="图2" style="zoom:50%;" />
+
+### 第三步 设置环境变量
+
+下载好后，在安装目录中找到cmake和MSVC的bin目录
+
+下面是参考目录,前面的目录即为之前强调的路径
+
+```
+E:\Program Files (x86)\Microsoft Visual Studio\2022\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin
+
+E:\Program Files (x86)\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.32.31326\bin\Hostx86\x64
+
+```
+
+将这两个目录添加到环境变量中
+
+这样，windows安装pyopenjtalk的前置依赖便解决了
+
+## Linux
+
+Linux如出现以下情况
+
+<img src="./images/nonebot_plugin_tts_gal_4.jpg" alt="图4" style="zoom:50%;" />
+
+原因为缺少cmake工具，可以使用apt命令下载安装
+
+```
+sudo apt install cmake
+```
+
+安装好后便可安装pyopenjtalk
+</details>
+
+</details>
+
+<details>
+<summary>无法生成语音</summary> 
+
+如果出现下载open_jtalk的错误信息，可以再次尝试，如果实在不行，可以使用`pip install openjtalk`
+
+</details>
+
+## 💡 鸣谢
+
++ 部分代码参考自[nonebot-plugin-petpet](https://github.com/noneplugin/nonebot-plugin-petpet)
++ **[CjangCjengh](https://github.com/CjangCjengh/)**：g2p转换，适用于日语调形标注的符号文件及分享的[柚子社多人模型](https://github.com/CjangCjengh/TTSModels)
++ **[luoyily](https://github.com/luoyily)**：分享的[ATRI模型](https://pan.baidu.com/s/1_vhOx50OE5R4bE02ZMe9GA?pwd=9jo4)
+
+## 📝 更新日志
+
+**2023.4.18 version 0.3.10：**
+
+修改部分
+
+**2023.4.9 version 0.3.9：**
+
+支持添加生成symbols的代码，以便添加更多的symbols,详见[Usage.md](https://github.com/dpm12345/nonebot_plugin_tts_gal/blob/master/Usage.md#%E5%85%B3%E4%BA%8Esymbols)
+
+fix bug [#39](https://github.com/dpm12345/nonebot_plugin_tts_gal/issues/39)
+
+**2023.1.12 version 0.3.8：**
+
+修复regex的匹配问题
+
+**2023.1.11 version 0.3.7：**
+
+支持百度翻译和腾讯翻译的api使用，增加是否需要@机器人、自定义前缀、自定义插件priority等配置
+
+**2022.12.9 version 0.3.3：**
+
+自动读取已加载的角色模型，可通过[PicMenu插件](https://github.com/hamo-reid/nonebot_plugin_PicMenu)进行显示可使用的角色;对代码进行相关优化
+
+**2022.10.27 version 0.3.2：**
+
+修改正则表达式，避免文本出现"说/发送"而造成name的匹配错误
+
+**2022.10.21 version 0.3.1：**
+
+修复对配置项auto_delete_voice的判断bug
+
+**2022.10.19 version 0.3.0：**
+
+支持添加中文模型，优化相关代码，增添更多提示
+
+**2022.10.7 version 0.2.3:**
+
+适配nonebot2-rc1版本，并添加部分报错信息提醒
+
+**2022.9.28 version 0.2.2:**
+
+添加中文逗号替换成英文逗号
+
+**version 0.2.1:**
+
+将pyopenjtalk依赖更新为0.3.0，使python3.10也能使用
+
+**2022.9.25 version 0.2.0:**
+
+优化修改代码逻辑，支持自行添加vits模型，简单修复了一下有道翻译的翻译问题，启动时自动检测所需文件是否缺失
+
+**2022.9.21 version 0.1.1:**
+
+修改依赖
+
---
+++ packages/nonebot-plugin-tts-gal/nonebot_plugin_tts_gal-0.3.10.tar.gz/nonebot_plugin_tts_gal-0.3.10/nonebot_plugin_tts_gal/__init__.py
@@ -0,0 +1,199 @@
+from nonebot import on_message, get_driver
+from nonebot.plugin import PluginMetadata
+from nonebot.adapters.onebot.v11.message import MessageSegment
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent
+from nonebot.exception import ActionFailed, NetworkError
+from nonebot.log import logger
+from nonebot.permission import SUPERUSER
+from nonebot import require
+require("nonebot_plugin_apscheduler")
+from nonebot_plugin_apscheduler import scheduler
+
+from pathlib import Path
+import hashlib
+import random
+import re
+import string
+from torch import no_grad, LongTensor
+from scipy.io.wavfile import write
+import asyncio
+import traceback
+
+from .depends import *
+from .initial import *
+from .config import *
+from .utils import *
+from .models import SynthesizerTrn
+from .function import *
+
+
+__plugin_meta__ = PluginMetadata(
+    name="vits角色语音合成本地化",
+    description="基于nonebot2和vits的本地化角色语音合成插件",
+    usage=f"触发方式：{trigger_rule}[角色名][发送|说][文本内容]\n"+
+        "※超级用户管理(若设置前缀，以下均加上前缀)\n"+
+        f"   {trigger_rule}[禁用翻译 xxx]   禁用xxx翻译项\n"+
+        f"   {trigger_rule}[启用翻译 xxx]   启用xxx翻译项\n"+
+        f"   {trigger_rule}[查看翻译]         查看目前可用的翻译项\n"+
+        f"   {trigger_rule}[查看禁用翻译]  查看已被禁用的翻译项",
+    extra={
+        "example": f"{trigger_rule} 宁宁说おはようございます.",
+        "author": "dpm12345 <1006975692@qq.com>",
+        "version": "0.3.10",
+    },
+)
+
+
+
+
+auto_delete_voice = tts_gal_config.auto_delete_voice
+tts_gal = load_tts_gal(tts_gal_config.tts_gal)
+tran_type = tts_gal_config.tts_gal_tran_type
+prefix = tts_gal_config.tts_gal_prefix
+priority = tts_gal_config.tts_gal_priority
+driver = get_driver()
+__valid_names__ = []
+lock_tran_list = {
+    "auto":[],
+    "manual":[]
+}
+
+
+@driver.on_startup
+async def _():
+    logger.info("正在检查目录是否存在...")
+    asyncio.ensure_future(checkDir(data_path, base_path, voice_path, model_path, config_path))
+    filenames = []
+    [filenames.append(model[0])
+     for model in tts_gal.values() if not model[0] in filenames]
+    logger.info("正在检查配置文件是否存在...")
+    asyncio.ensure_future(checkFile(model_path, config_path, filenames, tts_gal, __plugin_meta__, __valid_names__))
+    logger.info("正在检查配置项...")
+    asyncio.ensure_future(checkEnv(tts_gal_config, tran_type))
+
+
+voice = on_message(
+    regex(rf"(?:{prefix} *)(?P<name>.+?)(?:说|发送)(?P<text>.+?)$"), block=False, priority=priority)
+lock_tran = on_message(regex(rf"(?:{prefix} *)禁用翻译(?: *)(?P<tran>.+)$"), permission=SUPERUSER,block=False,priority=priority)
+unlock_tran = on_message(regex(rf"(?:{prefix} *)启用翻译(?: *)(?P<tran>.+)$"), permission=SUPERUSER,block=False,priority=priority)
+show_trans = on_message(regex(rf"(?:{prefix} *)查看翻译(?: *)$"), permission=SUPERUSER,block=False,priority=priority)
+show_lock_trans = on_message(regex(rf"(?:{prefix} *)查看禁用翻译(?: *)$"), permission=SUPERUSER,block=False,priority=priority)
+
+@voice.handle()
+async def voicHandler(
+    bot: Bot, event: MessageEvent,
+    name: str = RegexArg("name"),
+    text: str = RegexArg("text")
+):
+    # 预处理
+    config_file, model_file, index = check_character(name, __valid_names__, tts_gal)
+    if config_file == "":
+        await voice.finish(MessageSegment.at(event.get_user_id()) + "暂时还未有该角色")
+
+    first_name = "".join(random.sample([x for x in string.ascii_letters + string.digits], 8))
+    filename = hashlib.md5(first_name.encode()).hexdigest() + ".mp3"
+    # 加载配置文件
+    hps_ms = get_hparams_from_file(config_path / config_file)
+
+    # 翻译的目标语言
+    lang = load_language(hps_ms)
+    symbols = load_symbols(hps_ms, lang)
+    if not symbols:
+        await voice.finish("symbols项配置错误")
+
+    # 文本处理
+    text = changeE2C(text) if lang == "zh-CHS" else changeC2E(text)
+    text = await translate(tran_type, lock_tran_list, text, lang)
+    if not text:
+        await voice.finish("翻译文本时出错,请查看日志获取细节")
+    text = get_text(text, hps_ms, symbols, lang, False)
+
+    try:
+        logger.info("加载模型中...")
+        net_g_ms = SynthesizerTrn(
+            len(symbols),
+            hps_ms.data.filter_length // 2 + 1,
+            hps_ms.train.segment_size // hps_ms.data.hop_length,
+            n_speakers=hps_ms.data.n_speakers,
+            **hps_ms.model)
+        _ = net_g_ms.eval()
+        load_checkpoint(model_path / model_file, net_g_ms)
+    except:
+        traceback.print_exc()
+        await voice.finish("加载模型失败")
+
+    try:
+        logger.info("正在生成中...")
+        with no_grad():
+            x_tst = text.unsqueeze(0)
+            x_tst_lengths = LongTensor([text.size(0)])
+            sid = LongTensor([index]) if not index == None else None
+            audio = net_g_ms.infer(x_tst, x_tst_lengths, sid=sid, noise_scale=.667,
+                                   noise_scale_w=0.8, length_scale=1)[0][0, 0].data.cpu().float().numpy()
+        write(voice_path / filename, hps_ms.data.sampling_rate, audio)
+        new_voice = Path(change_by_decibel(voice_path / filename, voice_path, tts_gal_config.decibel))
+    except:
+        traceback.print_exc()
+        await voice.finish('生成失败')
+
+    try:
+        await voice.send(MessageSegment.record(file=new_voice))
+    except ActionFailed:
+        traceback.print_exc()
+        await voice.send("发送失败,请重试")
+    except NetworkError:
+        traceback.print_exc()
+        await voice.send("发送超时,也许等等就好了")
+    finally:
+        if auto_delete_voice:
+            os.remove(new_voice)
+
+@lock_tran.handle()
+async def _(tran: str = RegexArg("tran")):
+    if tran == "youdao":
+        await lock_tran.send(f"该翻译项禁止禁用!")
+    elif tran in tran_type:
+        if tran not in lock_tran_list["manual"]:
+            lock_tran_list["manual"].append(tran)
+        logger.info(f"禁用成功")
+        await lock_tran.send(f"禁用成功")
+    else:
+        await lock_tran.send(f"未有{tran}翻译项")
+
+@unlock_tran.handle()
+async def _(tran: str = RegexArg("tran")):
+    if tran in lock_tran_list["auto"] or tran in lock_tran_list["manual"]:
+        if tran in lock_tran_list["auto"]:
+            lock_tran_list["auto"].remove(tran)
+        if tran in lock_tran_list["manual"]:
+            lock_tran_list["manual"].remove(tran)
+        logger.info(f"启用成功")
+        await lock_tran.send(f"启用成功")
+    else:
+        await lock_tran.send(f"{tran}翻译项未被禁用")
+
+
+
+@show_trans.handle()
+async def _():
+    present_trans = [tran for tran in tran_type if tran not in lock_tran_list["auto"] 
+                    and tran not in lock_tran_list["manual"]]
+    await show_trans.send(f"目前支持的翻译:{','.join(present_trans)}")
+
+@show_lock_trans.handle()
+async def _():
+    lock_list = lock_tran_list["auto"].copy()
+    for tran in lock_tran_list["manual"]:
+        if tran not in lock_list:
+            lock_list.append(tran)
+    if len(lock_list) == 0:
+        await show_lock_trans.send("目前没有翻译项被禁用")
+    else:
+        await show_lock_trans.send(f"目前被禁用项{','.join(lock_list)}")
+
+# 每月重置自动禁用的翻译项
+@scheduler.scheduled_job("cron", day=1, minute=5, misfire_grace_time=60)
+async def reset_tran():
+    lock_tran_list["auto"].clear()
+    valid_tran_type = [tran for tran in tran_type if tran not in lock_tran_list["manual"]]
+    logger.info(f"目前可用翻译:{','.join(valid_tran_type)}")
