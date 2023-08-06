7f5c5c8b31d6d27fbfd448ffb82a8a5a64549fdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:20 +0000

    Add package Agentik/0.0.1/agentik-0.0.1.tar.gz

---
+++ packages/Agentik/agentik-0.0.1.tar.gz/agentik-0.0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="agentik", version="0.0.1", py_modules=["agentik"], author="me", author_email="me@me.com", description="This is a placeholder package created by registerit.", url="http://www.github.com")
