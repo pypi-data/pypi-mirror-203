9defd3eff769b58706660f2f0bdb80a5b1ab5418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:21 +0000

    Add package torch-snippets/0.499.26/torch_snippets-0.499.26-py3-none-any.whl

---
+++ packages/torch-snippets/torch_snippets-0.499.26-py3-none-any.whl/torch_snippets/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.499.26"
+from .loader import *
+from .paths import *
+from .markup import *
+from .inspector import *
+from .load_defaults import *
+from .pdf_loader import PDF
+
+try:
+    from .torch_loader import *
+except Exception as e:
+    ...
