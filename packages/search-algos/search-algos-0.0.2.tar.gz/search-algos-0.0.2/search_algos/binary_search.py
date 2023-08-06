821bcc583527f1d2b660b576d9ed8ea83f04d75a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:41 +0000

    Add package search-algos/0.0.2/search-algos-0.0.2.tar.gz

---
+++ packages/search-algos/search-algos-0.0.2.tar.gz/search-algos-0.0.2/search_algos/binary_search.py
@@ -0,0 +1,12 @@
+def binary_search(lst, target):
+    low = 0
+    high = len(lst) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if lst[mid] == target:
+            return mid
+        elif lst[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
