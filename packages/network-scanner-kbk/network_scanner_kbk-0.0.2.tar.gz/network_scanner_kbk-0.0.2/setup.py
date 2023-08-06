5abe50b5e8a788064709934a074f20142e12c6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:11 +0000

    Add package network-scanner-kbk/0.0.2/network_scanner_kbk-0.0.2.tar.gz

---
+++ packages/network-scanner-kbk/network_scanner_kbk-0.0.2.tar.gz/network_scanner_kbk-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='network_scanner_kbk',
+      version='0.0.2',
+      description='Project to scan and gather connectivity information on small networks',
+      author='Brian Kulig',
+      author_email='kbriankuig@gmail.com',
+      url='https://github.com/kbriankulig/network-scanner',
+      packages=find_packages(),
+     )
