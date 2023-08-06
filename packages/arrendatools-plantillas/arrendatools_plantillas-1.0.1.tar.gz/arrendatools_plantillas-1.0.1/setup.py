dd9608b24f60f6fb84e233b69c11b443cd587e32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:30 +0000

    Add package arrendatools-plantillas/1.0.1/arrendatools_plantillas-1.0.1.tar.gz

---
+++ packages/arrendatools-plantillas/arrendatools_plantillas-1.0.1.tar.gz/arrendatools_plantillas-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+__version__ = "1.0.1"
+
+setup(
+    name='arrendatools_plantillas',
+    version=__version__,
+    description='Módulo de Python que aplica plantillas usando jinja 2. Además incluye algunos filtros adicionales como el convertir un número a palabras.',
+    url='https://github.com/hokus15/ArrendaToolsPlantillas',
+    author='hokus15',
+    author_email='hokus@hotmail.com',
+    license='MIT',
+    packages=['arrendatools_plantillas', 'arrendatools_plantillas.filters'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
