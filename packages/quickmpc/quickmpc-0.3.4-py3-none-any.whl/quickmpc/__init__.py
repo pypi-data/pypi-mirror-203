ce293272ac66ab15c27b2dd9146d03f9fd417c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:35 +0000

    Add package quickmpc/0.3.4/quickmpc-0.3.4-py3-none-any.whl

---
+++ packages/quickmpc/quickmpc-0.3.4-py3-none-any.whl/quickmpc/__init__.py
@@ -0,0 +1,14 @@
+from quickmpc.qmpc import (
+    QMPC, JobStatus, JobErrorInfo,
+    Schema, ShareValueTypeEnum
+)
+from .version import __version__
+
+__all__ = [
+    "QMPC",
+    "JobStatus",
+    "JobErrorInfo",
+    "Schema",
+    "ShareValueTypeEnum",
+    "__version__",
+]
