627c027f6c3d344304bd132dac942a24b3268337
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:48 +0000

    Add package strongmind-clever-client/1.0.1/strongmind-clever_client-1.0.1.tar.gz

---
+++ packages/strongmind-clever-client/strongmind-clever_client-1.0.1.tar.gz/strongmind-clever_client-1.0.1/clever_client/api/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from clever_client.api.courses_api import CoursesApi
+from clever_client.api.districts_api import DistrictsApi
+from clever_client.api.events_api import EventsApi
+from clever_client.api.resources_api import ResourcesApi
+from clever_client.api.schools_api import SchoolsApi
+from clever_client.api.sections_api import SectionsApi
+from clever_client.api.terms_api import TermsApi
+from clever_client.api.users_api import UsersApi
