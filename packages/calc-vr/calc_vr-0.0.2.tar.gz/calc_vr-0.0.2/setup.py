7386fed2f170fb59e5754988c0a9d07ce8bf4097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:27 +0000

    Add package calc-vr/0.0.2/calc_vr-0.0.2.tar.gz

---
+++ packages/calc-vr/calc_vr-0.0.2.tar.gz/calc_vr-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="calc_vr",
+    version="0.0.2",
+    author="ramachandran",
+    author_email="rama5864@gmail.com",
+    description="a simple arithmetic package",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    url="https://github.com/rmvr/calc_vr",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>3.6'
+)
+
+    
