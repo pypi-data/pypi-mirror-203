5eabbac16b8c3dcabc063c06b7f46fe29ac71086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:05 +0000

    Add package class-file-Nikolay/0.3/class_file_Nikolay-0.3.tar.gz

---
+++ packages/class-file-Nikolay/class_file_Nikolay-0.3.tar.gz/class_file_Nikolay-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='class_file_Nikolay',
+      version='0.3',
+      description='удобный класс дял работы с файлом',
+      packages=['class_file_Nikolay'],
+      author_email='nikolajh913@gmail.com',
+      zip_safe=False)
+
+
+# Nikolay_1
+# 147Nikolay1996
