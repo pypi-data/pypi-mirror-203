10444a28eceb2d25c775c2162ceebaf9035a025a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:17 +0000

    Add package gamfra-test-probability/0.1/gamfra_test_probability-0.1.tar.gz

---
+++ packages/gamfra-test-probability/gamfra_test_probability-0.1.tar.gz/gamfra_test_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gamfra_test_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gamfra_test_probability'],
+      author = 'Francis Gambini',
+      author_email = 'francisgambini@outlook.com',
+      zip_safe=False)
