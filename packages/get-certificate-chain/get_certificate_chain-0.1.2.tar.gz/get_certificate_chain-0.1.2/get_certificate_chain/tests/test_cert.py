ff4a3adbb45e3347486651ea211324376862d964
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:41 +0000

    Add package get-certificate-chain/0.1.2/get_certificate_chain-0.1.2.tar.gz

---
+++ packages/get-certificate-chain/get_certificate_chain-0.1.2.tar.gz/get_certificate_chain-0.1.2/get_certificate_chain/tests/test_cert.py
@@ -0,0 +1,55 @@
+import os
+import pytest
+from main import (
+    SSLCertificateChainDownloader,
+)
+
+
+@pytest.fixture
+def root_ca_cert():
+    with open(
+        os.path.join(os.path.dirname(__file__), "test_data/root_ca_cert.pem"), "r"
+    ) as f:
+        cert = f.read()
+    return cert
+
+
+@pytest.fixture
+def server_cert():
+    with open(
+        os.path.join(os.path.dirname(__file__), "test_data/server_cert.pem"), "r"
+    ) as f:
+        cert = f.read()
+    return cert
+
+
+@pytest.fixture
+def cert_data(root_ca_cert, server_cert):
+    return {
+        "ca_cert_text": root_ca_cert,
+        "cert_text": server_cert,
+    }
+
+
+def test_check_domain():
+    downloader = SSLCertificateChainDownloader()
+
+    # Test without port
+    downloader.domain = "www.google.com"
+    domain = downloader.check_domain()
+    assert domain == {"hostname": "www.google.com", "port": 443}
+
+    # Test with port
+    downloader.domain = "www.google.com:8443"
+    domain = downloader.check_domain()
+    assert domain == {"hostname": "www.google.com", "port": 8443}
+
+
+def test_normalize_subject():
+    downloader = SSLCertificateChainDownloader()
+
+    subject = "/C=US/ST=California/L=Mountain View/O=Google LLC/CN=www.google.com"
+    assert (
+        downloader.normalize_subject(subject)
+        == "C_US_ST_California_L_Mountain_View_O_Google_LLC_CN_www_google_com"
+    )
