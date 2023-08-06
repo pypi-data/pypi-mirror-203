2edb24abcec56dc17986cb07c63398d8e906267d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:30 +0000

    Add package toxicTrig/0.1/toxicTrig-0.1.tar.gz

---
+++ packages/toxicTrig/toxicTrig-0.1.tar.gz/toxicTrig-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='toxicTrig',
+    version='0.1',
+    description='full set of words that trigger toxicity',
+    author='Xuhui Zhou, Maarten Sap',
+    author_email='xuhuiz@cs.cmu.edu',
+    url='https://github.com/XuhuiZhou/toxtrig',
+    packages=find_packages(),
+    install_requires=[
+       'setuptools',
+       'pandas',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
