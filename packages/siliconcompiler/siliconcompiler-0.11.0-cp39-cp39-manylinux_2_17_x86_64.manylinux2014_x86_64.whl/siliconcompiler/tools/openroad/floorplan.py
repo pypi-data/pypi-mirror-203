b8e66a12490e263c61059cb8d5723cb1143c860f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:40 +0000

    Add package siliconcompiler/0.11.0/siliconcompiler-0.11.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.11.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/siliconcompiler/tools/openroad/floorplan.py
@@ -0,0 +1,31 @@
+
+from siliconcompiler.tools.openroad.openroad import setup as setup_tool
+from siliconcompiler.tools.openroad.openroad import build_pex_corners, post_process
+
+def setup(chip):
+    '''
+    Perform floorplanning, pin placements, macro placements and power grid generation
+    '''
+
+    # Generic tool setup.
+    setup_tool(chip)
+
+    tool = 'openroad'
+    design = chip.top()
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+
+    if chip.valid('input', 'asic', 'floorplan'):
+        chip.add('tool', tool, 'task', task, 'require', ",".join(['input', 'asic', 'floorplan']), step=step, index=index)
+
+    if (not chip.valid('input', 'netlist', 'verilog') or
+        not chip.get('input', 'netlist', 'verilog', step=step, index=index)):
+        chip.add('tool', tool, 'task', task, 'input', design +'.vg', step=step, index=index)
+
+    if chip.valid('tool', tool, 'task', task, 'file', 'padring'):
+        chip.add('tool', tool, 'task', task, 'require', ','.join(['tool', tool, 'task', task, 'file', 'padring']))
+    chip.set('tool', tool, 'task', task, 'file', 'padring', 'script to insert the padring', field='help')
+
+def pre_process(chip):
+    build_pex_corners(chip)
