019ae8c2831181be8f349fff842cfef69957d346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:20 +0000

    Add package finetuner/0.7.6/finetuner-0.7.6.tar.gz

---
+++ packages/finetuner/finetuner-0.7.6.tar.gz/finetuner-0.7.6/finetuner/model.py
@@ -0,0 +1,33 @@
+from _finetuner.runner.stubs import model
+from _finetuner.runner.stubs.model import *  # noqa F401
+from _finetuner.runner.stubs.model import _EmbeddingModelStub
+
+
+def get_header() -> Tuple[str, ...]:
+    """Get table header."""
+    return 'name', 'task', 'output_dim', 'architecture', 'description'
+
+
+def get_row(model_stub) -> Tuple[str, ...]:
+    """Get table row."""
+    return (
+        model_stub.display_name,
+        model_stub.task,
+        str(model_stub.output_shape[1]),
+        model_stub.architecture,
+        model_stub.description,
+    )
+
+
+def list_model_classes() -> Dict[str, ModelStubType]:
+    rv = {}
+    members = inspect.getmembers(model, inspect.isclass)
+    for name, stub in members:
+        if (
+            name != 'MLPStub'
+            and not name.startswith('_')
+            and type(stub) != type
+            and issubclass(stub, _EmbeddingModelStub)
+        ):
+            rv[name] = stub
+    return rv
---
+++ packages/finetuner/finetuner-0.7.6.tar.gz/finetuner-0.7.6/finetuner/run.py
@@ -0,0 +1,186 @@
+import time
+from typing import Any, Dict, Iterator
+
+from finetuner.client import FinetunerV1Client
+from finetuner.console import console, print_examples, print_metrics
+from finetuner.constants import (
+    ARTIFACT_ID,
+    ARTIFACTS_DIR,
+    CREATED,
+    FAILED,
+    STARTED,
+    STATUS,
+)
+from finetuner.excepts import RunFailedError, RunInProgressError, RunPreparingError
+from finetuner.hubble import download_artifact
+
+
+class Run:
+    """Class for a run.
+
+    :param client: Client object for sending api requests.
+    :param name: Name of the run.
+    :param experiment_name: Name of the experiment.
+    :param config: Configuration for the run.
+    :param created_at: Creation time of the run.
+    :param description: Optional description of the run.
+    """
+
+    def __init__(
+        self,
+        client: FinetunerV1Client,
+        name: str,
+        experiment_name: str,
+        config: dict,
+        created_at: str,
+        description: str = '',
+    ):
+        self._client = client
+        self._name = name
+        self._experiment_name = experiment_name
+        self._config = config
+        self._created_at = created_at
+        self._description = description
+        self._run = self._get_run()
+
+    @property
+    def name(self) -> str:
+        """Get the name of the :class:`Run`."""
+        return self._name
+
+    @property
+    def config(self) -> dict:
+        """Get the config of the :class:`Run`."""
+        return self._config
+
+    def _get_run(self) -> dict:
+        """Get Run object as dict."""
+        return self._client.get_run(
+            experiment_name=self._experiment_name, run_name=self._name
+        )
+
+    def status(self) -> dict:
+        """Get :class:`Run` status.
+
+        :returns: A dict representing the :class:`Run` status.
+        """
+        return self._client.get_run_status(
+            experiment_name=self._experiment_name, run_name=self._name
+        )
+
+    def logs(self) -> str:
+        """Check the :class:`Run` logs.
+
+        :returns: A string dump of the run logs.
+        """
+        self._check_run_status_started()
+        return self._client.get_run_logs(
+            experiment_name=self._experiment_name, run_name=self._name
+        )
+
+    def stream_logs(self, interval: int = 5) -> Iterator[str]:
+        """Stream the :class:`Run` logs lively.
+
+        :param interval: The time interval to sync the status of finetuner `Run`.
+        :yield: An iterators keep stream the logs from server.
+        """
+        status = self.status()[STATUS]
+        msg_template = (
+            'Preparing to run, logs will be ready to pull when '
+            '`status` is `STARTED`. Current status is `%s`'
+        )
+        with console.status(msg_template % status, spinner="dots") as rich_status:
+            while status == CREATED:
+                time.sleep(interval)
+                status = self.status()[STATUS]
+                rich_status.update(msg_template % status)
+
+        return self._client.stream_run_logs(
+            experiment_name=self._experiment_name, run_name=self._name
+        )
+
+    def metrics(self) -> Dict[str, Dict[str, float]]:
+        """Get the evaluation metrics of the :class:`Run`.
+
+        :return: dictionary with evaluation metrics before and after fine-tuning.
+        """
+        self._check_run_status_finished()
+        return self._client.get_run_metrics(
+            experiment_name=self._experiment_name, run_name=self._name
+        )
+
+    def display_metrics(self):
+        """
+        Prints a table of retrieval metrics before and after fine-tuning
+        """
+        metrics = self.metrics()
+        for stage in metrics:
+            print_metrics(stage, metrics[stage])
+
+    def example_results(self) -> Dict[str, Any]:
+        """Get the results of example queries from the evaluation data of the
+        :class:`Run`.
+
+        :return: dictionary with results before and after fine-tuning.
+        """
+        self._check_run_status_finished()
+        return self._client.get_run_examples(
+            experiment_name=self._experiment_name, run_name=self._name
+        )
+
+    def display_examples(self, k: int = 5):
+        """
+        Prints a table of results of example queries before and after fine-tuning.
+
+        :param k: maximal number of results per query to display
+        """
+        example_results = self.example_results()
+        for stage in example_results:
+            print_examples(stage, example_results[stage], k=k)
+
+    def _check_run_status_finished(self):
+        status = self.status()[STATUS]
+        if status in [CREATED, STARTED]:
+            raise RunInProgressError(
+                'The run needs to be finished in order to save the artifact.'
+            )
+        if status == FAILED:
+            raise RunFailedError(
+                'The run failed, please check the `logs` for detailed information.'
+            )
+
+    def _check_run_status_started(self):
+        status = self.status()[STATUS]
+        if status == CREATED:
+            raise RunPreparingError(
+                'Preparing to run, logs will be ready to pull when '
+                '`status` is `STARTED`.'
+            )
+
+    def save_artifact(self, directory: str = ARTIFACTS_DIR) -> str:
+        """Save artifact if the :class:`Run` is finished.
+
+        :param directory: Directory where the artifact will be stored.
+        :returns: A string object that indicates the download path.
+        """
+        self._check_run_status_finished()
+        return download_artifact(
+            client=self._client,
+            artifact_id=self._run[ARTIFACT_ID],
+            run_name=self._name,
+            directory=directory,
+        )
+
+    @property
+    def artifact_id(self):
+        """Get artifact id of the :class:`Run`.
+
+        An artifact in finetuner contains fine-tuned model and its metadata.
+        Such as preprocessing function, collate function. This id could be useful
+        if you want to directly pull the artifact from the cloud storage, such as
+        using `FinetunerExecutor`.
+
+        :return: Artifact id as string object.
+        """
+        self._check_run_status_finished()
+        return self._run[ARTIFACT_ID]
