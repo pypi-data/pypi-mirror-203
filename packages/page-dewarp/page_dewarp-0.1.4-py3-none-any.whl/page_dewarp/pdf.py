0ec0bfbec17990f376573b35185c4adf8359a41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:24 +0000

    Add package page-dewarp/0.1.4/page_dewarp-0.1.4-py3-none-any.whl

---
+++ packages/page-dewarp/page_dewarp-0.1.4-py3-none-any.whl/page_dewarp/pdf.py
@@ -0,0 +1,7 @@
+__all__ = ["save_pdf"]
+
+
+def save_pdf(outfiles):
+    print("To convert to PDF (requires ImageMagick):")
+    print("  convert -compress Group4 " + " ".join(outfiles) + " output.pdf")
+    raise NotImplementedError("TODO: handle PDF conversion")
