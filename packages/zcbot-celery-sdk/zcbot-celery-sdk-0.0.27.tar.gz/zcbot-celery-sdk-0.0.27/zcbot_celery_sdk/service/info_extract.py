bbad0c0bb507a729f5a1733ea32da9e9953ceb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:15 +0000

    Add package zcbot-celery-sdk/0.0.27/zcbot-celery-sdk-0.0.27.tar.gz

---
+++ packages/zcbot-celery-sdk/zcbot-celery-sdk-0.0.27.tar.gz/zcbot-celery-sdk-0.0.27/zcbot_celery_sdk/service/info_extract.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+from .base import BaseService
+from ..model.callback import Callback
+from ..model.param import InfoPredictParam
+
+
+class BrandNameModelExtractService(BaseService):
+
+    def info_extract(self, task_params: InfoPredictParam = None, callback: Callback = None, **kwargs):
+        return self.celery_client.apply(task_name='info_extract.bnm', task_params=task_params.dict(), callback=callback,
+                                        **kwargs)
