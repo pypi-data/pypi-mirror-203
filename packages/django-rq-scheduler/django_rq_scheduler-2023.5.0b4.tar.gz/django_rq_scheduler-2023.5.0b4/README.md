434074a64f8c9c7938fa7d73e4fa5efce2e7f4b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:24 +0000

    Add package django-rq-scheduler/2023.5.0b4/django_rq_scheduler-2023.5.0b4.tar.gz

---
+++ packages/django-rq-scheduler/django_rq_scheduler-2023.5.0b4.tar.gz/django_rq_scheduler-2023.5.0b4/README.md
@@ -0,0 +1,34 @@
+Django RQ Scheduler
+===================
+[![Django CI](https://github.com/dsoftwareinc/django-rq-scheduler/actions/workflows/test.yml/badge.svg)](https://github.com/dsoftwareinc/django-rq-scheduler/actions/workflows/test.yml)
+![badge](https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/cunla/b756396efb895f0e34558c980f1ca0c7/raw/django-rq-scheduler-4.json)
+[![badge](https://img.shields.io/pypi/dm/django-rq-scheduler)](https://pypi.org/project/django-rq-scheduler/)
+[![Open Source Helpers](https://www.codetriage.com/dsoftwareinc/django-rq-scheduler/badges/users.svg)](https://www.codetriage.com/dsoftwareinc/django-rq-scheduler)
+
+Documentation can be found in https://django-rq-scheduler.readthedocs.io/en/latest/
+
+>  **Warning**: In v2023.3.0, django-rq-scheduler was refactored significantly to support
+> calculation of parameters in runtime.
+> You can now add a callable param to your scheduled job, and it will be
+> calculated when the job is performed.
+> 
+> 1. It is highly recommended you save your existing database before upgrading!
+> 2. Once you upgraded, recreate your jobs.
+
+> **Note** Starting v2023.1, requirement for rq_scheduler was removed and instead
+> one of the django-rq workers should run with `--with-scheduler` parameter
+> as mentioned [here](https://github.com/rq/django-rq#support-for-scheduled-jobs).
+
+
+# Sponsor
+
+django-rq-scheduler is developed for free.
+
+You can support this project by becoming a sponsor using [this link](https://github.com/sponsors/cunla).
+
+
+## Security contact information
+
+To report a security vulnerability, please use the
+[Tidelift security contact](https://tidelift.com/security).
+Tidelift will coordinate the fix and disclosure.
---
+++ packages/django-rq-scheduler/django_rq_scheduler-2023.5.0b4.tar.gz/django_rq_scheduler-2023.5.0b4/scheduler/tests/testtools.py
@@ -0,0 +1,112 @@
+from datetime import timedelta
+
+from scheduler import settings
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.test import Client, TestCase
+from django.utils import timezone
+
+from scheduler.models import CronJob, JobKwarg, RepeatableJob, ScheduledJob
+from scheduler.queues import get_queue
+
+
+def sequence_gen():
+    n = 1
+    while True:
+        yield n
+        n += 1
+
+
+seq = sequence_gen()
+
+
+def job_factory(cls, instance_only=False, **kwargs):
+    values = dict(
+        name='Scheduled Job %d' % next(seq),
+        job_id=None,
+        queue=list(settings.QUEUES.keys())[0],
+        callable='scheduler.tests.jobs.test_job',
+        enabled=True,
+        timeout=None)
+    if cls == ScheduledJob:
+        values.update(dict(
+            result_ttl=None,
+            scheduled_time=timezone.now() + timedelta(days=1), ))
+    elif cls == RepeatableJob:
+        values.update(dict(
+            result_ttl=None,
+            interval=1,
+            interval_unit='hours',
+            repeat=None,
+            scheduled_time=timezone.now() + timedelta(days=1), ))
+    elif cls == CronJob:
+        values.update(dict(cron_string="0 0 * * *", repeat=None, ))
+    values.update(kwargs)
+    if instance_only:
+        instance = cls(**values)
+    else:
+        instance = cls.objects.create(**values)
+    return instance
+
+
+def jobarg_factory(cls, **kwargs):
+    content_object = kwargs.pop('content_object', None)
+    if content_object is None:
+        content_object = job_factory(ScheduledJob)
+    values = dict(
+        arg_type='str',
+        val='',
+        object_id=content_object.id,
+        content_type=ContentType.objects.get_for_model(content_object),
+        content_object=content_object,
+    )
+    if cls == JobKwarg:
+        values['key'] = 'key%d' % next(seq),
+    values.update(kwargs)
+    instance = cls.objects.create(**values)
+    return instance
+
+
+def _get_job_from_queue(django_job):
+    queue = django_job.rqueue
+    jobs_to_schedule = queue.scheduled_job_registry.get_job_ids()
+    entry = next(i for i in jobs_to_schedule if i == django_job.job_id)
+    return queue.fetch_job(entry)
+
+
+def _get_executions(django_job):
+    queue = get_queue(django_job.queue)
+    job_ids = (queue.get_job_ids()
+               + queue.finished_job_registry.get_job_ids()
+               + queue.scheduled_job_registry.get_job_ids()
+               + queue.failed_job_registry.get_job_ids())
+    return list(filter(
+        lambda j: j.is_execution_of(django_job),
+        map(lambda jid: queue.fetch_job(jid), job_ids)))
+
+
+class SchedulerBaseCase(TestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        super().setUpTestData()
+        try:
+            User.objects.create_superuser('admin', 'admin@a.com', 'admin')
+        except Exception:
+            pass
+        cls.client = Client()
+
+    def setUp(self) -> None:
+        super(SchedulerBaseCase, self).setUp()
+        queue = get_queue('default')
+        queue.empty()
+
+    def tearDown(self) -> None:
+        super(SchedulerBaseCase, self).setUp()
+        queue = get_queue('default')
+        queue.empty()
+
+    @classmethod
+    def setUpClass(cls):
+        super(SchedulerBaseCase, cls).setUpClass()
+        queue = get_queue('default')
+        queue.connection.flushall()
