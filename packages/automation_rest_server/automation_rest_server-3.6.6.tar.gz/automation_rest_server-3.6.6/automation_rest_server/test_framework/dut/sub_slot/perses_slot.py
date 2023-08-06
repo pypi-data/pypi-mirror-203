8ee1feb8c4e8382635d06283bbfeb4584376e3ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:30 +0000

    Add package automation_rest_server/3.6.6/automation_rest_server-3.6.6.tar.gz

---
+++ packages/automation_rest_server/automation_rest_server-3.6.6.tar.gz/automation_rest_server-3.6.6/automation_rest_server/test_framework/dut/sub_slot/perses_slot.py
@@ -0,0 +1,26 @@
+import os
+import yaml
+from utils import log
+
+
+class PersesSlot(object):
+
+    def __init__(self):
+        self.root_path = os.getcwd()
+
+    def refresh(self, slot=None):
+        dut_dict = None
+        if slot is not None:
+            cmd = f"cd {self.root_path} && python3 run.py testfile debug_dut.py -s {slot}"
+        else:
+            cmd = f"cd {self.root_path} && python3 run.py testfile debug_dut.py"
+        log.INFO(cmd)
+        ret = os.system(cmd)
+        if ret == 0:
+            dut_yaml = os.path.join(self.root_path, "log", "dut.yaml")
+            if os.path.exists(dut_yaml):
+                dut_dict = yaml.load(open(dut_yaml).read(), Loader=yaml.FullLoader)
+                log.INFO(dut_dict)
+        return dut_dict
+
+
