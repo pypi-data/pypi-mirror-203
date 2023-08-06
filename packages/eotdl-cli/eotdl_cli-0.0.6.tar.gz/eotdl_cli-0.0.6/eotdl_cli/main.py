0da70afe7a677d0d40438752f03c7b95416868d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:04 +0000

    Add package eotdl-cli/0.0.6/eotdl_cli-0.0.6.tar.gz

---
+++ packages/eotdl-cli/eotdl_cli-0.0.6.tar.gz/eotdl_cli-0.0.6/eotdl_cli/main.py
@@ -0,0 +1,14 @@
+import typer
+from eotdl_cli.commands import auth, datasets
+
+app = typer.Typer()
+app.add_typer(auth.app, name="auth")
+app.add_typer(datasets.app, name="datasets")
+
+@app.command()
+def hello(name: str = 'World!'):
+    """Say hello to someone"""
+    typer.echo(f"hello, {name}")
+
+if __name__ == "__main__":
+    app()
