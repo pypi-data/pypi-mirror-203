c5ba07512606886f050244ea0444a00977a218a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:03 +0000

    Add package jsonbourne/0.25.1/jsonbourne-0.25.1-py3-none-any.whl

---
+++ packages/jsonbourne/jsonbourne-0.25.1-py3-none-any.whl/jsonbourne/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "jsonbourne"
+__description__ = "EZPZ JSON"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.25.1"
