d36378f3de09f406d57a1946594d336d6c0de8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:16 +0000

    Add package zndraw/0.1.2/zndraw-0.1.2.tar.gz

---
+++ packages/zndraw/zndraw-0.1.2.tar.gz/zndraw-0.1.2/README.md
@@ -0,0 +1,29 @@
+[![zincware](https://img.shields.io/badge/Powered%20by-zincware-darkcyan)](https://github.com/zincware)
+[![PyPI version](https://badge.fury.io/py/zndraw.svg)](https://badge.fury.io/py/zndraw)
+!['Threejs](https://img.shields.io/badge/threejs-black?style=for-the-badge&logo=three.js&logoColor=white)
+
+
+# ZnDraw
+
+Install via ``pip install zndraw``.
+
+## CLI
+
+You can use ZnDraw with the CLI ``zndraw atoms.xyz``.
+For a full list of arguments use `zndraw --help`.
+
+To interface with ``zndraw --update-function module.function`` you need to be able to import via ``from module import function``.
+
+The ZnDraw function expects as inputs
+- atom_ids: list[int], the ids of the currently selected atoms
+- atoms: ase.Atoms, the configuration as `ase.Atoms` file where atom_ids where selected.
+
+and as an output:
+- list[ase.Atoms], a list of ase Atoms objects to display.
+
+```python
+def function(atom_ids: list[int], atoms: ase.Atoms) -> list[ase.Atoms]|Generator[ase.Atoms, None, None]:
+    ...
+```
+
+![Alt text](https://raw.githubusercontent.com/zincware/ZnDraw/main/misc/zndraw_ui.png "ZnDraw UI")
---
+++ packages/zndraw/zndraw-0.1.2.tar.gz/zndraw-0.1.2/zndraw/app.py
@@ -0,0 +1,84 @@
+import dataclasses
+import uuid
+
+from flask import Flask, make_response, render_template, request, session
+
+from zndraw import globals, io
+
+app = Flask(__name__)
+app.secret_key = str(uuid.uuid4())
+
+
+@app.route("/")
+def index():
+    session["key"] = str(uuid.uuid4())  # TODO use session key e.g. for atoms cache
+    return render_template("index.html", config=dataclasses.asdict(globals.config))
+
+
+@app.route("/config")
+def config():
+    return dataclasses.asdict(globals.config) | {
+        "total_frames": len(globals._atoms_cache) - 1
+    }
+
+
+@app.route("/atoms/<step>")
+def atoms_step(step):
+    try:
+        atoms = globals.config.get_atoms(step=int(step))
+        graph = io.get_graph(atoms)
+        return [graph.nodes[idx] | {"id": idx} for idx in graph.nodes]
+    except (KeyError, IndexError):
+        return []
+
+
+@app.route("/positions/<start>&<stop>")
+def positions_step(start, stop):
+    result = []
+    try:
+        for step in range(int(start), int(stop)):
+            atoms = globals.config.get_atoms(step=int(step))
+            result.append(atoms.get_positions().tolist())
+        return result
+    except (KeyError, IndexError):
+        return result
+
+
+@app.route("/bonds/<step>/")
+def bonds_step(step):
+    atoms = globals.config.get_atoms(step=int(step))
+    graph = io.get_graph(atoms)
+    return list(graph.edges)
+
+
+@app.route("/select", methods=["POST"])
+def select():
+    session["selected"] = request.json
+    return {}
+
+
+@app.route("/update/<step>")
+def update_scene(step):
+    if "selected" not in session:
+        return []
+
+    function = globals.config.get_update_function()
+    atoms = function(
+        [int(x) for x in session["selected"]], globals.config.get_atoms(step=int(step))
+    )
+
+    offset = len(globals._atoms_cache)
+
+    for idx, atom in enumerate(atoms):
+        print(f"processing {idx}")
+        globals._atoms_cache[idx + offset] = atom
+
+    # this has to return before the scene is automatically updated
+    return {}
+
+
+@app.route("/load")
+def load():
+    """Function to call asynchronously to load atoms in the background."""
+    globals.config.load_atoms()
+    return {}
