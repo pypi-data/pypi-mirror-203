dc9c0a04c0e9a8c8adbb27c9e1746904b2eb369e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:02 +0000

    Add package mw-component/0.0.1/mw_component-0.0.1.tar.gz

---
+++ packages/mw-component/mw_component-0.0.1.tar.gz/mw_component-0.0.1/mw_component/frontend/build/precache-manifest.93a491426f36075dfc0723a2c85fbfce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "076af2bbc22aa12de9c051d02200884b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aaee55ae64499be39022",
+    "url": "./static/css/2.cbe79ea7.chunk.css"
+  },
+  {
+    "revision": "aaee55ae64499be39022",
+    "url": "./static/js/2.c9410cca.chunk.js"
+  },
+  {
+    "revision": "02b98e86acdf7c08ab784cad4f4d6697",
+    "url": "./static/js/2.c9410cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f37a9619540db61ad2",
+    "url": "./static/js/main.4489c64b.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  },
+  {
+    "revision": "5938dafd0f979f3814472e59dcfbe77e",
+    "url": "./static/media/monoton-all-400-normal.5938dafd.woff"
+  },
+  {
+    "revision": "68bccc3c76de5d7f57ea6a42a0e9d773",
+    "url": "./static/media/monoton-latin-400-normal.68bccc3c.woff2"
+  }
+]);
