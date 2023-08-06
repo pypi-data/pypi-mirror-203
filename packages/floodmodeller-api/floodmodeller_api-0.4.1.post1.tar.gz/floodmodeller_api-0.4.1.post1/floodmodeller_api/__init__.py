27fffebff5a6eaf7d98ad5be3bccd2bdc12c15c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:29 +0000

    Add package floodmodeller-api/0.4.1.post1/floodmodeller_api-0.4.1.post1.tar.gz

---
+++ packages/floodmodeller-api/floodmodeller_api-0.4.1.post1.tar.gz/floodmodeller_api-0.4.1.post1/floodmodeller_api/__init__.py
@@ -0,0 +1,12 @@
+from .version import __version__
+
+# Import files
+from .zzn import ZZN
+from .ief import IEF
+from .ied import IED
+from .dat import DAT
+from .inp import INP
+from .logs import LF1, LF2
+from .xml2d import XML2D
+
+from .util import open_docs
---
+++ packages/floodmodeller-api/floodmodeller_api-0.4.1.post1.tar.gz/floodmodeller_api-0.4.1.post1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    readme_txt = f.read()
+
+exec(open("floodmodeller_api/version.py").read())
+
+setup(
+    name="floodmodeller_api",
+    version=__version__,
+    author="Jacobs",
+    author_email="joe.pierce@jacobs.com",
+    packages=[
+        "floodmodeller_api",
+        "floodmodeller_api.units",
+        "floodmodeller_api.validation",
+        "floodmodeller_api.urban1d",
+        "floodmodeller_api.logs",
+        "floodmodeller_api.test",
+        "floodmodeller_api.test.test_data"
+    ],
+    scripts=[],
+    project_urls={
+        "API Documentation": "https://api.floodmodeller.com/api/",
+        "Flood Modeller Homepage": "https://www.floodmodeller.com/",
+        "Contact & Support": "https://www.floodmodeller.com/contact",
+    },
+    license="GNU General Public License V3. Copyright (C) 2023 Jacobs U.K. Limited.",
+    license_file="LICENSE.txt",
+    description="Extends the functionality of Flood Modeller to python users",
+    long_description=readme_txt,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=["pandas", "lxml", "tqdm", "pytest"],
+)
