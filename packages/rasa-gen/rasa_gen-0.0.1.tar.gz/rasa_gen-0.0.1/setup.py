fe6c77cabd66651855e66b5591b681c6e98e6b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:06 +0000

    Add package rasa-gen/0.0.1/rasa_gen-0.0.1.tar.gz

---
+++ packages/rasa-gen/rasa_gen-0.0.1.tar.gz/rasa_gen-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rasa_gen",
+    version="0.0.1",
+    keywords=["pip", "rasa_gen"],
+    description="Rasa train data generator",
+    long_description="Rasa train data generator version 0.0.1",
+    license="MIT Licence",
+
+    url="https://github.com/Adenialzz/SongUtils",
+    author="Charles Gao",
+    author_email="charlesgao2101024@gmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+)
