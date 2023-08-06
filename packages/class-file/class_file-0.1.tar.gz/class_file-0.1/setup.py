3e4bb4bec167f112b8c3dedc7d6df0ae2f6e7992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:28 +0000

    Add package class-file/0.1/class_file-0.1.tar.gz

---
+++ packages/class-file/class_file-0.1.tar.gz/class_file-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='class_file',
+      version='0.1',
+      description='удобный класс дял работы с файлом',
+      packages=['class_file'],
+      author_email='nikolajh913@gmail.com',
+      zip_safe=False)
