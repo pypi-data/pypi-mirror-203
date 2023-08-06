1a5e318cc3c6af11aa9768609c52461a41b96e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:52 +0000

    Add package flask-fd/1.1.0/flask-fd-1.1.0.tar.gz

---
+++ packages/flask-fd/flask-fd-1.1.0.tar.gz/flask-fd-1.1.0/flask_fd/gui/__init__.py
@@ -0,0 +1,17 @@
+import flask_fd.plop
+
+from flask_fd.gui.containers import (
+    main,
+    main_quit,
+    SillyBrowser,
+)
+from flask_fd.gui.indicators import (
+    Indicator,
+)
+from flask_fd.gui.helpers import (
+    open_in_main_browser,
+)
+
+from flask_fd.gui.header_bar import SillyHeaderBar
+
+from flask_fd.gui.builder import window_builder
