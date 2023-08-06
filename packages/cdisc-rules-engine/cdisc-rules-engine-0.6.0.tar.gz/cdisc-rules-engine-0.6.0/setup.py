227d5f2e4a0ae457e3bb7f822acda82d78e3d992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:19 +0000

    Add package cdisc-rules-engine/0.6.0/cdisc-rules-engine-0.6.0.tar.gz

---
+++ packages/cdisc-rules-engine/cdisc-rules-engine-0.6.0.tar.gz/cdisc-rules-engine-0.6.0/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import setuptools
+from version import __version__
+
+setuptools.setup(
+    name="cdisc-rules-engine",
+    version=__version__,
+    description="Open source offering of the cdisc rules engine",
+    author="cdisc-org",
+    url="https://github.com/cdisc-org/cdisc-rules-engine",
+    packages=setuptools.find_packages(exclude=["scripts", "tests"]),
+    license="MIT",
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=[
+        "pandas>=1.3.5",
+        "business-rules-enhanced==1.3.4",
+        "python-dotenv==0.20.0",
+        "cdisc-library-client==0.1.4",
+        "odmlib==0.1.4",
+        "xport==3.6.1",
+        "redis==4.0.2",
+        "openpyxl==3.0.10",
+        "importlib-metadata==5.0.0",
+    ],
+)
