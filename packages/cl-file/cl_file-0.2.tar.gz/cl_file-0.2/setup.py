3133ce0152594cd700d0cf3863abed9df6ee8336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:20 +0000

    Add package cl-file/0.2/cl_file-0.2.tar.gz

---
+++ packages/cl-file/cl_file-0.2.tar.gz/cl_file-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cl_file',
+      version='0.2',
+      description='Класс для работы с файлами',
+      packages=['cl_file'],
+      author_email='maleksa1296@gmail.com',
+      zip_safe=False)
