c19ce0ccb19614a3aab069e18d049bb672c0b0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:29 +0000

    Add package seawolf/0.1.1/seawolf-0.1.1.tar.gz

---
+++ packages/seawolf/seawolf-0.1.1.tar.gz/seawolf-0.1.1/setup.py
@@ -0,0 +1,20 @@
+from __future__ import division, print_function
+from setuptools import setup
+
+
+_version = '0.1'
+
+setup(name='seawolf',
+      version=_version,
+      description='Tools for matplotlib plots',
+      author='Bayron Torres Gordillo',
+      author_email='torres.bayron@gmail.com',
+      long_description=readme.read(),
+      long_description_content_type='text/markdown',
+      install_requires=['numpy','pandas','seaborn','matplotlib'],
+      packages=['seawolf'],
+      include_package_data=True,
+      license = 'BSD',
+      platforms = ["Windows", "Linux", "Solaris", "Mac OS-X", "Unix"],
+      python_requires='>=3.5'
+    )
