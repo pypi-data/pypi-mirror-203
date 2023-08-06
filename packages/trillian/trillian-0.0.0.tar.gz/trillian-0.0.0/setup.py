c54a9ac2ff358673f926e6f2dd8b79d5932c6fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:14 +0000

    Add package trillian/0.0.0/trillian-0.0.0.tar.gz

---
+++ packages/trillian/trillian-0.0.0.tar.gz/trillian-0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""The setup script."""
+
+# Third party modules
+from setuptools import setup
+
+setup(name='trillian',
+      version='0.0.0',
+      description='place holder',
+      url='http://github.com/SK/trillian',
+      author='sking',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['trillian'],
+      zip_safe=False)
