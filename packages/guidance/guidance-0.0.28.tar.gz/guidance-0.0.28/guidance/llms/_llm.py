5caeb00e38c57d4a68571e77a0ee8ef3f0ead1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:51 +0000

    Add package guidance/0.0.28/guidance-0.0.28.tar.gz

---
+++ packages/guidance/guidance-0.0.28.tar.gz/guidance-0.0.28/guidance/llms/_llm.py
@@ -0,0 +1,47 @@
+import os
+import pathlib
+import diskcache
+import platformdirs
+
+class LLM():
+    def __init__(self):
+
+        # by default models are not in role-based chat mode
+        self.chat_mode = False
+    
+    def __call__(self, *args, **kwargs):
+        """ Creates a session and calls the LLM with the given arguments.
+
+        Note that this is a convenience wrapper so you don't have to call session(),
+        for higher performance across multiple calls, use a session directly.
+        """
+        with self.session() as s:
+            out = s(*args, **kwargs)
+        return out
+    
+    def session(self):
+        return LLMSession(self) # meant to be overridden
+    
+    def encode(self, string, **kwargs):
+        return self._tokenizer.encode(string, **kwargs)
+    
+    def decode(self, tokens, **kwargs):
+        return self._tokenizer.decode(tokens, **kwargs)
+    
+    @staticmethod
+    def _open_cache(file_name):
+        return diskcache.Cache(os.path.join(platformdirs.user_cache_dir("guidance"), file_name))
+    
+
+class LLMSession():
+    def __init__(self, llm):
+        self.llm = llm
+    
+    def __enter__(self):
+        return self
+    
+    def __call__(self, *args, **kwargs):
+        return self.llm(*args, **kwargs)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
