28c9cc4979f5366714d6570827fa48183a19f54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:12 +0000

    Add package pgui/0.1/pgui-0.1.tar.gz

---
+++ packages/pgui/pgui-0.1.tar.gz/pgui-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='pgui',
+      version='0.1',
+      url='https://github.com/NikitaNakahara/PGUI',
+      license='MIT',
+      author='Nikita Nakahara',
+      author_email='nikita.maer14@gmail.com',
+      long_description=open('README.md').read(),
+      zip_safe=False)
