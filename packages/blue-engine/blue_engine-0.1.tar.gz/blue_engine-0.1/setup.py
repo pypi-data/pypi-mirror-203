4e50dbf1367ade7fe4c251863412823e42ce2b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:48 +0000

    Add package blue-engine/0.1/blue_engine-0.1.tar.gz

---
+++ packages/blue-engine/blue_engine-0.1.tar.gz/blue_engine-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blue_engine",
+    version=0.1,
+    license="MIT",
+    author="Debojyoti Ganguly",
+    package_dir={'':'src'},
+    keywords='blue engine',
+    install_requires=[
+    
+    ],
+)
