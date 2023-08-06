b60da22cd7f7545958602a8ddabe180ee70cc23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:13 +0000

    Add package siliconcompiler/0.11.0/siliconcompiler-0.11.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.11.0-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/siliconcompiler/apps/sc_run.py
@@ -0,0 +1,34 @@
+# Copyright 2020 Silicon Compiler Authors. All Rights Reserved.
+import sys
+import siliconcompiler
+
+def main():
+    progname = "sc-run"
+    chip = siliconcompiler.Chip(progname)
+    switchlist = ['-cfg',
+                  '-loglevel',
+                  '-checkonly',
+                  '-relax',
+                  '-quiet',
+                  '-version']
+    description = """
+    -----------------------------------------------------------
+    Restricted SC app that accepts one or more json based cfg files
+    as inputs and executes the SC run() method.
+    -----------------------------------------------------------
+    """
+    chip.create_cmdline(progname,
+                        switchlist=switchlist,
+                        description=description)
+
+
+    #Error checking
+    if not chip.get('cfg'):
+        print(progname+": error: the following arguments are required: -cfg")
+        sys.exit()
+    else:
+        chip.run()
+
+#########################
+if __name__ == "__main__":
+    sys.exit(main())
