e0efc47ceb164f52347f66779b690fef90f099a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:03 +0000

    Add package distro-Fennec/1.1/distro-Fennec-1.1.tar.gz

---
+++ packages/distro-Fennec/distro-Fennec-1.1.tar.gz/distro-Fennec-1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distro-Fennec',
+      version='1.1',
+      description='Gaussian distributions',
+      packages=['distro-Fennec'],
+      zip_safe=False)
