74921855ddec3bd6962f355158135f84c029e5e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:46 +0000

    Add package drup/1.1.0/drup-1.1.0-cp38-cp38-manylinux_2_17_x86_64.whl

---
+++ packages/drup/drup-1.1.0-cp38-cp38-manylinux_2_17_x86_64.whl/drup/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '1.1.0'
+
+import os
+import sys
+import ctypes
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+checker = ctypes.cdll.LoadLibrary(os.path.join(basedir, "librupchecker.so"))
+
+str_arr = (ctypes.c_char_p * (len(sys.argv)+1))()
+str_arr[:] = [i.encode("utf-8") for i in sys.argv] + [ctypes.c_char_p(0)]
+checker.do_startup(str_arr)
+
+from drup.wrappers import (
+	check_derivation,
+	check_proof_from_strings,
+	check_proof_from_files,
+	check_lemma
+)
