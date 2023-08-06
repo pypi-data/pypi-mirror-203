5493f19150985c2831c821ef111581988f0d04a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:12 +0000

    Add package hspylib-kafman/0.9.106/hspylib-kafman-0.9.106.tar.gz

---
+++ packages/hspylib-kafman/hspylib-kafman-0.9.106.tar.gz/hspylib-kafman-0.9.106/kafman/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-kafman v0.9.106
+#
+# Package: main.kafman
+"""Package initialization."""
+
+__all__ = [
+  '__classpath__', 
+  '__main__', 
+  'core', 
+  'views'
+]
+__version__ = '0.9.106'
