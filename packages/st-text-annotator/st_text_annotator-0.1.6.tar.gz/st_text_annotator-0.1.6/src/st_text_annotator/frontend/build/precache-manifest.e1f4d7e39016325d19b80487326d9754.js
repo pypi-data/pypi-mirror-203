113f72a4c1eb7ab3d5b727061ea793eb298077a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:53 +0000

    Add package st-text-annotator/0.1.6/st_text_annotator-0.1.6.tar.gz

---
+++ packages/st-text-annotator/st_text_annotator-0.1.6.tar.gz/st_text_annotator-0.1.6/src/st_text_annotator/frontend/build/precache-manifest.e1f4d7e39016325d19b80487326d9754.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e14c2683e875562d7b6e58ef892eab2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a3e3442fe61845021c19",
+    "url": "./static/js/2.0d16c425.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.0d16c425.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d87b8e1d32402b5daa52",
+    "url": "./static/js/main.c60fd880.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.c60fd880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "befa42638fa391b5bfa2",
+    "url": "./static/js/runtime-main.d5954658.js"
+  }
+]);
