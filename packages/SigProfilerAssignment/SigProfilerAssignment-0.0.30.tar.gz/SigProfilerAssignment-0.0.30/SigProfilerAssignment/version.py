be6d315ceb53c5d20337b257a875b797fb5f4029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:00 +0000

    Add package SigProfilerAssignment/0.0.30/SigProfilerAssignment-0.0.30.tar.gz

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.30.tar.gz/SigProfilerAssignment-0.0.30/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.30'
+version = '0.0.30'
+Update = 'v0.0.30: Sample reconstruction plot fix and pandas version fix.'
+
+    
+    
