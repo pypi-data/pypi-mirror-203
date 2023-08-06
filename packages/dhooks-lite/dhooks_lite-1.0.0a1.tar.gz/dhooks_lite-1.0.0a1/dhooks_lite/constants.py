8392680c8717f30460c72e9be10ced804dd174ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:56 +0000

    Add package dhooks-lite/1.0.0a1/dhooks_lite-1.0.0a1.tar.gz

---
+++ packages/dhooks-lite/dhooks_lite-1.0.0a1.tar.gz/dhooks_lite-1.0.0a1/dhooks_lite/constants.py
@@ -0,0 +1,3 @@
+APP_NAME = "dhooks_lite"
+APP_VERSION = "1.0.0a1"
+HOMEPAGE_URL = "https://github.com/ErikKalkoken/dhooks-lite"
---
+++ packages/dhooks-lite/dhooks_lite-1.0.0a1.tar.gz/dhooks_lite-1.0.0a1/dhooks_lite/serializers.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import json
+from typing import Any
+
+
+class JsonDateTimeEncoder(json.JSONEncoder):
+    """Add ability to encode datetime to JSON encoder"""
+
+    def default(self, o: Any) -> Any:
+        if isinstance(o, datetime):
+            return o.isoformat()
+        return json.JSONEncoder.default(self, o)
