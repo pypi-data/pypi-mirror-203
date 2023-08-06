0e5ba834f5360e89855bde672c5713a333703309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:37 +0000

    Add package copied/0.1.2/copied-0.1.2.tar.gz

---
+++ packages/copied/copied-0.1.2.tar.gz/copied-0.1.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='copied',
+    version='0.1.2',
+    description='A module for copying bot with IDs, events and commands for Discord API',
+    long_description=open('README.md').read(),     long_description_content_type='text/markdown',
+    py_modules=['copied'],
+)
+
