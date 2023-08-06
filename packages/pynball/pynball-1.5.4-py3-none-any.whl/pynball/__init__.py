2c1aae09874c12e682b124727b0c0115a024ca07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:07 +0000

    Add package pynball/1.5.4/pynball-1.5.4-py3-none-any.whl

---
+++ packages/pynball/pynball-1.5.4-py3-none-any.whl/pynball/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for _pynball."""
+
+__title__ = "_pynball"
+__version__ = "1.5.4"
+__author__ = "Stephen R A King"
+__description__ = "Utility command line to switch between python versions"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 Stephen R A King"
