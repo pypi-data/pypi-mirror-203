cf092b21c0569c1e1035beb2e40f6ff9593cbb5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:01 +0000

    Add package fastapi-allauth/0.1.2/fastapi_allauth-0.1.2-py3-none-any.whl

---
+++ packages/fastapi-allauth/fastapi_allauth-0.1.2-py3-none-any.whl/fastapi_allauth/auth/login.py
@@ -0,0 +1,16 @@
+from ..model.BaseUser import BaseUser
+from sqlalchemy.orm import Session
+from ..auth.authenticate import AuthHandler
+from ..secret_handler import SecretType, _get_secret_value
+
+def login(user: BaseUser,  secret: SecretType, lifetime_seconds: int):
+    authhandler = AuthHandler(_get_secret_value(secret), lifetime_seconds)
+
+    _payload = {}
+    for key in user.payload:
+        _payload[key] = user[key]
+        
+    token = authhandler.encode_login_token(payload=_payload)
+
+
+    return token
