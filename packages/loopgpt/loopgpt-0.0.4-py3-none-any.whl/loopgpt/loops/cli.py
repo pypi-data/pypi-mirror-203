786148461f124bd19fb8b13de54c3cb5f8c97eb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:31 +0000

    Add package loopgpt/0.0.4/loopgpt-0.0.4-py3-none-any.whl

---
+++ packages/loopgpt/loopgpt-0.0.4-py3-none-any.whl/loopgpt/loops/cli.py
@@ -0,0 +1,39 @@
+from loopgpt.agent import Agent
+
+import argparse
+import json
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("action", choices=["run"], help="Action to perform.")
+    parser.add_argument("filename", help="Agent state JSON.")
+    parser.add_argument(
+        "--readonly",
+        help="Read only mode. Does not write agent state to disk.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--reset",
+        help="Reset agent state before use. Name, goals and description are not affected.",
+        action="store_true",
+    )
+
+    args = parser.parse_args()
+
+    filename = args.filename
+
+    with open(filename, "r") as f:
+        agent_state = json.load(f)
+
+    agent = Agent.from_config(agent_state)
+
+    if args.reset:
+        agent.clear_state()
+
+    agent.cli()
+
+    if not args.readonly:
+        with open(filename, "w") as f:
+            json.dump(agent.config(), f)
