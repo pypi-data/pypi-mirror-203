dc144db8d5c3ecd7ac02a1a34be6c51450248502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:46 +0000

    Add package audiolm-pytorch/0.26.6/audiolm_pytorch-0.26.6-py3-none-any.whl

---
+++ packages/audiolm-pytorch/audiolm_pytorch-0.26.6-py3-none-any.whl/audiolm_pytorch/__init__.py
@@ -0,0 +1,13 @@
+from audiolm_pytorch.audiolm_pytorch import AudioLM
+from audiolm_pytorch.soundstream import SoundStream, AudioLMSoundStream, MusicLMSoundStream
+from audiolm_pytorch.encodec import EncodecWrapper
+
+from audiolm_pytorch.audiolm_pytorch import SemanticTransformer, CoarseTransformer, FineTransformer
+from audiolm_pytorch.audiolm_pytorch import FineTransformerWrapper, CoarseTransformerWrapper, SemanticTransformerWrapper
+
+from audiolm_pytorch.vq_wav2vec import FairseqVQWav2Vec
+from audiolm_pytorch.hubert_kmeans import HubertWithKmeans
+
+from audiolm_pytorch.trainer import SoundStreamTrainer, SemanticTransformerTrainer, FineTransformerTrainer, CoarseTransformerTrainer
+
+from audiolm_pytorch.audiolm_pytorch import get_embeds
