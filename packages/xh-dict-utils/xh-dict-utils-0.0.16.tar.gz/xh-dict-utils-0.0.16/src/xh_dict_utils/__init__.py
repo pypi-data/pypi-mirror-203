3bbff3065d15e9c3adfa50761049ddfa52593187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:53 +0000

    Add package xh-dict-utils/0.0.16/xh-dict-utils-0.0.16.tar.gz

---
+++ packages/xh-dict-utils/xh-dict-utils-0.0.16.tar.gz/xh-dict-utils-0.0.16/src/xh_dict_utils/__init__.py
@@ -0,0 +1,2 @@
+from xh_dict_utils.dict_utils import EntryType, Selector, Entry, Entries
+from xh_dict_utils.ExtractFromFile import *
---
+++ packages/xh-dict-utils/xh-dict-utils-0.0.16.tar.gz/xh-dict-utils-0.0.16/src/xh_dict_utils/__main__.py
@@ -0,0 +1,6 @@
+from xh_dict_utils.app import main
+
+# main()
+#
+if __name__ == '__main__':
+    main()
