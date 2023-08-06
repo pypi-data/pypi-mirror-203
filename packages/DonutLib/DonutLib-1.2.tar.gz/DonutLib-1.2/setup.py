97e6aa60b0de44708b8b30fd3524a5ca26babf99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:06 +0000

    Add package DonutLib/1.2/DonutLib-1.2.tar.gz

---
+++ packages/DonutLib/DonutLib-1.2.tar.gz/DonutLib-1.2/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="DonutLib",
+    version="1.2",
+    author="xKotelek",
+    author_email="kontakt@kotelek.tk",
+    description="DonutLib is a library that allows you to spin donut using Python",
+    long_description="If you have problems with module try visiting my github and issuses tab :)",
+    long_description_content_type="text/markdown",
+    url="https://github.com/xKotelek/DonutLib",
+    packages=["."],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
