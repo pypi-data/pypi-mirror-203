f587c1505b513964cb315a042fce6cecdd8ec9d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:15 +0000

    Add package jaquar/0.5.2/jaquar-0.5.2.tar.gz

---
+++ packages/jaquar/jaquar-0.5.2.tar.gz/jaquar-0.5.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jaquar',
+    version='0.5.2',
+    description='A web framework for python',
+    url='http://skillfam.writermint.com',
+    license='BSD',
+    author='Kipono Japhet',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'Jinja2',
+        'sqlalchemy',
+        'werkzeug',
+    ],
+    entry_points='''
+        [console_scripts]
+        jaquar=jaquar.cli:cli
+    ''',
+)
