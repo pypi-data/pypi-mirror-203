881adb649a70ee91a686075c3fce0981f4f3d7a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:20 +0000

    Add package watchtower-browser-testing/0.3.10/watchtower_browser_testing-0.3.10.tar.gz

---
+++ packages/watchtower-browser-testing/watchtower_browser_testing-0.3.10.tar.gz/watchtower_browser_testing-0.3.10/watchtower_browser_testing/config.py
@@ -0,0 +1,24 @@
+import os
+
+PACKAGE_PATH = os.path.dirname(os.path.realpath(__file__))
+TEMPLATES_PATH = os.path.join(PACKAGE_PATH, 'templates')
+
+TEST_FILE_PREFIX = 'tests_'
+DEFAULT_PIPELINE_PATTERN = 'https?://pipeline[.].*'
+SCENARIO_METHOD_PREFIX = 'scenario_'
+VALIDATION_METHOD_PREFIX = 'validation_'
+DEFAULT_TEST_DESCRIPTION = ''
+DEFAULT_BROWSERS = ['chromium', 'firefox', 'webkit']
+HARVEST_USER_SS_COOKIE_NAME = '_harvest_ss_user'
+HARVEST_USER_WEB_COOKIE_NAME = '_harvest_web_user'
+OLD_HARVEST_USER_ID_KEY = 'oldHarvestUserId'
+GTM_WEB_PREVIEW_LINK_REGEX = r'^https://tagassistant[.]google[.]com/.*'
+DEFAULT_PATH_TO_TAG_ASSISTANT_EXTENSION = r'tag_assistant'
+
+VALIDATION_MD_STRING = '''<details>
+<summary>Open to see performed validation for {event_name}</summary>
+
+```
+{json_string}
+```
+</details>'''
