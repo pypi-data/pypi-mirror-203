4bd95b0bbc770cd4e69f6bf7fce73452a645fdf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:11 +0000

    Add package qtpy-frameless-window/0.1.1/QtPy_Frameless_Window-0.1.1-py3-none-any.whl

---
+++ packages/qtpy-frameless-window/QtPy_Frameless_Window-0.1.1-py3-none-any.whl/qtframelesswindow/resource.py
@@ -0,0 +1,306 @@
+# Resource object code (Python 3)
+# Created by: object code
+# Created by: The Resource Compiler for Qt version 6.5.0
+# WARNING! All changes made in this file will be lost!
+
+from qtpy import QtCore
+
+qt_resource_data = b"\
+\x00\x00\x01,\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x221e\
+m\x22 height=\x221em\x22 \
+viewBox=\x220 0 16 \
+16\x22><path fill=\x22\
+#f0f0f0\x22 d=\x22M4.5\
+ 3A1.5 1.5 0 0 0\
+ 3 4.5v7A1.5 1.5\
+ 0 0 0 4.5 13h7a\
+1.5 1.5 0 0 0 1.\
+5-1.5v-7A1.5 1.5\
+ 0 0 0 11.5 3h-7\
+Zm0 1h7a.5.5 0 0\
+ 1 .5.5v7a.5.5 0\
+ 0 1-.5.5h-7a.5.\
+5 0 0 1-.5-.5v-7\
+a.5.5 0 0 1 .5-.\
+5Z\x22/></svg>\
+\x00\x00\x01\x85\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x221e\
+m\x22 height=\x221em\x22 \
+viewBox=\x220 0 16 \
+16\x22><path fill=\x22\
+#f0f0f0\x22 d=\x22m2.5\
+89 2.716l.057-.0\
+7a.5.5 0 0 1 .63\
+8-.057l.07.057L8\
+ 7.293l4.646-4.6\
+47a.5.5 0 0 1 .7\
+08.708L8.707 8l4\
+.647 4.646a.5.5 \
+0 0 1 .057.638l-\
+.057.07a.5.5 0 0\
+ 1-.638.057l-.07\
+-.057L8 8.707l-4\
+.646 4.647a.5.5 \
+0 0 1-.708-.708L\
+7.293 8L2.646 3.\
+354a.5.5 0 0 1-.\
+057-.638l.057-.0\
+7l-.057.07Z\x22/></\
+svg>\
+\x00\x00\x00\x9d\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x221e\
+m\x22 height=\x221em\x22 \
+viewBox=\x220 0 16 \
+16\x22><rect width=\
+\x2210\x22 height=\x221\x22 \
+x=\x223\x22 y=\x227.5\x22 fi\
+ll=\x22#f0f0f0\x22 rx=\
+\x22.5\x22/></svg>\
+\x00\x00\x01\x89\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x221e\
+m\x22 height=\x221em\x22 \
+viewBox=\x220 0 16 \
+16\x22><path fill=\x22\
+#f0f0f0\x22 d=\x22M5.0\
+85 4A1.5 1.5 0 0\
+ 1 6.5 3H10a3 3 \
+0 0 1 3 3v3.5a1.\
+5 1.5 0 0 1-1 1.\
+415V6a2 2 0 0 0-\
+2-2H5.085ZM4.5 5\
+h5A1.5 1.5 0 0 1\
+ 11 6.5v5A1.5 1.\
+5 0 0 1 9.5 13h-\
+5A1.5 1.5 0 0 1 \
+3 11.5v-5A1.5 1.\
+5 0 0 1 4.5 5Zm0\
+ 1a.5.5 0 0 0-.5\
+.5v5a.5.5 0 0 0 \
+.5.5h5a.5.5 0 0 \
+0 .5-.5v-5a.5.5 \
+0 0 0-.5-.5h-5Z\x22\
+/></svg>\
+\x00\x00\x01,\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x221e\
+m\x22 height=\x221em\x22 \
+viewBox=\x220 0 16 \
+16\x22><path fill=\x22\
+#383838\x22 d=\x22M4.5\
+ 3A1.5 1.5 0 0 0\
+ 3 4.5v7A1.5 1.5\
+ 0 0 0 4.5 13h7a\
+1.5 1.5 0 0 0 1.\
+5-1.5v-7A1.5 1.5\
+ 0 0 0 11.5 3h-7\
+Zm0 1h7a.5.5 0 0\
+ 1 .5.5v7a.5.5 0\
+ 0 1-.5.5h-7a.5.\
+5 0 0 1-.5-.5v-7\
+a.5.5 0 0 1 .5-.\
+5Z\x22/></svg>\
+\x00\x00\x01\x85\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x221e\
+m\x22 height=\x221em\x22 \
+viewBox=\x220 0 16 \
+16\x22><path fill=\x22\
+#383838\x22 d=\x22m2.5\
+89 2.716l.057-.0\
+7a.5.5 0 0 1 .63\
+8-.057l.07.057L8\
+ 7.293l4.646-4.6\
+47a.5.5 0 0 1 .7\
+08.708L8.707 8l4\
+.647 4.646a.5.5 \
+0 0 1 .057.638l-\
+.057.07a.5.5 0 0\
+ 1-.638.057l-.07\
+-.057L8 8.707l-4\
+.646 4.647a.5.5 \
+0 0 1-.708-.708L\
+7.293 8L2.646 3.\
+354a.5.5 0 0 1-.\
+057-.638l.057-.0\
+7l-.057.07Z\x22/></\
+svg>\
+\x00\x00\x00\x9d\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x221e\
+m\x22 height=\x221em\x22 \
+viewBox=\x220 0 16 \
+16\x22><rect width=\
+\x2210\x22 height=\x221\x22 \
+x=\x223\x22 y=\x227.5\x22 fi\
+ll=\x22#383838\x22 rx=\
+\x22.5\x22/></svg>\
+\x00\x00\x01\x89\
+<\
+svg xmlns=\x22http:\
+//www.w3.org/200\
+0/svg\x22 width=\x221e\
+m\x22 height=\x221em\x22 \
+viewBox=\x220 0 16 \
+16\x22><path fill=\x22\
+#383838\x22 d=\x22M5.0\
+85 4A1.5 1.5 0 0\
+ 1 6.5 3H10a3 3 \
+0 0 1 3 3v3.5a1.\
+5 1.5 0 0 1-1 1.\
+415V6a2 2 0 0 0-\
+2-2H5.085ZM4.5 5\
+h5A1.5 1.5 0 0 1\
+ 11 6.5v5A1.5 1.\
+5 0 0 1 9.5 13h-\
+5A1.5 1.5 0 0 1 \
+3 11.5v-5A1.5 1.\
+5 0 0 1 4.5 5Zm0\
+ 1a.5.5 0 0 0-.5\
+.5v5a.5.5 0 0 0 \
+.5.5h5a.5.5 0 0 \
+0 .5-.5v-5a.5.5 \
+0 0 0-.5-.5h-5Z\x22\
+/></svg>\
+\x00\x00\x00\xd6\
+Q\
+PushButton\x0d\x0a{\x0d\x0a \
+   border:none;\x0d\
+\x0a    background:\
+transparent;\x0d\x0a}\x0d\
+\x0aQPushButton:hov\
+er\x0d\x0a{\x0d\x0a    backg\
+round-color:rgba\
+(--R, --G, --B, \
+100);\x0d\x0a}\x0d\x0aQPushB\
+utton:pressed\x0d\x0a{\
+\x0d\x0a    background\
+-color:rgba(--R,\
+ --G, --B, 150);\
+\x0d\x0a}\x0d\x0a\
+\x00\x00\x00\xde\
+Q\
+PushButton\x0d\x0a{\x0d\x0a \
+   border-top-ri\
+ght-radius:5px;\x0d\
+\x0a    background:\
+transparent;\x0d\x0a}\x0d\
+\x0aQPushButton:hov\
+er\x0d\x0a{\x0d\x0a    backg\
+round-color:rgba\
+(255, 0, 0, 250)\
+;\x0d\x0a}\x0d\x0aQPushButto\
+n:pressed\x0d\x0a{\x0d\x0a  \
+  background-col\
+or:rgba(255, 0, \
+0, 200);\x0d\x0a}\x0d\x0a\
+"
+
+qt_resource_name = b"\
+\x00\x11\
+\x0bF\xa4\x07\
+\x00q\
+\x00t\x00f\x00r\x00a\x00m\x00e\x00l\x00e\x00s\x00s\x00w\x00i\x00n\x00d\x00o\x00w\
+\
+\x00\x03\
+\x00\x00x\xa3\
+\x00q\
+\x00s\x00s\
+\x00\x06\
+\x07\x03}\xc3\
+\x00i\
+\x00m\x00a\x00g\x00e\x00s\
+\x00\x05\
+\x00i'\x9b\
+\x00b\
+\x00l\x00a\x00c\x00k\
+\x00\x05\
+\x00}\xf0\xa5\
+\x00w\
+\x00h\x00i\x00t\x00e\
+\x00\x0c\
+\x0fy\xbaG\
+\x00m\
+\x00a\x00x\x00i\x00m\x00i\x00z\x00e\x00.\x00s\x00v\x00g\
+\x00\x09\
+\x06\x98\x8e\xa7\
+\x00c\
+\x00l\x00o\x00s\x00e\x00.\x00s\x00v\x00g\
+\x00\x0c\
+\x0f\x88\xfaG\
+\x00m\
+\x00i\x00n\x00i\x00m\x00i\x00z\x00e\x00.\x00s\x00v\x00g\
+\x00\x0d\
+\x0b\xd6+g\
+\x00n\
+\x00o\x00r\x00m\x00a\x00l\x00i\x00z\x00e\x00.\x00s\x00v\x00g\
+\x00\x0a\
+\x0bla\xc3\
+\x00b\
+\x00u\x00t\x00t\x00o\x00n\x00.\x00q\x00s\x00s\
+\x00\x10\
+\x00v\x9e#\
+\x00b\
+\x00u\x00t\x00t\x00o\x00n\x00_\x00c\x00l\x00o\x00s\x00e\x00.\x00q\x00s\x00s\
+"
+
+qt_resource_struct = b"\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00(\x00\x02\x00\x00\x00\x02\x00\x00\x00\x0e\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x004\x00\x02\x00\x00\x00\x02\x00\x00\x00\x04\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00F\x00\x02\x00\x00\x00\x04\x00\x00\x00\x0a\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00V\x00\x02\x00\x00\x00\x04\x00\x00\x00\x06\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x84\x00\x00\x00\x00\x00\x01\x00\x00\x010\
+\x00\x00\x01\x87\x8bP\x8a9\
+\x00\x00\x00\xba\x00\x00\x00\x00\x00\x01\x00\x00\x03Z\
+\x00\x00\x01\x87\x8bP\x8a:\
+\x00\x00\x00f\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
+\x00\x00\x01\x87\x8bP\x8a9\
+\x00\x00\x00\x9c\x00\x00\x00\x00\x00\x01\x00\x00\x02\xb9\
+\x00\x00\x01\x87\x8bP\x8a:\
+\x00\x00\x00\x84\x00\x00\x00\x00\x00\x01\x00\x00\x06\x17\
+\x00\x00\x01\x87\x8bP\x8a8\
+\x00\x00\x00\xba\x00\x00\x00\x00\x00\x01\x00\x00\x08A\
+\x00\x00\x01\x87\x8bP\x8a9\
+\x00\x00\x00f\x00\x00\x00\x00\x00\x01\x00\x00\x04\xe7\
+\x00\x00\x01\x87\x8bP\x8a8\
+\x00\x00\x00\x9c\x00\x00\x00\x00\x00\x01\x00\x00\x07\xa0\
+\x00\x00\x01\x87\x8bP\x8a8\
+\x00\x00\x00\xf4\x00\x00\x00\x00\x00\x01\x00\x00\x0a\xa8\
+\x00\x00\x01\x87\x8bP\x8a;\
+\x00\x00\x00\xda\x00\x00\x00\x00\x00\x01\x00\x00\x09\xce\
+\x00\x00\x01\x87\x8bP\x8a:\
+"
+
+def qInitResources():
+    QtCore.qRegisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+def qCleanupResources():
+    QtCore.qUnregisterResourceData(0x03, qt_resource_struct, qt_resource_name, qt_resource_data)
+
+qInitResources()
