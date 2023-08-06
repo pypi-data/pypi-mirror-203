8e1085fc92c1f902b5ce2059b21ffc18b50906cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:09 +0000

    Add package gmanka-yml/23.0.3/gmanka_yml-23.0.3.tar.gz

---
+++ packages/gmanka-yml/gmanka_yml-23.0.3.tar.gz/gmanka_yml-23.0.3/changelog.md
@@ -0,0 +1,24 @@
+# changelog
+
+## 23.0.3
+
+- fixed reading
+
+- "read_str" and "read_file" renamed to "from_str" and "from_file"
+
+## 23.0.2
+
+- added ability to pass expected type
+
+## 23.0.1
+
+- added ability to pass default value
+
+## 23.0.0
+
+- fixed typing
+- "file_path" renamed to "path"
+
+## 22.0.5
+
+- now using unicode
---
+++ packages/gmanka-yml/gmanka_yml-23.0.3.tar.gz/gmanka_yml-23.0.3/gmanka_yml/__init__.py
@@ -0,0 +1,96 @@
+from io import TextIOWrapper
+from pathlib import Path
+import yaml
+
+
+version = '23.0.1'
+_sential = object()
+
+
+def to_str(
+    data,
+) -> str:
+    try:
+        return yaml.dump(
+            data,
+            allow_unicode = True,
+            Dumper = yaml.CDumper,
+        )
+    except AttributeError:
+        return yaml.dump(
+            data,
+            allow_unicode = True,
+            Dumper = yaml.Dumper,
+        )
+
+
+def to_file(
+    data,
+    path: str | Path,
+) -> None:
+    Path(path).parent.mkdir(
+        exist_ok = True,
+        parents = True,
+    )
+    with open(
+        file = path,
+        mode = 'w',
+        encoding = 'utf-8',
+    ) as file:
+        file.write(
+            to_str(
+                data,
+            ),
+        )
+
+
+def from_str(
+    data: str | TextIOWrapper,
+    default = _sential,
+    expected_type = None,
+):
+    try:
+        parsed = yaml.safe_load(
+            stream = data,
+        )
+    except Exception as error:
+        if default == _sential:
+            raise error
+        else:
+            return default
+    if expected_type == None:
+        return parsed
+    elif isinstance(
+        parsed,
+        expected_type,
+    ):
+        return parsed
+    elif default == _sential:
+        raise TypeError(
+            f'{expected_type} expected, but got type(data)'
+        )
+    else:
+        return default
+
+
+def from_file(
+    path: str | Path,
+    default = _sential,
+    expected_type = None,
+):
+    try:
+        with open(
+            file = path,
+            mode = 'r',
+        ) as file:
+            return from_str( 
+                data = file, 
+                default = default, 
+                expected_type = expected_type, 
+            )
+    except Exception as error:
+        if default == _sential:
+            raise error
+        else:
+            return default
+
