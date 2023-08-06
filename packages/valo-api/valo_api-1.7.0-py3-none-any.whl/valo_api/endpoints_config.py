9226a42e7147bcf63d913d8f0ac88ca78583b0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:00 +0000

    Add package valo-api/1.7.0/valo_api-1.7.0-py3-none-any.whl

---
+++ packages/valo-api/valo_api-1.7.0-py3-none-any.whl/valo_api/endpoints_config.py
@@ -0,0 +1,30 @@
+class EndpointsConfig:
+    RAW = "/valorant/{version}/raw"
+    CONTENT = "/valorant/{version}/content"
+    STORE_FEATURED = "/valorant/{version}/store-featured"
+    STORE_OFFERS = "/valorant/{version}/store-offers"
+    STATUS = "/valorant/{version}/status/{region}"
+    VERSION_INFO = "/valorant/{version}/version/{region}"
+    WEBSITE = "/valorant/{version}/website/{countrycode}"
+    LEADERBOARD = "/valorant/{version}/leaderboard/{region}"
+    ACCOUNT_BY_NAME = "/valorant/{version}/account/{name}/{tag}"
+    ACCOUNT_BY_PUUID = "/valorant/{version}/by-puuid/account/{puuid}"
+    MATCH_DETAILS = "/valorant/{version}/match/{match_id}"
+
+    MMR_DETAILS_BY_PUUID = "/valorant/{version}/by-puuid/mmr/{region}/{puuid}"
+    MMR_DETAILS_BY_NAME = "/valorant/{version}/mmr/{region}/{name}/{tag}"
+
+    MMR_HISTORY_BY_PUUID = "/valorant/{version}/by-puuid/mmr-history/{region}/{puuid}"
+    MMR_HISTORY_BY_NAME = "/valorant/{version}/mmr-history/{region}/{name}/{tag}"
+
+    MATCH_HISTORY_BY_PUUID = "/valorant/{version}/by-puuid/matches/{region}/{puuid}"
+    MATCH_HISTORY_BY_NAME = "/valorant/{version}/matches/{region}/{name}/{tag}"
+
+    LIFETIME_MATCHES_BY_PUUID = (
+        "/valorant/{version}/by-puuid/lifetime/matches/{region}/{puuid}"
+    )
+    LIFETIME_MATCHES_BY_NAME = (
+        "/valorant/{version}/lifetime/matches/{region}/{name}/{tag}"
+    )
+
+    CROSSHAIR = "/valorant/{version}/crosshair/generate"
