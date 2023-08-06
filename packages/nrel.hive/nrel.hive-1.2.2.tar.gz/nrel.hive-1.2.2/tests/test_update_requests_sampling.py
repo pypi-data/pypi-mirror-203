d101e15462b67de3257c4658c00e0584ae38b8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:03 +0000

    Add package nrel.hive/1.2.2/nrel.hive-1.2.2.tar.gz

---
+++ packages/nrel.hive/nrel.hive-1.2.2.tar.gz/nrel.hive-1.2.2/tests/test_update_requests_sampling.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from nrel.hive.state.simulation_state.update.update_requests_sampling import UpdateRequestsSampling
+from nrel.hive.resources.mock_lobster import *
+
+
+class TestUpdateRequestsSampling(TestCase):
+    def test_update(self):
+        sim = mock_sim(
+            sim_time=SimTime.build(180),
+            road_network=mock_osm_network(),
+        )
+        env = mock_env()
+
+        requests = tuple(mock_request(request_id=str(i)) for i in range(5))
+
+        fn = UpdateRequestsSampling.build(sampled_requests=requests)
+        result, _ = fn.update(sim, env)
+        self.assertEqual(len(result.requests), 5, "should have added 5 requests")
+
+        for r in result.requests.values():
+            self.assertNotEqual(
+                r.origin,
+                r.destination,
+                "origin and destination should not be equal",
+            )
