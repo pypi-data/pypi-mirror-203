ef9a3dd1b335ce6d591409da37be11b7ee6b356b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:27 +0000

    Add package nanoatp/0.3.1/nanoatp-0.3.1-py3-none-any.whl

---
+++ packages/nanoatp/nanoatp-0.3.1-py3-none-any.whl/nanoatp/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023 Susumu OTA <1632335+susumuota@users.noreply.github.com>
+# SPDX-License-Identifier: MIT
+
+from .nanoatp import BskyAgent, parseUri
+from .richtext import RichText
+
+__version__ = "0.3.1"
+__all__ = ['__version__', 'BskyAgent', 'parseUri', 'RichText']
