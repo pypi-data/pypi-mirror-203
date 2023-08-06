fee72febb7e7c1b0b8a6a72c801899506ef63fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:21 +0000

    Add package ffpuppet/0.9.3/ffpuppet-0.9.3.tar.gz

---
+++ packages/ffpuppet/ffpuppet-0.9.3.tar.gz/ffpuppet-0.9.3/src/ffpuppet/puppet_logger.py
@@ -0,0 +1,321 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+"""browser and debugger log management"""
+
+from json import dump as json_dump
+from logging import getLogger
+from os import close as os_close
+from os import getpid, makedirs, mkdir, scandir, stat, stat_result
+from os.path import abspath, isdir, isfile
+from os.path import join as pathjoin
+from os.path import realpath
+from shutil import copy2, copyfileobj, copytree, rmtree
+from subprocess import STDOUT, CalledProcessError, check_output
+from tempfile import mkdtemp, mkstemp
+from typing import IO, Any, Dict, Iterator, KeysView, Optional
+
+from .helpers import onerror, warn_open
+
+LOG = getLogger(__name__)
+
+__author__ = "Tyson Smith"
+__credits__ = ["Tyson Smith"]
+
+__all__ = ("PuppetLogger",)
+
+
+class PuppetLogger:  # pylint: disable=missing-docstring
+    BUF_SIZE = 0x10000  # buffer size used to copy logs
+    META_FILE = "log_metadata.json"
+    PATH_RR = "rr-traces"
+    PREFIX_SAN = f"ffp_asan_{getpid()}.log"
+    PREFIX_VALGRIND = f"valgrind.{getpid()}"
+
+    __slots__ = ("_base", "_logs", "_rr_packed", "closed", "watching", "working_path")
+
+    def __init__(self, base_path: Optional[str] = None) -> None:
+        self._base = base_path
+        self._logs: Dict[str, IO[bytes]] = {}
+        self._rr_packed = False
+        self.closed = True
+        self.watching: Dict[str, int] = {}
+        self.working_path: Optional[str] = None
+        self.reset()
+
+    def __enter__(self) -> "PuppetLogger":
+        return self
+
+    def __exit__(self, *exc: Any) -> None:
+        self.clean_up()
+
+    def add_log(self, log_id: str, logfp: Optional[IO[bytes]] = None) -> IO[bytes]:
+        """Add a log file to the log manager.
+
+        Args:
+            log_id: ID of the log to add.
+            logfp: File object to use. If None is provided a new log
+                          file will be created.
+
+        Returns:
+            Newly added log file.
+        """
+        assert log_id not in self._logs
+        assert not self.closed
+        if logfp is None:
+            logfp = PuppetLogger.open_unique(base_dir=self.working_path)
+        self._logs[log_id] = logfp
+        return logfp
+
+    def add_path(self, name: str) -> str:
+        """Add a directory that can be used as temporary storage for
+        miscellaneous items such as additional debugger output.
+
+        Args:
+            name: Name of directory to create.
+
+        Returns:
+            Path of newly created directory.
+        """
+        assert not self.closed
+        assert self.working_path is not None
+        path = pathjoin(self.working_path, name)
+        LOG.debug("adding path %r as %r", name, path)
+        mkdir(path)
+        return path
+
+    def available_logs(self) -> KeysView[str]:
+        """List of IDs for the available logs.
+
+        Args:
+            None
+
+        Returns:
+            A list containing log IDs.
+        """
+        return self._logs.keys()
+
+    def clean_up(self, ignore_errors: bool = False) -> None:
+        """Remove log files from disk.
+
+        Args:
+            ignore_errors: Ignore errors triggered by removing files and
+                           directories will be ignored.
+
+        Returns:
+            None
+        """
+        if not self.closed:
+            self.close()
+        if self.working_path is not None and isdir(self.working_path):
+            for attempt in range(2):
+                try:
+                    rmtree(
+                        self.working_path, ignore_errors=ignore_errors, onerror=onerror
+                    )
+                except OSError:
+                    if attempt > 0:
+                        warn_open(self.working_path)
+                        raise
+                    continue
+                break
+        self._logs.clear()
+        self.working_path = None
+
+    def clone_log(
+        self,
+        log_id: str,
+        offset: Optional[int] = None,
+        target_file: Optional[str] = None,
+    ) -> Optional[str]:
+        """Create a copy of the specified log.
+
+        Args:
+            log_id: ID of the log to clone.
+            target_file: The log contents will be saved to target_file.
+            offset: Where to begin reading the log from.
+
+        Returns:
+            Name of the file containing the cloned log or None on failure.
+        """
+        log_fp = self.get_fp(log_id)
+        if log_fp is None:
+            return None
+        if not log_fp.closed:
+            log_fp.flush()
+        with open(log_fp.name, "rb") as in_fp:
+            if offset is not None:
+                in_fp.seek(offset)
+            if target_file is None:
+                cpyfp = PuppetLogger.open_unique(base_dir=self._base)
+                target_file = cpyfp.name
+            else:
+                # pylint: disable=consider-using-with
+                cpyfp = open(target_file, "wb")
+            try:
+                copyfileobj(in_fp, cpyfp, self.BUF_SIZE)
+            finally:
+                cpyfp.close()
+        return target_file
+
+    def close(self) -> None:
+        """Close all open file objects.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        for lfp in self._logs.values():
+            if not lfp.closed:
+                lfp.close()
+        self.closed = True
+
+    @property
+    def files(self) -> Iterator[str]:
+        """File names of log files.
+
+        Args:
+            None
+
+        Yields:
+            File names of log files.
+        """
+        for lfp in self._logs.values():
+            if lfp.name is not None:
+                yield lfp.name
+
+    def get_fp(self, log_id: str) -> Optional[IO[bytes]]:
+        """Lookup log file object by ID.
+
+        Args:
+            log_id: ID of the log (stderr, stdout... etc).
+
+        Returns:
+            The file matching given ID otherwise None.
+        """
+        try:
+            log_fp = self._logs[log_id]
+        except KeyError:
+            LOG.warning("log_id %r does not exist", log_id)
+            return None
+        if log_fp.name is None or not isfile(log_fp.name):
+            raise OSError(f"log file {log_fp.name!r} does not exist")
+        return log_fp
+
+    def log_length(self, log_id: str) -> Optional[int]:
+        """Get the length of the specified log.
+
+        Args:
+            log_id: ID of the log to measure.
+
+        Returns:
+            Length of the specified log in bytes or None if the log does not exist.
+        """
+        log_fp = self.get_fp(log_id)
+        if log_fp is None:
+            return None
+        if not log_fp.closed:
+            log_fp.flush()
+        return stat(log_fp.name).st_size
+
+    @staticmethod
+    def open_unique(base_dir: Optional[str] = None, mode: str = "wb") -> IO[bytes]:
+        """Create and open a unique file.
+
+        Args:
+            base_dir: This is where the file will be created. If None is
+                      passed mkstemp() will use the system default.
+            mode: File mode. See documentation for open().
+
+        Returns:
+            An open file object.
+        """
+        tmp_fd, log_file = mkstemp(suffix=".txt", prefix="ffp_log_", dir=base_dir)
+        os_close(tmp_fd)
+        # use open() so the file object 'name' attribute is correct
+        return open(log_file, mode)  # pylint: disable=consider-using-with
+
+    def reset(self) -> None:
+        """Reset logger for reuse.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
+        self.clean_up()
+        self.closed = False
+        self._rr_packed = False
+        self.working_path = realpath(mkdtemp(prefix="ffplogs_", dir=self._base))
+
+    def save_logs(
+        self,
+        dest: str,
+        logs_only: bool = False,
+        meta: bool = False,
+        bin_path: Optional[str] = None,
+        rr_pack: bool = False,
+    ) -> None:
+        """The browser logs will be saved to dest. This can only be called
+        after close() has been called.
+
+        Args:
+            dest: Destination path for log data. Existing files will be
+                        overwritten.
+            logs_only: Do not include other data, including debugger
+                              output files.
+            meta: Output JSON file containing log file meta data.
+            bin_path: Path to Firefox binary.
+            rr_pack: Pack rr trace if required.
+
+        Returns:
+            None
+        """
+        assert self.closed, "save_logs() cannot be called before calling close()"
+        assert self.working_path is not None
+
+        # copy log to location specified by dest
+        makedirs(dest, exist_ok=True)
+        dest = abspath(dest)
+
+        meta_map = {}
+        for log_id, log_fp in self._logs.items():
+            out_name = f"log_{log_id}.txt"
+            if meta:
+                file_stat = stat(log_fp.name)
+                meta_map[out_name] = {
+                    field: getattr(file_stat, field)
+                    for field in dir(stat_result)
+                    if field.startswith("st_")
+                }
+            copy2(log_fp.name, pathjoin(dest, out_name))
+
+        if not logs_only:
+            rr_trace = pathjoin(self.working_path, self.PATH_RR, "latest-trace")
+            if isdir(rr_trace):
+                if rr_pack and not self._rr_packed:
+                    LOG.debug("packing rr trace")
+                    try:
+                        check_output(["rr", "pack", rr_trace], stderr=STDOUT)
+                        self._rr_packed = True
+                    except (OSError, CalledProcessError):
+                        LOG.warning("Error calling 'rr pack %s'", rr_trace)
+                # copy `taskcluster-build-task` for use with Pernosco if available
+                if bin_path is not None:
+                    task_info = pathjoin(bin_path, "taskcluster-build-task")
+                    if isfile(task_info):
+                        moz_rr = pathjoin(rr_trace, "files.mozilla")
+                        makedirs(moz_rr, exist_ok=True)
+                        copy2(task_info, moz_rr)
+                        LOG.debug("Copied 'taskcluster-build-task' to trace")
+
+            for entry in scandir(self.working_path):
+                if entry.is_dir():
+                    copytree(entry.path, pathjoin(dest, entry.name), symlinks=True)
+
+        if meta_map:
+            with open(pathjoin(dest, self.META_FILE), "w") as json_fp:
+                json_dump(meta_map, json_fp, indent=2, sort_keys=True)
---
+++ packages/ffpuppet/ffpuppet-0.9.3.tar.gz/ffpuppet-0.9.3/src/ffpuppet/test_puppet_logger.py
@@ -0,0 +1,280 @@
+# type: ignore
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+"""ffpuppet puppet logger tests"""
+# pylint: disable=protected-access
+
+import os
+from json import loads
+from tempfile import SpooledTemporaryFile
+from time import sleep
+
+from pytest import raises
+
+from .helpers import onerror
+from .puppet_logger import PuppetLogger
+
+
+def test_puppet_logger_01(tmp_path):
+    """test simple PuppetLogger()"""
+    plog = PuppetLogger(base_path=str(tmp_path))
+    assert not plog.closed
+    assert not plog._logs
+    assert plog.working_path is not None
+    assert os.path.isdir(plog.working_path)
+    assert plog._base is not None
+    assert any(os.scandir(plog._base))
+    plog.close()
+    assert any(os.scandir(plog._base))
+    assert plog.closed
+    with raises(AssertionError):
+        plog.add_log("test")
+    assert plog.log_length("missing") is None
+
+
+def test_puppet_logger_02(tmp_path):
+    """test PuppetLogger.add_log() and PuppetLogger.available_logs()"""
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        assert not plog._logs
+        assert not plog.available_logs()
+        assert not any(plog.files)
+        plog.add_log("test_new")  # non-existing log
+        assert "test_new" in plog.available_logs()
+        plog_fp_test_new = plog.get_fp("test_new")
+        assert plog_fp_test_new is not None
+        assert os.path.isfile(plog_fp_test_new.name)
+        with (tmp_path / "test_existing.txt").open("w+b") as in_fp:
+            in_fp.write(b"blah")
+            plog.add_log("test_existing", logfp=in_fp)
+        assert len(plog.available_logs()) == 2
+        assert len(tuple(plog.files)) == 2
+        plog_fp_test_existing = plog.get_fp("test_existing")
+        assert plog_fp_test_existing is not None
+        assert os.path.isfile(plog_fp_test_existing.name)
+        assert plog.log_length("test_new") == 0
+        assert plog.log_length("test_existing") == 4
+
+
+def test_puppet_logger_03(tmp_path):
+    """test PuppetLogger.clean_up()"""
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        assert not plog.closed
+        assert not plog._logs
+        assert plog.working_path is not None
+        assert os.path.isdir(plog.working_path)
+        assert plog._base is not None
+        assert any(os.scandir(plog._base))
+        plog.add_log("test_new")
+        plog.clean_up()
+        assert plog.closed
+        assert not any(os.scandir(plog._base))
+        assert plog.working_path is None
+        assert plog.closed
+        assert not plog._logs
+
+
+def test_puppet_logger_04(tmp_path):
+    """test PuppetLogger.reset()"""
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        plog.add_log("test_new")
+        plog.clean_up()
+        plog.reset()
+        assert not plog.closed
+        assert not plog._logs
+        assert plog.working_path is not None
+        assert os.path.isdir(plog.working_path)
+        assert plog._base is not None
+        assert len(os.listdir(plog._base)) == 1
+
+
+def test_puppet_logger_05(tmp_path):
+    """test PuppetLogger.clone_log()"""
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        plog.add_log("test_empty")
+        plog.add_log("test_extra")
+        plog_fp_test_extra = plog.get_fp("test_extra")
+        assert plog_fp_test_extra is not None
+        plog_fp_test_extra.write(b"stuff")
+        plog_fp_test_extra.flush()
+        # test clone
+        plog.add_log("test_new")
+        pl_fp = plog.get_fp("test_new")
+        assert pl_fp is not None
+        pl_fp.write(b"test1")
+        cloned = plog.clone_log("test_new")
+        assert cloned is not None
+        assert os.path.isfile(cloned)
+        with open(cloned, "rb") as log_fp:
+            assert log_fp.read() == b"test1"
+        os.remove(cloned)
+        # test target exists
+        target = tmp_path / "target.txt"
+        target.touch()
+        pl_fp.write(b"test2")
+        pl_fp.flush()
+        cloned = plog.clone_log("test_new", target_file=str(target))
+        assert cloned is not None
+        assert os.path.isfile(cloned)
+        with open(cloned, "rb") as log_fp:
+            assert log_fp.read() == b"test1test2"
+        os.remove(cloned)
+        # test target does not exist with offset
+        assert not target.is_file()
+        pl_fp.write(b"test3")
+        pl_fp.flush()
+        cloned = plog.clone_log("test_new", target_file=str(target), offset=4)
+        assert cloned is not None
+        assert os.path.isfile(cloned)
+        with open(cloned, "rb") as log_fp:
+            assert log_fp.read() == b"1test2test3"
+        assert plog.log_length("test_new") == 15
+        os.remove(cloned)
+        # test non existent log
+        assert plog.clone_log("no_log") is None
+        # test empty log
+        assert plog.log_length("test_empty") == 0
+        cloned = plog.clone_log("test_empty")
+        assert cloned is not None
+        assert os.path.isfile(cloned)
+        assert not os.stat(cloned).st_size
+        os.remove(cloned)
+
+
+def test_puppet_logger_06(tmp_path):
+    """test PuppetLogger.save_logs()"""
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        plog.close()
+        # save when there are no logs
+        dest = tmp_path / "dest"
+        plog.save_logs(str(dest))
+        assert not any(dest.iterdir())
+        plog.reset()
+        dest.rmdir()
+        # add small log
+        plog.add_log("test_1")
+        plog_fp_test_1 = plog.get_fp("test_1")
+        assert plog_fp_test_1 is not None
+        plog_fp_test_1.write(b"test1\ntest1\n")
+        # add binary data in log
+        plog.add_log("test_2")
+        plog_fp_test_2 = plog.get_fp("test_2")
+        assert plog_fp_test_2 is not None
+        plog_fp_test_2.write(b"\x00TEST\xFF\xEF")
+        # add empty log
+        plog.add_log("test_empty")
+        # add larger log (not a power of 2 to help catch buffer issues)
+        plog.add_log("test_3")
+        data = b"A" * 1234
+        plog_fp_test_3 = plog.get_fp("test_3")
+        assert plog_fp_test_3 is not None
+        for _ in range(500):
+            plog_fp_test_3.write(data)
+        meta_test = tmp_path / "test_meta.txt"
+        with meta_test.open("w+b") as meta_fp:
+            meta_fp.write(b"blah")
+            plog.add_log("test_meta", logfp=meta_fp)
+        # delay to check if creation time was copied when save_logs is called
+        sleep(0.1)
+        plog.close()
+        dest.mkdir()
+        plog.save_logs(str(dest), meta=True)
+        # grab meta data and remove test file
+        meta_ctime = meta_test.stat().st_ctime
+        meta_test.unlink()
+        # check saved file count
+        assert len(plog.available_logs()) == 5
+        assert len(tuple(dest.iterdir())) == 6
+        # verify meta data was copied
+        meta_file = dest / PuppetLogger.META_FILE
+        assert meta_file.is_file()
+        meta_map = loads(meta_file.read_text())
+        assert len(meta_map) == 5
+        assert meta_ctime == meta_map["log_test_meta.txt"]["st_ctime"]
+        # verify all data was copied
+        assert os.stat(plog_fp_test_1.name).st_size == 12
+        assert os.stat(plog_fp_test_2.name).st_size == 7
+        assert os.stat(plog_fp_test_3.name).st_size == 500 * 1234
+
+
+def test_puppet_logger_07(mocker, tmp_path):
+    """test PuppetLogger.save_logs() rr trace directory"""
+    fake_ck = mocker.patch("ffpuppet.puppet_logger.check_output", autospec=True)
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        assert plog.working_path is not None
+        os.makedirs(os.path.join(plog.working_path, plog.PATH_RR, "latest-trace"))
+        plog.close()
+        # test call to rr failing
+        fake_ck.side_effect = OSError
+        plog.save_logs(str(tmp_path / "dest1"), rr_pack=True)
+        assert fake_ck.call_count == 1
+        assert not plog._rr_packed
+        # test call to rr passing
+        fake_ck.side_effect = None
+        plog.save_logs(str(tmp_path / "dest2"), rr_pack=True)
+        assert fake_ck.call_count == 2
+        assert plog._rr_packed
+        # test 'taskcluster-build-task' copied
+        bin_path = tmp_path / "bin_path"
+        bin_path.mkdir()
+        (bin_path / "taskcluster-build-task").write_text("task-info\n")
+        plog.save_logs(str(tmp_path / "dest3"), bin_path=str(bin_path))
+        assert (
+            tmp_path
+            / "dest3"
+            / "rr-traces"
+            / "latest-trace"
+            / "files.mozilla"
+            / "taskcluster-build-task"
+        ).is_file()
+        assert fake_ck.call_count == 2
+        assert plog._rr_packed
+
+
+def test_puppet_logger_08(tmp_path):
+    """test PuppetLogger.add_log() with file not on disk"""
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        with SpooledTemporaryFile(max_size=2048) as log_fp:
+            plog.add_log("test", logfp=log_fp)
+            with raises(OSError, match="log file None does not exist"):
+                plog.get_fp("test")
+
+
+def test_puppet_logger_09(mocker, tmp_path):
+    """test PuppetLogger.clean_up() with in-use file or inaccessible directory"""
+    fake_rmtree = mocker.patch("ffpuppet.puppet_logger.rmtree", autospec=True)
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        plog.add_log("test")
+        working_path = plog.working_path
+        # test with ignore_errors=False
+        fake_rmtree.side_effect = OSError
+        with raises(OSError):
+            plog.clean_up()
+        assert fake_rmtree.call_count == 2
+        fake_rmtree.assert_called_with(
+            working_path, ignore_errors=False, onerror=onerror
+        )
+        assert plog.working_path is not None
+        fake_rmtree.reset_mock()
+        # test with ignore_errors=True
+        fake_rmtree.side_effect = None
+        plog.clean_up(ignore_errors=True)
+        assert fake_rmtree.call_count == 1
+        fake_rmtree.assert_called_with(
+            working_path, ignore_errors=True, onerror=onerror
+        )
+        assert plog.working_path is None
+
+
+def test_puppet_logger_10(tmp_path):
+    """test PuppetLogger.add_path()"""
+    with PuppetLogger(base_path=str(tmp_path)) as plog:
+        path = plog.add_path("test")
+        assert os.path.isdir(path)
+        with open(os.path.join(path, "simple.txt"), "w") as o_fp:
+            o_fp.write("test")
+        plog.close()
+        dest = tmp_path / "dest"
+        plog.save_logs(str(dest))
+        assert (dest / "test").is_dir()
+        assert (dest / "test" / "simple.txt").is_file()
