f92d6f7bed068b433bb6e653787fdeecbbb2d191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:51 +0000

    Add package xia-engine-terraform-gcs/0.0.3/xia_engine_terraform_gcs-0.0.3-cp310-none-win_amd64.whl

---
+++ packages/xia-engine-terraform-gcs/xia_engine_terraform_gcs-0.0.3-cp310-none-win_amd64.whl/xia_engine_terraform_gcs/__init__.py
@@ -0,0 +1,8 @@
+from xia_engine_terraform_gcs.engine import TerraformGcsEngine, TerraformGcsConnectParam, TerraformGcsClient
+
+
+__all__ = [
+    "TerraformGcsEngine", "TerraformGcsConnectParam", "TerraformGcsClient"
+]
+
+__version__ = "0.0.3"
