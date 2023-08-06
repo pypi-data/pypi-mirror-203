fde3ff4963c387a90df4dab2d16e6084dba196f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:52 +0000

    Add package Lezargus/0.0.1.dev0/lezargus-0.0.1.dev0.tar.gz

---
+++ packages/Lezargus/lezargus-0.0.1.dev0.tar.gz/lezargus-0.0.1.dev0/src/lezargus/__init__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Sparrow <psmd.iberutaru@gmail.com>
+# SPDX-License-Identifier: MIT
+
+print("hello world.")
---
+++ packages/Lezargus/lezargus-0.0.1.dev0.tar.gz/lezargus-0.0.1.dev0/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Sparrow <psmd.iberutaru@gmail.com>
+#
+# SPDX-License-Identifier: MIT
