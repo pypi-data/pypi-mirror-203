4cfcd5c52fefa26a060f2aa4c38a961983dc5478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:41 +0000

    Add package frasa/0.1.52/frasa-0.1.52-py3-none-any.whl

---
+++ packages/frasa/frasa-0.1.52-py3-none-any.whl/frasa/preprocess/__init__.py
@@ -0,0 +1,8 @@
+from .lemma import Lemmatisasi
+from .token import Token
+from .stopword import Stopword
+
+if __name__ != '__main__':
+    lemma = Lemmatisasi()
+    token = Token()
+    stopword = Stopword()
