f2f62152e724e671e35e54acd63a8f65cf6cdd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:20 +0000

    Add package policyengine-ng/0.5.1/policyengine_ng-0.5.1-py3-none-any.whl

---
+++ packages/policyengine-ng/policyengine_ng-0.5.1-py3-none-any.whl/policyengine_ng/variables/tax.py
@@ -0,0 +1,15 @@
+from policyengine_ng.model_api import *
+
+
+class tax(Variable):
+    label = "individual tax"
+    entity = Person
+    definition_period = YEAR
+    value_type = float
+    unit = "currency-NGN"
+
+    def formula(person, period, parameters):
+        exempt = person("is_tax_exempt", period)
+        main_rates = person("main_tax_rates", period)
+        minimum_tax = person("minimum_tax", period)
+        return ~exempt * max_(main_rates, minimum_tax)
