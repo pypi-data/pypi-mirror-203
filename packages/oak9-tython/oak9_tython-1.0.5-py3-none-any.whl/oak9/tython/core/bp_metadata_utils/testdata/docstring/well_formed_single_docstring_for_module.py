add691903540f8fced613933328987d8d050e2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:06 +0000

    Add package oak9-tython/1.0.5/oak9_tython-1.0.5-py3-none-any.whl

---
+++ packages/oak9-tython/oak9_tython-1.0.5-py3-none-any.whl/oak9/tython/core/bp_metadata_utils/testdata/docstring/well_formed_single_docstring_for_module.py
@@ -0,0 +1,47 @@
+# Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the License is located at
+#
+#        http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
+'''
+Rule Name:
+  ALB_HTTP_TO_HTTPS_REDIRECTION_CHECK
+
+Description:
+  Checks whether HTTP to HTTPS redirection is configured.
+
+Trigger:
+  Periodic
+
+Reports on:
+  AWS::ElasticLoadBalancingV2::LoadBalancer
+
+Rule Parameters:
+  None
+
+Scenarios:
+  Scenario: 1
+     Given: No Application Load Balancer is present
+      Then: Return NOT_APPLICABLE
+  Scenario: 2
+     Given: There is at least 1 Application Load Balancer
+       And: Application Load Balancer has only HTTPS listener(s)
+      Then: Return COMPLIANT
+  Scenario: 3
+     Given: There is at least 1 Application Load Balancer
+       And: Application Load Balancer has one or more HTTP listeners configured
+       And: At least one HTTP listener rule does not have HTTP to HTTPS redirection action configured
+      Then: Return NON_COMPLIANT
+  Scenario: 4
+     Given: There is at least 1 Application Load Balancer
+       And: Application Load Balancer has one or more HTTP listeners configured
+       And: All HTTP listener rules have HTTP to HTTPS redirection action configured
+      Then: Return COMPLIANT
+'''
+
+print('Well formed docstring at the module level')
---
+++ packages/oak9-tython/oak9_tython-1.0.5-py3-none-any.whl/oak9/tython/core/bp_metadata_utils/testdata/oak9/well_formed_implements_field_single_value_with_a_space_after_implements.py
@@ -0,0 +1,17 @@
+def check_transparent_data_encryption_aks(aks, context):
+    """
+    Temp disks and cache for agent node pools in Azure Kubernetes Service clusters should be encrypted at host.
+
+    To enhance data security, the data stored on the virtual machine (VM) host of your Azure Kubernetes Service
+    nodes VMs should be encrypted at rest. This is a common requirement in many regulatory and industry compliance
+    standards.
+
+    Implements:
+        AZURE_POLICY: https://github.com/Azure/azure-policy/blob/master/built-in-policies/policyDefinitions/Kubernetes/AKS_EncryptionAtHost_Deny.json
+    Coverage:
+        Partial:
+            TODO (atcherniakhovski): Validate that this handles the case of multiple node-pools.
+    """
+    violations = []
+
+    return violations
