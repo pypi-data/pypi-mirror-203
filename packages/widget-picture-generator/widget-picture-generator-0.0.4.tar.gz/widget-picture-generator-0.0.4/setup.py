a8af730b1aed0270edc1ccaeae4febe7938b4882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:42 +0000

    Add package widget-picture-generator/0.0.4/widget-picture-generator-0.0.4.tar.gz

---
+++ packages/widget-picture-generator/widget-picture-generator-0.0.4.tar.gz/widget-picture-generator-0.0.4/setup.py
@@ -0,0 +1,25 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="widget-picture-generator",
+    version="0.0.4",
+    author="Massimo Moffa",
+    author_email="massimo.moffa@sensesquare.eu",
+    description="This library can be used to generate high-quality images of any type of widget, such as charts, maps, and leaderboards",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sense-Square/widget-picture-generator",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'selenium'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
