a2f3b351a00794207a4bd09509ea9a9378dbaefa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:32 +0000

    Add package fastapi-ecommerce-ext/0.2.1/fastapi_ecommerce_ext-0.2.1.tar.gz

---
+++ packages/fastapi-ecommerce-ext/fastapi_ecommerce_ext-0.2.1.tar.gz/fastapi_ecommerce_ext-0.2.1/setup.py
@@ -0,0 +1,24 @@
+import os
+
+from dotenv import load_dotenv
+from setuptools import setup
+
+load_dotenv()
+
+package_name = 'fastapi_ecommerce_ext'
+url = f'{os.getenv("GIT_REPO")}/pypi/{package_name}'
+version = 'v0.2.1'
+setup(
+    name=package_name,
+    version=version,
+    packages=['fastapi_ecommerce_ext.logger'],
+    install_requires=[
+        'loguru',
+        'starlette',
+    ],
+    url=url,
+    download_url=f'{url}-{version}.tar.gz',
+    license='MIT',
+    author=os.getenv("GIT_USERNAME"),
+    description='logging extension for fastapi-microservices'
+)
