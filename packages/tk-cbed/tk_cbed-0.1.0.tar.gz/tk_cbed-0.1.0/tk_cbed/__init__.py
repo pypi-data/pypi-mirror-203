bb74156a1bb38b61ac8af0d583894a4031fd4225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:39 +0000

    Add package tk-cbed/0.1.0/tk_cbed-0.1.0.tar.gz

---
+++ packages/tk-cbed/tk_cbed-0.1.0.tar.gz/tk_cbed-0.1.0/tk_cbed/__init__.py
@@ -0,0 +1,10 @@
+__name__ = 'tk_cbed'
+__version__ = '0.1.0'
+__description__ = 'Enhanced 4D-STEM through Neural Networks: Optimizing CBED Alignment and Restoration for Superior Ptychographic Reconstructions'
+__author__ = 'Ivan Lobato'
+__author_email__='ivanlh20@gmail.com'
+__url__ = 'https://github.com/Ivanlh20/tk_cbed/'
+__credits__ = 'The Rosalind Franklin Institute'
+__license__ = 'GPLv3'
+
+from .tk_cbed import load_network, load_sim_test_data
