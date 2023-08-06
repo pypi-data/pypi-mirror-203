d7a7d19ab55f9771c74f9db26586e4e4f84ab5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:55 +0000

    Add package iamai/3.14.5.post1/iamai-3.14.5.post1-py3-none-any.whl

---
+++ packages/iamai/iamai-3.14.5.post1-py3-none-any.whl/iamai/log.py
@@ -0,0 +1,22 @@
+"""iamai 日志。
+
+iamai 使用 [loguru](https://github.com/Delgan/loguru) 来记录日志信息。
+自定义 logger 请参考 [loguru](https://github.com/Delgan/loguru) 文档。
+"""
+import sys
+
+from loguru import logger as _logger
+
+logger = _logger
+
+
+def error_or_exception(message: str, exception: Exception, verbose: bool):
+    logger.remove()
+    logger.add(
+        sys.stderr,
+        format="<magenta>{time:YYYY-MM-DD HH:mm:ss.SSS}</magenta> <level>[{level}]</level> > <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    )
+    if verbose:
+        logger.exception(message)
+    else:
+        logger.critical(f"{message} {exception!r}")
