e0f46947dc22290fc0f0def3f5879b27b9717d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:03 +0000

    Add package dlt/0.2.5/dlt-0.2.5.tar.gz

---
+++ packages/dlt/dlt-0.2.5.tar.gz/dlt-0.2.5/dlt/common/configuration/specs/config_providers_context.py
@@ -0,0 +1,50 @@
+
+
+from typing import List
+
+from dlt.common.configuration.exceptions import DuplicateConfigProviderException
+from dlt.common.configuration.providers import ConfigProvider, EnvironProvider, ContextProvider, SecretsTomlProvider, ConfigTomlProvider
+from dlt.common.configuration.specs.base_configuration import ContainerInjectableContext, configspec
+
+
+@configspec
+class ConfigProvidersContext(ContainerInjectableContext):
+    """Injectable list of providers used by the configuration `resolve` module"""
+    providers: List[ConfigProvider]
+    context_provider: ConfigProvider
+
+    def __init__(self) -> None:
+        super().__init__()
+        # add default providers
+        self.providers = ConfigProvidersContext.initial_providers()
+        # ContextProvider will provide contexts when embedded in configurations
+        self.context_provider = ContextProvider()
+
+    def __getitem__(self, name: str) -> ConfigProvider:
+        try:
+            return next(p for p in self.providers if p.name == name)
+        except StopIteration:
+            raise KeyError(name)
+
+    def __contains__(self, name: object) -> bool:
+        try:
+            self.__getitem__(name)  # type: ignore
+            return True
+        except KeyError:
+            return False
+
+    def add_provider(self, provider: ConfigProvider) -> None:
+        if provider.name in self:
+            raise DuplicateConfigProviderException(provider.name)
+        self.providers.append(provider)
+
+    @staticmethod
+    def initial_providers() -> List[ConfigProvider]:
+        return [EnvironProvider(), SecretsTomlProvider(add_global_config=True), ConfigTomlProvider(add_global_config=True)]
+
+
+# TODO: implement ConfigProvidersConfiguration and
+# @configspec
+# class ConfigProvidersConfiguration(BaseConfiguration):
+#     with_aws_secrets: bool = False
+#     with_google_secrets: bool = False
