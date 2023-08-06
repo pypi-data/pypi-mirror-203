0c8e3ee98171046cef8fa1aa89ae22600f42a334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:07 +0000

    Add package nemreader/0.8.3/nemreader-0.8.3.tar.gz

---
+++ packages/nemreader/nemreader-0.8.3.tar.gz/nemreader-0.8.3/tests/test_incomplete_files.py
@@ -0,0 +1,53 @@
+import pytest
+
+from nemreader import NEMFile
+
+
+def test_correct_NMIs():
+    nf = NEMFile("examples/invalid/Example_NEM12_powercor.csv", strict=False)
+    meter_data = nf.nem_data()
+    assert len(meter_data.readings) == 1
+    assert "VABD000163" in meter_data.readings
+
+
+def test_incomplete_interval_row():
+    nf = NEMFile("examples/invalid/Example_NEM12_incomplete_interval.csv", strict=False)
+    meter_data = nf.nem_data()
+    assert len(meter_data.readings) == 1
+    assert "VABD000163" in meter_data.readings
+
+
+def test_correct_channels():
+    nf = NEMFile("examples/invalid/Example_NEM12_powercor.csv", strict=False)
+    meter_data = nf.nem_data()
+    readings = meter_data.readings["VABD000163"]
+    assert len(readings) == 2
+    assert "E1" in readings
+    assert "Q1" in readings
+
+
+def test_correct_records():
+    nf = NEMFile("examples/invalid/Example_NEM12_powercor.csv", strict=False)
+    meter_data = nf.nem_data()
+    readings = meter_data.readings["VABD000163"]
+
+    assert len(readings["E1"]) == 96
+    assert readings["E1"][10].read_value == pytest.approx(1.11, 0.1)
+    assert readings["E1"][-1].read_value == pytest.approx(3.33, 0.1)
+
+    assert len(readings["Q1"]) == 96
+    assert readings["Q1"][10].read_value == pytest.approx(2.22, 0.1)
+    assert readings["Q1"][-1].read_value == pytest.approx(4.44, 0.1)
+    assert readings["Q1"][-1].quality_method == "A"
+
+
+def test_zipped_load():
+    nf = NEMFile("examples/invalid/Example_NEM12_powercor.csv.zip", strict=False)
+    meter_data = nf.nem_data()
+    assert "VABD000163" in meter_data.nmis
+
+
+def test_missing_fields():
+    nf = NEMFile("examples/invalid/Example_NEM12_powercor.csv", strict=False)
+    meter_data = nf.nem_data()
+    assert "VABD000163" in meter_data.nmis
