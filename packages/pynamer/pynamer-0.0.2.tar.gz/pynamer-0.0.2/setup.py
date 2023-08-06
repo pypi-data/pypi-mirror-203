cf5c94fb20bccae8d511897524f43f28d7137a7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:42 +0000

    Add package pynamer/0.0.2/pynamer-0.0.2.tar.gz

---
+++ packages/pynamer/pynamer-0.0.2.tar.gz/pynamer-0.0.2/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""The setup script."""
+
+# Third party modules
+from setuptools import setup
+
+setup(name='pynamer',
+      version='0.0.2',
+      description='place holder for future project',
+      url='http://github.com/SK/pynamer',
+      author='sking',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pynamer'],
+      zip_safe=False)
