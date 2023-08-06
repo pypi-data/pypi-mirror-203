fadee21ed46514c2f56a0e5188196e686c8a64b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:33 +0000

    Add package i3dFeatureExtraction/0.2.4/i3dFeatureExtraction-0.2.4.tar.gz

---
+++ packages/i3dFeatureExtraction/i3dFeatureExtraction-0.2.4.tar.gz/i3dFeatureExtraction-0.2.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+DESCRIPTION = "This package helps extract i3D features with ResNet-50 backbone given a folder of videos"
+REQUIREMENTS = [i for i in open("requirements.txt").readlines()]
+setuptools.setup(
+    name="i3dFeatureExtraction",
+    version="0.2.4",
+    author="Hao Vy Phan",
+    author_email="vyhao03@gmail.com",
+    description=DESCRIPTION,
+    python_requires='>=3.8',
+    packages=setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: Freely Distributable",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    install_requires=REQUIREMENTS
+)
