a3b988dda3c8386363e773c84a29457cae7e7c83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:24 +0000

    Add package HTMLDocumentGenerator/1.0.3/HTMLDocumentGenerator-1.0.3.tar.gz

---
+++ packages/HTMLDocumentGenerator/HTMLDocumentGenerator-1.0.3.tar.gz/HTMLDocumentGenerator-1.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='HTMLDocumentGenerator',
+    version='1.0.3',
+    install_requires=[
+        'importlib-metadata; python_version == "3.6"',
+    ],
+)
