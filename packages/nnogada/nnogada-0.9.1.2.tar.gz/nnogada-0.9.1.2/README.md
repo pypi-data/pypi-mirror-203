512ccd1f3ffc1797154ca4a1fd55170dc5bb10ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:26 +0000

    Add package nnogada/0.9.1.2/nnogada-0.9.1.2.tar.gz

---
+++ packages/nnogada/nnogada-0.9.1.2.tar.gz/nnogada-0.9.1.2/README.md
@@ -0,0 +1,26 @@
+Neural Networks Optimized by Genetic Algorithms for Data Analysis (NNOGADA) 
+
+**nnogada** is a Python package that performs hyperparemeter tuning for artificial neural networks, particularly for Multi Layer Perceptrons, using simple genetic algorithms. Useful for generate better neural network models for data analysis. Currently, only works with feedforward neural networks in tensorflow.keras (classification and regression) and torch (regression at this moment).
+
+Before use the code, please install the requirements:
+
+    $ pip3 install nnogada
+ 
+Or you can try to install nnogada in your computer:
+
+     $ git clone https://github.com/igomezv/nnogada
+
+     $ cd nnogada
+
+     $ pip3 install -e .
+
+then you can delete the cloned repo because you must have nnogada installed locally.
+
+Other way to install nnogada (without clonning) is:
+
+    $ pip3 install -e git+https://github.com/igomezv/nnogada#egg=nnogada
+
+If you use the code, please cite the paper *Gómez-Vargas, I., Andrade, J. B., & Vázquez, J. A. (2023). Neural networks optimized by genetic algorithms in cosmology. Physical Review D, 107(4), 043509.*
+
+Contributions are welcome!
+
---
+++ packages/nnogada/nnogada-0.9.1.2.tar.gz/nnogada-0.9.1.2/setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    desc = fh.read()
+
+required = ["numpy", "scipy", "sklearn", "tensorflow", "deap", "bitstring", "pandas",
+            "torch", "torchinfo", "torch_optimizer", "tqdm"]
+
+setup(
+    name="nnogada",
+    version='0.9.1.2',
+    author='I Gomez-Vargas',
+    author_email="igomez@icf.unam.mx",
+    url="https://github.com/igomezv/nnogada",
+    license="MIT",
+    description="Genetic hyperparameter tuning for neural nets",
+    long_description=desc,
+    install_requires=required,
+    include_package_data=False,
+    keywords=["Hyperparameter",
+              "optimization",
+              "machine learning",
+              "deep learning",
+              "genetic algorithms"],
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Natural Language :: English',
+    ],
+
+)
