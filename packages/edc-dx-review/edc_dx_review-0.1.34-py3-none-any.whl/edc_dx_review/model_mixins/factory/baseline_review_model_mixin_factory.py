2aa3192fce4d917afa82bae866034d51d067eebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:53 +0000

    Add package edc-dx-review/0.1.34/edc_dx_review-0.1.34-py3-none-any.whl

---
+++ packages/edc-dx-review/edc_dx_review-0.1.34-py3-none-any.whl/edc_dx_review/model_mixins/factory/baseline_review_model_mixin_factory.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from django.db import models
+from edc_constants.choices import YES_NO
+from edc_constants.constants import CHOL, DM, HIV, HTN
+from edc_dx import get_diagnosis_labels
+
+default_prompts = {
+    HIV.lower(): "Has the patient ever tested <U>positive</U> for HIV infection?",
+    DM: "Has the patient ever been diagnosed with Diabetes?",
+    HTN: "Has the patient ever been diagnosed with Hypertension?",
+    CHOL: "Has the patient ever been diagnosed with High Cholesterol?",
+}
+
+
+def baseline_review_model_mixin_factory(prompts: dict[str, str] | None = None):
+    prompts = prompts or default_prompts
+
+    class AbstractModel(models.Model):
+        class Meta:
+            abstract = True
+
+    opts = {}
+    for dx in get_diagnosis_labels():
+        opts.update(
+            {
+                f"{dx}_dx": models.CharField(
+                    verbose_name=prompts.get(dx),
+                    max_length=15,
+                    choices=YES_NO,
+                )
+            }
+        )
+
+    for name, fld_cls in opts.items():
+        AbstractModel.add_to_class(name, fld_cls)
+
+    return AbstractModel
