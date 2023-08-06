8aa82dbb6ca7913f8e408a8685a20d6ca9d62a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:22 +0000

    Add package flask-fd/1.0.3/flask-fd-1.0.3.tar.gz

---
+++ packages/flask-fd/flask-fd-1.0.3.tar.gz/flask-fd-1.0.3/flask_fd/plop/ploppers.py
@@ -0,0 +1,40 @@
+"""
+This module handles the 'plop' command when used to copy an already
+existing file (in the package) to the user's cwd.
+"""
+import os
+import shutil
+
+from flask_fd.helpers import set_executable
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def _plop_icon(cwd):
+    file2 = os.path.join(BASE_DIR, "icon/FlaskFdIcon")
+    shutil.copy(file2, cwd)
+    print("Generic icon provided too")
+
+
+def plop_converter():
+    print("Flask-fd converter file ploped !")
+    cwd = os.getcwd()
+    # file
+    file = os.path.join(BASE_DIR, "flask/starter/flask_for_desktop.py")
+    shutil.copy(file, cwd)
+    set_executable(os.path.join(cwd, 'flask_for_desktop.py'))
+    print(f"flask_for_desktop.py and an icon just plopped in {cwd} :)")
+    # icon
+    _plop_icon(cwd)
+
+
+def plop_starter():
+    cwd = os.getcwd()
+    # copy starter folders
+    folder = os.path.join(BASE_DIR, "flask/starter")
+    if os.path.exists(os.path.join(cwd, "flask_fd_app")):
+        print("Aborted: flask_fd_app already exists in the current directory")
+    else:
+        shutil.copytree(folder, os.path.join(cwd, "flask_fd_app"))
+        set_executable(os.path.join(cwd, 'flask_fd_app/flask_for_desktop.py'))
+        print("Flask-fd starter kit ploped !")
