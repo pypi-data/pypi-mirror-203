75813fb8e410d7bfc90f166f0242a0f28049a287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:55 +0000

    Add package yeagerai-agent/0.0.41/yeagerai-agent-0.0.41.tar.gz

---
+++ packages/yeagerai-agent/yeagerai-agent-0.0.41.tar.gz/yeagerai-agent-0.0.41/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="yeagerai-agent",
+    version="0.0.41",
+    description="The @yeagerai Agent is used to create your custom LangChain Agents on-the-fly.",
+    author="YeagerAI LLC",
+    author_email="jm@yeager.ai",
+    packages=find_packages(),
+    install_requires=[
+        "langchain",
+        "openai",
+        "GitPython",
+        "click",
+        "gradio",
+        "python-dotenv",
+    ],
+    entry_points={
+        "console_scripts": [
+            "yeagerai-agent = yeagerai.interfaces.gradio_chat:main",
+        ],
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
