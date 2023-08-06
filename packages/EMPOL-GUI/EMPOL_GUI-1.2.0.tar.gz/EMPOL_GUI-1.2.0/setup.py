50efda779153615f015a38b0e35ed2c15f514359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:02 +0000

    Add package EMPOL-GUI/1.2.0/EMPOL_GUI-1.2.0.tar.gz

---
+++ packages/EMPOL-GUI/EMPOL_GUI-1.2.0.tar.gz/EMPOL_GUI-1.2.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="EMPOL_GUI",
+    version="1.2.0",
+    author="Aastha Gupta",
+    author_email="aastha.gupta1208@email.com",
+    description="Astronomial Large Data Reduction GUI - EMPOL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aasthagupta128/Merged_Code",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
