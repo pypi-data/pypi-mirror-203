5d91755b2778799b8ddd3d5ab2e9d9f53c9280af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:20 +0000

    Add package frenchmaid/0.2.3/frenchmaid-0.2.3.tar.gz

---
+++ packages/frenchmaid/frenchmaid-0.2.3.tar.gz/frenchmaid-0.2.3/setup.py
@@ -0,0 +1,38 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="frenchmaid",
+    version="0.2.3",
+    description="Remove all pests from your project! frenchmaid is a lightweight all platform cli package that will delete all pycache and other folders (contents included) in your project directory. Are you tired of doing it manually each time? Fear not, the frenchmaid will do it for you!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lewisjr/frenchmaid.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/lewisjr/frenchmaid/issues",
+    },
+    author = "Cerebrus Inc | Lewis Mosho Jr",
+    author_email = "lewis@cerebrus.dev",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Typing :: Typed"
+    ],
+    packages=["frenchmaid", "frenchmaid.modules", "frenchmaid.switches"],
+    include_package_data=True,
+    install_requires=["click", "colorama", "typer"],
+    entry_points={
+        "console_scripts": [
+            "frenchmaid=frenchmaid.__main__:main",
+        ]
+    },
+    package_dir={"": "src"},
+    python_requires=">=3.0",
+)
---
+++ packages/frenchmaid/frenchmaid-0.2.3.tar.gz/frenchmaid-0.2.3/src/frenchmaid/__init__.py
@@ -0,0 +1,15 @@
+
+__app_name__ = "frenchmaid"
+__version__ = "0.2.3"
+
+
+(
+    SUCCESS,
+    DIR_ERROR,
+    DEL_ERROR,
+) = range(3)
+
+ERRORS = {
+    DIR_ERROR: "Failed to find directory",
+    DEL_ERROR: "Failed to delete directory"
+}
