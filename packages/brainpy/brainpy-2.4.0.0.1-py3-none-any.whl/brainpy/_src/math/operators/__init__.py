172c2815827566a5a97ff185ba39bf570a12d441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:01 +0000

    Add package brainpy/2.4.0.0.1/brainpy-2.4.0.0.1-py3-none-any.whl

---
+++ packages/brainpy/brainpy-2.4.0.0.1-py3-none-any.whl/brainpy/_src/math/operators/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Operators for brain dynamics modeling.
+"""
+
+
+from .event_ops import *
+from .sparse_ops import *
+from .jitconn_ops import *
+from .op_registers import *
+from .pre_syn_post import *
