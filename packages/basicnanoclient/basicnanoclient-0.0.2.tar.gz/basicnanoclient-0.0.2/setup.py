772d3fce438f30626f46854fa37563175eb51d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:29 +0000

    Add package basicnanoclient/0.0.2/basicnanoclient-0.0.2.tar.gz

---
+++ packages/basicnanoclient/basicnanoclient-0.0.2.tar.gz/basicnanoclient-0.0.2/setup.py
@@ -0,0 +1,30 @@
+"""Setup."""
+from setuptools import find_packages, setup
+
+
+def load_long_description(filename: str) -> str:
+    """Convert README.md to a string."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+setup(
+    name="basicnanoclient",
+    version="0.0.2",
+    author="Nathaniel Schultz",
+    author_email="nate@nanoswap.finance",
+    description="Nano Cryptocurrency RPC Client",
+    long_description=load_long_description("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nanoswap/basicnanoclient",
+    project_urls={
+        "Bug Tracker": "https://github.com/nanoswap/basicnanoclient/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: The Unlicense (Unlicense)"
+    ],
+    package_dir={'basicnanoclient': "basicnanoclient"},
+    packages=find_packages("basicnanoclient"),
+    python_requires=">=3.6"
+)
