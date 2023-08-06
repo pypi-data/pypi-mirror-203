457f5c11c580694d4e8be317130a2cfcdc8f9621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:21 +0000

    Add package sunnyday-IZPy/1.0.0/sunnyday-IZPy-1.0.0.tar.gz

---
+++ packages/sunnyday-IZPy/sunnyday-IZPy-1.0.0.tar.gz/sunnyday-IZPy-1.0.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='sunnyday-IZPy',
+    packages=['sunnyday_IZPy'],
+    version='1.0.0',
+    license='MIT',
+    description='Weather forecast data',
+    author='Ivo Zelic',
+    author_email='izpy81@gmail.com',
+    url='https://github.com/DevIvo81/App-011-WeatherApp',
+    keywords=['weather', 'forecast', 'openweather'],
+    install_requires=['requests', ],
+
+    classifiers=[
+    ]
+)
