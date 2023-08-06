9accfd425719724f9ce8e9e41aa584c13dd8ac0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:47 +0000

    Add package forwarderd/13a2/forwarderd-13a2-py3-none-any.whl

---
+++ packages/forwarderd/forwarderd-13a2-py3-none-any.whl/forwarderd/__init__.py
@@ -0,0 +1,29 @@
+from ._shared import (
+    MessageType,
+    SpawnTunnelRequest,
+    SpawnTunnelResponse,
+    asdict, load_frame, dump_frame, connect
+)
+
+
+class DaemonError(Exception):
+    """Error sent by daemon"""
+
+
+async def _make_request(req_type, req_data):
+    r, w = await connect()
+
+    w.write(dump_frame(req_type, req_data))
+    resp_type, resp_data = load_frame(await r.readline())
+
+    if resp_type == MessageType.ERROR:
+        raise DaemonError(resp_data['error'])
+    elif resp_type == MessageType.OK:
+        return resp_data
+    else:
+        raise ValueError(f'Unknown response type {resp_type!r}')
+
+
+async def spawn_tunnel(data: SpawnTunnelRequest) -> SpawnTunnelResponse:
+    resp = await _make_request(MessageType.SPAWN_TUNNEL, asdict(data))
+    return SpawnTunnelResponse(**resp)
