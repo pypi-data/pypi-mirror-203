19b5805cce91987112cf7041b259e93eadc0afdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:35 +0000

    Add package edc-dx-review/0.1.32/edc_dx_review-0.1.32-py3-none-any.whl

---
+++ packages/edc-dx-review/edc_dx_review-0.1.32-py3-none-any.whl/edc_dx_review/radio_fields.py
@@ -0,0 +1,17 @@
+from typing import Dict
+
+from django.contrib import admin
+from edc_dx import get_diagnosis_labels_prefixes
+
+
+def get_clinical_review_cond_radio_fields() -> Dict[str, int]:
+    radio_fields = {}
+    for prefix in get_diagnosis_labels_prefixes():
+        cond = prefix.lower()
+        radio_fields.update(
+            {
+                f"{cond}_dx": admin.VERTICAL,
+                # f"{cond}_test": admin.VERTICAL,
+            }
+        )
+    return radio_fields
