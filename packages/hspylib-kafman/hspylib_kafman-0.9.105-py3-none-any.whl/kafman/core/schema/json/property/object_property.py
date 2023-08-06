26499d43d6afc6ff0a0d6080e4e0105fabfb2894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:21 +0000

    Add package hspylib-kafman/0.9.105/hspylib_kafman-0.9.105-py3-none-any.whl

---
+++ packages/hspylib-kafman/hspylib_kafman-0.9.105-py3-none-any.whl/kafman/core/schema/json/property/object_property.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HSPyLib-Kafman
+   @package: kafman.core.schema.json.property
+      @file: object_property.py
+   @created: Fri, 1 Jul 2022
+    @author: "<B>H</B>ugo <B>S</B>aporetti <B>J</B>unior")"
+      @site: "https://github.com/yorevs/hspylib")
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2022, HSPyLib team
+"""
+
+from typing import Tuple
+
+from kafman.core.schema.json.json_type import JsonType
+from kafman.core.schema.json.property.property import Property
+from kafman.core.schema.schema_field import SchemaField
+
+
+class ObjectProperty(SchemaField):
+    def __init__(self, name: str, description: str, properties: Tuple[Property], required: bool = True):
+        super().__init__(name, description, JsonType.OBJECT, required=required)
+
+        self.properties = properties
