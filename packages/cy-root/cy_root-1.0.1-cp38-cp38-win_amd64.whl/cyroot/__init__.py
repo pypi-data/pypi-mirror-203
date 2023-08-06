9e93574654c2e8722185210072e8ec711ee0b758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:52 +0000

    Add package cy-root/1.0.1/cy_root-1.0.1-cp38-cp38-win_amd64.whl

---
+++ packages/cy-root/cy_root-1.0.1-cp38-cp38-win_amd64.whl/cyroot/__init__.py
@@ -0,0 +1,16 @@
+"""
+A simple root-finding package written in Cython.
+"""
+
+from ._defaults import set_default_stop_condition_args
+from ._version import __version__
+from .scalar_bracketing import *
+from .scalar_derivative_approximation import *
+from .scalar_newton import *
+from .scalar_quasi_newton import *
+from .scalar_root import *
+from .vector_bracketing import *
+from .vector_newton import *
+from .vector_quasi_newton import *
+from .vector_root import *
+from .vector_derivative_approximation import *
