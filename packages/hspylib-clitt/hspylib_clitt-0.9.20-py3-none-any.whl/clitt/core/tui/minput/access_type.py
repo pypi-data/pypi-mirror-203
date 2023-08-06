3caf030d20812d51381e1cef542816fe70c36a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:10 +0000

    Add package hspylib-clitt/0.9.20/hspylib_clitt-0.9.20-py3-none-any.whl

---
+++ packages/hspylib-clitt/hspylib_clitt-0.9.20-py3-none-any.whl/clitt/core/tui/minput/access_type.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HSPyLib
+   @package: clitt.core.tui.minput
+      @file: access_type.py
+   @created: Thu, 20 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2022, HSPyLib team
+"""
+
+from hspylib.core.enums.enumeration import Enumeration
+
+
+class AccessType(Enumeration):
+    """MenuInput input field access types.
+    """
+
+    # fmt: off
+    NO_ACCESS   = "no-access"
+    READ_ONLY   = "read-only"
+    READ_WRITE  = "read-write"
+    # fmt: on
