ff449d6c1665f3573cf813b7a8e425a685d82a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:48 +0000

    Add package jdEolConverter/1.2/jdEolConverter-1.2.tar.gz

---
+++ packages/jdEolConverter/jdEolConverter-1.2.tar.gz/jdEolConverter-1.2/jdEolConverter/translations/jdEolConverter_de.ts
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>jdEolConverter converts the End of Line of all files in the given Directory. Select a Directory and click OK to get started.</source>
+      <translation>jdEolConverter ändert das Zeilenende von allen Dateien im angegebenen Verzeichnis. Um loszulegen gib ein Verzeichnis an und klicke auf OK.</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Directory:</source>
+      <translation>Verzeichnis:</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Browse</source>
+      <translation>Durchsuchen</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>End of Line</source>
+      <translation>Zeilenende</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Options</source>
+      <translation>Optionen</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Convert only files which name matches this RegEx</source>
+      <translation>Ändere nur Dateien mit diesem RegEx</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>RegEx:</source>
+      <translation>RegEx:</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Skip hidden files</source>
+      <translation>Versteckte Dateien überspringen</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Recursive</source>
+      <translation>Rekursiv</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="34" />
+      <source>Searching {{path}}</source>
+      <translation>Durchsuche {{path}}</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="49" />
+      <source>Could not read {{path}}</source>
+      <translation>Kann {{path}} nicht lesen</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="57" />
+      <source>Converting {{path}}</source>
+      <translation>Konvertiere {{path}}</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="64" />
+      <source>Could not convert {{path}}</source>
+      <translation>Kann {{path}} nicht konvertieren</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="65" />
+      <source>Finished</source>
+      <translation>Fertig</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="101" />
+      <source>No Directory</source>
+      <translation>Kein Verzeichnis</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="101" />
+      <source>You have not set a Directory</source>
+      <translation>Du hast kein Verzeichnis angegeben</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="105" />
+      <source>Not a Directory</source>
+      <translation>Kein Verzeichnis</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="105" />
+      <source>{{path}} is not a Directory</source>
+      <translation>{{path}} ist kein Verzeichnis</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="110" />
+      <source>No RegEx</source>
+      <translation>Kein RegEx</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="110" />
+      <source>You have not set a RegEx</source>
+      <translation>Du hast keinen RegEx angegeben</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="116" />
+      <source>Invalid RegEx</source>
+      <translation>Ungültiger RegEx</translation>
+    </message>
+  </context>
+</TS>
---
+++ packages/jdEolConverter/jdEolConverter-1.2.tar.gz/jdEolConverter-1.2/jdEolConverter/translations/jdEolConverter_nl.ts
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>jdEolConverter converts the End of Line of all files in the given Directory. Select a Directory and click OK to get started.</source>
+      <translation>jdEolConverter converteert regeleinden van alle bestanden in de gekozen map. Kies een map en klik op ‘Oké’ om het proces te starten.</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Directory:</source>
+      <translation>Map:</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Browse</source>
+      <translation>Bladeren…</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>End of Line</source>
+      <translation>Regeleinde</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Options</source>
+      <translation>Opties</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Convert only files which name matches this RegEx</source>
+      <translation>Alleen bestanden converteren die overeenkomen met reguliere uitdrukking</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>RegEx:</source>
+      <translation>Reguliere uitdrukking:</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Skip hidden files</source>
+      <translation>Verborgen bestanden overslaan</translation>
+    </message>
+    <message>
+      <location filename="../MainWindow.ui" line="0" />
+      <source>Recursive</source>
+      <translation>Recursief</translation>
+    </message>
+    <message>
+      <location filename="../__init__.py" line="34" />
+      <source>Searching {{path}}</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="49" />
+      <source>Could not read {{path}}</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="57" />
+      <source>Converting {{path}}</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="64" />
+      <source>Could not convert {{path}}</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="65" />
+      <source>Finished</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="101" />
+      <source>No Directory</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="101" />
+      <source>You have not set a Directory</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="105" />
+      <source>Not a Directory</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="105" />
+      <source>{{path}} is not a Directory</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="110" />
+      <source>No RegEx</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="110" />
+      <source>You have not set a RegEx</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../__init__.py" line="116" />
+      <source>Invalid RegEx</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+</TS>
