0f879f6665cf782d639995c192956e5b83ddc918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:42 +0000

    Add package circuitpython-laser-egismos/1.0.1/circuitpython-laser-egismos-1.0.1.tar.gz

---
+++ packages/circuitpython-laser-egismos/circuitpython-laser-egismos-1.0.1.tar.gz/circuitpython-laser-egismos-1.0.1/examples/laser_egismos_simpletest.py
@@ -0,0 +1,24 @@
+# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
+# SPDX-FileCopyrightText: Copyright (c) 2023 Phil Underwood for Underwood Underground
+#
+# SPDX-License-Identifier: Unlicense
+import time
+
+import board
+import busio
+import digitalio
+
+from laser_egismos import Laser
+
+laser_power = digitalio.DigitalInOut(board.D10)
+laser_power.switch_to_output(True)
+
+
+uart = busio.UART(board.D8, board.D9, baudrate=9600)
+laser = Laser(uart)
+laser.buzzer_off()
+laser.laser_on()
+time.sleep(3)
+laser.laser_off()
+time.sleep(0.1)
+print(f"Distance is {laser.distance}cm")
