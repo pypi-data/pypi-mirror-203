0b1cde7c861536a724dd8d27d4e8701dff6d1a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:15 +0000

    Add package p-template-res/0.1.23/p_template_res-0.1.23-py3-none-any.whl

---
+++ packages/p-template-res/p_template_res-0.1.23-py3-none-any.whl/template_res/main.py
@@ -0,0 +1,40 @@
+import sys
+import argparse
+import urllib3
+
+from template_res import template
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+subparsers = parser.add_subparsers()
+parser_widget = subparsers.add_parser("list", help="资源列表")
+
+def listTemplate():
+    searchPath = ""
+    if len(sys.argv) > 2:
+         searchPath = sys.argv[2]
+    template.listTemplate(searchPath)
+
+def listMusic():
+    searchPath = ""
+    if len(sys.argv) > 2:
+         searchPath = sys.argv[2]
+    template.listMusic(searchPath)
+    
+module_func = {
+    "list": listTemplate,
+    "music": listMusic
+}
+
+def main():
+    if len(sys.argv) < 2:
+        return
+    urllib3.disable_warnings()
+    module = sys.argv[1]
+    if module in module_func:
+        module_func[module]()
+    else:
+        print("Unknown command:", module)
+        sys.exit(0)
+        
+if __name__ == '__main__':
+        main()
