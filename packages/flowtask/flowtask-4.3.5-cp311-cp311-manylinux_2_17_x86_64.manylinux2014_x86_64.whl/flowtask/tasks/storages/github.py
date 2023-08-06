bf82b8b981ec6dacad8f7bca1ca98637a59afbfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:17 +0000

    Add package flowtask/4.3.5/flowtask-4.3.5-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/flowtask/flowtask-4.3.5-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/flowtask/tasks/storages/github.py
@@ -0,0 +1,44 @@
+from pathlib import PurePath
+from git import Repo
+from git import GitCommandError
+from flowtask.exceptions import FlowTaskError
+from .filesystem import FileTaskStorage
+
+class GitTaskStorage(FileTaskStorage):
+    """Saving Tasks on Filesystem with Github Support.
+    """
+    def __init__(self, path: PurePath, git_url: str, git_private_key: str = None, *args, **kwargs):
+        super(GitTaskStorage, self).__init__(path, *args, **kwargs)
+
+        if git_url:
+            self.git_url = git_url
+            self.git_private_key = git_private_key
+            self.refresh_repository()
+
+    def refresh_repository(self):
+        try:
+            if not self.path.exists() or not any(self.path.iterdir()):
+                # If the directory is empty or doesn't exist, clone the repository
+                if self.git_private_key:
+                    Repo.clone_from(
+                        self.git_url,
+                        self.path, env={"GIT_SSH_COMMAND": f"ssh -i {self.git_private_key}"}
+                    )
+                else:
+                    Repo.clone_from(self.git_url, self.path)
+                self.logger.info(
+                    f'Cloned repository: {self.git_url}'
+                )
+            else:
+                # If the directory exists and is not empty, pull the latest changes
+                repo = Repo(self.path)
+                if self.git_private_key:
+                    with repo.git.custom_environment(GIT_SSH_COMMAND=f"ssh -i {self.git_private_key}"):
+                        repo.git.pull()
+                else:
+                    repo.git.pull()
+                self.logger.info(f'Pulled the latest changes from repository: {self.git_url}')
+        except GitCommandError as err:
+            raise FlowTaskError(
+                f"Error interacting with Git repository: {err}"
+            ) from err
