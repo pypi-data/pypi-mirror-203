ceccd64bf22e8ef27aea7b1ef9c729d86e4e4b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:26 +0000

    Add package fastapi-startapp/0.1.2/fastapi-startapp-0.1.2.tar.gz

---
+++ packages/fastapi-startapp/fastapi-startapp-0.1.2.tar.gz/fastapi-startapp-0.1.2/setup.py
@@ -0,0 +1,22 @@
+# -*-coding:utf-8-*-
+from setuptools import (
+    setup
+)
+
+
+setup(
+    version='0.1.2',
+    name='fastapi-startapp',
+    install_requires=['click'],
+    descritption='fastapi startapp',
+    include_package_data=True,
+    packages=['scripts', 'core'],
+    entry_points='''
+        [console_scripts]
+        startapp=scripts.startapp:cli
+    ''',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Operating System :: OS Independent',
+    ]
+)
