4abd3b05f10b24ffad63d808915bbed46cc97dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:27 +0000

    Add package may-py2023/0.1/may_py2023-0.1.tar.gz

---
+++ packages/may-py2023/may_py2023-0.1.tar.gz/may_py2023-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='may_py2023',
+      version='0.1',
+      description='Удобный класс для работы с файлами',
+      packages=['may_py2023'],
+      author_email='gleb-grebenek1@mail.ru',
+      zip_safe=False)
