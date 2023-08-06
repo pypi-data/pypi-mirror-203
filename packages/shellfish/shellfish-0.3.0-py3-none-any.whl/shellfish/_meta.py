750b184399044e5d1e5f0fcd16586abbafb3efb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:33 +0000

    Add package shellfish/0.3.0/shellfish-0.3.0-py3-none-any.whl

---
+++ packages/shellfish/shellfish-0.3.0-py3-none-any.whl/shellfish/_meta.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Package metadata/info"""
+
+__title__ = "shellfish"
+__description__ = "shellfish ~ shell & file-system utils"
+__pkgroot__ = __file__.replace("_meta.py", "").rstrip("/\\")
+__version__ = "0.3.0"
