7c014279f52d7a3ac35929de50fee618f37a4ed3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:46 +0000

    Add package polyfasNast/1.1/polyfasNast-1.1.tar.gz

---
+++ packages/polyfasNast/polyfasNast-1.1.tar.gz/polyfasNast-1.1/polyfasNast/_init_.py
@@ -0,0 +1,1 @@
+from class_file import File
---
+++ packages/polyfasNast/polyfasNast-1.1.tar.gz/polyfasNast-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='polyfasNast',
+      version='1.1',
+      description='Класс для работы с файлами',
+      packages=['polyfasNast'],
+      author_email='khavrotinan@gmail.com',
+      zip_safe=False)
