36928134da6cc4dd5af0024ae78dce22a28fdb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:19 +0000

    Add package vhdx-2-zvol/0.4.4.3/vhdx_2_zvol-0.4.4.3.tar.gz

---
+++ packages/vhdx-2-zvol/vhdx_2_zvol-0.4.4.3.tar.gz/vhdx_2_zvol-0.4.4.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vhdx_2_zvol",
+    version="0.4.4.3",
+    description="Convert from vhdx to qcow to zvol",
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={"console_scripts": ["vhdx2zvol=vhdx_2_zvol.main:main"]},
+)
