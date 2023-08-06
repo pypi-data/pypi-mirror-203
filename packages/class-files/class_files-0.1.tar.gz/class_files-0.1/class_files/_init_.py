3d9c2e3814db3cc593f3c54aa9fbfd3157897159
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:24 +0000

    Add package class-files/0.1/class_files-0.1.tar.gz

---
+++ packages/class-files/class_files-0.1.tar.gz/class_files-0.1/class_files/_init_.py
@@ -0,0 +1,7 @@
+# _init_.py
+
+# from .Gaussiandistribution import Gaussian
+
+class File:
+    x = 50
+
---
+++ packages/class-files/class_files-0.1.tar.gz/class_files-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='class_files',
+      version='0.1',
+      description='Удобный класс для работы с файлами.',
+      packages=['class_files'],
+      author_email='BusinessJson6@gmail.com',
+      zip_safe=False)
