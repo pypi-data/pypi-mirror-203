55f47c1f4696a2fa784bc422ef71877d23c36979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:55 +0000

    Add package modelreport/0.2/modelreport-0.2-py3-none-any.whl

---
+++ packages/modelreport/modelreport-0.2-py3-none-any.whl/modelreport/_init_.py
@@ -0,0 +1,12 @@
+from modelreport.metric import result_summary
+
+# y_test=[1,0,0,0,0,1,1,1,0]
+# y_pred=[1,0,0,1,1,0,0,1,0]
+# # # print(result_summary(y_test,y_pred))
+
+
+
+# from modelreport import metrics
+
+# print(metrics(y_test,y_pred))
+
