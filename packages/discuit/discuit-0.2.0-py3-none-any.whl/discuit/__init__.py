e66d8ba0e03eca20b72f9ab576060a6c0bf56063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:47 +0000

    Add package discuit/0.2.0/discuit-0.2.0-py3-none-any.whl

---
+++ packages/discuit/discuit-0.2.0-py3-none-any.whl/discuit/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about discuit"""
+import logging
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Dörte de Kok"
+__email__ = "me@doerte.eu"
+__version__ = "0.1.0"
