ed74400ffdfa5c4120d95cc30475ffad731b989a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:10 +0000

    Add package mpsiemlib/1.5.1.1a0/mpsiemlib-1.5.1.1a0.tar.gz

---
+++ packages/mpsiemlib/mpsiemlib-1.5.1.1a0.tar.gz/mpsiemlib-1.5.1.1a0/setup.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+from mpsiemlib import __url__, __version__, __author__, __license__, __title__, __description__
+
+this_directory = Path(__file__).parent
+long_description = (this_directory/'README.md').read_text(encoding='utf-8')
+
+setup(
+    name=__title__,
+    version=__version__,
+    packages=find_packages(exclude=['tests']),
+    url=__url__,
+    license=__license__,
+    author=__author__,
+    author_email='',
+    description=__description__,
+    zip_safe=False,
+    python_requires='>=3.7',
+    install_requires=["requests",
+                      "PyYAML",
+                      "pytz",
+                      "elasticsearch>=7.17.9"],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
