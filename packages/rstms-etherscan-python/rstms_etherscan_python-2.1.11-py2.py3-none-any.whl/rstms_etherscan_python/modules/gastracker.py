10fa855b1366d8037f0f9568fdb20923f87a39e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:23 +0000

    Add package rstms-etherscan-python/2.1.11/rstms_etherscan_python-2.1.11-py2.py3-none-any.whl

---
+++ packages/rstms-etherscan-python/rstms_etherscan_python-2.1.11-py2.py3-none-any.whl/rstms_etherscan_python/modules/gastracker.py
@@ -0,0 +1,24 @@
+from ..enums.actions_enum import ActionsEnum as actions
+from ..enums.fields_enum import FieldsEnum as fields
+from ..enums.modules_enum import ModulesEnum as modules
+
+
+class GasTracker:
+    @staticmethod
+    def get_est_confirmation_time(gas_price: int) -> str:
+        # NOTE: gas_price in wei, result in seconds
+        url = (
+            f"{fields.MODULE}"
+            f"{modules.GASTRACKER}"
+            f"{fields.ACTION}"
+            f"{actions.GAS_ESTIMATE}"
+            f"{fields.GAS_PRICE}"
+            f"{gas_price}"
+        )
+        return url
+
+    @staticmethod
+    def get_gas_oracle() -> str:
+        # NOTE: gas_price in wei, result in seconds
+        url = f"{fields.MODULE}" f"{modules.GASTRACKER}" f"{fields.ACTION}" f"{actions.GAS_ORACLE}"
+        return url
