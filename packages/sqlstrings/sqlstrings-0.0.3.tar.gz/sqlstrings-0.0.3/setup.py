bdbd4e9d206fa6228be84cc5ed83f7ef7b1ceb85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:24 +0000

    Add package sqlstrings/0.0.3/sqlstrings-0.0.3.tar.gz

---
+++ packages/sqlstrings/sqlstrings-0.0.3.tar.gz/sqlstrings-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlstrings',
+    version='0.0.3',
+    description='A Python library for generating strings in different SQL dialects.',
+    url='https://github.com/alikellaway/sqlstrings',
+    author='Ali Kellaway',
+    author_email='ali.kellaway139@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # TODO Research licenses.
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='sql database python library strings generate',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.8, <4',
+)
