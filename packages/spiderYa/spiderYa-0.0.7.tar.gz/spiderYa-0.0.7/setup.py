b77d4eacf3c3ba2980687a65517b8c76059e5fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:39 +0000

    Add package spiderYa/0.0.7/spiderYa-0.0.7.tar.gz

---
+++ packages/spiderYa/spiderYa-0.0.7.tar.gz/spiderYa-0.0.7/setup.py
@@ -0,0 +1,58 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spiderYa",
+    version="0.0.7",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    
+    install_requires=[
+        'anndata>=0.8.0',
+        'cellrank',
+        'fa2',
+        'gseapy',
+        'h5py',
+        'igraph',
+        'leidenalg',
+        'louvain',
+        'matplotlib',
+        'matplotlib-venn',
+        'networkx',
+        'numba',
+        'numpy',
+        'pandas',
+        'plotly',
+        'pygco',
+        'scanpy',
+        'scgco',
+        'scikit-learn',
+        'scipy',
+        'scvelo',
+        'seaborn',
+        'sklearn',
+        'somde',
+        'somoclu',
+        # 'spaotsc',
+        'spatialde',
+        'stlearn',
+        'umap-learn',
+        'statsmodels',
+        'importlib',
+        'scprep',
+        'python-magic-bin',
+        'NaiveDE'
+    ],
+)
