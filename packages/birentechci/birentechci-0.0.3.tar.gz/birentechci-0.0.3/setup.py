a83119dca619970665a159545eab1206d4c471aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:16 +0000

    Add package birentechci/0.0.3/birentechci-0.0.3.tar.gz

---
+++ packages/birentechci/birentechci-0.0.3.tar.gz/birentechci-0.0.3/setup.py
@@ -0,0 +1,23 @@
+import setuptools 
+ 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+ 
+setuptools.setup(
+    name="birentechci", 
+    version="0.0.3",    
+    author="br_infra",    
+    author_email="br_infra@birentech.com",    
+    description="biren ci sdk",
+    long_description=long_description,    
+    long_description_content_type="text/markdown",
+    url="https://gitlab.birentech.com/software/br_ci_db",    
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',   
+)
