4ffa31b7e1138624f5b3607104b4c5a0e460b961
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:07:50 +0000

    Add package QuickReaderPDF/0.1.1/QuickReaderPDF-0.1.1.tar.gz

---
+++ packages/QuickReaderPDF/QuickReaderPDF-0.1.1.tar.gz/QuickReaderPDF-0.1.1/QuickReaderPDF/pdfeditor.py
@@ -0,0 +1,90 @@
+"""Module bolds first three letters of each word."""
+from pypdf import PdfReader
+import pdfkit
+import os
+import sys
+
+
+def pdf_reader(file):
+
+    doc = ""
+    reader = PdfReader(file)
+    number_of_pages = len(reader.pages)
+    for n in range(number_of_pages):
+        page = reader.pages[n]
+        text = page.extract_text()
+        doc += text
+    return doc
+
+
+def create_html(filename):
+    Func = open(filename, "x")
+    Func.close()
+
+
+def format_file(splitted, Func):
+    for line in splitted:
+        words = line.split(" ")
+        for i in words:
+            if len(i) > 3:
+                highlight = i[0:3]
+                nonhighlight = i[3:]
+                Func.write("<b>")
+                Func.write(highlight)
+                Func.write("</b>")
+                Func.write(nonhighlight)
+                Func.write(" ")
+
+        Func.write("<br>")
+
+
+def text_to_pdf(text, html_file):
+    splitted = text.split('\n')
+    create_html(html_file)
+    Func = open(html_file, "r+")
+
+    Func.write("""<!DOCTYPE html>
+                <html>
+                <body>
+                <center>
+                <p style="color:black;font-size:21px;">
+                """)
+    format_file(splitted, Func)
+    Func.write("""
+    </p>
+    </body>
+    </html>""")
+    Func.close()
+
+
+def pdf_bolden(file, html_file):
+    doc = pdf_reader(file)
+    text_to_pdf(doc, html_file)
+
+
+def pdf_to_html(html_file, file_name):
+    config = pdfkit.configuration()
+    print(config)
+    try:
+        pdfkit.from_file([html_file], file_name, configuration=config)
+    except OSError:
+        # not present in PATH
+        print(OSError)
+
+
+def remove_html(html_file):
+    os.remove(html_file)
+
+
+def main(file="sample.pdf", file_name="out.pdf"):
+    html_file = "htmltoPDF.html"
+    pdf_bolden(file, html_file)
+    pdf_to_html(html_file, file_name)
+    remove_html(html_file)
+
+
+if __name__ == "__main__":
+    print("Executed when invoked directly")
+    main(sys.argv[1], sys.argv[2])
+
+# run pytest --cov -report
---
+++ packages/QuickReaderPDF/QuickReaderPDF-0.1.1.tar.gz/QuickReaderPDF-0.1.1/QuickReaderPDF/tests/test_all.py
@@ -0,0 +1,70 @@
+import sys
+import os
+from fpdf import FPDF
+from QuickReaderPDF import pdf_reader,text_to_pdf,create_html,pdf_to_html,remove_html,format_file # noqa: E501
+sys.path.append('../')
+sys.path.append('./')
+
+
+def test_pdf_reader(tmp_path):
+    file_path = os.path.join(tmp_path, 'test_file.pdf')
+    data=[1,2,3,4,5,6]
+    pdf = FPDF(format='letter')
+    pdf.add_page()
+    pdf.set_font("helvetica", size=12)
+    for i in str(data):
+        pdf.write(5,i)
+    pdf.output(file_path)
+    assert '[1, 2, 3, 4, 5, 6]'== pdf_reader(file_path)
+
+
+def test_text_to_pdf(tmp_path):
+    file_path = os.path.join(tmp_path, 'test_file.html')
+    text="hello"
+    text_to_pdf(text,file_path)
+    Func = open(file_path,"r+")
+    assert Func.read().split() == ['<!DOCTYPE', 'html>', '<html>', '<body>', '<center>', '<p', 'style="color:black;font-size:21px;">', '<b>hel</b>lo', '<br>', '</p>', '</body>', '</html>']# noqa: E501
+
+
+def test_create_html(tmp_path):
+    # Define the file path for the new file
+    file_path = os.path.join(tmp_path, 'test_file.html')
+    # Call the create_file function with the file path and contents
+    create_html(file_path)
+    # Check that the file was created
+    assert os.path.isfile(file_path)
+
+    
+def test_pdf_to_html(tmp_path):
+            
+    file_path_html = os.path.join(tmp_path, 'test_file.html')
+    file_path_pdf = os.path.join(tmp_path, 'test_file.pdf')
+    f = open(file_path_html,'w')
+    message = """<html>
+    <head></head>
+    <body><p>Hello World!</p></body>
+    </html>""" # noqa: E501
+    f.write(message)
+    f.close()
+
+    pdf_to_html(file_path_html,file_path_pdf)    
+    assert os.path.isfile(file_path_pdf)
+
+
+def test_remove_html(tmp_path):
+    file_path = os.path.join(tmp_path, 'test_file.html')
+    # Create a test file
+    with open(file_path, 'w') as f:
+        f.write('This is a test.')
+    # Call the delete_file function with the file path
+    remove_html(file_path)
+
+    # Check that the file was deleted
+    assert not os.path.isfile(file_path)
+    pass
+
+def test_format_file(tmp_path):
+    file_path_html = os.path.join(tmp_path, 'testfile.html')
+    with open(file_path_html, 'w') as Func:
+        format_file(["Hello"],Func)
+        assert os.path.isfile(file_path_html)
