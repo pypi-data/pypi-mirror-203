dd97824ab2db021fd50f329cba6c7704f8e10ce0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:00 +0000

    Add package liebre/2.2304.0/liebre-2.2304.0-py3-none-any.whl

---
+++ packages/liebre/liebre-2.2304.0-py3-none-any.whl/liebre/message.py
@@ -0,0 +1,21 @@
+class Message:
+
+    def __init__(
+        self,
+        content,
+        properties,
+        method,
+        queue,
+    ):
+        self.content = content
+        self.properties = properties
+        self.method = method
+        self.queue = queue
+
+    @property
+    def id(self):
+        return self.properties.message_id
+
+    @property
+    def uid(self):
+        return (self.id, self.queue)
