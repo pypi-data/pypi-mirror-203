6f38acaa618ccb64564a9dfcce603f2486d6b953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:28 +0000

    Add package pyobjc-framework-AudioVideoBridging/9.1.1/pyobjc-framework-AudioVideoBridging-9.1.1.tar.gz

---
+++ packages/pyobjc-framework-AudioVideoBridging/pyobjc-framework-AudioVideoBridging-9.1.1.tar.gz/pyobjc-framework-AudioVideoBridging-9.1.1/setup.py
@@ -0,0 +1,24 @@
+"""
+Wrappers for the "AudioVideoBridging" framework on macOS.
+
+These wrappers don't include documentation, please check Apple's documentation
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+from pyobjc_setup import setup
+
+VERSION = "9.1.1"
+
+setup(
+    name="pyobjc-framework-AudioVideoBridging",
+    description="Wrappers for the framework AudioVideoBridging on macOS",
+    min_os_level="10.8",
+    packages=["AudioVideoBridging"],
+    version=VERSION,
+    install_requires=[
+        "pyobjc-core>=" + VERSION,
+        "pyobjc-framework-Cocoa>=" + VERSION,
+    ],
+    long_description=__doc__,
+)
