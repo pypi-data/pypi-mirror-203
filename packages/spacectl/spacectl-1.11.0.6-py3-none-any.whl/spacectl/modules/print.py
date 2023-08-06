cb86d2b214adbaae7e34c44751d6834df91e3f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:41:49 +0000

    Add package spacectl/1.11.0.6/spacectl-1.11.0.6-py3-none-any.whl

---
+++ packages/spacectl/spacectl-1.11.0.6-py3-none-any.whl/spacectl/modules/print.py
@@ -0,0 +1,50 @@
+import click
+import os
+from spaceone.core import utils
+from spacectl.lib.apply.task import BaseTask
+from spacectl.lib.apply.task import execute_wrapper
+from spacectl.lib.output import print_data
+
+
+class Task(BaseTask):
+
+    output_path = 'result'
+
+    @execute_wrapper
+    def execute(self):
+        self._validate()
+
+        output = self.spec.get('output', 'json')
+        path = self.spec.get('path')
+
+        if path:
+            if isinstance(path, list):
+                directory = '/'.join(path[:-1])
+                file_name = path[-1:][0].replace("/", "")
+                path = f'{directory}/{file_name}'
+            else:
+                directory = path.rsplit('/', 1)[0]
+
+            # Create directory
+            os.makedirs(directory, exist_ok=True)
+
+            # set output data
+            if isinstance(self.spec['data'], list):
+                output_data = {
+                    'results': self.spec['data']
+                }
+            else:
+                output_data = self.spec['data']
+
+            # Save data to file
+            click.echo(f'Save data to file: {path}')
+            click.echo('')
+            utils.save_json_to_file(output_data, path, indent=4)
+        else:
+            print_data(self.spec['data'], output)
+            click.echo('')
+
+    def _validate(self):
+        if 'data' not in self.spec:
+            raise ValueError(f'Required key: data\n'
+                             f'spec: {utils.dump_json(self.spec, 4)}')
