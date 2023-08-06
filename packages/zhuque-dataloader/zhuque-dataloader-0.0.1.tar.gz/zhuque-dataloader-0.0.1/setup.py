bd5314cd051031cb613ad510c8f132346275a542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:39 +0000

    Add package zhuque-dataloader/0.0.1/zhuque-dataloader-0.0.1.tar.gz

---
+++ packages/zhuque-dataloader/zhuque-dataloader-0.0.1.tar.gz/zhuque-dataloader-0.0.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zhuque-dataloader',
+    version='0.0.1',
+    description='zhuque graph platform dataloader component',
+    author='yanrisheng',
+    author_email='yanrs@zhejianglab.com',
+    packages=find_packages(),
+    requires=[],  # 定义依赖
+    license='GPL 3.0'
+)
