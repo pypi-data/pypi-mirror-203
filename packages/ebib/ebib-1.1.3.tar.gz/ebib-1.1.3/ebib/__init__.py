429e0c416c37a19f85a6b9947eed8e528392972b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:50 +0000

    Add package ebib/1.1.3/ebib-1.1.3.tar.gz

---
+++ packages/ebib/ebib-1.1.3.tar.gz/ebib-1.1.3/ebib/__init__.py
@@ -0,0 +1,19 @@
+# Copyright (C) 2023 Ernesto Lanchares <elancha98@gmail.com>
+# 
+# This file is part of ebib.
+# 
+# ebib is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# ebib is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public
+# License along with ebib. If not, see <https://www.gnu.org/licenses/>.
+
+from .english_stemmer import EnglishStemmer
+from .lexer import Lexer
