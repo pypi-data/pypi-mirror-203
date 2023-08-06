953902dfe827c4735d24f11f0d064978f6434f0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:05 +0000

    Add package molgraph/0.3.1/molgraph-0.3.1-py3-none-any.whl

---
+++ packages/molgraph/molgraph-0.3.1-py3-none-any.whl/molgraph/layers/convolutional/gin_conv.py
@@ -0,0 +1,210 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import initializers
+from tensorflow.keras import regularizers
+from tensorflow.keras import constraints
+from tensorflow.keras import activations
+from tensorflow.keras import layers
+
+from typing import Optional
+from typing import Callable
+from typing import Union
+from typing import Tuple
+
+from molgraph.tensors.graph_tensor import GraphTensor
+from molgraph.layers.base import BaseLayer
+from molgraph.layers.ops import compute_edge_weights_from_degrees
+from molgraph.layers.ops import propagate_node_features
+
+
+
+@keras.utils.register_keras_serializable(package='molgraph')
+class GINConv(BaseLayer):
+
+    '''Graph isomorphism convolution layer (GIN).
+
+    Implementation based on Dwivedi et al. (2022) [#]_ and Xu et al. (2019) [#]_.
+
+    **Examples:**
+
+    Inputs a ``GraphTensor`` encoding (two) subgraphs:
+
+    >>> graph_tensor = molgraph.GraphTensor(
+    ...     data={
+    ...         'edge_dst': [[0, 1], [0, 0, 1, 1, 2, 2]],
+    ...         'edge_src': [[1, 0], [1, 2, 0, 2, 1, 0]],
+    ...         'node_feature': [
+    ...             [[1.0, 0.0], [1.0, 0.0]],
+    ...             [[1.0, 0.0], [1.0, 0.0], [0.0, 1.0]]
+    ...         ],
+    ...     }
+    ... )
+    >>> # Build a model with GINConv
+    >>> gnn_model = tf.keras.Sequential([
+    ...     tf.keras.Input(type_spec=graph_tensor.unspecific_spec),
+    ...     molgraph.layers.GINConv(16, activation='relu'),
+    ...     molgraph.layers.GINConv(16, activation='relu')
+    ... ])
+    >>> gnn_model.output_shape
+    (None, None, 16)
+
+    Inputs a ``GraphTensor`` encoding a single disjoint graph:
+
+    >>> graph_tensor = molgraph.GraphTensor(
+    ...     data={
+    ...         'edge_dst': [0, 1, 2, 2, 3, 3, 4, 4],
+    ...         'edge_src': [1, 0, 3, 4, 2, 4, 3, 2],
+    ...         'node_feature': [
+    ...             [1.0, 0.0],
+    ...             [1.0, 0.0],
+    ...             [1.0, 0.0],
+    ...             [1.0, 0.0],
+    ...             [0.0, 1.0]
+    ...         ],
+    ...         'graph_indicator': [0, 0, 1, 1, 1],
+    ...     }
+    ... )
+    >>> # Build a model with GINConv
+    >>> gnn_model = tf.keras.Sequential([
+    ...     tf.keras.Input(type_spec=graph_tensor.unspecific_spec),
+    ...     molgraph.layers.GINConv(16, activation='relu'),
+    ...     molgraph.layers.GINConv(16, activation='relu')
+    ... ])
+    >>> gnn_model.output_shape
+    (None, 16)
+
+    Args:
+        units (int, None):
+            Number of output units.
+        self_projection (bool):
+            Whether to apply self projection. Default to True.
+        batch_norm: (bool):
+            Whether to apply batch normalization to the output. Default to True.
+        residual: (bool)
+            Whether to add skip connection to the output. Default to True.
+        dropout: (float, None):
+            Dropout applied to the output of the layer. Default to None.
+        activation (tf.keras.activations.Activation, callable, str, None):
+            Activation function applied to the output of the layer. Default to 'relu'.
+        use_bias (bool):
+            Whether the layer should use biases. Default to True.
+        kernel_initializer (tf.keras.initializers.Initializer, str):
+            Initializer function for the kernels. Default to
+            tf.keras.initializers.TruncatedNormal(stddev=0.005).
+        bias_initializer (tf.keras.initializers.Initializer, str):
+            Initializer function for the biases. Default to
+            tf.keras.initializers.Constant(0.).
+        kernel_regularizer (tf.keras.regularizers.Regularizer, None):
+            Regularizer function applied to the kernels. Default to None.
+        bias_regularizer (tf.keras.regularizers.Regularizer, None):
+            Regularizer function applied to the biases. Default to None.
+        activity_regularizer (tf.keras.regularizers.Regularizer, None):
+            Regularizer function applied to the final output of the layer.
+            Default to None.
+        kernel_constraint (tf.keras.constraints.Constraint, None):
+            Constraint function applied to the kernels. Default to None.
+        bias_constraint (tf.keras.constraints.Constraint, None):
+            Constraint function applied to the biases. Default to None.
+
+    References:
+        .. [#] https://arxiv.org/pdf/2003.00982.pdf
+        .. [#] https://arxiv.org/pdf/1810.00826.pdf
+
+    '''
+
+    def __init__(
+        self,
+        units: Optional[int] = None,
+        self_projection: bool = True,
+        batch_norm: bool = True,
+        residual: bool = True,
+        dropout: Optional[float] = None,
+        activation: Union[None, str, Callable[[tf.Tensor], tf.Tensor]] = 'relu',
+        use_bias: bool = True,
+        kernel_initializer: Union[
+            str, initializers.Initializer
+        ] = initializers.TruncatedNormal(stddev=0.005),
+        bias_initializer: Union[
+            str, initializers.Initializer
+        ] = initializers.Constant(0.),
+        kernel_regularizer: Optional[regularizers.Regularizer] = None,
+        bias_regularizer: Optional[regularizers.Regularizer] = None,
+        activity_regularizer: Optional[regularizers.Regularizer] = None,
+        kernel_constraint: Optional[constraints.Constraint] = None,
+        bias_constraint: Optional[constraints.Constraint] = None,
+        **kwargs
+    ):
+        super().__init__(
+            units=units,
+            batch_norm=batch_norm,
+            residual=residual,
+            dropout=dropout,
+            activation=activation,
+            use_bias=use_bias,
+            kernel_initializer=kernel_initializer,
+            bias_initializer=bias_initializer,
+            kernel_regularizer=kernel_regularizer,
+            bias_regularizer=bias_regularizer,
+            activity_regularizer=activity_regularizer,
+            kernel_constraint=kernel_constraint,
+            bias_constraint=bias_constraint,
+            **kwargs)
+
+        self.batch_norm = batch_norm
+        self.activation = activations.get('relu')
+        self.apply_self_projection = self_projection
+
+    def subclass_build(
+        self,
+        node_feature_shape: Optional[tf.TensorShape] = None,
+        edge_feature_shape: Optional[tf.TensorShape] = None
+    ) -> None:
+
+        self.projection_1 = self.get_dense(self.units)
+        self.projection_2 = self.get_dense(self.units)
+        self.epsilon = self.add_weight(
+            shape=(),
+            initializer='zeros',
+            regularizer=None,
+            trainable=True,
+            name='epsilon')
+
+        if self.batch_norm:
+            self.batch_norm = layers.BatchNormalization()
+        else:
+            self.batch_norm = None
+
+        if self.apply_self_projection:
+           self.self_projection = self.get_dense(self.units)
+
+    def subclass_call(self, tensor: GraphTensor) -> GraphTensor:
+
+        node_feature = propagate_node_features(
+            node_feature=tensor.node_feature,
+            edge_dst=tensor.edge_dst,
+            edge_src=tensor.edge_src)
+
+        node_feature = (
+            (1 + self.epsilon) * tensor.node_feature + node_feature)
+
+        node_feature = self.projection_1(node_feature)
+
+        if self.apply_self_projection:
+            node_feature += self.self_projection(tensor.node_feature)
+
+        if self.batch_norm is not None:
+            node_feature = self.batch_norm(node_feature)
+
+        node_feature = self.activation(node_feature)
+
+        node_feature = self.projection_2(node_feature)
+
+        return tensor.update({'node_feature': node_feature})
+
+    def get_config(self):
+        base_config = super().get_config()
+        config = {
+            'self_projection': self.apply_self_projection,
+        }
+        base_config.update(config)
+        return base_config
