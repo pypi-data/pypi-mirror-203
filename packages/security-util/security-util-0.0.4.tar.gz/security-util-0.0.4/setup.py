2be0cb0143bc7eed699e0db22819a7d36555e384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:21:41 +0000

    Add package security-util/0.0.4/security-util-0.0.4.tar.gz

---
+++ packages/security-util/security-util-0.0.4.tar.gz/security-util-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from setuptools.command.install import install
+
+setup(name='security-util',
+      version='0.0.4',
+      description='Security util for python',
+      author='Battle Furry <btlfry@gmail.com>',
+      license='MIT',
+      zip_safe=False)
