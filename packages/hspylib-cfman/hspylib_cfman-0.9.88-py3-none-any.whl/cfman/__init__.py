fcfa9712680ac6b598cfb456ed57a2db3830c948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:09 +0000

    Add package hspylib-cfman/0.9.88/hspylib_cfman-0.9.88-py3-none-any.whl

---
+++ packages/hspylib-cfman/hspylib_cfman-0.9.88-py3-none-any.whl/cfman/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-cfman v0.9.88
+#
+# Package: main.cfman
+"""Package initialization."""
+
+__all__ = [
+  '__classpath__', 
+  '__main__', 
+  'core', 
+  'exception'
+]
+__version__ = '0.9.88'
