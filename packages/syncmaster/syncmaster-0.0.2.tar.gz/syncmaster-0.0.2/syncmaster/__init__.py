74588c47c3fcc0162ea70620e38cfbdc2bb0217a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:17:51 +0000

    Add package syncmaster/0.0.2/syncmaster-0.0.2.tar.gz

---
+++ packages/syncmaster/syncmaster-0.0.2.tar.gz/syncmaster-0.0.2/syncmaster/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2"
+
+# Import required libraries
+import numpy as np
+import serial
+import serial.tools.list_ports
+
+# Import main functions
+from syncmaster.device import SyncMaster
+from syncmaster.analysis import getEvents
