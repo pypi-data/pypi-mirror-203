2793e3c8f638d1483ce2dace4475492f6809144f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:46 +0000

    Add package domino-code-assist/1.2.0/domino_code_assist-1.2.0-py2.py3-none-any.whl

---
+++ packages/domino-code-assist/domino_code_assist-1.2.0-py2.py3-none-any.whl/domino_code_assist/css.vue
@@ -0,0 +1,25 @@
+<template>
+</template>
+<style id="domino-css">
+  .domino-plotly-auto-height .js-plotly-plot {
+    height: 100%;
+  }
+
+  .domino-card-grid .modebar-container {
+    display: none;
+  }
+  .dca-transform-header {
+    background-color: var(--jp-border-color3);
+  }
+  .milkdown-menu .button.active {
+    background-color: var(--jp-border-color2);
+  }
+
+  .domino-thumbnail .js-plotly-plot {
+    height: 100%;
+  }
+  .domino-thumbnail .js-plotly-plot .plot-container.plotly {
+    height: 100%;
+  }
+
+</style>
