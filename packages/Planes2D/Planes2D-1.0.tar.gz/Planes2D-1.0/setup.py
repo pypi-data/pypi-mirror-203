f081c3769114fbd476cb276fe4118dce3b6a72ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:08 +0000

    Add package Planes2D/1.0/Planes2D-1.0.tar.gz

---
+++ packages/Planes2D/Planes2D-1.0.tar.gz/Planes2D-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0'
+DESCRIPTION = 'Streaming video data via networks'
+
+setup(
+    name="Planes2D",
+    version=VERSION,
+    author="KomornikBank0",
+    description=DESCRIPTION,
+    packages=find_packages(),
+)
