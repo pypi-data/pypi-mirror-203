c5a6e9d5e17a20a3525a2c2db2912b012dbd459f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:28 +0000

    Add package pyobjc-framework-LocalAuthenticationEmbeddedUI/9.1.1/pyobjc-framework-LocalAuthenticationEmbeddedUI-9.1.1.tar.gz

---
+++ packages/pyobjc-framework-LocalAuthenticationEmbeddedUI/pyobjc-framework-LocalAuthenticationEmbeddedUI-9.1.1.tar.gz/pyobjc-framework-LocalAuthenticationEmbeddedUI-9.1.1/setup.py
@@ -0,0 +1,26 @@
+"""
+Wrappers for the "LocalAuthenticationEmbeddedUI" framework on macOS.
+
+These wrappers don't include documentation, please check Apple's documentation
+for information on how to use this framework and PyObjC's documentation
+for general tips and tricks regarding the translation between Python
+and (Objective-)C frameworks
+"""
+
+from pyobjc_setup import setup
+
+VERSION = "9.1.1"
+
+setup(
+    name="pyobjc-framework-LocalAuthenticationEmbeddedUI",
+    description="Wrappers for the framework LocalAuthenticationEmbeddedUI on macOS",
+    min_os_level="12.0",
+    packages=["LocalAuthenticationEmbeddedUI"],
+    version=VERSION,
+    install_requires=[
+        "pyobjc-core>=" + VERSION,
+        "pyobjc-framework-Cocoa>=" + VERSION,
+        "pyobjc-framework-LocalAuthentication>=" + VERSION,
+    ],
+    long_description=__doc__,
+)
