30bca7d6c6b59d0207df69c0b033db9c44f5cfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:32:47 +0000

    Add package cf-wesgrh/0.2/cf_wesgrh-0.2.tar.gz

---
+++ packages/cf-wesgrh/cf_wesgrh-0.2.tar.gz/cf_wesgrh-0.2/cf_wesgrh/_init_.py
@@ -0,0 +1,5 @@
+from cf_wesgrh import File
+
+
+
+
---
+++ packages/cf-wesgrh/cf_wesgrh-0.2.tar.gz/cf_wesgrh-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='cf_wesgrh',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['cf_wesgrh'],
+      author_email='arinafim300999@mail.ru',
+      zip_safe=False)
