56de285d8c98e428db0b8a9faa2ae90ce5374eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:39 +0000

    Add package xlibs/0.0.1/xlibs-0.0.1.tar.gz

---
+++ packages/xlibs/xlibs-0.0.1.tar.gz/xlibs-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+#with open("requirements.txt") as f: required = f.read().splitlines()
+
+setup(
+  name = 'xlibs',       
+  packages = ['x'],  
+  version = 'v0.0.1',   
+  #install_requires = required,
+  python_requires='>3.8'
+)
---
+++ packages/xlibs/xlibs-0.0.1.tar.gz/xlibs-0.0.1/x/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+x: the everything pkg
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import thelogger as tl
