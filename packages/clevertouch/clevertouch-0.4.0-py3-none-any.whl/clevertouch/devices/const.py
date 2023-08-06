69f69886db9ec797aa855f4a0671c8d688823991
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:31 +0000

    Add package clevertouch/0.4.0/clevertouch-0.4.0-py3-none-any.whl

---
+++ packages/clevertouch/clevertouch-0.4.0-py3-none-any.whl/clevertouch/devices/const.py
@@ -0,0 +1,20 @@
+"""Define general constants"""
+from enum import IntEnum
+from ..util import StrEnum
+
+
+class DeviceType(StrEnum):
+    """Recognized device types"""
+
+    RADIATOR = "Radiator"
+    LIGHT = "Light"
+    OUTLET = "Outlet"
+    UNKNOWN = "Unknown"
+
+class DeviceTypeId(IntEnum):
+    """Recognized device type ids"""
+
+    RADIATOR = 0
+    LIGHT = 1
+    OUTLET = 12
+    UNDEFINED = -1
