315eac7721df04431e9292b08c51764033f4c7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:13 +0000

    Add package friebox/1.0.2/friebox-1.0.2.tar.gz

---
+++ packages/friebox/friebox-1.0.2.tar.gz/friebox-1.0.2/friebox/view/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin/env python 3.9.11
+"""
+@File    :  __init__.py
+@Time    :  2023/3/7 15:21 PM
+@Author  :  YuYanQing
+@Version :  1.0
+@Contact :  mryu168@163.com
+@License :  (C)Copyright 2022-2026
+@Desc    :  None
+"""
+from __future__ import absolute_import
+
+
