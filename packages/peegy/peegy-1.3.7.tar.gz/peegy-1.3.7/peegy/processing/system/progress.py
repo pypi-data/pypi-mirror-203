91bcb30adc9d4784f876e84a92a4fadb03f942d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:53 +0000

    Add package peegy/1.3.7/peegy-1.3.7.tar.gz

---
+++ packages/peegy/peegy-1.3.7.tar.gz/peegy-1.3.7/peegy/processing/system/progress.py
@@ -0,0 +1,22 @@
+from tqdm.auto import tqdm
+from joblib import Parallel
+
+
+class ParallelTqdm(Parallel):
+    def __init__(self, use_tqdm=True, total=None, desc='', *args, **kwargs):
+        self._use_tqdm = use_tqdm
+        self._total = total
+        self._desc = desc
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, *args, **kwargs):
+        with tqdm(disable=not self._use_tqdm,
+                  total=self._total,
+                  desc=self._desc) as self._pbar:
+            return Parallel.__call__(self, *args, **kwargs)
+
+    def print_progress(self):
+        if self._total is None:
+            self._pbar.total = self.n_dispatched_tasks
+        self._pbar.n = self.n_completed_tasks
+        self._pbar.refresh()
