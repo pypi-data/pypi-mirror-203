9515e17bcb2399de40b87e8f5873a5299512906e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:12 +0000

    Add package cressida/0.0.0/cressida-0.0.0.tar.gz

---
+++ packages/cressida/cressida-0.0.0.tar.gz/cressida-0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""The setup script."""
+
+# Third party modules
+from setuptools import setup
+
+setup(name='cressida',
+      version='0.0.0',
+      description='place holder',
+      url='http://github.com/SK/cressida',
+      author='sking',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['cressida'],
+      zip_safe=False)
