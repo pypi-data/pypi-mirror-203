eb176bc7087d8c495f938b354111e303d19fe304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:25 +0000

    Add package vutils-python/0.2.1/vutils_python-0.2.1-py2.py3-none-any.whl

---
+++ packages/vutils-python/vutils_python-0.2.1-py2.py3-none-any.whl/vutils/python/__init__.pyi
@@ -0,0 +1,8 @@
+#
+# File:    ./src/vutils/python/__init__.pyi
+# Author:  Jiří Kučera <sanczes AT gmail.com>
+# Date:    2022-06-24 14:03:43 +0200
+# Project: vutils-python: Python language tools
+#
+# SPDX-License-Identifier: MIT
+#
