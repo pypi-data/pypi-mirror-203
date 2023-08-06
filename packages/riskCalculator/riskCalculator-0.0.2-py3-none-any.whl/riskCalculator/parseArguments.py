883c8061f32ca38bd072159ca4b819e0b3b50681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:14 +0000

    Add package riskCalculator/0.0.2/riskCalculator-0.0.2-py3-none-any.whl

---
+++ packages/riskCalculator/riskCalculator-0.0.2-py3-none-any.whl/riskCalculator/parseArguments.py
@@ -0,0 +1,39 @@
+from argparse import *
+
+
+def parseArguments(defaultAttackers, defaultDefenders):
+    parser = ArgumentParser(description="arguments for risk calculation")
+    parser.add_argument(
+        'attackers',
+        type=int,
+        help='number of attackers',
+        default=defaultAttackers
+    )
+    parser.add_argument(
+        'defenders',
+        type=int,
+        help='number of defenders',
+        default=defaultDefenders
+    )
+    parser.add_argument(
+        '--read',
+        action=BooleanOptionalAction,
+        dest="read",
+        help="should use saved results"
+    )
+    parser.add_argument(
+        '--write',
+        action=BooleanOptionalAction,
+        dest="write",
+        help="should save results"
+    )
+    parser.add_argument(
+        '--recursion-limit',
+        type=int,
+        default=1000,
+        metavar="R",
+        dest="recursionLimit",
+        help="recursion limit, default 1000"
+    )
+
+    return parser.parse_args()
