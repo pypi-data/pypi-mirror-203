84591b83ed5e4e3fceaba6b725ccf7f0b77ad30f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:33 +0000

    Add package resolwe/34.0.0/resolwe-34.0.0-py3-none-any.whl

---
+++ packages/resolwe/resolwe-34.0.0-py3-none-any.whl/resolwe/api_urls.py
@@ -0,0 +1,32 @@
+"""Urls for Django Rest Framework."""
+from django.urls import include, path
+
+from rest_framework import routers
+
+from .flow.views import (
+    CollectionViewSet,
+    DataViewSet,
+    DescriptorSchemaViewSet,
+    EntityViewSet,
+    ProcessViewSet,
+    RelationViewSet,
+    StorageViewSet,
+)
+from .observers.views import BackgroundTaksViewSet
+from .storage.views import UploadConfig
+
+api_router = routers.DefaultRouter(trailing_slash=False)
+api_router.register(r"collection", CollectionViewSet)
+api_router.register(r"process", ProcessViewSet)
+api_router.register(r"data", DataViewSet)
+api_router.register(r"entity", EntityViewSet)
+api_router.register(r"relation", RelationViewSet)
+api_router.register(r"descriptorschema", DescriptorSchemaViewSet)
+api_router.register(r"storage", StorageViewSet)
+api_router.register(r"upload_config", UploadConfig, basename="upload_config")
+api_router.register(r"task", BackgroundTaksViewSet, basename="backgroundtask")
+
+
+urlpatterns = [
+    path("api/", include((api_router.urls, "resolwe-api"), namespace="resolwe-api")),
+]
