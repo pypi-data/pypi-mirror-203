d05a9fd585423b1da40863271149dc533184cc5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:41 +0000

    Add package siliconcompiler/0.11.0/siliconcompiler-0.11.0-cp39-cp39-win_amd64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.11.0-cp39-cp39-win_amd64.whl/siliconcompiler/tools/openroad/place.py
@@ -0,0 +1,24 @@
+
+from siliconcompiler.tools.openroad.openroad import setup as setup_tool
+from siliconcompiler.tools.openroad.openroad import build_pex_corners, post_process
+
+def setup(chip):
+    '''
+    Perform global and detail placements along with design violation repairs
+    '''
+
+    # Generic tool setup.
+    setup_tool(chip)
+
+    tool = 'openroad'
+    design = chip.top()
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+
+    if (not chip.valid('input', 'layout', 'def') or
+        not chip.get('input', 'layout', 'def', step=step, index=index)):
+        chip.add('tool', tool, 'task', task, 'input', design +'.def', step=step, index=index)
+
+def pre_process(chip):
+    build_pex_corners(chip)
