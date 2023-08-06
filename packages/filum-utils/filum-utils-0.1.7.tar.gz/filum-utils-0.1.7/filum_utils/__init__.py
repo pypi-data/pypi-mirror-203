cb7c093fd23fa7ea2692f0371efaac20266f44cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:43 +0000

    Add package filum-utils/0.1.7/filum-utils-0.1.7.tar.gz

---
+++ packages/filum-utils/filum-utils-0.1.7.tar.gz/filum-utils-0.1.7/filum_utils/__init__.py
@@ -0,0 +1,14 @@
+from .clients.action import ActionClient
+from .clients.analytics import AnalyticsClient
+from .clients.connection import ConnectionClient
+from .clients.mini_app import InstalledMiniAppClient
+from .clients.subscription_object import AutomatedActionClient, CampaignClient
+
+__all__ = [
+    ActionClient,
+    AnalyticsClient,
+    AutomatedActionClient,
+    CampaignClient,
+    ConnectionClient,
+    InstalledMiniAppClient
+]
