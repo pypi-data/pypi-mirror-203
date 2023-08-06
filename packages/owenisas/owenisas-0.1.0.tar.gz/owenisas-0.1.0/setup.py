7f2636f1779c72bcdab89af2126174a94fac4329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:29 +0000

    Add package owenisas/0.1.0/owenisas-0.1.0.tar.gz

---
+++ packages/owenisas/owenisas-0.1.0.tar.gz/owenisas-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="owenisas",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # Add package dependencies here, e.g.
+        # "numpy >= 1.18.0",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
