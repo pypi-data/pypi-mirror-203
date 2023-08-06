a674ecda4a4b48a1df9613f6836bc5893aeb9dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:47 +0000

    Add package lovematch/1.0.0/lovematch-1.0.0.tar.gz

---
+++ packages/lovematch/lovematch-1.0.0.tar.gz/lovematch-1.0.0/lovematch/__init__.py
@@ -0,0 +1,7 @@
+
+# Define package-level variables or configurations
+VERSION = '1.0.0'
+MAX_SCORE = 100
+
+# Import modules from the package
+from lovemeter import calculate_love_score, calculate_friendship_score
