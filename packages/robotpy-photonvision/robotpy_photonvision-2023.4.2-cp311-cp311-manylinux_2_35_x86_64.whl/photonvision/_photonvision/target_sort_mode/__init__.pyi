6979392aeb413b09dc21c19622c8ef0d296d97fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:13 +0000

    Add package robotpy-photonvision/2023.4.2/robotpy_photonvision-2023.4.2-cp311-cp311-manylinux_2_35_x86_64.whl

---
+++ packages/robotpy-photonvision/robotpy_photonvision-2023.4.2-cp311-cp311-manylinux_2_35_x86_64.whl/photonvision/_photonvision/target_sort_mode/__init__.pyi
@@ -0,0 +1,36 @@
+from __future__ import annotations
+import photonvision._photonvision.target_sort_mode
+import typing
+
+__all__ = [
+    "CenterMost",
+    "Highest",
+    "Largest",
+    "LeftMost",
+    "Lowest",
+    "RightMost",
+    "Smallest"
+]
+
+
+class CenterMost():
+    def __init__(self) -> None: ...
+    pass
+class Highest():
+    def __init__(self) -> None: ...
+    pass
+class Largest():
+    def __init__(self) -> None: ...
+    pass
+class LeftMost():
+    def __init__(self) -> None: ...
+    pass
+class Lowest():
+    def __init__(self) -> None: ...
+    pass
+class RightMost():
+    def __init__(self) -> None: ...
+    pass
+class Smallest():
+    def __init__(self) -> None: ...
+    pass
