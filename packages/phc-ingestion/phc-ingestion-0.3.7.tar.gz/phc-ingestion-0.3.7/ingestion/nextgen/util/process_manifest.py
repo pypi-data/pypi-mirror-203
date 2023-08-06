15f8a29920cc9b683cbdca30479a091ee94daa13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:44 +0000

    Add package phc-ingestion/0.3.7/phc-ingestion-0.3.7.tar.gz

---
+++ packages/phc-ingestion/phc-ingestion-0.3.7.tar.gz/phc-ingestion-0.3.7/ingestion/nextgen/util/process_manifest.py
@@ -0,0 +1,14 @@
+from ruamel.yaml import YAML
+from logging import Logger
+
+
+def process_manifest(xml_in_file: str, root_path: str, prefix: str, files: dict, log: Logger):
+    yaml = YAML()
+    manifest = {}
+    manifest["testType"] = "NextGen"
+    manifest_path = f"{root_path}/{prefix}/{prefix}.ga4gh.genomics.yml"
+    log.info(f"Saving file to {manifest_path}")
+    with open(manifest_path, "w") as file:
+        yaml.dump(manifest, file)
+
+    return manifest_path
