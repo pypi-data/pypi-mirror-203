a6818849d9edff554db79e9f26a84bc1ab165053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:56 +0000

    Add package dovado-rtl/0.10.4/dovado_rtl-0.10.4-py3-none-any.whl

---
+++ packages/dovado-rtl/dovado_rtl-0.10.4-py3-none-any.whl/dovado_rtl/parsers/utilities/parallel.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+class Parallel(ABC):
+    def __init__(self) -> None:
+        self._jobs: Optional[int] = None
+
+    @property
+    def jobs(self) -> int:
+        if self._jobs is None:
+            raise ValueError("Trying to retrieve None jobs")
+
+        return self._jobs
+
+    @jobs.setter
+    def jobs(self, num_jobs: int):
+        if self._jobs is not None:
+            raise ValueError("Number of jobs is " + str(self._jobs) + " trying to set twice")
+
+        self._jobs = num_jobs
+
+
