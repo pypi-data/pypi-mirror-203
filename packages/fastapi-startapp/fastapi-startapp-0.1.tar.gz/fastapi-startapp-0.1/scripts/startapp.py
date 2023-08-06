deb9f0ed04508eec9cba5adc097924d2cbae1c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:53 +0000

    Add package fastapi-startapp/0.1/fastapi-startapp-0.1.tar.gz

---
+++ packages/fastapi-startapp/fastapi-startapp-0.1.tar.gz/fastapi-startapp-0.1/scripts/startapp.py
@@ -0,0 +1,23 @@
+# -*-coding:utf-8-*-
+import click
+from ..core.generate_app import GenerateApp
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def clear():
+    return click.clear()
+
+
+@cli.command()
+@click.option('--app_name', '-a', required=True, help='app name')
+def cli(app_name: str):
+    click.echo('hello_world')
+    GenerateApp(app_name)
+
+
+
