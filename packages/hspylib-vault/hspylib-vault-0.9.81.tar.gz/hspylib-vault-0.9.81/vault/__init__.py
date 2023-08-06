60de558d44cdb6ff201bb899950e719e4a991a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:13 +0000

    Add package hspylib-vault/0.9.81/hspylib-vault-0.9.81.tar.gz

---
+++ packages/hspylib-vault/hspylib-vault-0.9.81.tar.gz/hspylib-vault-0.9.81/vault/__init__.py
@@ -0,0 +1,15 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-vault v0.9.81
+#
+# Package: main.vault
+"""Package initialization."""
+
+__all__ = [
+  '__classpath__', 
+  '__main__', 
+  'core', 
+  'domain', 
+  'exception'
+]
+__version__ = '0.9.81'
