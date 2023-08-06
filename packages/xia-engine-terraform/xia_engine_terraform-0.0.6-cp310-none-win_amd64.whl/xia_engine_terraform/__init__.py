b3348e2f500aac38bd464b7e5f3d87cf2ec609c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:14:16 +0000

    Add package xia-engine-terraform/0.0.6/xia_engine_terraform-0.0.6-cp310-none-win_amd64.whl

---
+++ packages/xia-engine-terraform/xia_engine_terraform-0.0.6-cp310-none-win_amd64.whl/xia_engine_terraform/__init__.py
@@ -0,0 +1,10 @@
+from xia_engine_terraform.engine import TerraformEngine, TerraformConnectParam, TerraformClient
+from xia_engine_terraform.engine import TerraformLocalEngine, TerraformLocalConnectParam, TerraformLocalClient
+
+
+__all__ = [
+    "TerraformEngine", "TerraformConnectParam", "TerraformClient",
+    "TerraformLocalEngine", "TerraformLocalConnectParam", "TerraformLocalClient"
+]
+
+__version__ = "0.0.6"
