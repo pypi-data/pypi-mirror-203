810b60a08118558fe1243ad7a35c69951859069e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:41 +0000

    Add package pyworld/0.3.3/pyworld-0.3.3.tar.gz

---
+++ packages/pyworld/pyworld-0.3.3.tar.gz/pyworld-0.3.3/setup.py
@@ -0,0 +1,52 @@
+from __future__ import with_statement, print_function, absolute_import
+
+from setuptools import setup, find_packages, Extension
+from distutils.version import LooseVersion
+
+from pathlib import Path
+import numpy
+
+from setuptools.command.build_ext import build_ext
+
+
+_VERSION = 'v0.3.3'
+
+world_src_top = Path("lib") / "World" / "src"
+world_sources = [str(f) for f in world_src_top.glob("*.cpp")]
+
+pyx = str(Path("pyworld") / "pyworld.pyx")
+world_sources
+
+ext_modules = [
+    Extension(
+        name="pyworld.pyworld",
+        include_dirs=[world_src_top, numpy.get_include()],
+        sources=[pyx] + world_sources,
+        language="c++")]
+
+setup(
+    name="pyworld",
+    description="PyWorld: a Python wrapper for WORLD vocoder",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext},
+    version=_VERSION,
+    packages=find_packages(),
+    setup_requires=[
+        'numpy',
+    ],
+    install_requires=[
+        'numpy',
+        'cython>=0.24',
+    ],
+    extras_require={
+        'test': ['nose'],
+        'sdist': ['numpy', 'cython'],
+    },
+    author="Pyworld Contributors",
+    author_email="jeremycchsu@gmail.com",
+    url="https://github.com/JeremyCCHsu/Python-Wrapper-for-World-Vocoder",
+    keywords=['vocoder'],
+    classifiers=[],
+)
