42d2fe90e0a67a37735a39e035406c97cc84849b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:06 +0000

    Add package fold-core/0.0.5/fold_core-0.0.5.tar.gz

---
+++ packages/fold-core/fold_core-0.0.5.tar.gz/fold_core-0.0.5/fold/loop/checks.py
@@ -0,0 +1,14 @@
+from typing import Optional, Tuple
+
+import pandas as pd
+
+
+def check_types(
+    X: Optional[pd.DataFrame], y: pd.Series
+) -> Tuple[pd.DataFrame, pd.Series]:
+    if X is None:
+        X = pd.DataFrame(0, index=y.index, columns=["X_not_available"])
+    else:
+        assert isinstance(X, pd.DataFrame), "X must be a pandas DataFrame."
+    assert isinstance(y, pd.Series), "y must be a pandas Series."
+    return X, y
