5bcfaf0db5c9256177ff6cccf38e1ddf96b8747f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:14 +0000

    Add package archytas/0.2.3/archytas-0.2.3-py3-none-any.whl

---
+++ packages/archytas/archytas-0.2.3-py3-none-any.whl/archytas/repl.py
@@ -0,0 +1,38 @@
+from archytas.react import ReActAgent, FailedTaskError
+from archytas.tools import datetime_tool, timestamp
+from archytas.demo_tools import fib_n, example_tool, calculator, Jackpot, ModelSimulation
+
+from rich import traceback, print; traceback.install(show_locals=True)
+from easyrepl import REPL
+
+import pdb
+
+
+def start_repl():
+    # make an instance of a class tool
+    jackpot = Jackpot(chips=1000)
+
+    # make a list of the tools to use
+    tools = [datetime_tool, timestamp, fib_n, example_tool, calculator, jackpot, ModelSimulation]
+
+    # create the agent
+    agent = ReActAgent(tools=tools, verbose=True)
+
+    # print the agent's prompt
+    # print(agent.prompt)
+
+    # run the REPL
+    for query in REPL(history_file='chat_history.txt'):
+        try:
+            answer = agent.react(query)
+            print(f'[green]{answer}[/green]')
+        except FailedTaskError as e:
+            print(f"[red]{e}[/red]")
+        except KeyboardInterrupt:
+            print('[yellow]KeyboardInterrupt[/yellow]')
+
+
+
+
+if __name__ == '__main__':
+    start_repl()
