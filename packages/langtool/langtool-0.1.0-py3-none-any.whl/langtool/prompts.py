b712150cf41e2d4e66f53e9b63b34580d840977a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:08 +0000

    Add package langtool/0.1.0/langtool-0.1.0-py3-none-any.whl

---
+++ packages/langtool/langtool-0.1.0-py3-none-any.whl/langtool/prompts.py
@@ -0,0 +1,22 @@
+import jinja2
+
+environment = jinja2.Environment()
+
+system_prompt = "You are a helpful assistant that only writes python code to follow instructions. Don't write anything other than code and comments."
+
+tool_prompt = environment.from_string(
+    """You have the following python {{ type }} available:
+
+name: {{ name }}
+description: {{ description }}
+documentation: {{ documentation }}
+
+Do the following only using the {{ name }} {{ type }}.:
+{{ instruction }}"""
+)
+
+
+def render_tool_prompt(name, type, description, instruction):
+    return tool_prompt.render(
+        name=name, type=type, description=description, instruction=instruction
+    )
