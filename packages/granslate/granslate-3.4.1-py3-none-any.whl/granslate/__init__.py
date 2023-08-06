426f0d030870638e5d814a7d794c5aeb328f25a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:40 +0000

    Add package granslate/3.4.1/granslate-3.4.1-py3-none-any.whl

---
+++ packages/granslate/granslate-3.4.1-py3-none-any.whl/granslate/__init__.py
@@ -0,0 +1,7 @@
+"""Free Google Translate API for Python. Translates totally free of charge."""
+__all__ = ("Translator",)
+__version__ = "3.4.1"
+
+
+from granslate.client import Translator
+from granslate.constants import LANGCODES, LANGUAGES
