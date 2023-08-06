2e5b2b30a6bc33eea97d0b79cf59bc09b1414a04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:10 +0000

    Add package Group-45/1.1/Group_45-1.1.tar.gz

---
+++ packages/Group-45/Group_45-1.1.tar.gz/Group_45-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+import setuptools
+setup(name='Group_45',
+      version='1.1',
+      description='ENSF 338 Final Project',
+      author='Aser-Ghobara',
+      author_email ='aser.ghobara@ucalgary.ca',
+      packages=setuptools.find_packages(),
+      install_requires=['pytest'],
+      )
