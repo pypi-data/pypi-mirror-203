751368cde0063ae9607f7a8f0a797ac7082ada7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:11 +0000

    Add package fwdviewpy/0.1.1/fwdviewpy-0.1.1.tar.gz

---
+++ packages/fwdviewpy/fwdviewpy-0.1.1.tar.gz/fwdviewpy-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='fwdviewpy',
+    version='0.1.1',
+    description='Python package developed by FWD View to aid automation of Delphix actions on both the Virtulaization and Continuous Compliance Delphix engines.',
+    packages=['fwdviewpy'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
+
+
+
+
+
