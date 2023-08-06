11d487e51c794af7b97485bf085060bda6bdf170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:04 +0000

    Add package inventree-rapidoc/1.0/inventree_rapidoc-1.0-py3-none-any.whl

---
+++ packages/inventree-rapidoc/inventree_rapidoc-1.0-py3-none-any.whl/inventree_rapidoc/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for InvenTree RapiDoc."""
+
+from .RapidocPlugin import RapidocPlugin
+
+__all__ = [RapidocPlugin, ]
+__author__ = "Matthias Mair"
+__email__ = "code@mjmair.com"
