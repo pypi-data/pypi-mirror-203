eb0f63002f7b22715974ab2926dcb8585eb3da1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:36 +0000

    Add package unlok/0.1.11/unlok-0.1.11-py3-none-any.whl

---
+++ packages/unlok/unlok-0.1.11-py3-none-any.whl/unlok/rath.py
@@ -0,0 +1,49 @@
+from graphql import OperationType
+from pydantic import Field
+from rath import rath
+import contextvars
+import logging
+
+from rath.links.base import TerminatingLink
+from rath.contrib.fakts.links.aiohttp import FaktsAIOHttpLink
+from rath.contrib.fakts.links.websocket import FaktsWebsocketLink
+from rath.contrib.herre.links.auth import HerreAuthLink
+from rath.links.aiohttp import AIOHttpLink
+from rath.links.auth import AuthTokenLink
+
+from rath.links.base import TerminatingLink
+from rath.links.compose import compose
+from rath.links.dictinglink import DictingLink
+from rath.links.shrink import ShrinkingLink
+from rath.links.split import SplitLink
+from rath.links.websockets import WebSocketLink
+from rath.links.compose import TypedComposedLink, compose
+
+
+current_unlok_rath = contextvars.ContextVar("current_unlok_rath")
+
+
+class UnlokLinkComposition(TypedComposedLink):
+    dicting: DictingLink = Field(default_factory=DictingLink)
+    auth: AuthTokenLink
+    split: SplitLink
+
+    def _repr_html_inline_(self):
+        return f"<table><tr><td>refresh attempts</td><td>{self.auth.maximum_refresh_attempts}</td></tr></table>"
+
+
+
+class UnlokRath(rath.Rath):
+    link: UnlokLinkComposition
+
+    async def __aenter__(self):
+        await super().__aenter__()
+        current_unlok_rath.set(self)
+        return self
+
+    def _repr_html_inline_(self):
+        return f"<table><tr><td>link</td><td>{self.link._repr_html_inline_()}</td></tr></table>"
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await super().__aexit__(exc_type, exc_val, exc_tb)
+        current_unlok_rath.set(None)
