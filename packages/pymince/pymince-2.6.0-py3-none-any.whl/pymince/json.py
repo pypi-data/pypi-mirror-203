2303edb630d37bb4dfefc79c09797ac894db93b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:16 +0000

    Add package pymince/2.6.0/pymince-2.6.0-py3-none-any.whl

---
+++ packages/pymince/pymince-2.6.0-py3-none-any.whl/pymince/json.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+"""Useful functions for working with JSONs."""
+
+import csv
+import dataclasses
+import datetime
+import decimal
+import functools
+import itertools
+import json
+import operator
+import textwrap
+import uuid
+import zipfile
+
+dumps = functools.partial(json.dumps, ensure_ascii=False)
+dump = functools.partial(json.dump, ensure_ascii=False)
+ENCODING = "utf-8"
+
+
+def load_from(filename, encoding=ENCODING):
+    """
+    Load JSON from a file using "utf-8" encoding.
+
+    Examples:
+        from pymince.json import load_from
+
+        dictionary = load_from("foo.json")
+    """
+    with open(filename, encoding=encoding) as file:
+        return json.load(file)
+
+
+def dump_into(filename, payload, encoding=ENCODING, **kwargs):
+    """
+    Dump JSON to a file using "utf-8" encoding.
+
+    Examples:
+        from pymince.json import dump_into
+
+        dump_into("foo.json", {"key": "value"})
+    """
+    with open(filename, "w", encoding=encoding) as file:
+        dump(payload, file, **kwargs)
+
+
+def dump_into_zip(zip_path, arcname, payload, **kwargs):
+    """
+    Dump JSON into the zip archive under the name arcname.
+
+    Examples:
+        from pymince.json import dump_into_zip
+
+        dump_into_zip("archive.zip", "foo.json", {"key": "value"})
+    """
+    with zipfile.ZipFile(zip_path, mode="w") as zf:
+        json_string = dumps(payload, **kwargs)
+        zf.writestr(arcname, json_string)
+
+
+def load_from_zip(zip_path, arcname):
+    """
+    Load JSON from a file named "arcname" inside a zip archive.
+
+    Examples:
+        from pymince.json import load_from_zip
+
+        dictionary = load_from_zip("archive.zip", "foo.json")
+    """
+    with zipfile.ZipFile(zip_path, mode="r") as zf, zf.open(arcname) as file:
+        return json.load(file)
+
+
+def dump_from_csv(
+    csv_path,
+    json_path,
+    /,
+    *,
+    fieldnames=None,
+    start=0,
+    stop=None,
+    strip=True,
+    encoding=ENCODING,
+    **kwargs,
+):
+    """
+    Dump CSV file to a JSON file using "utf-8" encoding.
+
+    :param str csv_path:
+    :param str json_path:
+    :param fieldnames: list of keys for the JSON
+    :param int start:
+        If start is specified, will skip all preceding elements;
+        otherwise, start defaults to zero.
+    :param int stop:
+    :param bool strip:
+        Whether or not white space should be removed from the
+        beginning and end of field values.
+    :param str encoding: utf-8 is used by default.
+    """
+
+    def read():
+        reader = csv.DictReader(csv_f, fieldnames=fieldnames)
+        if strip:
+            apply = operator.methodcaller("strip")
+            for row in reader:
+                yield {k and apply(k): (apply(v) if isinstance(v, str) else v) for k, v in row.items()}
+        else:
+            yield from reader
+
+    with open(csv_path, encoding=encoding, newline="") as csv_f:
+        # The official csv doc recommends opening the file with newline='' on all platforms to disable
+        # universal newlines translation.
+
+        data = itertools.islice(read(), start, stop)
+        pool = tuple(data)
+
+    dump_into(json_path, pool, encoding=encoding, **kwargs)
+
+
+def idump_into(filename, iterable, encoding=ENCODING, **kwargs):
+    """
+    Dump an iterable incrementally into a JSON file
+    using the "utf-8" encoding.
+    The result will always be an array with the elements of the iterable.
+
+    Examples:
+        from pymince.json import idump_into
+
+        it = iter([{"key": "foo"}, {"key": "bar"}])
+        dump_into("foo.json", it)
+    """
+
+    def worker(items):
+        encode = functools.partial(dumps, **kwargs)
+        indent = kwargs.get("indent")
+        prefix = " " * indent if indent else ""
+        yield "[\n"
+        obj = next(items, None)
+        if obj is not None:
+            yield textwrap.indent(encode(obj), prefix)
+        for obj in items:
+            yield ",\n"
+            yield textwrap.indent(encode(obj), prefix)
+
+        yield "\n"
+        yield "]"
+
+    it = iter(iterable)
+    with open(filename, mode="w", encoding=encoding) as f:
+        f.writelines(worker(it))
+
+
+class JSONEncoder(json.JSONEncoder):
+    """
+    JSON encoder that handles additional types compared
+    to `json.JSONEncoder`
+
+    - `datetime` and `date` are serialized to strings according to the isoformat.
+    - `decimal.Decimal` is serialized to a string.
+    - `uuid.UUID` is serialized to a string.
+    - `dataclasses.dataclass` is passed to `dataclasses.asdict`.
+    - `frozenset` and `set` are serialized by ordering their values.
+    """
+
+    def default(self, obj):
+        if isinstance(obj, datetime.date):
+            return obj.isoformat()
+        elif isinstance(obj, (frozenset, set)):
+            return sorted(obj)
+        elif isinstance(obj, (decimal.Decimal, uuid.UUID)):
+            return str(obj)
+        elif dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+        else:
+            # Let the base class default method raise the TypeError
+            return super().default(obj)
