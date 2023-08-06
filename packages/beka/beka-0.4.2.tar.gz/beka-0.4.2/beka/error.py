05586b578ba3f1922a0ee4f4a88b236ebcf52727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:09:03 +0000

    Add package beka/0.4.2/beka-0.4.2.tar.gz

---
+++ packages/beka/beka-0.4.2.tar.gz/beka-0.4.2/beka/error.py
@@ -0,0 +1,7 @@
+"""Exception classes for beka"""
+
+class SocketClosedError(Exception):
+    """Exception raised when socket closed error occurs"""
+
+class IdleError(Exception):
+    """Exception raised when idle error occurs"""
