97d36a335a9cdd69d45b091236646505f9c22f7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:13 +0000

    Add package dblite/1.3.1/dblite-1.3.1.tar.gz

---
+++ packages/dblite/dblite-1.3.1.tar.gz/dblite-1.3.1/src/dblite/__init__.py
@@ -0,0 +1,15 @@
+"""
+------------------------------------------------------------------------------
+This file is part of dblite - simple query interface for SQL databases.
+Released under the MIT License.
+
+@author      Erki Suurjaak
+@created     18.11.2022
+@modified    18.04.2023
+------------------------------------------------------------------------------
+"""
+__version__      = "1.3.1"
+__version_info__ = (1, 3, 1)
+__version_date__ = "2023-04-18"
+
+from . api import *
