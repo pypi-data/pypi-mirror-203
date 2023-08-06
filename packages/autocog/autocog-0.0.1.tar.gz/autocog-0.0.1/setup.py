f687f1a73446a7d5f441c8fe152a9457271bc4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:03 +0000

    Add package autocog/0.0.1/autocog-0.0.1.tar.gz

---
+++ packages/autocog/autocog-0.0.1.tar.gz/autocog-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="autocog",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "click",
+        "Pillow",
+    ],
+    entry_points={
+        "console_scripts": [
+            "autocog = autocog.autocog:autocog",
+        ],
+    },
+)
