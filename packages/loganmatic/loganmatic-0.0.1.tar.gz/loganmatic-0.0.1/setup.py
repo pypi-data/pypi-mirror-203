28a2bcdface913a25c66b2f0c373c622175f3c3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:39 +0000

    Add package loganmatic/0.0.1/loganmatic-0.0.1.tar.gz

---
+++ packages/loganmatic/loganmatic-0.0.1.tar.gz/loganmatic-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='loganmatic',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        # lista de dependências do pacote
+    ],
+    author='Gabriel Logan',
+    description='Biblioteca de matematica com algumas funções para facilitar calculos',
+    url='https://github.com/gabriel-logan/Math_Lib_Js',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
