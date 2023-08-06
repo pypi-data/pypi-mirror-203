b9629f2d3ee2821f96111d3efa5d349fe256ccd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:30 +0000

    Add package viggonuvemfiscal/1.0.5/viggonuvemfiscal-1.0.5.tar.gz

---
+++ packages/viggonuvemfiscal/viggonuvemfiscal-1.0.5.tar.gz/viggonuvemfiscal-1.0.5/viggonuvemfiscal/subsystem/controle/configuracao_nuvem_fiscal/manager.py
@@ -0,0 +1,31 @@
+import requests
+import os
+import flask
+from vex.subsystem.aplicacao.tabela_preco.resource import TabelaPreco
+from viggocore.common.subsystem import operation, manager
+from viggocore.common import exception, utils
+
+
+class Create(operation.Create):
+
+    def pre(self, session, **kwargs):
+        configuracao_nuvem_fiscals = self.manager.list()
+        if len(configuracao_nuvem_fiscals) > 0:
+            raise exception.BadRequest(
+                'Já existe uma configuração, por favor edite ela!')
+        return super().pre(session, **kwargs)
+
+
+class Manager(manager.Manager):
+
+    def __init__(self, driver):
+        super(Manager, self).__init__(driver)
+        self.create = Create(self)
+    
+    def get_configuracao_nuvem_fiscal(self):
+        configs = self.list()
+        if len(configs) == 0:
+            raise exception.NotFound(
+                'Nenhuma configuração da Nuvem Fiscal cadastrada, por favor ' +
+                'cadastre uma para usar a api.')
+        return configs[0]
