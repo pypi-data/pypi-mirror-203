33e5696caf677bbe3442d8671b88cd4d83a6d948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:56 +0000

    Add package np-distributions/1.0/np_distributions-1.0.tar.gz

---
+++ packages/np-distributions/np_distributions-1.0.tar.gz/np_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='np_distributions',
+      version='1.0',
+      description='Gaussian and binomial distributions',
+      packages=['np_distributions'],
+      author= 'Nadine Puetzer',
+      author_email= 'nadine.puetzer@freenet.de',
+      zip_safe=False)
