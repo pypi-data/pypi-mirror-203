47e9739b0f41d62e192744e1ee52f62c37e0a175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:58 +0000

    Add package pyriksprot/2023.4.2/pyriksprot-2023.4.2.tar.gz

---
+++ packages/pyriksprot/pyriksprot-2023.4.2.tar.gz/pyriksprot-2023.4.2/pyriksprot/corpus/parlaclarin/__init__.py
@@ -0,0 +1,6 @@
+# type: ignore
+
+from .convert import ProtocolConverter, convert_protocol, dehyphen, pretokenize
+from .iterate import XmlProtocolSegmentIterator, XmlUntangleSegmentIterator
+from .parse import ProtocolMapper
+from .tf import TermFrequencyCounter
