74afcbee9ef445c10ba2bd60d5c89f13422bc959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:44 +0000

    Add package from-m3u8-get-mp4/1.2/from_m3u8_get_mp4-1.2.tar.gz

---
+++ packages/from-m3u8-get-mp4/from_m3u8_get_mp4-1.2.tar.gz/from_m3u8_get_mp4-1.2/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name = 'from_m3u8_get_mp4',
+    version = '1.2',
+    author = 'WUT_ljs',
+    author_email = '3480339804@qq.com',
+    url = 'https://github.com/wutljs/from_m3u8_get_mp4',
+    description = 'Get vedio',
+    long_description = 'download MP3 files from M3U8 files quickly and accurately',
+    packages = setuptools.find_packages(),
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
