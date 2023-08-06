c1754efb7b92e44ccd92222b6a90a8491da181ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:46 +0000

    Add package torchfitter/4.2.2/torchfitter-4.2.2-py3-none-any.whl

---
+++ packages/torchfitter/torchfitter-4.2.2-py3-none-any.whl/torchfitter/__init__.py
@@ -0,0 +1,19 @@
+""" PyTorch models fitting package. """
+
+# relative subpackages import
+from . import callbacks, conventions, io, manager, testing, trainer, utils
+
+__all__ = [
+    "io",
+    "utils",
+    "trainer",
+    "testing",
+    "manager",
+    "callbacks",
+    "conventions",
+]
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
