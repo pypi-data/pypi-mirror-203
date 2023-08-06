f33c1690c48495973de952dbf0dafd722ef08d20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:46 +0000

    Add package dynamobase/1.0.0/dynamobase-1.0.0.tar.gz

---
+++ packages/dynamobase/dynamobase-1.0.0.tar.gz/dynamobase-1.0.0/tests/test.py
@@ -0,0 +1,13 @@
+import sys, os
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
+
+from DynamoBase import DynamoBase
+
+DynamoBase.table_region = "ap-southeast-2"
+DynamoBase.table_name = "users"
+
+# DynamoBase.put_item(Item={"first_name": "Jackson", "age": 15})
+user = DynamoBase.get_first(query={"first_name": "Jackson"}, ScanIndexForward=False)
+# DynamoBase.delete_item(query=user)
+print(user)
