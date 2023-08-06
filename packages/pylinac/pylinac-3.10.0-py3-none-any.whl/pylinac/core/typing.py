890b179f45040073896bac69af3ee3ebf13a05d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:01 +0000

    Add package pylinac/3.10.0/pylinac-3.10.0-py3-none-any.whl

---
+++ packages/pylinac/pylinac-3.10.0-py3-none-any.whl/pylinac/core/typing.py
@@ -0,0 +1,7 @@
+from typing import Union
+
+import numpy as np
+
+ArrayLike = Union[list, tuple, np.ndarray]
+
+NumberOrArray = Union[float, ArrayLike]
---
+++ packages/pylinac/pylinac-3.10.0-py3-none-any.whl/pylinac/image_analysis.py
@@ -0,0 +1,14 @@
+
+# profile analysis protocols
+
+
+class FieldAnalysis:
+
+
+    def __init__(self):
+        pass
+
+    def from_device(self):
+        pass
+
+
