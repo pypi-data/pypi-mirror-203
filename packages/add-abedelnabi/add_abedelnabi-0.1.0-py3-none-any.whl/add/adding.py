804302dc94b3e44ea1e86d31194f1301ab4a119b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:35 +0000

    Add package add-abedelnabi/0.1.0/add_abedelnabi-0.1.0-py3-none-any.whl

---
+++ packages/add-abedelnabi/add_abedelnabi-0.1.0-py3-none-any.whl/add/adding.py
@@ -0,0 +1,19 @@
+def add_two_numbers(x, y):
+    """Takes two numbers and adds them to each other
+
+    Parameters
+    ----------
+    x : int
+        First number to add
+        
+    y : int
+        Second number to add
+        
+
+    Returns
+    -------
+    int
+        the sum of both numbers
+
+    """
+    return x + y
