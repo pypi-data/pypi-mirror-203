bcc71a59b002e7d72029f15d904d4864bd7235f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:35 +0000

    Add package SrbAI/0.0.4/SrbAI-0.0.4.tar.gz

---
+++ packages/SrbAI/SrbAI-0.0.4.tar.gz/SrbAI-0.0.4/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="SrbAI",
+    version="0.0.4",
+    author="Serbian AI Society",
+    author_email="nikola.milosevic86@gmail.com",
+    description="Library for processing serbian language",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "nltk",
+        "torch>=1.10.2",
+        "classla==1.1.0",
+        "nlu"
+
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6.15",
+)
