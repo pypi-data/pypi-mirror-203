eee70c74f499b2852135522e990d5707075415a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:08 +0000

    Add package aicspylibczi/3.1.2/aicspylibczi-3.1.2.tar.gz

---
+++ packages/aicspylibczi/aicspylibczi-3.1.2.tar.gz/aicspylibczi-3.1.2/c_tests/test_main.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <pybind11/embed.h>
+
+namespace py = pybind11;
+
+py::scoped_interpreter guard{};
+
+TEST_CASE("cast_test", "Is FILE * default constructible")
+{
+  REQUIRE(std::is_default_constructible<FILE*>());
+  // REQUIRE(std::is_default_constructible<std::istream>()); // istream is NOT
+  // default constructible/**/!
+}
