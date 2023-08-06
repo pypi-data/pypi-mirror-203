10fbed3486e089faaade5c8124b75280b30ead4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:11 +0000

    Add package betp-libv/0.1.0/betp-libv-0.1.0.tar.gz

---
+++ packages/betp-libv/betp-libv-0.1.0.tar.gz/betp-libv-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='betp-libv',
+    version='0.1.0',
+    description='A sample Python package',
+    author='Puddl',
+    author_email='devppong@gmail.com',
+    url='https://github.com/devppong/betp-libv',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    python_requires='>=3.6',
+)
