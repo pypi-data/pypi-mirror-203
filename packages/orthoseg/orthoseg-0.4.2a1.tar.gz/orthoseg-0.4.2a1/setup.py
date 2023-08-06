7947f22ca5f494992f9cc661e500dbbd17379e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:22 +0000

    Add package orthoseg/0.4.2a1/orthoseg-0.4.2a1.tar.gz

---
+++ packages/orthoseg/orthoseg-0.4.2a1.tar.gz/orthoseg-0.4.2a1/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("orthoseg/version.txt", mode="r") as file:
+    version = file.readline()
+
+setuptools.setup(
+    name="orthoseg",
+    version=version,
+    author="Pieter Roggemans",
+    author_email="pieter.roggemans@gmail.com",
+    description="Package to make it easier to segment orthophotos.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/orthoseg/orthoseg",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "fiona",
+        "gdown",
+        "geofileops>0.5,<0.7",
+        "geopandas",
+        "owslib",
+        "pillow",
+        "pycron",
+        "rasterio",
+        "segmentation-models>=1.0,<1.1",
+        "simplification",
+        "tensorflow>=2.5,<2.11",
+    ],
+    entry_points="""
+            [console_scripts]
+            orthoseg_load_images=orthoseg.load_images:main
+            orthoseg_train=orthoseg.train:main
+            orthoseg_predict=orthoseg.predict:main
+            orthoseg_postprocess=orthoseg.postprocess:main
+            osscriptrunner=orthoseg.scriptrunner:main
+            orthoseg_load_sampleprojects=orthoseg.load_sampleprojects:main
+            """,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
