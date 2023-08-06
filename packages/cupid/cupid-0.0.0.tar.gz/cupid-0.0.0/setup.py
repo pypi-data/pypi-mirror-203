f3624c916491c4ac7e2f86be67edd69ff5b938a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:11 +0000

    Add package cupid/0.0.0/cupid-0.0.0.tar.gz

---
+++ packages/cupid/cupid-0.0.0.tar.gz/cupid-0.0.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""The setup script."""
+
+# Third party modules
+from setuptools import setup
+
+setup(name='cupid',
+      version='0.0.0',
+      description='place holder',
+      url='http://github.com/SK/cupid',
+      author='sking',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['cupid'],
+      zip_safe=False)
