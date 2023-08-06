26c3231a1516042a6ea9eb06d74b9dc764241cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:22 +0000

    Add package protolizer/1.1.0/protolizer-1.1.0.tar.gz

---
+++ packages/protolizer/protolizer-1.1.0.tar.gz/protolizer-1.1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="protolizer",
+    version="1.1.0",
+    author='MosyDev',
+    author_email='mostafa.uwsgi@gmail.com',
+    description='A simple library to serialize and deserialize protobuf messages',
+    url='https://github.com/its0x4d/protolizer',
+    packages=setuptools.find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    keywords='protobuf serialization deserialization',
+)
