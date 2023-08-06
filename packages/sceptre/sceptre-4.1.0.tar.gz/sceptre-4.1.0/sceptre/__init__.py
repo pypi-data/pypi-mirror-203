7fa86d3095d62a37ca0d3b082d2c509c86109297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:12 +0000

    Add package sceptre/4.1.0/sceptre-4.1.0.tar.gz

---
+++ packages/sceptre/sceptre-4.1.0.tar.gz/sceptre-4.1.0/sceptre/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+import warnings
+
+
+__author__ = "Cloudreach"
+__email__ = "sceptre@cloudreach.com"
+__version__ = "4.1.0"
+
+
+# Set up logging to ``/dev/null`` like a library is supposed to.
+# http://docs.python.org/3.3/howto/logging.html#configuring-logging-for-a-library
+class NullHandler(logging.Handler):  # pragma: no cover
+    def emit(self, record):
+        pass
+
+
+if not sys.warnoptions:
+    warnings.filterwarnings("default", category=DeprecationWarning, module="sceptre")
+
+logging.getLogger("sceptre").addHandler(NullHandler())
