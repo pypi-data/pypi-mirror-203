20056364100b51e93dd4cb1b9e931ced8fd8ad29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:19 +0000

    Add package karrio.amazon-mws/2023.4/karrio.amazon_mws-2023.4-py3-none-any.whl

---
+++ packages/karrio.amazon-mws/karrio.amazon_mws-2023.4-py3-none-any.whl/karrio/providers/amazon_mws/rate.py
@@ -0,0 +1,106 @@
+import amazon_mws_lib.rate_request as amazon
+from amazon_mws_lib.rate_response import ServiceRate
+
+import typing
+import karrio.lib as lib
+import karrio.core.units as units
+import karrio.core.models as models
+import karrio.core.errors as errors
+import karrio.providers.amazon_mws.error as provider_error
+import karrio.providers.amazon_mws.units as provider_units
+import karrio.providers.amazon_mws.utils as provider_utils
+
+
+def parse_rate_response(
+    _response: lib.Deserializable[dict], settings: provider_utils.Settings
+) -> typing.Tuple[typing.List[models.RateDetails], typing.List[models.Message]]:
+    response = _response.deserialize()
+    errors: typing.List[models.Message] = sum(
+        [
+            provider_error.parse_error_response(data, settings)
+            for data in response.get("errors", [])
+        ],
+        [],
+    )
+    rates = [
+        _extract_details(data, settings) for data in response.get("serviceRates", [])
+    ]
+
+    return rates, errors
+
+
+def _extract_details(
+    data: dict,
+    settings: provider_utils.Settings,
+) -> models.RateDetails:
+    rate = lib.to_object(ServiceRate, data)
+    transit = (
+        lib.to_date(rate.promise.deliveryWindow.start, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+        - lib.to_date(rate.promise.receiveWindow.end, "%Y-%m-%dT%H:%M:%S.%fZ").date()
+    ).days
+
+    return models.RateDetails(
+        carrier_id=settings.carrier_id,
+        carrier_name=settings.carrier_name,
+        service=provider_units.Service.map(rate.serviceType).name_or_key,
+        total_charge=lib.to_decimal(rate.totalCharge.value),
+        currency=rate.totalCharge.unit,
+        transit_days=transit,
+        meta=dict(
+            service_name=rate.serviceType,
+        ),
+    )
+
+
+def rate_request(payload: models.RateRequest, _) -> lib.Serializable:
+    packages = lib.to_packages(payload.parcels)
+    options = lib.to_shipping_options(payload.options)
+    services = lib.to_services(payload.services, provider_units.Service)
+
+    request = amazon.RateRequest(
+        shipFrom=amazon.Ship(
+            name=payload.shipper.person_name,
+            city=payload.shipper.city,
+            addressLine1=lib.text(
+                payload.shipper.street_number, payload.shipper.address_line1
+            ),
+            addressLine2=payload.shipper.address_line2,
+            stateOrRegion=payload.shipper.state_code,
+            email=payload.shipper.email,
+            copyEmails=lib.join(payload.shipper.email),
+            phoneNumber=payload.shipper.phone_number,
+        ),
+        shipTo=amazon.Ship(
+            name=payload.recipient.person_name,
+            city=payload.recipient.city,
+            addressLine1=lib.text(
+                payload.recipient.street_number, payload.recipient.address_line1
+            ),
+            addressLine2=payload.recipient.address_line2,
+            stateOrRegion=payload.recipient.state_code,
+            email=payload.recipient.email,
+            copyEmails=lib.join(payload.recipient.email),
+            phoneNumber=payload.recipient.phone_number,
+        ),
+        serviceTypes=list(services),
+        shipDate=lib.fdatetime(
+            options.shipment_date.state, "%Y-%m-%d", "%Y-%m-%dT%H:%M:%S.%fZ"
+        ),
+        containerSpecifications=[
+            amazon.ContainerSpecification(
+                dimensions=amazon.Dimensions(
+                    height=package.height.IN,
+                    length=package.length.IN,
+                    width=package.width.IN,
+                    unit="IN",
+                ),
+                weight=amazon.Weight(
+                    value=package.weight.LB,
+                    unit="LB",
+                ),
+            )
+            for package in packages
+        ],
+    )
+
+    return lib.Serializable(request, lib.to_dict)
---
+++ packages/karrio.amazon-mws/karrio.amazon_mws-2023.4-py3-none-any.whl/karrio/providers/amazon_mws/shipment/create.py
@@ -0,0 +1,123 @@
+import amazon_mws_lib.purchase_shipment_request as amazon
+from amazon_mws_lib.purchase_shipment_response import LabelResult
+
+import typing
+import karrio.lib as lib
+import karrio.core.units as units
+import karrio.core.models as models
+import karrio.providers.amazon_mws.error as provider_error
+import karrio.providers.amazon_mws.units as provider_units
+import karrio.providers.amazon_mws.utils as provider_utils
+
+
+def parse_shipment_response(
+    _response: lib.Deserializable[dict], settings: provider_utils.Settings
+) -> typing.Tuple[models.ShipmentDetails, typing.List[models.Message]]:
+    response = _response.deserialize()
+    errors: typing.List[models.Message] = sum(
+        [
+            provider_error.parse_error_response(data, settings)
+            for data in response.get("errors", [])
+        ],
+        [],
+    )
+    shipment = lib.to_multi_piece_shipment(
+        [
+            (index, _extract_details(data, data.get("shipmentId"), settings))
+            for index, data in enumerate(response.get("labelResults", []))
+        ]
+    )
+
+    return shipment, errors
+
+
+def _extract_details(
+    data: dict,
+    shipment_identifier: str,
+    settings: provider_utils.Settings,
+) -> models.ShipmentDetails:
+    result = lib.to_object(LabelResult, data)
+    label = lib.image_to_pdf(result.label.labelStream)
+
+    return models.ShipmentDetails(
+        carrier_id=settings.carrier_id,
+        carrier_name=settings.carrier_name,
+        tracking_number=result.trackingId,
+        shipment_identifier=shipment_identifier,
+        label_type="PDF",
+        docs=models.Documents(label=label),
+        meta=dict(
+            containerReferenceId=result.containerReferenceId,
+        ),
+    )
+
+
+def shipment_request(payload: models.ShipmentRequest, _) -> lib.Serializable:
+    packages = lib.to_packages(payload.parcels)
+    options = lib.to_shipping_options(payload.options)
+
+    request = amazon.PurchaseShipmentRequest(
+        clientReferenceId=payload.reference or payload.id,
+        shipFrom=amazon.Ship(
+            name=payload.shipper.person_name,
+            city=payload.shipper.city,
+            addressLine1=lib.text(
+                payload.shipper.street_number, payload.shipper.address_line1
+            ),
+            addressLine2=payload.shipper.address_line2,
+            stateOrRegion=payload.shipper.state_code,
+            email=payload.shipper.email,
+            phoneNumber=payload.shipper.phone_number,
+        ),
+        shipTo=amazon.Ship(
+            name=payload.recipient.person_name,
+            city=payload.recipient.city,
+            addressLine1=lib.text(
+                payload.recipient.street_number, payload.recipient.address_line1
+            ),
+            addressLine2=payload.recipient.address_line2,
+            stateOrRegion=payload.recipient.state_code,
+            email=payload.recipient.email,
+            phoneNumber=payload.recipient.phone_number,
+        ),
+        shipDate=lib.fdatetime(options.shipment_date.state, "%Y-%m-%d"),
+        serviceType=provider_units.Service.map(payload.service).name_or_key,
+        containers=[
+            amazon.Container(
+                containerType="PACKAGE",
+                containerReferenceId=package.parcel.id,
+                dimensions=amazon.Dimensions(
+                    height=package.height.IN,
+                    length=package.length.IN,
+                    width=package.width.IN,
+                    unit="IN",
+                ),
+                weight=amazon.UnitWeight(
+                    value=package.weight.LB,
+                    unit="LB",
+                ),
+                items=[
+                    amazon.Item(
+                        quantity=item.quantity,
+                        title=lib.text(item.title or item.description, max=35),
+                        unitPrice=amazon.UnitPrice(
+                            value=item.value_amount,
+                            unit=item.value_currency,
+                        ),
+                        unitWeight=amazon.UnitWeight(
+                            value=units.Weight(item.weight, item.weight_unit).LB,
+                            unit="LB",
+                        ),
+                    )
+                    for item in package.parcel.items
+                ],
+            )
+            for package in packages
+        ],
+        labelSpecification=amazon.LabelSpecification(
+            labelFormat="PNG",
+            labelStockSize="4X6",
+        ),
+    )
+
+    return lib.Serializable(request, lib.to_dict)
