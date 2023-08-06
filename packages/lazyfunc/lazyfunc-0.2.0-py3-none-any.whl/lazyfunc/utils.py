e756df8b273bca19bde5a67eccc244b9beeac39d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:01 +0000

    Add package lazyfunc/0.2.0/lazyfunc-0.2.0-py3-none-any.whl

---
+++ packages/lazyfunc/lazyfunc-0.2.0-py3-none-any.whl/lazyfunc/utils.py
@@ -0,0 +1,16 @@
+def callable_name(func):
+    """Return the name of any callable, regardless of whether it is a function or
+    class."""
+    try:
+        return func.__name__
+    except AttributeError:
+        return func.__class__.__name__
+
+
+def add_parentheses(s):
+    """Add parentheses around the string s."""
+    return "".join(["(", s, ")"])
+
+
+def insert(original_string, inserted_string, index):
+    return original_string[:index] + inserted_string + original_string[index:]
