71eb2ea75c8be18a41bd2eeec56dc3eac0c5317b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:30 +0000

    Add package wonder-sdk/0.1.13/wonder-sdk-0.1.13.tar.gz

---
+++ packages/wonder-sdk/wonder-sdk-0.1.13.tar.gz/wonder-sdk-0.1.13/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='wonder-sdk',
+    version='0.1.13',
+    description='Python SDK for common Wonder functions',
+    author='basri',
+    author_email='basri@wonder.co',
+    packages=['wonder_sdk'],
+    install_requires=[],
+)
