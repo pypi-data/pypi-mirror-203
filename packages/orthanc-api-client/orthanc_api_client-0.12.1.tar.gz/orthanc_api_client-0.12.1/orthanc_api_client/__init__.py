2f7ab4b7a4245d2140d0466e2f07cbcb4238b23f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:51 +0000

    Add package orthanc-api-client/0.12.1/orthanc_api_client-0.12.1.tar.gz

---
+++ packages/orthanc-api-client/orthanc_api_client-0.12.1.tar.gz/orthanc_api_client-0.12.1/orthanc_api_client/__init__.py
@@ -0,0 +1,15 @@
+from .api_client import OrthancApiClient
+from .exceptions import *
+from .helpers import *
+from .change import ChangeType, ResourceType
+from .study import Study, StudyInfo
+from .series import Series, SeriesInfo
+from .instance import Instance, InstanceInfo
+from .instances_set import InstancesSet
+from .job import Job, JobInfo, JobType, JobStatus
+from .http_client import HttpClient
+from .downloaded_instance import DownloadedInstance
+
+# __all__ = [
+#     'OrthancApiClient'
+# ]
