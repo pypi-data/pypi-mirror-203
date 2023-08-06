21f01b725b020fb06296ff04f14130160cf97e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:06:51 +0000

    Add package gaussidan/0.1.5/gaussidan-0.1.5.tar.gz

---
+++ packages/gaussidan/gaussidan-0.1.5.tar.gz/gaussidan-0.1.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gaussidan']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['lmfit==1.0.2', 'numpy>=1.19.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'gaussidan',
+    'version': '0.1.5',
+    'description': '',
+    'long_description': None,
+    'author': 'Daniel Williams',
+    'author_email': 'daniel.mays.williams@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.8,<3.12',
+}
+
+
+setup(**setup_kwargs)
