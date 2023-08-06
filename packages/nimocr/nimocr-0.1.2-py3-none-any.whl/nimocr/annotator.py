ef2bca9fac42432c5a77bdd358aa3a8058f67244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:36 +0000

    Add package nimocr/0.1.2/nimocr-0.1.2-py3-none-any.whl

---
+++ packages/nimocr/nimocr-0.1.2-py3-none-any.whl/nimocr/annotator.py
@@ -0,0 +1,37 @@
+from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QIcon
+from nimocr.model.image_list import ImageListModel
+from nimocr.presenter.presenter import Presenter
+from nimocr.view.main_window import MainWindow
+import logging
+
+
+def setup_logger():
+    format = "%(filename)s - %(levelname)s - %(funcName)s - %(message)s"
+    logging.basicConfig(level=logging.INFO, format=format)
+    logging.info("Logger initialized with INFO level")
+
+
+def run():
+    app = QApplication([])
+    app.setWindowIcon(QIcon("assets/logo.png"))
+
+    model = ImageListModel()
+    view = MainWindow()
+    presenter = Presenter(model, view)
+
+    app.exec()
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--verbose", action="store_true", help="Enable verbose logging"
+    )
+    args = parser.parse_args()
+
+    if args.verbose:
+        setup_logger()
+    run()
