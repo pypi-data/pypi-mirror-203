190e47d2d13a39fb454190d7075c60efbf77fbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:13:56 +0000

    Add package shunting-yard/1.0.8/shunting-yard-1.0.8.tar.gz

---
+++ packages/shunting-yard/shunting-yard-1.0.8.tar.gz/shunting-yard-1.0.8/shunting_yard/constants.py
@@ -0,0 +1,13 @@
+from string import ascii_lowercase, digits
+
+
+UNARY_OPERATORS = '+-'
+UNARY_OPERATORS_SYMBOLS = ('-u', '+u')
+BASE_OPERATORS = '+-*/^'
+NUMBER_CHARS = digits + '.'
+# functions cannot start with a number
+FUNCTION_FIRST_CHARS = ascii_lowercase + '_'
+FUNCTION_CHARS = FUNCTION_FIRST_CHARS + NUMBER_CHARS
+
+SEPARATORS_NO_CLOSING_BRACKET = '(,;'
+SEPARATORS = SEPARATORS_NO_CLOSING_BRACKET + ')'
