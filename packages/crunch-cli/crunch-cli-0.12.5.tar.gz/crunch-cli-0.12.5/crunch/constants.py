8bcf70a38e84558f95645d7423ecbbe559710401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:44 +0000

    Add package crunch-cli/0.12.5/crunch-cli-0.12.5.tar.gz

---
+++ packages/crunch-cli/crunch-cli-0.12.5.tar.gz/crunch-cli-0.12.5/crunch/constants.py
@@ -0,0 +1,12 @@
+DOT_CRUNCHDAO_DIRECTORY = ".crunchdao"
+DOT_DATA_DIRECTORY = ".data"
+TOKEN_FILE = "token"
+PROJECT_FILE = "project"
+DOT_GITIGNORE_FILE = ".gitignore"
+CONVERTED_MAIN_PY = "__converted_main.py"
+
+IGNORED_FILES = [
+    ".git/",
+    f"{DOT_CRUNCHDAO_DIRECTORY}/",
+    f"{DOT_DATA_DIRECTORY}/",
+]
