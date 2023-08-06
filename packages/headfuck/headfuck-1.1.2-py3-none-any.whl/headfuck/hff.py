74c7793726f413b7953fe237ca9afc8a814d1f6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:06 +0000

    Add package headfuck/1.1.2/headfuck-1.1.2-py3-none-any.whl

---
+++ packages/headfuck/headfuck-1.1.2-py3-none-any.whl/headfuck/hff.py
@@ -0,0 +1,12 @@
+from headfuck import interpreter
+import sys
+def read_from_file():
+    try:
+        f = open(sys.argv[1], "r")
+        result = interpreter.interpret(f.read())
+        print(result)
+        quit()
+    except IndexError:
+        print("Error: You did not name a file to interpret.")
+    except FileNotFoundError:
+        print("Error: The file you named does not exist.")
