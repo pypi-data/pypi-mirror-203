39dadf426fdff804b2be15b861b2a551cffa3d63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:02 +0000

    Add package osbeautify/1.8/osbeautify-1.8.tar.gz

---
+++ packages/osbeautify/osbeautify-1.8.tar.gz/osbeautify-1.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name="osbeautify",
+    version='1.8',
+    license='Eclipse Public License 2.0',
+    authors=["osdev"],
+    author_email="rootosdev@gmail.com",
+    description="python3 package to colors your output",
+    long_description='Documentation: https://github.com/',
+    packages=['osbeautify']
+)
+
