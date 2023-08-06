85f3a5928297372dee1a6d6f4f35f45ecb4d9490
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:01 +0000

    Add package pertpy/0.4.0/pertpy-0.4.0-py3-none-any.whl

---
+++ packages/pertpy/pertpy-0.4.0-py3-none-any.whl/pertpy/__init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for pertpy."""
+
+__author__ = "Lukas Heumos"
+__email__ = "lukas.heumos@posteo.net"
+__version__ = "0.4.0"
+
+
+from pypi_latest import PypiLatest
+
+pertpy_pypi_latest = PypiLatest("pertpy", __version__)
+pertpy_pypi_latest.check_latest()
+
+from . import data as dt
+from . import plot as pl
+from . import preprocessing as pp
+from . import tools as tl
