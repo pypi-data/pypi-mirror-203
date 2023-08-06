2a056abe4f5bf7eabf3ccc4674badb4e9e37d3dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:46 +0000

    Add package vectice/23.2.1.0/vectice-23.2.1.0-py2.py3-none-any.whl

---
+++ packages/vectice/vectice-23.2.1.0-py2.py3-none-any.whl/vectice/models/resource/metadata/source.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+from pandas import DataFrame
+
+from vectice.models.resource.metadata.column_metadata import Column
+from vectice.models.resource.metadata.dataframe_column_parser import capture_columns
+
+
+class Source:
+    def __init__(
+        self,
+        name: str,
+        size: int | None = None,
+        columns: list[Column] | None = None,
+        updated_date: str | None = None,
+        dataframe: DataFrame | None = None,
+    ):
+        """Initialize a MetadataDB instance.
+
+        Parameters:
+            name: The name of the source.
+            size: The size of the source.
+            columns: The columns that compose the source.
+            updated_date: The date of last update of the source.
+            dataframe: A pandas dataframe which will capture the sources metadata.
+        """
+        self.name = name
+        self.size = size
+        self.columns = columns
+        self.updated_date = updated_date
+        self._dataframe = dataframe
+        if dataframe is not None:
+            self.size = dataframe.size
+
+    def asdict(self) -> dict:
+        return {
+            "name": self.name,
+            "size": self.size,
+            "updatedDate": self.updated_date,
+            "columns": capture_columns(init_columns=self.columns, dataframe=self._dataframe),
+        }
