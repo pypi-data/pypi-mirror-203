be2bf4ce670983f27b42183c46f794e2d4ebdff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:46 +0000

    Add package aladhan.py/1.2.1/aladhan.py-1.2.1-py3-none-any.whl

---
+++ packages/aladhan.py/aladhan.py-1.2.1-py3-none-any.whl/aladhan/__init__.py
@@ -0,0 +1,24 @@
+"""
+Aladhan prayer times API Wrapper
+~~~~~~~~~~~~~~~~~~~~~
+Basic wrapper for the Aladhan prayer times API.
+"""
+
+from collections import namedtuple
+
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+
+version_info = VersionInfo(
+    major=1, minor=2, micro=1, releaselevel="final", serial=0
+)
+
+__title__ = "aladhan.py"
+__author__ = "HETHAT"
+__version__ = "1.2.1"
+
+
+from . import methods
+from .client import Client
+from .data_classes import *
+from .enums import *
+from .methods import Method
