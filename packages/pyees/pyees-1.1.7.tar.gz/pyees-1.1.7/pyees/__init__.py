af1483cc42e310d4df722fb7b467f0f2b800704b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:46 +0000

    Add package pyees/1.1.7/pyees-1.1.7.tar.gz

---
+++ packages/pyees/pyees-1.1.7.tar.gz/pyees-1.1.7/pyees/__init__.py
@@ -0,0 +1,15 @@
+# import the necessary modules
+from pyees.variable import variable
+from pyees.fit import dummy_fit, pol_fit, lin_fit, exp_fit, pow_fit, logistic_fit, logistic_100_fit
+from pyees.readData import readData
+from pyees.prop import prop
+from pyees.solve import solve
+from pyees.unit import addNewUnit
+
+
+## TODO undersøg om man kan bruge evalueringer af funktioner til at bestemme uncertanty propagation - selv med produktreglen
+## TODO fit - få det til at virke som i bogen
+## TODO fit - lav normalized residuals plot
+## TODO fit - evaluer, om 68% af datapunkterne ligger 1 standard afvigelse fra fittet
+
+    
