9e8f1726220d3deac5627b9e82c7a44e67ec548e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:31 +0000

    Add package torquepy/0.2/torquepy-0.2.tar.gz

---
+++ packages/torquepy/torquepy-0.2.tar.gz/torquepy-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='torquepy',
+    version='0.2',
+    description='Torque library',
+    author='Semen Pankratov',
+    author_email='sema2012r@mail.ru',
+
+    packages=find_packages(),
+)
