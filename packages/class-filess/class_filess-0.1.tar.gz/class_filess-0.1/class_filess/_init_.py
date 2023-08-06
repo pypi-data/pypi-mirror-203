64bee8425423d17629faab1939e96ad127befb58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:37 +0000

    Add package class-filess/0.1/class_filess-0.1.tar.gz

---
+++ packages/class-filess/class_filess-0.1.tar.gz/class_filess-0.1/class_filess/_init_.py
@@ -0,0 +1,5 @@
+
+class File:
+    x = 50
+
+
---
+++ packages/class-filess/class_filess-0.1.tar.gz/class_filess-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='class_filess',
+      version='0.1',
+      description='Удобный класс для работы с файлами',
+      packages=['class_filess'],
+      author_email='masha22340@gmail.com',
+      zip_safe=False)
