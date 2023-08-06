222eec3c8662a6813d12ad9778e37c465a7c6b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:32 +0000

    Add package bnipython/0.6.1/bnipython-0.6.1-py3-none-any.whl

---
+++ packages/bnipython/bnipython-0.6.1-py3-none-any.whl/bnipython/lib/util/response.py
@@ -0,0 +1,34 @@
+def responseOGP(params={'res', 'resObj'}):
+    try:
+        if (params['res'][params['resObj']]['parameters']['responseCode'] != '0001'):
+            code = params['res'][params['resObj']
+                                 ]['parameters']['responseCode']
+            message = params['res'][params['resObj']
+                                    ]['parameters']['responseMessage']
+            raise ValueError(f'{code}:{message}')
+        else:
+            return params['res']
+    except Exception:
+        code = params['res']['Response']['parameters']['responseCode']
+        message = params['res']['Response']['parameters']['responseMessage']
+        raise ValueError(f'{code}:{message}')
+
+
+def responseSnapBI(params={'res'}):
+    statusCodeSuccess = [
+        '2000000',
+        '2001100',
+        '2001400',
+        '2001500',
+        '2001600',
+        '2001700',
+        '2001800',
+        '2002200',
+        '2002300',
+        '2003600',
+        '2007300'
+    ]
+    if not params['res']['responseCode'] in statusCodeSuccess:
+        raise ValueError(
+            f"{params['res']['responseCode']} : {params['res']['responseMessage']}")
+    return params['res']
