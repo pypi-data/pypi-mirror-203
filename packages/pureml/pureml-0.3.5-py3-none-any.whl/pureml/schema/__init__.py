a3f5a6a8887229fcf4fec64ba1c1e3a0b4d1468b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:32 +0000

    Add package pureml/0.3.5/pureml-0.3.5-py3-none-any.whl

---
+++ packages/pureml/pureml-0.3.5-py3-none-any.whl/pureml/schema/__init__.py
@@ -0,0 +1,10 @@
+from .dataset import DatasetSchema
+from .paths import PathSchema
+from .model import ModelSchema
+from .predict import PredictSchema, Input, Output, DataTypes
+from .storage import StorageSchema
+from .packaging import DockerSchema, FastAPISchema
+from .log import LogSchema
+from .backend import BackendSchema
+from .types import DataTypes
+from .config import ConfigKeys
