c68f17d08d3cc19cd54f99c148edb465fb9bef79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:44 +0000

    Add package wonder-sdk/0.1.16/wonder-sdk-0.1.16.tar.gz

---
+++ packages/wonder-sdk/wonder-sdk-0.1.16.tar.gz/wonder-sdk-0.1.16/wonder_sdk/srgan/__init__.py
@@ -0,0 +1,11 @@
+from basicsr.archs.rrdbnet_arch import RRDBNet
+from realesrgan import RealESRGANer
+
+from . import configure_srgan
+
+def configure_srgan(model_path, scale=2, half_precision=False):
+    return RealESRGANer(
+      scale=scale, model_path=model_path, 
+      model=RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=scale),
+      tile=0, tile_pad=10, pre_pad=0, half=half_precision
+    )
