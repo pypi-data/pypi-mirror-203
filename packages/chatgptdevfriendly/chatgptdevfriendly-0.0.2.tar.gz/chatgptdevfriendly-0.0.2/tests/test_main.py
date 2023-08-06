bcb26e1efe6a401f4899685c729c36f8a91f1ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:03 +0000

    Add package chatgptdevfriendly/0.0.2/chatgptdevfriendly-0.0.2.tar.gz

---
+++ packages/chatgptdevfriendly/chatgptdevfriendly-0.0.2.tar.gz/chatgptdevfriendly-0.0.2/tests/test_main.py
@@ -0,0 +1,16 @@
+api_key = "api_key"
+model_name = "gpt-3.5-turbo"
+
+from chatgptdevfriendly.v1 import ChatGptSmartClient
+
+
+chatgptsmtclient = ChatGptSmartClient(api_key=api_key, model=model_name, log_info=True)
+
+for _ in range(10):
+    chatgptsmtclient.query("List the top 10 upcoming startups in India?")
+    chatgptsmtclient.query("Ok thanks, can you giv me the valuation of these startups in tabuar format")
+
+chatgptsmtclient.dump_context_to_a_file("context")
+chatgptsmtclient.load_context_from_a_file("context")
+
+chatgptsmtclient.print_metrics()
