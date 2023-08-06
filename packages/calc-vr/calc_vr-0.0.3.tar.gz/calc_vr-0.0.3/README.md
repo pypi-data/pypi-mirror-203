55c3462ad924a11763dc7b5aab0eb60b88377dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:51 +0000

    Add package calc-vr/0.0.3/calc_vr-0.0.3.tar.gz

---
+++ packages/calc-vr/calc_vr-0.0.3.tar.gz/calc_vr-0.0.3/README.md
@@ -0,0 +1,1 @@
+This is a sample arithmetic package for learning
---
+++ packages/calc-vr/calc_vr-0.0.3.tar.gz/calc_vr-0.0.3/arithmetic/__init__.py
@@ -0,0 +1,6 @@
+#from .add import add
+#from .subtract import subtract
+#from .multiply import multiply
+#from .divide import divide
+
+
