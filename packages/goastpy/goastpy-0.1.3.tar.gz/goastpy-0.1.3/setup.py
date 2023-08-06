304a8a7338fdce488e294bcb5a81e19a59ea7531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:42 +0000

    Add package goastpy/0.1.3/goastpy-0.1.3.tar.gz

---
+++ packages/goastpy/goastpy-0.1.3.tar.gz/goastpy-0.1.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="goastpy",
+    version="0.1.3",
+    packages=["goastpy"],
+    install_requires=[],
+    description='a python wrapper for the built-in go parser using c-types',
+    author='Itay Gersten',
+    author_email='Itay.Gersten@gmail.com',
+    url='https://github.com/itayg25/goastpy',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    keywords=['GO', 'GOLANG', 'PYTHON', 'AST', 'GOPY', 'PYGO', 'PARSER', 'ASTPARSER'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    python_requires=">=3.6",
+
+)
