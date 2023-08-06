e4afa3e62fb0f716af156f051e4d40f87f30513b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:37 +0000

    Add package Planes2D/1.1/Planes2D-1.1.tar.gz

---
+++ packages/Planes2D/Planes2D-1.1.tar.gz/Planes2D-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Planes2D",
+    author="Komornik",
+    version="1.1",
+    description="A simple liblary for creating and managing 2D planes",
+    packages=find_packages(),
+)
