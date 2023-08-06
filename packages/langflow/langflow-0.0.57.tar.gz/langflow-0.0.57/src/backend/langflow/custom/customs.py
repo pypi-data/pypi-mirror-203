7a3ab876af6e93c53bd04737265a3b84057b5244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:22 +0000

    Add package langflow/0.0.57/langflow-0.0.57.tar.gz

---
+++ packages/langflow/langflow-0.0.57.tar.gz/langflow-0.0.57/src/backend/langflow/custom/customs.py
@@ -0,0 +1,20 @@
+from langflow.template import nodes
+
+# These should always be instantiated
+CUSTOM_NODES = {
+    "prompts": {"ZeroShotPrompt": nodes.ZeroShotPromptNode()},
+    "tools": {"PythonFunction": nodes.PythonFunctionNode(), "Tool": nodes.ToolNode()},
+    "agents": {
+        "JsonAgent": nodes.JsonAgentNode(),
+        "CSVAgent": nodes.CSVAgentNode(),
+        "initialize_agent": nodes.InitializeAgentNode(),
+        "VectorStoreAgent": nodes.VectorStoreAgentNode(),
+        "VectorStoreRouterAgent": nodes.VectorStoreRouterAgentNode(),
+        "SQLAgent": nodes.SQLAgentNode(),
+    },
+}
+
+
+def get_custom_nodes(node_type: str):
+    """Get custom nodes."""
+    return CUSTOM_NODES.get(node_type, {})
