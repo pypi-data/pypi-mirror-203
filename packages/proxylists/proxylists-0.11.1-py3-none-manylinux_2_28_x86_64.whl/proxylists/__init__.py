34acaf8e4068c69129fef61c5138932d6881cfae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:02 +0000

    Add package proxylists/0.11.1/proxylists-0.11.1-py3-none-manylinux_2_28_x86_64.whl

---
+++ packages/proxylists/proxylists-0.11.1-py3-none-manylinux_2_28_x86_64.whl/proxylists/__init__.py
@@ -0,0 +1,19 @@
+import asyncio
+from .proxies import PROXY_LIST
+from .proxies.connections import check_host, check_address
+
+
+__all__ = ["PROXY_LIST", "check_host", "check_address"]
+
+
+async def proxy_list():
+    proxies = []
+    for proxy in PROXY_LIST:
+        p = await proxy().get_list()
+        proxies = proxies + p
+    return proxies
+
+
+def get_proxies():
+    loop = asyncio.get_event_loop()
+    return loop.run_until_complete(proxy_list())
