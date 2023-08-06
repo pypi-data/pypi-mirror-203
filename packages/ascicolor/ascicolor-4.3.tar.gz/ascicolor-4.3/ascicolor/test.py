ed6aab1909dbf1b7591a22053612b86ec808b226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:35 +0000

    Add package ascicolor/4.3/ascicolor-4.3.tar.gz

---
+++ packages/ascicolor/ascicolor-4.3.tar.gz/ascicolor-4.3/ascicolor/test.py
@@ -0,0 +1,10 @@
+
+#from ascicolor import *
+#text = "Hello world!"
+#print(Colors.blue + text)
+# or
+#print(Colorate.Color(Colors.blue, text, True))
+#print(Colorate.Horizontal(Colors.yellow_to_red, "Hello, Welcome to ascicolor.", 1))
+#name = Write.Input("Enter your name -> ", Colors.red_to_purple, interval=0.0025)
+#Write.Print(f"Nice to meet you, {name}!", Colors.blue_to_green, interval=0.05)
+#print(Center.XCenter("Hello, Welcome to ascicolor."))
