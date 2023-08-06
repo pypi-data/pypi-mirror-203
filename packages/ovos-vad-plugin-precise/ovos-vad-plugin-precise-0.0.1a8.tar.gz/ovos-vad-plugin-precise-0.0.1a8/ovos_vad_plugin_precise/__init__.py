32645099b39036f7ba809e3c29083b19478f0e16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:20 +0000

    Add package ovos-vad-plugin-precise/0.0.1a8/ovos-vad-plugin-precise-0.0.1a8.tar.gz

---
+++ packages/ovos-vad-plugin-precise/ovos-vad-plugin-precise-0.0.1a8.tar.gz/ovos-vad-plugin-precise-0.0.1a8/ovos_vad_plugin_precise/__init__.py
@@ -0,0 +1,20 @@
+from os.path import join, dirname
+from ovos_utils.log import LOG
+from ovos_plugin_manager.templates.vad import VADEngine
+from precise_lite_runner.runner import Listener
+
+
+class PreciseVAD(VADEngine):
+    def __init__(self, config=None, sample_rate=None):
+        super().__init__(config, sample_rate)
+        model = self.config.get("model") or join(dirname(__file__), "vad.tflite")
+        self.vad_threshold = self.config.get("threshold", 0.5)
+        self.debug = self.config.get("debug", False)
+        self.vad = Listener(model)
+
+    def is_silence(self, chunk):
+        # return True or False
+        prob = self.vad.get_prediction(chunk)
+        if self.debug:
+            LOG.debug(f"speech probability: {prob}")
+        return prob < self.vad_threshold
