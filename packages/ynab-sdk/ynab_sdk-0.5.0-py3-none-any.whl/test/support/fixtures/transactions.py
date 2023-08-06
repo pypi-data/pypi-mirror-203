b32e0cd130e80b43f6660fa205a8eab51633b635
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:26 +0000

    Add package ynab-sdk/0.5.0/ynab_sdk-0.5.0-py3-none-any.whl

---
+++ packages/ynab-sdk/ynab_sdk-0.5.0-py3-none-any.whl/test/support/fixtures/transactions.py
@@ -0,0 +1,84 @@
+VALID_TRANSACTIONS = {
+    "data": {
+        "transactions": [
+            {
+                "id": "string",
+                "date": "string",
+                "amount": 0,
+                "memo": "string",
+                "cleared": "cleared",
+                "approved": True,
+                "flag_color": "red",
+                "account_id": "string",
+                "payee_id": "string",
+                "category_id": "string",
+                "transfer_account_id": "string",
+                "transfer_transaction_id": "string",
+                "matched_transaction_id": "string",
+                "import_id": "string",
+                "deleted": True,
+                "account_name": "string",
+                "payee_name": "string",
+                "category_name": "string",
+                "subtransactions": [
+                    {
+                        "id": "string",
+                        "transaction_id": "string",
+                        "amount": 0,
+                        "memo": "string",
+                        "payee_id": "string",
+                        "category_id": "string",
+                        "transfer_account_id": "string",
+                        "deleted": True,
+                    }
+                ],
+            }
+        ],
+        "server_knowledge": 0,
+    }
+}
+
+VALID_TRANSACTION = {
+    "data": {
+        "transaction": {
+            "id": "string",
+            "date": "string",
+            "amount": 0,
+            "memo": "string",
+            "cleared": "cleared",
+            "approved": True,
+            "flag_color": "red",
+            "account_id": "string",
+            "payee_id": "string",
+            "category_id": "string",
+            "transfer_account_id": "string",
+            "transfer_transaction_id": "string",
+            "matched_transaction_id": "string",
+            "import_id": "string",
+            "deleted": True,
+            "account_name": "string",
+            "payee_name": "string",
+            "category_name": "string",
+            "subtransactions": [
+                {
+                    "id": "string",
+                    "transaction_id": "string",
+                    "amount": 0,
+                    "memo": "string",
+                    "payee_id": "string",
+                    "category_id": "string",
+                    "transfer_account_id": "string",
+                    "deleted": True,
+                }
+            ],
+        }
+    }
+}
+
+CREATED_TRANSACTIONS = {
+    "data": {
+        "transaction_ids": [VALID_TRANSACTION["data"]["transaction"]["id"]],
+        "duplicate_import_ids": [],
+        "transactions": [VALID_TRANSACTION["data"]["transaction"]],
+    }
+}
