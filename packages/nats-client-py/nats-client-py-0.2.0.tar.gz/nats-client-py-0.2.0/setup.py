3dd04a8d93f47802320f3d566d8cf610d51f86ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:01 +0000

    Add package nats-client-py/0.2.0/nats-client-py-0.2.0.tar.gz

---
+++ packages/nats-client-py/nats-client-py-0.2.0.tar.gz/nats-client-py-0.2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+# Metadata goes in pyproject.toml.
+# These are here for GitHub's dependency graph.
+setup(
+    name="nats-client-py",
+    version="0.2.0",
+    includepackagedata=True,
+)
