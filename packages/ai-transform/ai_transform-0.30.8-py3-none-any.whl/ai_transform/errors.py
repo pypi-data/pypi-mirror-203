9ab38e4e34579ac4466f61a005ce946fd8ca077d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:23 +0000

    Add package ai-transform/0.30.8/ai_transform-0.30.8-py3-none-any.whl

---
+++ packages/ai-transform/ai_transform-0.30.8-py3-none-any.whl/ai_transform/errors.py
@@ -0,0 +1,21 @@
+class AuthException(Exception):
+    pass
+
+
+class MaxRetriesError(Exception):
+    pass
+
+
+class WorkflowFailedError(Exception):
+    pass
+
+
+class UserFacingError(Exception):
+    """This error is shown to the user"""
+
+    def __init__(self, error_message: str, client, job_id: str, workflow_name: str, **kwargs):
+        client._api._set_workflow_status(
+            job_id=job_id, user_errors=error_message, workflow_name=workflow_name, status="failed", **kwargs
+        )
+        self.error_message = error_message
+        super().__init__()
