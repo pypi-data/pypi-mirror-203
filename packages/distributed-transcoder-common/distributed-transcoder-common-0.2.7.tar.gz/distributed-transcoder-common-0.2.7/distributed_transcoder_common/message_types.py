e26aa0ef93932a413ae1d978bc957123e1efb0b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:02 +0000

    Add package distributed-transcoder-common/0.2.7/distributed-transcoder-common-0.2.7.tar.gz

---
+++ packages/distributed-transcoder-common/distributed-transcoder-common-0.2.7.tar.gz/distributed-transcoder-common-0.2.7/distributed_transcoder_common/message_types.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class JobSubmissionMessage:
+    job_id: str
+    input_s3_path: str
+    output_s3_path: str
+    transcode_options: str
+
+
+@dataclass
+class JobResultMessage:
+    timestamp: float
+    worker_id: str
+    status: str
+    job_id: str
+    output_s3_path: str
+    error: Optional[str]
+    error_type: Optional[str]
+
+
+@dataclass
+class JobProgressMessage:
+    timestamp: float
+    worker_id: str
+    job_id: str
+    progress: float
