ed95a9da5f48e49efbb8fa6c416baee6ba6649b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:45 +0000

    Add package test-melih-mely/0.0.1/test_melih_mely-0.0.1-py3-none-any.whl

---
+++ packages/test-melih-mely/test_melih_mely-0.0.1-py3-none-any.whl/test-melih_mely/test_script_melih.py
@@ -0,0 +1,12 @@
+
+class PingDevice:
+    def __init__(self, host):
+        self.host = host
+
+    def ping(self):
+        try:
+            output = subprocess.check_output(["ping", "-c", "1", self.host])
+            return True
+        except subprocess.CalledProcessError:
+            return False
+
