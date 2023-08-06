61a5ac48c42a03dd7c8958ecc576021dfc6330f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package zenoml/0.4.6/zenoml-0.4.6.tar.gz

---
+++ packages/zenoml/zenoml-0.4.6.tar.gz/zenoml-0.4.6/zeno/__init__.py
@@ -0,0 +1,27 @@
+from zeno.api import (
+    distill,
+    DistillReturn,
+    inference,
+    InferenceReturn,
+    metric,
+    MetricReturn,
+    model,
+    ModelReturn,
+    ZenoOptions,
+    ZenoParameters,
+)
+from zeno.runner import zeno
+
+__all__ = [
+    "DistillReturn",
+    "InferenceReturn",
+    "MetricReturn",
+    "ModelReturn",
+    "ZenoOptions",
+    "ZenoParameters",
+    "distill",
+    "inference",
+    "metric",
+    "model",
+    "zeno",
+]
