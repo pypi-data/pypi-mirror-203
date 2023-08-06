8cf28e5faf04d452937ca21d6a4b768ab8245950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:42 +0000

    Add package xpydf/0.1.1/xpydf-0.1.1-cp310-cp310-macosx_13_0_arm64.whl

---
+++ packages/xpydf/xpydf-0.1.1-cp310-cp310-macosx_13_0_arm64.whl/xpydf/pdf_loader.pyi
@@ -0,0 +1,38 @@
+from typing import List, Optional, TypedDict
+
+class XpdfPythonCapsule: ...
+
+class ImageInfo(TypedDict):
+    width: float
+    height: float
+
+class PageInfo(TypedDict):
+    page_number: int
+    width: float
+    height: float
+    images: List[ImageInfo]
+
+class PdfLoader:
+    filename: str
+    capsule: Optional[XpdfPythonCapsule] = None
+
+    def __init__(
+        self,
+        filename: str,
+        cliptext: bool = False,
+        discard_diag: bool = True,
+        discard_rotated_text: bool = True,
+        no_page_breaks: bool = False,
+        insert_bom: bool = False,
+        verbose: bool = False,
+        quiet: bool = True,
+        mode: str = "table",
+        map_numeric_char_names: bool = False,
+        map_unknown_char_names: bool = True,
+        owner_password: Optional[str] = None,
+        user_password: Optional[str] = None,
+    ) -> None: ...
+    def extract_bytes(self) -> List[bytes]: ...
+    def extract_strings(self) -> List[str]: ...
+    def extract_images(self) -> List[PageInfo]: ...
+    def __del__(self) -> None: ...
