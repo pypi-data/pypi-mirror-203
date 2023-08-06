571a0978b2f1c26fbbd80dc0613856df6f4fcd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:24 +0000

    Add package helloworldplayer/0.0.1/helloworldplayer-0.0.1-py3-none-any.whl

---
+++ packages/helloworldplayer/helloworldplayer-0.0.1-py3-none-any.whl/helloworldplayer/player.py
@@ -0,0 +1,24 @@
+"""
+This is the module that includes the music reproducerr class
+"""
+
+class Player:
+    """
+    This class creates a 
+    music reproducerr
+    """
+
+    def play(self, song):
+        """
+        Reproduce the song that was receive as parameter
+
+        Parameters:
+        song (str): this is a string with the song's path
+
+        Return:
+        int: returns 1 if success, returns 0 if failure
+        """
+        print("reproducing song")
+
+    def stop(self):
+        print("stopping")
