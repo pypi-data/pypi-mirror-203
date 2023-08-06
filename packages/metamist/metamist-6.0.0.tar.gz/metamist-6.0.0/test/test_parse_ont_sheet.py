ab657d13f199f344bceec258f0db7d4691b8c38d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:59 +0000

    Add package metamist/6.0.0/metamist-6.0.0.tar.gz

---
+++ packages/metamist/metamist-6.0.0.tar.gz/metamist-6.0.0/test/test_parse_ont_sheet.py
@@ -0,0 +1,110 @@
+import unittest
+from io import StringIO
+from unittest.mock import patch
+
+from test.testbase import run_as_sync
+from metamist.parser.generic_parser import ParsedParticipant
+from scripts.parse_ont_sheet import OntParser
+
+
+class TestOntSampleSheetParser(unittest.TestCase):
+    """Test the TestOntSampleSheetParser"""
+
+    @run_as_sync
+    @patch('from metamist.apis.ParticipantApi.get_participant_id_map_by_external_ids')
+    @patch('from metamist.apis.SampleApi.get_sample_id_map_by_external')
+    @patch('from metamist.apis.AssayApi.get_assays_by_sample_ids')
+    async def test_simple_sheet(
+        self, mock_get_sequence_ids, mock_get_sample_id, mock_get_participant_id
+    ):
+        """
+        Test importing a two rows, forms objects and checks response
+        - MOCKS:
+            - get_participant_id_map_by_external_ids
+            - get_sample_id_map_by_external
+            - get_sequence_ids_for_sample_ids_by_type
+        """
+        mock_get_participant_id.return_value = {'Sample01': 1}
+        mock_get_sample_id.return_value = {'Sample01': 'CPG001'}
+        mock_get_sequence_ids.return_value = {}
+
+        rows = [
+            'Sequencing_date,Experiment name,Sample ID,Protocol,Flow cell,Barcoding,Device,Flowcell ID,MUX total,Basecalling,Fail FASTQ filename,Pass FASTQ filename',
+            '10/12/2034,PBXP_Awesome,Sample01,LSK1,PRO002,None,PromethION,XYZ1,7107,4.0.11+f1071ce,Sample01_fail.fastq.gz,Sample01_pass.fastq.gz',
+            '21/10/2021,PBXP_Awesome,Sample02,LSK1,PRO002,None,PromethION,XYZ1,8056,4.0.11+f1071ce,Sample02_fail.fastq.gz,Sample02_pass.fastq.gz',
+        ]
+        parser = OntParser(
+            search_locations=[],
+            # doesn't matter, we're going to mock the call anyway
+            project='dev',
+        )
+
+        parser.skip_checking_gcs_objects = True
+        fs = [
+            'Sample01_pass.fastq.gz',
+            'Sample01_fail.fastq.gz',
+            'Sample02_pass.fastq.gz',
+            'Sample02_fail.fastq.gz',
+        ]
+        parser.filename_map = {k: 'gs://BUCKET/FAKE/' + k for k in fs}
+        parser.skip_checking_gcs_objects = True
+
+        file_contents = '\n'.join(rows)
+        participants: list[ParsedParticipant]
+        summary, participants = await parser.parse_manifest(
+            StringIO(file_contents), delimiter=',', dry_run=True
+        )
+
+        participants_to_add = summary['participants']['insert']
+        participants_to_update = summary['participants']['update']
+        samples_to_add = summary['samples']['insert']
+        samples_to_update = summary['samples']['update']
+        sequencing_to_add = summary['sequences']['insert']
+        sequencing_to_update = summary['sequences']['update']
+
+        self.assertEqual(1, participants_to_add)
+        self.assertEqual(1, participants_to_update)
+        self.assertEqual(1, samples_to_add)
+        self.assertEqual(2, sequencing_to_add)
+        self.assertEqual(1, samples_to_update)
+        self.assertEqual(0, sequencing_to_update)
+
+        meta_dict = {
+            'barcoding': 'None',
+            'basecalling': '4.0.11+f1071ce',
+            'device': 'PromethION',
+            'experiment_name': 'PBXP_Awesome',
+            'flow_cell': 'PRO002',
+            'flowcell_id': 'XYZ1',
+            'mux_total': '7107',
+            'protocol': 'LSK1',
+            'reads': [
+                {
+                    'basename': 'Sample01_pass.fastq.gz',
+                    'checksum': None,
+                    'class': 'File',
+                    'location': 'gs://BUCKET/FAKE/Sample01_pass.fastq.gz',
+                    'size': None,
+                    'datetime_added': None,
+                }
+            ],
+            'reads_type': 'fastq',
+            'sequencing_date': '10/12/2034',
+        }
+
+        seqgroup_meta = {
+            'failed_reads': [
+                [
+                    {
+                        'location': 'gs://BUCKET/FAKE/Sample01_fail.fastq.gz',
+                        'basename': 'Sample01_fail.fastq.gz',
+                        'class': 'File',
+                        'checksum': None,
+                        'size': None,
+                    }
+                ]
+            ],
+        }
+        sequence_group = participants[0].samples[0].sequencing_groups[0]
+        self.assertDictEqual(seqgroup_meta, sequence_group.meta)
+        self.assertDictEqual(meta_dict, sequence_group.assays[0].meta)
---
+++ packages/metamist/metamist-6.0.0.tar.gz/metamist-6.0.0/test/test_web.py
@@ -0,0 +1,377 @@
+from db.python.layers.assay import AssayLayer, AssayUpsertInternal
+from db.python.layers.participant import (
+    ParticipantLayer,
+    ParticipantUpsertInternal,
+)
+from db.python.layers.sample import SampleLayer, SampleUpsertInternal
+from db.python.layers.sequencing_group import SequencingGroupUpsertInternal
+from db.python.layers.web import (
+    WebLayer,
+    ProjectSummary,
+    WebProject,
+    SearchItem,
+    MetaSearchEntityPrefix,
+)
+from test.testbase import DbIsolatedTest, run_as_sync
+
+default_assay_meta = {
+    'sequencing_type': 'genome',
+    'sequencing_technology': 'short-read',
+    'sequencing_platform': 'illumina',
+}
+
+
+def data_to_class(data: dict | list) -> dict | list:
+    """Convert the data into it's class using the _class field"""
+    if isinstance(data, list):
+        return [data_to_class(x) for x in data]
+
+    if not isinstance(data, dict):
+        return data
+
+    cls = data.pop('_class', None)
+    mapped_data = {k: data_to_class(v) for k, v in data.items()}
+
+    if isinstance(cls, type):
+        return cls(**mapped_data)
+
+    return mapped_data
+
+
+def merge(d1: dict, d2: dict):
+    """Merges two dictionaries"""
+    return dict(d1, **d2)
+
+
+class TestWeb(DbIsolatedTest):
+    """Test web class containing web endpoints"""
+
+    @run_as_sync
+    async def setUp(self) -> None:
+        super().setUp()
+        self.webl = WebLayer(self.connection)
+        self.partl = ParticipantLayer(self.connection)
+        self.sampl = SampleLayer(self.connection)
+        self.seql = AssayLayer(self.connection)
+
+    @run_as_sync
+    async def test_project_summary(self):
+        """Test getting the summary for a project"""
+        result = await self.webl.get_project_summary(token=0, grid_filter=[])
+
+        # Expect an empty project
+        expected = ProjectSummary(
+            project=WebProject(
+                **{'id': 1, 'name': 'test', 'meta': {}, 'dataset': 'test'}
+            ),
+            total_samples=0,
+            total_samples_in_query=0,
+            total_participants=0,
+            total_sequences=0,
+            batch_sequence_stats={},
+            cram_seqr_stats={},
+            participants=[],
+            participant_keys=[],
+            sample_keys=[],
+            sequence_keys=[],
+            seqr_links={},
+            seqr_sync_types=[],
+        )
+
+        self.assertEqual(expected, result)
+
+        # Now add a participant with a sample and sequence
+        data = [
+            ParticipantUpsertInternal(
+                external_id='Demeter',
+                meta={},
+                samples=[
+                    SampleUpsertInternal(
+                        external_id='sample_id001',
+                        meta={},
+                        type='blood',
+                        sequencing_groups=[
+                            SequencingGroupUpsertInternal(
+                                type='genome',
+                                technology='short-read',
+                                platform=None,
+                                assays=[
+                                    AssayUpsertInternal(
+                                        type='sequencing',
+                                        meta={
+                                            'reads': [
+                                                [
+                                                    {
+                                                        'basename': 'sample_id001.filename-R1.fastq.gz',
+                                                        'checksum': None,
+                                                        'class': 'File',
+                                                        'location': '/path/to/sample_id001.filename-R1.fastq.gz',
+                                                        'size': 111,
+                                                    },
+                                                    {
+                                                        'basename': 'sample_id001.filename-R2.fastq.gz',
+                                                        'checksum': None,
+                                                        'class': 'File',
+                                                        'location': '/path/to/sample_id001.filename-R2.fastq.gz',
+                                                        'size': 111,
+                                                    },
+                                                ]
+                                            ],
+                                            'reads_type': 'fastq',
+                                            'batch': 'M001',
+                                            **default_assay_meta,
+                                        },
+                                    ),
+                                ],
+                            )
+                        ],
+                    )
+                ],
+            )
+        ]
+
+        await self.partl.upsert_participants(participants=data)
+
+        result = await self.webl.get_project_summary(token=0, grid_filter=[])
+
+        expected_data = ProjectSummary(
+            project=WebProject(
+                **{'id': 1, 'name': 'test', 'meta': {}, 'dataset': 'test'}
+            ),
+            total_samples=1,
+            total_samples_in_query=1,
+            total_participants=1,
+            total_sequences=1,
+            cram_seqr_stats={
+                'genome': {
+                    'Sequences': '1',
+                    'Crams': '0',
+                    'Seqr': '0',
+                }
+            },
+            batch_sequence_stats={'M001': {'genome': '1'}},
+            participants=[],
+            participant_keys=[('external_id', 'Participant ID')],
+            sample_keys=[
+                ('id', 'Sample ID'),
+                ('external_id', 'External Sample ID'),
+                ('created_date', 'Created date'),
+            ],
+            sequence_keys=[
+                ('type', 'type'),
+                ('technology', 'technology'),
+                ('meta.batch', 'batch'),
+                ('meta.reads_type', 'reads_type'),
+            ],
+            seqr_links={},
+            seqr_sync_types=[],
+        )
+        # TODO: fix this test
+
+        self.assertEqual(expected_data, result)
+
+        filtered_result_success = await self.webl.get_project_summary(
+            token=0,
+            grid_filter=[
+                SearchItem(
+                    model_type=MetaSearchEntityPrefix.ASSAY,
+                    query='M001',
+                    field='batch',
+                    is_meta=True,
+                )
+            ],
+        )
+
+        self.assertEqual(expected_data, filtered_result_success)
+
+        filtered_result_empty = await self.webl.get_project_summary(
+            token=0,
+            grid_filter=[
+                SearchItem(
+                    model_type=MetaSearchEntityPrefix.ASSAY,
+                    query='M002',
+                    field='batch',
+                    is_meta=True,
+                )
+            ],
+        )
+
+        self.assertEqual(expected, filtered_result_empty)
+
+        new_participants = [
+            ParticipantUpsertInternal(
+                external_id='Meter',
+                meta={},
+                samples=[
+                    SampleUpsertInternal(
+                        external_id='sample_id002',
+                        meta={},
+                        type='blood',
+                        sequencing_groups=[
+                            SequencingGroupUpsertInternal(
+                                type='genome',
+                                technology='short-read',
+                                platform='Illumina',
+                                assays=[
+                                    AssayUpsertInternal(
+                                        meta={
+                                            'reads': [
+                                                {
+                                                    'basename': 'sample_id002.filename-R1.fastq.gz',
+                                                    'checksum': None,
+                                                    'class': 'File',
+                                                    'location': '/path/to/sample_id002.filename-R1.fastq.gz',
+                                                    'size': 112,
+                                                },
+                                                {
+                                                    'basename': 'sample_id002.filename-R2.fastq.gz',
+                                                    'checksum': None,
+                                                    'class': 'File',
+                                                    'location': '/path/to/sample_id002.filename-R2.fastq.gz',
+                                                    'size': 112,
+                                                },
+                                            ],
+                                            'reads_type': 'fastq',
+                                            'batch': 'M001',
+                                            **default_assay_meta,
+                                        }
+                                    ),
+                                ],
+                            ),
+                        ],
+                    )
+                ],
+            )
+        ]
+
+        await self.partl.upsert_participants(participants=new_participants)
+
+        expected_data_two_samples = ProjectSummary(
+            project=WebProject(
+                **{'id': 1, 'name': 'test', 'meta': {}, 'dataset': 'test'}
+            ),
+            total_samples=2,
+            total_samples_in_query=2,
+            total_participants=2,
+            total_sequences=2,
+            cram_seqr_stats={
+                'genome': {
+                    'Sequences': '2',
+                    'Crams': '0',
+                    'Seqr': '0',
+                }
+            },
+            batch_sequence_stats={'M001': {'genome': '2'}},
+            participants=[],  # data_to_class(expected_data_list),
+            participant_keys=[('external_id', 'Participant ID')],
+            sample_keys=[
+                ('id', 'Sample ID'),
+                ('external_id', 'External Sample ID'),
+                ('created_date', 'Created date'),
+            ],
+            sequence_keys=[
+                ('type', 'type'),
+                ('technology', 'technology'),
+                ('meta.batch', 'batch'),
+                ('meta.reads_type', 'reads_type'),
+            ],
+            seqr_links={},
+            seqr_sync_types=[],
+        )
+
+        two_samples_result = await self.webl.get_project_summary(
+            token=0, grid_filter=[]
+        )
+        for k, v in two_samples_result.__dict__.items():
+            if k == 'participants':
+                continue
+
+            self.assertEqual(v, expected_data_two_samples.__dict__[k])
+        # self.assertEqual(expected_data_two_samples, two_samples_result)
+
+        # expected_data_list_filtered: list = [
+        #     merge(
+        #         participant,
+        #         {
+        #             '_class': NestedParticipant,
+        #             'id': 2,
+        #             'families': [],
+        #             'samples': [
+        #                 merge(
+        #                     sample,
+        #                     {
+        #                         '_class': NestedSample,
+        #                         'id': 2,
+        #                         'created_date': str(date.today()),
+        #                         'sequences': [
+        #                             merge(
+        #                                 sequence,
+        #                                 {
+        #                                     '_class': NestedSequence,
+        #                                     'id': 2,
+        #                                 },
+        #                             )
+        #                             for sequence in sample.get('sequences')
+        #                         ],
+        #                     },
+        #                 )
+        #                 for sample in participant.get('samples')
+        #             ],
+        #         },
+        #     )
+        #     for participant in new_data
+        # ]
+
+        expected_data_two_samples_filtered = ProjectSummary(
+            project=WebProject(
+                **{'id': 1, 'name': 'test', 'meta': {}, 'dataset': 'test'}
+            ),
+            total_samples=2,
+            total_samples_in_query=1,
+            total_participants=2,
+            total_sequences=2,
+            cram_seqr_stats={
+                'genome': {
+                    'Sequences': '2',
+                    'Crams': '0',
+                    'Seqr': '0',
+                }
+            },
+            batch_sequence_stats={'M001': {'genome': '2'}},
+            participants=[],  # data_to_class(expected_data_list_filtered),
+            participant_keys=[('external_id', 'Participant ID')],
+            sample_keys=[
+                ('id', 'Sample ID'),
+                ('external_id', 'External Sample ID'),
+                ('created_date', 'Created date'),
+            ],
+            sequence_keys=[
+                ('type', 'type'),
+                ('technology', 'technology'),
+                ('meta.batch', 'batch'),
+                ('meta.reads_type', 'reads_type'),
+            ],
+            seqr_links={},
+            seqr_sync_types=[],
+        )
+
+        two_samples_result_filtered = await self.webl.get_project_summary(
+            token=0,
+            grid_filter=[
+                SearchItem(
+                    model_type=MetaSearchEntityPrefix.SAMPLE,
+                    query='sample_id002',
+                    field='external_id',
+                    is_meta=False,
+                )
+            ],
+        )
+
+        # self.assertEqual(
+        #     expected_data_two_samples_filtered, two_samples_result_filtered
+        # )
+        for k, v in two_samples_result_filtered.__dict__.items():
+            if k == 'participants':
+                continue
+
+            self.assertEqual(v, expected_data_two_samples_filtered.__dict__[k])
