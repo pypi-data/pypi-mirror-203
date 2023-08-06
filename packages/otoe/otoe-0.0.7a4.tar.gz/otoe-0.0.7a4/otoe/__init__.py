58c9fa8175e48f8298f08185ec8fd018d767290b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:31:03 +0000

    Add package otoe/0.0.7a4/otoe-0.0.7a4.tar.gz

---
+++ packages/otoe/otoe-0.0.7a4.tar.gz/otoe-0.0.7a4/otoe/__init__.py
@@ -0,0 +1,13 @@
+import argparse
+
+from otoe.obsidian import ObsidianParser
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Parse Obsidian markdown files')
+    parser.add_argument('md_dir', type=str, help='Directory where the markdown files are located')
+    parser.add_argument('--per-project', action='store_true', help='Generate a yaml file per project', default=True)
+    args = parser.parse_args()
+    obsidian_parser = ObsidianParser(args.md_dir, args.per_project)
+    matches = obsidian_parser.generate_matches()
+    obsidian_parser.write_yamls(matches)
