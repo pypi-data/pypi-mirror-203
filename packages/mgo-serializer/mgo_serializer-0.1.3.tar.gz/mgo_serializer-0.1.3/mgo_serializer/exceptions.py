ec91408e36cafe1a91e910fd6c4ba4edc70deb61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:04 +0000

    Add package mgo-serializer/0.1.3/mgo_serializer-0.1.3.tar.gz

---
+++ packages/mgo-serializer/mgo_serializer-0.1.3.tar.gz/mgo_serializer-0.1.3/mgo_serializer/exceptions.py
@@ -0,0 +1,19 @@
+class ValidationError(Exception):
+
+    def __init__(self, field=None, detail=None):
+        self.detail = detail
+
+
+class FieldInvalidDataError(Exception):
+
+    def __init__(self, field, data=None, expected_type=None):
+        self.field = field
+        self.data = data
+        self.expected_type = expected_type
+
+    def __str__(self):
+        text = f'Field {self.field} has invalid data: [{self.data}] => [{type(self.data)}]'
+        if self.expected_type:
+            text += f', expected type: {self.expected_type}'
+        return text
+
