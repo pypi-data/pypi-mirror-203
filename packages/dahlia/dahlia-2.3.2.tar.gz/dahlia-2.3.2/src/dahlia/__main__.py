b68d934e09a6e0e21412e67d1aa35c21d90b7454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:20 +0000

    Add package dahlia/2.3.2/dahlia-2.3.2.tar.gz

---
+++ packages/dahlia/dahlia-2.3.2.tar.gz/dahlia-2.3.2/src/dahlia/__main__.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+import sys
+from argparse import ArgumentParser
+
+from .dahlia import Dahlia, Depth, clean
+
+UNSET = object()
+
+
+def main() -> None:
+    parser = ArgumentParser(prog="Dahlia")
+    parser.add_argument(
+        "-d",
+        "--depth",
+        help="set the color depth",
+        type=int,
+        choices={3, 4, 8, 24},
+        default=4,
+    )
+    parser.add_argument("-t", "--test", help="test the colors", action="store_true")
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="print the version",
+        action="version",
+        version="%(prog)s 2.3.2",
+    )
+    parser.add_argument("-c", "--clean", help="clean codes", action="store_true")
+    parser.add_argument("string", nargs="?", help="the string to color", default=UNSET)
+    args = parser.parse_args()
+
+    d = Dahlia(depth=Depth(args.depth))
+    string = args.string
+
+    if string is UNSET:
+        if args.test:
+            d.test()
+        sys.exit()
+    if args.clean:
+        print(clean(string))
+    else:
+        d.print(string)
+
+
+if __name__ == "__main__":
+    main()
