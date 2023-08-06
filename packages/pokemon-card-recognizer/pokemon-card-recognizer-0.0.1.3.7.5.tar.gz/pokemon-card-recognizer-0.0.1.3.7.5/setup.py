976c2484197ddfe2d89a6df1e7ab351b2ae8c830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:49 +0000

    Add package pokemon-card-recognizer/0.0.1.3.7.5/pokemon-card-recognizer-0.0.1.3.7.5.tar.gz

---
+++ packages/pokemon-card-recognizer/pokemon-card-recognizer-0.0.1.3.7.5.tar.gz/pokemon-card-recognizer-0.0.1.3.7.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="pokemon-card-recognizer",
+    version="0.0.1.3.7.5",
+    author="Prateek Tandon",
+    author_email="prateek1.tandon@gmail.com",
+    description="Pokemon TCG Card Recognizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prateekt/pokemon-card-recognizer",
+    packages=setuptools.find_packages(),
+    package_data={"reference_data": ["card_recognizer/reference/data/ref_build"]},
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9.7",
+    install_requires=required,
+)
