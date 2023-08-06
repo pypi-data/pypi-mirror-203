815ed8dafea3dcbd8a0cb67f1bc9a39ec09eae6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:39 +0000

    Add package plone.contentrules/3.0.0/plone.contentrules-3.0.0.tar.gz

---
+++ packages/plone.contentrules/plone.contentrules-3.0.0.tar.gz/plone.contentrules-3.0.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "3.0.0"
+
+setup(
+    name="plone.contentrules",
+    version=version,
+    description="Plone ContentRules Engine",
+    long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope2",
+        "Framework :: Zope :: 4",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="Plone content rules events",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.contentrules",
+    license="GPL version 2",
+    packages=find_packages(),
+    namespace_packages=["plone"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    install_requires=[
+        "setuptools",
+        "zope.componentvocabulary",
+        "Zope",
+    ],
+)
