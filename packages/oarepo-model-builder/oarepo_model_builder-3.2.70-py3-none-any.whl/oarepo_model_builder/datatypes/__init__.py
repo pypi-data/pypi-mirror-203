06f3b3a0771c72699f2ae07166bc8d0684900a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:05 +0000

    Add package oarepo-model-builder/3.2.70/oarepo_model_builder-3.2.70-py3-none-any.whl

---
+++ packages/oarepo-model-builder/oarepo_model_builder-3.2.70-py3-none-any.whl/oarepo_model_builder/datatypes/__init__.py
@@ -0,0 +1,66 @@
+"""
+Datatypes supported out of the box:
+
+integer, float, double:
+  constraints: minimumExclusive, maximumExclusive, minimum, maximum
+
+boolean:
+  no constraints supported
+
+date, time, datetime
+  no constraints supported
+
+edtf, edtf-interval
+  no constraints supported
+
+fulltext, keyword, fulltext+keyword:
+  minLength, maxLength, pattern
+
+flatten:
+  object represented as flatten type in elasticsearch
+"""
+
+
+from .containers import ArrayDataType, FlattenDataType, NestedDataType, ObjectDataType
+from .datatypes import DataType, Import, datatypes  # noqa
+from .dates import (
+    DateDataType,
+    DateTimeDataType,
+    EDTFDataType,
+    EDTFIntervalType,
+    TimeDataType,
+)
+from .primitive_types import NumberDataType  # noqa , just for export
+from .primitive_types import (
+    BooleanDataType,
+    DoubleDataType,
+    FloatDataType,
+    IntegerDataType,
+)
+from .strings import StringDataType  # noqa , just for export
+from .strings import (
+    FulltextDataType,
+    FulltextKeywordDataType,
+    KeywordDataType,
+    URLDataType,
+)
+
+DEFAULT_DATATYPES = [
+    IntegerDataType,
+    FloatDataType,
+    DoubleDataType,
+    BooleanDataType,
+    DateDataType,
+    TimeDataType,
+    DateTimeDataType,
+    EDTFDataType,
+    EDTFIntervalType,
+    FulltextDataType,
+    KeywordDataType,
+    FulltextKeywordDataType,
+    ObjectDataType,
+    NestedDataType,
+    FlattenDataType,
+    ArrayDataType,
+    URLDataType,
+]
