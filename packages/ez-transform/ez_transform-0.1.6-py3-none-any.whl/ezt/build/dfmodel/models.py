22cbabad24cac7792438b2d8497cf3433e217448
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:22 +0000

    Add package ez-transform/0.1.6/ez_transform-0.1.6-py3-none-any.whl

---
+++ packages/ez-transform/ez_transform-0.1.6-py3-none-any.whl/ezt/build/dfmodel/models.py
@@ -0,0 +1,118 @@
+import os
+
+import deltalake as dl
+import polars as pl
+import pyarrow.parquet as pq
+from ezt.util.config import Config
+from ezt.util.helpers import (
+    get_s3_filesystem,
+    prepare_s3_path,
+    get_adls_filesystem,
+    prepare_adls_path,
+    prepare_adls_path_pqdataset,
+)
+from ezt.util.exceptions import EztAuthenticationException
+
+
+def get_model(name: str, project_base_dir: str = None) -> pl.LazyFrame:
+    """Returns the model as a polars lazyframe."""
+
+    if project_base_dir:
+        model = Config(project_base_dir).get_model(name)
+    else:
+        model = Config(os.getcwd()).get_model(name)
+
+    model_getter_func = _get_model_getter_func(model)
+    return model_getter_func(model)
+
+
+def _get_model_getter_func(model):
+
+    if model["filesystem"] == "local":
+
+        if model["write_settings"]["file_type"] == "parquet":
+            return _get_local_parq_model
+        elif model["write_settings"]["file_type"] == "delta":
+            return _get_local_delta_model
+
+    elif model["filesystem"] == "s3":
+
+        if model["write_settings"]["file_type"] == "parquet":
+            return _get_s3_parq_model
+        elif model["write_settings"]["file_type"] == "delta":
+            return _get_s3_delta_model
+
+    elif model["filesystem"] == "adls":
+
+        if model["write_settings"]["file_type"] == "parquet":
+            return _get_adls_parq_model
+        elif model["write_settings"]["file_type"] == "delta":
+            return _get_adls_delta_model
+
+
+def _get_local_delta_model(model):
+
+    result_lazy = pl.from_arrow(
+        dl.DeltaTable(table_uri=f'{model["destination"]}/{model["name"]}').to_pyarrow_table()
+    ).lazy()
+    return result_lazy
+
+
+def _get_s3_delta_model(model):
+
+    if os.getenv("AWS_ACCESS_KEY_ID") is None or os.getenv("AWS_SECRET_ACCESS_KEY") is None:
+        raise EztAuthenticationException(
+            "Environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY need to be set to authenticate to S3."
+        )
+
+    # required for writing delta tables to s3 without LockClient. Opts out of concurrent writes.
+    os.environ["AWS_S3_ALLOW_UNSAFE_RENAME"] = "true"
+
+    table_path = prepare_s3_path(model["destination"])
+
+    result_lazy = pl.from_arrow(
+        dl.DeltaTable(table_uri=f'{table_path}/{model["name"]}').to_pyarrow_table()
+    ).lazy()
+    return result_lazy
+
+
+def _get_s3_parq_model(model):
+
+    s3 = get_s3_filesystem()
+    dataset = pq.ParquetDataset(
+        path_or_paths=f"{model['destination']}/{model['name']}", filesystem=s3
+    )
+
+    return pl.from_arrow(dataset.read()).lazy()
+
+
+def _get_local_parq_model(model):
+
+    table = pq.read_table(f"{model['destination']}/{model['name']}.parquet")
+    return pl.from_arrow(table).lazy()
+
+
+def _get_adls_delta_model(model):
+
+    if os.getenv("AWS_ACCESS_KEY_ID") is None or os.getenv("AWS_SECRET_ACCESS_KEY") is None:
+        raise EztAuthenticationException(
+            "Environment variables AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY need to be set to authenticate to S3."
+        )
+
+    table_path = prepare_adls_path(model["destination"])
+
+    result_lazy = pl.from_arrow(
+        dl.DeltaTable(table_uri=f'{table_path}/{model["name"]}').to_pyarrow_table()
+    ).lazy()
+    return result_lazy
+
+
+def _get_adls_parq_model(model):
+
+    adls = get_adls_filesystem()
+
+    pa_dataset_path = prepare_adls_path_pqdataset(f"{model['destination']}/{model['name']}")
+
+    dataset = pq.ParquetDataset(path_or_paths=pa_dataset_path, filesystem=adls)
+
+    return pl.from_arrow(dataset.read()).lazy()
---
+++ packages/ez-transform/ez_transform-0.1.6-py3-none-any.whl/ezt/util/helpers.py
@@ -0,0 +1,179 @@
+import dis
+import os
+
+# from distutils.dir_util import copy_tree
+import shutil
+
+from adlfs import AzureBlobFileSystem
+from s3fs import S3FileSystem
+from yaml import Loader, load
+
+from ezt.util.exceptions import EztAuthenticationException
+
+
+def copy_starter(path, name) -> None:
+    """
+    Function that copies the standard starting library to the location where the user wants to initialize their ezt project.
+    """
+    from ezt.include.starter_project import PACKAGE_PATH as starter_project_directory
+
+    IGNORE = "__pycache__"
+
+    shutil.copytree(
+        starter_project_directory, f"{path}/{name}", ignore=shutil.ignore_patterns(IGNORE)
+    )
+
+
+def parse_yaml(file: str) -> dict:
+    """
+    Function for parsing yaml configuration files and returningt hem as a python dictionary.
+    """
+    with open(file, "r") as f:
+        result = load(f, Loader=Loader)
+
+    return result
+
+
+# def get_model_dependencies_all(model, model_name) -> list:
+#     """
+#     Function to analyze what function gets called with which parameters in order to analyze data lineage for python models.
+#     """
+#     deps = []
+#     bytecode = dis.Bytecode(model)
+#     instrs = list(reversed([instr for instr in bytecode]))
+#     for (ix, instr) in enumerate(instrs):
+#         if instr.opname == "CALL_FUNCTION":
+#             load_func_instr = instrs[ix + instr.arg + 1]
+#             func = load_func_instr.argval
+#             set_param_instr = instrs[ix + instr.arg]
+#             param = set_param_instr.argval
+#             if func in "get_source":
+#                 deps.append((param, model_name))
+#             elif func in "get_model":
+#                 deps.append((param, model_name))
+#         elif instr.opname == "CALL_FUNCTION_KW":
+#             load_func_instr = instrs[ix + instr.arg + 2]
+#             func = load_func_instr.argval
+#             set_param_instr = instrs[ix + instr.arg + 1]
+#             param = set_param_instr.argval
+#             if func in "get_source":
+#                 deps.append((param, model_name))
+#             elif func in "get_model":
+#                 deps.append((param, model_name))
+
+#     return deps
+
+
+def get_model_dependencies(model):
+    """
+    Same as get_model_dependencies_all except it skips sources.
+    """
+    deps = set()
+    bytecode = dis.Bytecode(model)
+    instrs = list(reversed([instr for instr in bytecode]))
+    for (ix, instr) in enumerate(instrs):
+        if instr.opname == "CALL_FUNCTION":
+            load_func_instr = instrs[ix + instr.arg + 1]
+            func = load_func_instr.argval
+            set_param_instr = instrs[ix + instr.arg]
+            param = set_param_instr.argval
+            if isinstance(func, str):
+                if func in "get_model":
+                    deps.add(param)
+        elif instr.opname == "CALL_FUNCTION_KW":
+            load_func_instr = instrs[ix + instr.arg + 2]
+            func = load_func_instr.argval
+            set_param_instr = instrs[ix + instr.arg + 1]
+            param = set_param_instr.argval
+            if isinstance(func, str):
+                if func in "get_model":
+                    deps.add(param)
+
+    return deps
+
+
+def get_s3_filesystem() -> S3FileSystem:
+
+    access_key = os.getenv("AWS_ACCESS_KEY_ID")
+    secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+    return S3FileSystem(key=access_key, secret=secret_key)
+
+
+def get_adls_filesystem() -> AzureBlobFileSystem:
+
+    if (
+        os.getenv("AZURE_STORAGE_ACCOUNT_NAME") is None
+        or os.getenv("AZURE_STORAGE_TENANT_ID") is None
+        or os.getenv("AZURE_STORAGE_CLIENT_ID") is None
+        or os.getenv("AZURE_STORAGE_CLIENT_SECRET") is None
+    ):
+        raise EztAuthenticationException(
+            "Environment variables AZURE_STORAGE_ACCOUNT_NAME, AZURE_STORAGE_TENANT_ID, AZURE_STORAGE_CLIENT_ID, AZURE_STORAGE_CLIENT_SECRET all need to be set to authenticate to ADLS. \
+            Access key authentication not supported yet."
+        )
+
+    account_name = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+    tenant_id = os.getenv("AZURE_STORAGE_TENANT_ID")
+    client_id = os.getenv("AZURE_STORAGE_CLIENT_ID")
+    client_secret = os.getenv("AZURE_STORAGE_CLIENT_SECRET")
+
+    return AzureBlobFileSystem(
+        account_name=account_name,
+        tenant_id=tenant_id,
+        client_id=client_id,
+        client_secret=client_secret,
+    )
+
+
+def prepare_s3_path(path):
+
+    if path.startswith("s3://"):
+        pass
+    else:
+        path = f"s3://{path}"
+
+    if path.endswith("/"):
+        path = path[:-1]
+    else:
+        pass
+
+    return path
+
+
+def prepare_adls_path(path):
+
+    if path.startswith("abfss://"):
+        pass
+    elif path.startswith("abfs://"):
+        path = f"abfss://{path[8:]}"
+    elif path.startswith("adl://"):
+        path = f"abfss://{path[6:]}"
+    else:
+        path = f"abfss://{path}"
+
+    if path.endswith("/"):
+        path = path[:-1]
+    else:
+        pass
+
+    return path
+
+
+def prepare_adls_path_pqdataset(path):
+
+    if path.startswith("abfss://"):
+        path = path[8:]
+    elif path.startswith("abfs://"):
+        path = path[7:]
+    elif path.startswith("adl://"):
+        path = path[6:]
+    else:
+        pass
+
+    if path.endswith("/"):
+        path = path[:-1]
+    else:
+        pass
+
+    return path
