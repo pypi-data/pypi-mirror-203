deca5a26dcf26c7c9d2d14b683147be225e514d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:45 +0000

    Add package xlibs/0.0.2/xlibs-0.0.2-py3-none-any.whl

---
+++ packages/xlibs/xlibs-0.0.2-py3-none-any.whl/x/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+x: the everything pkg
+"""
+
+import os
+import numpy as np
+import pandas as pd
+import pyarrow as pa
+import thelogger as tl
+
+from _ver import __version__
