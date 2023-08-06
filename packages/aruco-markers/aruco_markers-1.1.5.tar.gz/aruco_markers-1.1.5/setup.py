2164d6b6b752c5dc0fcca19a280bec604779f7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:20 +0000

    Add package aruco-markers/1.1.5/aruco_markers-1.1.5.tar.gz

---
+++ packages/aruco-markers/aruco_markers-1.1.5.tar.gz/aruco_markers-1.1.5/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aruco_markers",
+    description="A compact Python package for handling ArUCo markers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="1.1.5",
+    author="Christopher E. Mower",
+    author_email="christopher.mower@kcl.ac.uk",
+    url="https://github.com/cmower/aruco_markers",
+    packages=["aruco_markers"],
+    license="Apache License 2.0",
+    entry_points={
+        "console_scripts": [
+            "aruco = aruco_markers.main:main",
+        ],
+    },
+    project_urls={
+        "Homepage": "https://cmower.github.io/aruco_markers/",
+        "Documentation": "https://cmower.github.io/aruco_markers/",
+        "Bug Tracker": "https://github.com/cmower/aruco_markers/issues",
+        "Source": "https://github.com/cmower/aruco_markers",
+    },
+    install_requires=[
+        "opencv-contrib-python",
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "reportlab",
+        "attrs",
+    ],
+)
