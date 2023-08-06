8d44a226a0f5096a8b8a6da1b6a5c37ed1590885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:15:22 +0000

    Add package vsui-client/1.1.4/vsui_client-1.1.4-py3-none-any.whl

---
+++ packages/vsui-client/vsui_client-1.1.4-py3-none-any.whl/vsui_client/_main.py
@@ -0,0 +1,30 @@
+from vsui_client import _oo_interface
+
+client = None
+
+def get_client() -> _oo_interface.VSUIClient:
+    ''' return the current vsui client instance
+    Returns:
+        _oo_interface.VSUIClient: the client instance
+    '''
+    if client is None:
+        raise Exception('vsui client not initialized')
+    return client
+
+def init_vsui(enabled: bool, task_id: str = None, logging_target: str = None) -> _oo_interface.VSUIClient:
+    ''' initialize the vsui client
+    Args:
+        enabled (bool): whether vsui is enabled
+        task_id (str): the id of the task
+        logging_target (str): the key of the logging target
+    Returns:
+        _oo_interface.VSUIClient: the client instance
+    '''
+    if enabled:
+        client = _oo_interface.VSUIEnabled()
+    else:
+        client = _oo_interface.VSUIDisabled()
+    # set task id and logging target
+    client.set_task_id(task_id)
+    client.set_logging_target(logging_target)
+    return client
