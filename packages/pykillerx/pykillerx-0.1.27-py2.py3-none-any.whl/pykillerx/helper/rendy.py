ddaecade56227fb061b13de5584bcc0f8c4ec4e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:33 +0000

    Add package pykillerx/0.1.27/pykillerx-0.1.27-py2.py3-none-any.whl

---
+++ packages/pykillerx/pykillerx-0.1.27-py2.py3-none-any.whl/pykillerx/helper/rendy.py
@@ -0,0 +1,12 @@
+import os
+import sys
+from pyrogram import Client
+
+def restart():
+    os.execvp(sys.executable, [sys.executable, "-m", "TigerX"])
+
+async def join(client):
+    try:
+        await client.join_chat("RendyProjects")
+    except BaseException:
+        pass
