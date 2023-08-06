957d270d8e4719bd1209d2af6d702344f30d38b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:47 +0000

    Add package arkitekt/0.4.79/arkitekt-0.4.79-py3-none-any.whl

---
+++ packages/arkitekt/arkitekt-0.4.79-py3-none-any.whl/arkitekt/apps/connected.py
@@ -0,0 +1,20 @@
+from arkitekt.apps.mikro import MikroApp
+from arkitekt.apps.rekuest import RekuestApp
+from arkitekt.apps.unlok import UnlokApp
+
+
+class ConnectedApp(MikroApp, RekuestApp, UnlokApp):
+    identifier: str
+    version: str
+    pass
+
+    def _repr_html_(self):
+        return (
+            f'''<div><h4>{self.identifier or 'Arkitekt App'} {f"@{self.version}" if self.version else ""}</h4><table>'''
+            + "\n".join(["<tr><td>{}</td><td>{}</td></tr>".format(key, value._repr_html_inline_()) for key, value in self if hasattr(value, "_repr_html_inline_")])
+            + "</table></div>"
+        )
+
+
+class App(ConnectedApp):
+    pass
