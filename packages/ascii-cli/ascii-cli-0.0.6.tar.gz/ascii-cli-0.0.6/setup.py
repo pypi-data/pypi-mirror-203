ecdd9b28fa1d1d35fa80ba220fbcf584a0bffacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:38 +0000

    Add package ascii-cli/0.0.6/ascii-cli-0.0.6.tar.gz

---
+++ packages/ascii-cli/ascii-cli-0.0.6.tar.gz/ascii-cli-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ascii-cli",
+    author="mrq-andras",
+    version="0.0.6",
+    packages=find_packages(),
+    install_requires=["Pillow"],
+    entry_points={
+        "console_scripts": [
+            "ascii-cli = ascii",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    url="https://github.com/mrq-andras/ascii-cli",
+    license="MIT",
+    description="A command-line tool that converts images to ASCII art.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
