cada76880f9f5423025665917bf721a4d529c702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:48 +0000

    Add package macicons/0.1.1/macicons-0.1.1.tar.gz

---
+++ packages/macicons/macicons-0.1.1.tar.gz/macicons-0.1.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="macicons",
+    version="0.1.1",
+    author="Cangyuan Li",
+    author_email="everest229@gmail.com",
+    description="CLI utility to change Mac Icons.",
+    url="https://github.com/CangyuanLi/macicons",
+    packages=["macicons"],
+    include_package_data=True,
+    entry_points={"console_scripts": ["macicons=macicons.cli:main"]},
+    install_requires=["tqdm", "pyobjc-core", "pyobjc-framework-Cocoa"],
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS",
+    ],
+)
