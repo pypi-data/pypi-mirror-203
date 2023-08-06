67f1953d2b858f82e89bacd214e488740a2e285b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:18 +0000

    Add package latex-ml-helper/0.0.3/latex_ml_helper-0.0.3.tar.gz

---
+++ packages/latex-ml-helper/latex_ml_helper-0.0.3.tar.gz/latex_ml_helper-0.0.3/libraries/__init__.py
@@ -0,0 +1,3 @@
+from libraries.definitions import LogModel
+from libraries.snippets import Imports, Grad, Loss, Perc, SVM_Margin, Contour, Scaler, SoftMax, Help
+# from libraries.copy import copy_text
