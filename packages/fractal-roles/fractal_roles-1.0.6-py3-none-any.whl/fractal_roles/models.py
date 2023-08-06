b4ffc8d59b012115405d5d51f55bf7fde0b52238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:25 +0000

    Add package fractal-roles/1.0.6/fractal_roles-1.0.6-py3-none-any.whl

---
+++ packages/fractal-roles/fractal_roles-1.0.6-py3-none-any.whl/fractal_roles/models.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+from typing import Callable, Optional, Protocol
+
+from fractal_specifications.generic.specification import (
+    EmptySpecification,
+    Specification,
+)
+
+
+class Role:
+    def __getattr__(self, item):
+        return Methods()
+
+
+@dataclass
+class Method:
+    specification_func: Callable = lambda payload: EmptySpecification()
+
+
+@dataclass
+class Methods:
+    get: Optional[Method]
+    post: Optional[Method]
+    put: Optional[Method]
+    delete: Optional[Method]
+
+    def __init__(self, method: Optional[Method] = None, **kwargs):
+        if not method:
+            method = Method()
+        self.get = kwargs.get("get", method)
+        self.post = kwargs.get("post", method)
+        self.put = kwargs.get("put", method)
+        self.delete = kwargs.get("delete", method)
+
+
+@dataclass
+class TokenPayloadRolesMixin(Protocol):
+    roles: Optional[list] = None
+    specification_func: Callable = lambda **kwargs: None
+
+    @property
+    def specification(self) -> Specification:
+        if self.specification_func is not None:
+            return self.specification_func(self)
+        return EmptySpecification()
