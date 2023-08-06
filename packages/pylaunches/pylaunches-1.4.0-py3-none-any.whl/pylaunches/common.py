45574f64567dbe574254523297273116a6a977e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:47 +0000

    Add package pylaunches/1.4.0/pylaunches-1.4.0-py3-none-any.whl

---
+++ packages/pylaunches/pylaunches-1.4.0-py3-none-any.whl/pylaunches/common.py
@@ -0,0 +1,31 @@
+"""Common attributes and functions."""
+from asyncio import CancelledError, TimeoutError
+from typing import Mapping, Optional
+
+from aiohttp import ClientSession, ClientTimeout
+
+from pylaunches.const import HEADERS
+from pylaunches.exceptions import PyLaunchesException
+
+
+async def call_api(
+    session: ClientSession,
+    endpoint: str,
+    token: Optional[str],
+    params: Optional[Mapping[str, str]],
+) -> dict or None:
+    """Call the API."""
+    headers = HEADERS
+    if token is not None:
+        headers["Token"] = token
+    try:
+        response = await session.get(
+            endpoint, headers=headers, timeout=ClientTimeout(total=20), params=params
+        )
+        if response.status != 200:
+            raise PyLaunchesException(f"Unexpected statuscode {response.status}")
+        return await response.json()
+    except (CancelledError, PyLaunchesException) as exception:
+        raise PyLaunchesException(exception)
+    except TimeoutError:
+        raise PyLaunchesException(f"Timeout while fetching data from {endpoint}")
