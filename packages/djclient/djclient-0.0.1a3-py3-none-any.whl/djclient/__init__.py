2d55249388e7ff6c26e0f45248446e403336c526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:05:58 +0000

    Add package djclient/0.0.1a3/djclient-0.0.1a3-py3-none-any.whl

---
+++ packages/djclient/djclient-0.0.1a3-py3-none-any.whl/djclient/__init__.py
@@ -0,0 +1,28 @@
+"""
+A DataJunction client for connecting to a DataJunction server
+"""
+__version__ = "0.0.1a1"
+
+from djclient.dj import (
+    Cube,
+    Dimension,
+    DJClient,
+    Metric,
+    Namespace,
+    Node,
+    NodeMode,
+    Source,
+    Transform,
+)
+
+__all__ = [
+    "DJClient",
+    "Source",
+    "Dimension",
+    "Transform",
+    "Metric",
+    "Cube",
+    "Node",
+    "NodeMode",
+    "Namespace",
+]
