0b0cea17fb430faa634596701fa61777de0f3a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:08 +0000

    Add package ankitrazorpay/0.1/ankitrazorpay-0.1.tar.gz

---
+++ packages/ankitrazorpay/ankitrazorpay-0.1.tar.gz/ankitrazorpay-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ankitrazorpay',
+    version='0.1',
+    packages=find_packages(),
+    description='A simple calculator package',
+    author='Your Name',
+    author_email='your.email@example.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
