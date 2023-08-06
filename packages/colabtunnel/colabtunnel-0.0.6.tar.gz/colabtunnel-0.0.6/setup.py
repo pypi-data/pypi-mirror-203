2afafbd24e7a7b2b879333097eee7d3e6a12da9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:43 +0000

    Add package colabtunnel/0.0.6/colabtunnel-0.0.6.tar.gz

---
+++ packages/colabtunnel/colabtunnel-0.0.6.tar.gz/colabtunnel-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="colabtunnel",
+    version="0.0.6",
+    license="MIT",
+    description="Connect to Google Colab VM locally from VSCode",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.7",
+    author="Amit Chaudhary",
+    author_email="meamitkc@gmail.com",
+    url="https://github.com/amitness/colab-tunnel",
+    packages=find_packages(),
+)
